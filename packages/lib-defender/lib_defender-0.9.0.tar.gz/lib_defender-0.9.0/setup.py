09793b31d76ab4229400649159325d2e2fcb2c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:08:15 +0000

    Add package lib-defender/0.9.0/lib_defender-0.9.0.tar.gz

---
+++ packages/lib-defender/lib_defender-0.9.0.tar.gz/lib_defender-0.9.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lib_defender",
+    version="0.9.0",
+    description="Super Secret Stuff",
+    author="D S",
+    author_email="dustin.silveri+poc1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
