559b7b1e31e870202a6c74425ad553d153a0221b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:16 +0000

    Add package types-aiobotocore-wafv2/2.15.2/types_aiobotocore_wafv2-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-wafv2/types_aiobotocore_wafv2-2.15.2.tar.gz/types_aiobotocore_wafv2-2.15.2/types_aiobotocore_wafv2/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for wafv2 service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_wafv2 import (
+        Client,
+        WAFV2Client,
+    )
+
+    session = get_session()
+    async with session.create_client("wafv2") as client:
+        client: WAFV2Client
+        ...
+
+    ```
+"""
+
+from .client import WAFV2Client
+
+Client = WAFV2Client
+
+
+__all__ = ("Client", "WAFV2Client")
