a4383a49c05c3dcb0bded74222b8413b52815ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:45 +0000

    Add package reemployct-data-entry/2.1.0/reemployct_data_entry-2.1.0-py3-none-any.whl

---
+++ packages/reemployct-data-entry/reemployct_data_entry-2.1.0-py3-none-any.whl/reemployct_data_entry/entry_weeklyCertification.py
@@ -0,0 +1,40 @@
+import colorama
+from selenium.webdriver.common.by import By
+
+from reemployct_data_entry.lib import browser_control
+
+
+def main(driver):
+  #######################################
+  # Weekly Certification Details - WC-004
+  #######################################
+
+  # Were you physically able to work full time?
+  browser_control.wait_find_element(driver, By.XPATH, '/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[1]/td[5]/table/tbody/tr/td[1]/div/div[2]').click()
+  # Were you available for full time work?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[4]/td[5]/table/tbody/tr/td[1]/div/div[2]').click()
+  # Did you start school, college or training, which you have not already reported to the Labor Department?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[6]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  # Did you work in any self-employment not previously reported to the Labor Department?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[7]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  # Did you perform any work?
+  try:
+    driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[8]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  except:
+    print(colorama.Fore.YELLOW + "Skipping button since not able to click it. It might be disabled." + colorama.Style.RESET_ALL)
+  # Do you have a definite date to return to full time employment?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[10]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  # Did you refuse any offer of work or rehire?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[12]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  # Did you receive your first payment from a pension, other than Social Security, that you have not already reported or was there a change in the amount previously reported to the Labor Department?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/div[3]/table[1]/tbody/tr[13]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+  # Did you receive dismissal pay(such as severance pay, vacation pay,etc.) or workers compensation benefits, not previously reported to the Labor Department?
+  driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[5]/form/table[4]/tbody/tr[1]/td[5]/table/tbody/tr/td[2]/div/div[2]').click()
+
+  # m_driver.wait_find_element(driver, By.ID, 'method__1').click() # Next
+
+  #######################################
+  # Verify Weekly Certification Responses
+  #######################################
+
+  # m_driver.wait_find_element(driver, By.ID, 'method__2').click() # Next
---
+++ packages/reemployct-data-entry/reemployct_data_entry-2.1.0-py3-none-any.whl/reemployct_data_entry/lib/wrangle_job_data.py
@@ -0,0 +1,54 @@
+import colorama
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from reemployct_data_entry.lib import browser_control
+
+
+def get_existing_entries(driver: webdriver.Firefox, silent=False) -> dict:
+  '''
+  Remove rows of job data that already exist as entries in ReEmployCT
+  Should be executed on the work search page (where job data is entered) where there may be previous job entries present so they can be read.
+  '''
+
+  # identify the number of existing job entries (this would implicitly reduce the number of entries needed to enter)
+  entries_existing_n = 0
+  entries_existing = []
+  entries_min = 3 # minimum 3 work search entries for compliance
+  if(driver.title == 'Work Search Summary'): # this page will appear instead of 'Work Search Record Details' if there are already existing entries
+    entries_container = browser_control.wait_find_element(driver, By.XPATH, '/html/body/div[2]/div[5]/form/table[3]/tbody')
+    entries_existing_scraped = entries_container.find_elements(By.XPATH, "./tr")
+    entries_existing_n = len(entries_existing_scraped)
+
+    if(not silent):
+      if(entries_existing_n >= 3):
+        print(colorama.Fore.GREEN +
+        '\n{} existing work entries found. Minimum requirements already met.'.format(entries_existing_n)
+          + colorama.Style.RESET_ALL)
+      else:
+        print(colorama.Fore.YELLOW +
+        '\n{} existing work entries found. Must enter {} more for DOL compliance.'.format(entries_existing_n, entries_min - entries_existing_n)
+          + colorama.Style.RESET_ALL)
+
+    # rebuild data from existing entries
+    for entry in entries_existing_scraped:
+      entries_existing.append(rebuild_entry(entry))
+    del entries_existing_scraped
+
+  return entries_existing
+
+
+def rebuild_entry(entry) -> dict:
+  '''
+  Convert a job entry's raw data scrapped from page into a clean dict
+  '''
+  entry = entry.find_elements(By.XPATH, './child::*')
+  date_raw = entry[0].text
+  summary_raw = entry[2].text.split('\n')
+  summary = {}
+  summary['Date of Work Search'] = pd.to_datetime(date_raw, format='%m/%d/%Y')
+  for i in summary_raw:
+    i = i.split(':')
+    summary[i[0]] = i[1]
+  return summary
