635423ea840247ce38dd3bf86ac4885cae8f0fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:26 +0000

    Add package types-aiobotocore-iot-jobs-data/2.15.2/types_aiobotocore_iot_jobs_data-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-iot-jobs-data/types_aiobotocore_iot_jobs_data-2.15.2-py3-none-any.whl/types_aiobotocore_iot_jobs_data/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for iot-jobs-data service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iot_jobs_data import (
+        Client,
+        IoTJobsDataPlaneClient,
+    )
+
+    session = get_session()
+    async with session.create_client("iot-jobs-data") as client:
+        client: IoTJobsDataPlaneClient
+        ...
+
+    ```
+"""
+
+from .client import IoTJobsDataPlaneClient
+
+Client = IoTJobsDataPlaneClient
+
+__all__ = ("Client", "IoTJobsDataPlaneClient")
