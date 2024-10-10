0586efc6852c78dbbd3e50d7260a81afc86216c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:25 +0000

    Add package types-aiobotocore-sagemaker-edge/2.15.2/types_aiobotocore_sagemaker_edge-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-sagemaker-edge/types_aiobotocore_sagemaker_edge-2.15.2.tar.gz/types_aiobotocore_sagemaker_edge-2.15.2/types_aiobotocore_sagemaker_edge/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for sagemaker-edge service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sagemaker_edge import (
+        Client,
+        SagemakerEdgeManagerClient,
+    )
+
+    session = get_session()
+    async with session.create_client("sagemaker-edge") as client:
+        client: SagemakerEdgeManagerClient
+        ...
+
+    ```
+"""
+
+from .client import SagemakerEdgeManagerClient
+
+Client = SagemakerEdgeManagerClient
+
+__all__ = ("Client", "SagemakerEdgeManagerClient")
