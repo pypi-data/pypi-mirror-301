8eafecb3e3be695d1fd0f7a4cc964a4cffff2899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:04 +0000

    Add package django-copybook/1.3.1/django_copybook-1.3.1.tar.gz

---
+++ packages/django-copybook/django_copybook-1.3.1.tar.gz/django_copybook-1.3.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-copybook",
+    version='1.3.1',
+    author="imtapps",
+    author_email="webadmin@imtapps.com",
+    description="Convert Objects and Django models to/from fixed format records.",
+    url="https://github.com/imtapps/django-copybook",
+    long_description=open('README.rst', 'r').read(),
+    packages=find_packages(),
+    install_requires=['six'],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
+    ],
+)
