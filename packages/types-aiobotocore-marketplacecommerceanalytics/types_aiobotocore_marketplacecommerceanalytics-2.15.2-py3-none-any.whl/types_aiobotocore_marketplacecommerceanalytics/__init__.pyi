60b44f570f0c8694f0040b4184c126943c47409f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:28 +0000

    Add package types-aiobotocore-marketplacecommerceanalytics/2.15.2/types_aiobotocore_marketplacecommerceanalytics-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-marketplacecommerceanalytics/types_aiobotocore_marketplacecommerceanalytics-2.15.2-py3-none-any.whl/types_aiobotocore_marketplacecommerceanalytics/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for marketplacecommerceanalytics service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_marketplacecommerceanalytics import (
+        Client,
+        MarketplaceCommerceAnalyticsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("marketplacecommerceanalytics") as client:
+        client: MarketplaceCommerceAnalyticsClient
+        ...
+
+    ```
+"""
+
+from .client import MarketplaceCommerceAnalyticsClient
+
+Client = MarketplaceCommerceAnalyticsClient
+
+__all__ = ("Client", "MarketplaceCommerceAnalyticsClient")
