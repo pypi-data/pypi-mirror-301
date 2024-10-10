7006029220eb1c82c015f550b16fed0ee8306857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package blue-options/4.100.1/blue_options-4.100.1.tar.gz

---
+++ packages/blue-options/blue_options-4.100.1.tar.gz/blue_options-4.100.1/blue_options/.bash/blue_options.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+function blue_options() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ]; then
+        blue_options version
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=blue_options,task=$task \
+        "${@:2}"
+}
+
+function blue_options_action_git_before_push() {
+    [[ "$(abcli_git get_branch)" == "main" ]] &&
+        blue_options pypi build
+}
+
+python3 -m blue_options version \
+    --show_icon 1
+
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/options.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/source.sh
+
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/browse.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/env.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/generic_task.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/install.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/list.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/logging.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/string.sh
+
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/alias.sh
