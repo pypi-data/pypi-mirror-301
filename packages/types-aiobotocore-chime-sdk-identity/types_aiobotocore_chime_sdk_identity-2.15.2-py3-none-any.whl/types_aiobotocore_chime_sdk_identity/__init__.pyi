b04453430d1a39dee1729bf89e8ee203211e9ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:23 +0000

    Add package types-aiobotocore-chime-sdk-identity/2.15.2/types_aiobotocore_chime_sdk_identity-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-chime-sdk-identity/types_aiobotocore_chime_sdk_identity-2.15.2-py3-none-any.whl/types_aiobotocore_chime_sdk_identity/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for chime-sdk-identity service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_chime_sdk_identity import (
+        ChimeSDKIdentityClient,
+        Client,
+    )
+
+    session = get_session()
+    async with session.create_client("chime-sdk-identity") as client:
+        client: ChimeSDKIdentityClient
+        ...
+
+    ```
+"""
+
+from .client import ChimeSDKIdentityClient
+
+Client = ChimeSDKIdentityClient
+
+__all__ = ("ChimeSDKIdentityClient", "Client")
