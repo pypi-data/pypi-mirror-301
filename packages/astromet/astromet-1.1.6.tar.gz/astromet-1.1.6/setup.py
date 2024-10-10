7bbf47fb7d62ba966d368b4ca0be3dff5d760cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:58 +0000

    Add package astromet/1.1.6/astromet-1.1.6.tar.gz

---
+++ packages/astromet/astromet-1.1.6.tar.gz/astromet-1.1.6/setup.py
@@ -0,0 +1,19 @@
+# to run: python setup.py install
+try:
+    from setuptools import find_packages, setup
+except ImportError:
+    from distutils.core import find_packages, setup
+#from distutils.extension import Extension
+
+setup(name="astromet",
+      version='1.1.6',
+      description='One and two body astrometry',
+      author='Zephyr Penoyre',
+      author_email='zephyrpenoyre@gmail.com',
+      url='https://github.com/zpenoyre/astromet.py',
+      license='GNU GPLv3',
+      packages=find_packages(),
+      install_requires=['numpy','astropy','scipy'],
+      include_package_data=True,
+      package_data={'': ['data/*.csv']},
+      )
