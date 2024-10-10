24f839b85766eb1472524be18475e1e92a166bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:35 +0000

    Add package python-snippets/0.1.2/python_snippets-0.1.2.tar.gz

---
+++ packages/python-snippets/python_snippets-0.1.2.tar.gz/python_snippets-0.1.2/.history/bin/safe_pipr_20230818161303.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2023/08/18 16:12:33
+@Author  :   ChenHao
+@Contact :   jerrychen1990@gmail.com
+'''
+
+import click
+
+
+
+def main(req):
+    
---
+++ packages/python-snippets/python_snippets-0.1.2.tar.gz/python_snippets-0.1.2/.history/snippets/tmp_20230814144700.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Time    :   2023/08/14 14:46:59
+@Author  :   ChenHao
+@Contact :   jerrychen1990@gmail.com
+'''
