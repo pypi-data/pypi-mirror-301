363bdbc0aae47d64bedd81fc9ec40a7033f78b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:50 +0000

    Add package digitalhub-runtime-kfp/0.8.0b1/digitalhub_runtime_kfp-0.8.0b1.tar.gz

---
+++ packages/digitalhub-runtime-kfp/digitalhub_runtime_kfp-0.8.0b1.tar.gz/digitalhub_runtime_kfp-0.8.0b1/digitalhub_runtime_kfp/entities/task/spec.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from digitalhub.entities.task.spec import TaskParamsK8s, TaskSpecK8s
+
+
+class TaskSpecPipeline(TaskSpecK8s):
+    """
+    Task Pipeline specification.
+    """
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: dict | None = None,
+        volumes: list | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list | None = None,
+        envs: list | None = None,
+        secrets: list | None = None,
+        profile: str | None = None,
+        schedule: str | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function,
+            node_selector,
+            volumes,
+            resources,
+            affinity,
+            tolerations,
+            envs,
+            secrets,
+            profile,
+            **kwargs,
+        )
+        self.schedule = schedule
+
+
+class TaskParamsPipeline(TaskParamsK8s):
+    """
+    TaskParamsPipeline model.
+    """
+
+    schedule: str = None
+    """KFP schedule specification."""
+
+
+class TaskSpecBuild(TaskSpecK8s):
+    """
+    Task Build specification.
+    """
+
+
+class TaskParamsBuild(TaskParamsK8s):
+    """
+    TaskParamsBuild model.
+    """
---
+++ packages/digitalhub-runtime-kfp/digitalhub_runtime_kfp-0.8.0b1.tar.gz/digitalhub_runtime_kfp-0.8.0b1/digitalhub_runtime_kfp/entities/workflow/status.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from digitalhub.entities.workflow.status import WorkflowStatus
+
+
+class WorkflowStatusKFP(WorkflowStatus):
+    """
+    Workflow KFP status.
+    """
