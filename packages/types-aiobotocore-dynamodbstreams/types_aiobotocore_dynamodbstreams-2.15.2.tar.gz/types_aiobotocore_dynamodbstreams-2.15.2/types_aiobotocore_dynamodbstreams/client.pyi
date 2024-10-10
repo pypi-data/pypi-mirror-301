2358a145e320ddea550b2c19bc64db40e4dde9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:23 +0000

    Add package types-aiobotocore-dynamodbstreams/2.15.2/types_aiobotocore_dynamodbstreams-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-dynamodbstreams/types_aiobotocore_dynamodbstreams-2.15.2.tar.gz/types_aiobotocore_dynamodbstreams-2.15.2/types_aiobotocore_dynamodbstreams/client.pyi
@@ -0,0 +1,158 @@
+"""
+Type annotations for dynamodbstreams service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_dynamodbstreams.client import DynamoDBStreamsClient
+
+    session = get_session()
+    async with session.create_client("dynamodbstreams") as client:
+        client: DynamoDBStreamsClient
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    DescribeStreamInputRequestTypeDef,
+    DescribeStreamOutputTypeDef,
+    GetRecordsInputRequestTypeDef,
+    GetRecordsOutputTypeDef,
+    GetShardIteratorInputRequestTypeDef,
+    GetShardIteratorOutputTypeDef,
+    ListStreamsInputRequestTypeDef,
+    ListStreamsOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("DynamoDBStreamsClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    ExpiredIteratorException: Type[BotocoreClientError]
+    InternalServerError: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    TrimmedDataAccessException: Type[BotocoreClientError]
+
+class DynamoDBStreamsClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        DynamoDBStreamsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#close)
+        """
+
+    async def describe_stream(
+        self, **kwargs: Unpack[DescribeStreamInputRequestTypeDef]
+    ) -> DescribeStreamOutputTypeDef:
+        """
+        Returns information about a stream, including the current status of the stream,
+        its Amazon Resource Name (ARN), the composition of its shards, and its
+        corresponding DynamoDB
+        table.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.describe_stream)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#describe_stream)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#generate_presigned_url)
+        """
+
+    async def get_records(
+        self, **kwargs: Unpack[GetRecordsInputRequestTypeDef]
+    ) -> GetRecordsOutputTypeDef:
+        """
+        Retrieves the stream records from a given shard.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.get_records)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#get_records)
+        """
+
+    async def get_shard_iterator(
+        self, **kwargs: Unpack[GetShardIteratorInputRequestTypeDef]
+    ) -> GetShardIteratorOutputTypeDef:
+        """
+        Returns a shard iterator.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.get_shard_iterator)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#get_shard_iterator)
+        """
+
+    async def list_streams(
+        self, **kwargs: Unpack[ListStreamsInputRequestTypeDef]
+    ) -> ListStreamsOutputTypeDef:
+        """
+        Returns an array of stream ARNs associated with the current account and
+        endpoint.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client.list_streams)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/#list_streams)
+        """
+
+    async def __aenter__(self) -> "DynamoDBStreamsClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodbstreams.html#DynamoDBStreams.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dynamodbstreams/client/)
+        """
