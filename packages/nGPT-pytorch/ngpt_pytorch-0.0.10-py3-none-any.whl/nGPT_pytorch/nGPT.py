ce31410b66ad948c976feffa1d74d4dab544fd24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:07:35 +0000

    Add package nGPT-pytorch/0.0.10/ngpt_pytorch-0.0.10-py3-none-any.whl

---
+++ packages/nGPT-pytorch/ngpt_pytorch-0.0.10-py3-none-any.whl/nGPT_pytorch/nGPT.py
@@ -0,0 +1,260 @@
+from functools import partial
+
+import torch
+from torch import nn
+from torch.nn import Module, ModuleList
+import torch.nn.functional as F
+from torch.nn.utils.parametrize import register_parametrization
+
+from einops import rearrange, einsum
+from einops.layers.torch import Rearrange
+
+from rotary_embedding_torch import RotaryEmbedding
+
+# functions
+
+def exists(v):
+    return v is not None
+
+def default(v, d):
+    return v if exists(v) else d
+
+def l2norm(t, dim = -1):
+    return F.normalize(t, dim = dim, p = 2)
+
+# for use with parametrize
+
+class L2Norm(Module):
+    def __init__(self, dim = -1):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, t):
+        return l2norm(t, dim = self.dim)
+
+class NormLinear(Module):
+    def __init__(
+        self,
+        dim,
+        dim_out,
+        norm_dim_in = True,
+        parametrize = True
+    ):
+        super().__init__()
+        self.linear = nn.Linear(dim, dim_out, bias = False)
+
+        self.parametrize = parametrize
+        self.l2norm = L2Norm(dim = -1 if norm_dim_in else 0)
+
+        if parametrize:
+            register_parametrization(
+                self.linear,
+                'weight',
+                self.l2norm
+            )
+
+        self.norm_weights_()
+
+    @torch.no_grad()
+    def norm_weights_(self):
+        if self.parametrize:
+            normed = self.weight
+            original = self.linear.parametrizations.weight.original
+
+            original.copy_(normed)
+        else:
+            self.weight.copy_(self.l2norm(self.weight))
+
+    @property
+    def weight(self):
+        return self.linear.weight
+
+    def forward(self, x):
+        return self.linear(x)
+
+# attention and feedforward
+
+class Attention(Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        dim_head = 64,
+        heads = 8,
+        norm_qk = True,
+        manual_norm_weights = False
+    ):
+        super().__init__()
+        NormLinear_ = partial(NormLinear, parametrize = not manual_norm_weights)
+
+        dim_inner = dim_head * heads
+        self.to_q = NormLinear_(dim, dim_inner)
+        self.to_k = NormLinear_(dim, dim_inner)
+        self.to_v = NormLinear_(dim, dim_inner)
+
+        self.rotary_emb = RotaryEmbedding(dim_head)
+        self.qk_scale = nn.Parameter(torch.ones(dim_head) * (dim_head ** 0.25))
+
+        self.norm_qk = norm_qk
+        self.split_heads = Rearrange('b n (h d) -> b h n d', h = heads)
+        self.merge_heads = Rearrange('b h n d -> b n (h d)')
+
+        self.to_out = NormLinear_(dim_inner, dim, norm_dim_in = False)
+
+    def forward(
+        self,
+        x
+    ):
+        q, k, v = self.to_q(x), self.to_k(x), self.to_v(x)
+
+        q, k, v = map(self.split_heads, (q, k, v))
+
+        # maybe query key norm
+
+        if self.norm_qk:
+            q, k = map(l2norm, (q, k))
+
+        # scaling queries and keys - this would line up with the popular use of qk rmsnorm from google deepmind and now black forest labs
+
+        q, k = (q * self.qk_scale), (k * self.qk_scale)
+
+        # rotary positions
+
+        q = self.rotary_emb.rotate_queries_or_keys(q)
+        k = self.rotary_emb.rotate_queries_or_keys(k)
+
+        # scale is 1., as scaling factor is moved to s_qk (dk ^ 0.25) - eq. 16
+
+        out = F.scaled_dot_product_attention(
+            q, k, v,
+            is_causal = True,
+            scale = 1.
+        )
+
+        out = self.merge_heads(out)
+        return self.to_out(out)
+
+class FeedForward(Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        expand_factor = 4,
+        manual_norm_weights = False
+    ):
+        super().__init__()
+        NormLinear_ = partial(NormLinear, parametrize = not manual_norm_weights)
+
+        self.dim = dim
+        dim_inner = int(dim * expand_factor * 2 / 3)
+
+        self.to_hidden = NormLinear_(dim, dim_inner)
+        self.to_gate = NormLinear_(dim, dim_inner)
+
+        self.hidden_scale = nn.Parameter(torch.ones(dim_inner))
+        self.gate_scale = nn.Parameter(torch.ones(dim_inner))
+
+        self.to_out = NormLinear_(dim_inner, dim, norm_dim_in = False)
+
+    def forward(self, x):
+        hidden, gate = self.to_hidden(x), self.to_gate(x)
+
+        hidden = hidden * self.hidden_scale
+        gate = gate * self.gate_scale * (self.dim ** 0.5)
+
+        hidden = F.silu(gate) * hidden
+        return self.to_out(hidden)
+
+# classes
+
+class nGPT(Module):
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        depth,
+        dim_head = 64,
+        heads = 8,
+        attn_norm_qk = True,  # they say the query/key normalization is optional
+        ff_expand_factor = 4.,
+        ce_ignore_index = -1,
+        residual_lerp_scale_init = None,
+        manual_norm_weights = False,
+        tied_embedding = False
+    ):
+        super().__init__()
+        NormLinear_ = partial(NormLinear, parametrize = not manual_norm_weights)
+
+        self.dim = dim
+
+        residual_lerp_scale_init = default(residual_lerp_scale_init, 1. / depth)
+
+        self.token_embed = NormLinear_(dim, num_tokens)
+
+        self.layers = ModuleList([])
+        self.residual_lerp_scales = nn.ParameterList([])
+
+        for _ in range(depth):
+            self.layers.append(ModuleList([
+                Attention(dim, dim_head = dim_head, heads = heads, norm_qk = attn_norm_qk, manual_norm_weights = manual_norm_weights),
+                FeedForward(dim, expand_factor = ff_expand_factor, manual_norm_weights = manual_norm_weights),
+            ]))
+
+            self.residual_lerp_scales.append(nn.ParameterList([
+                nn.Parameter(torch.ones(dim) * residual_lerp_scale_init),
+                nn.Parameter(torch.ones(dim) * residual_lerp_scale_init),
+            ]))
+
+        self.to_logits = NormLinear_(dim, num_tokens) if not tied_embedding else None
+
+        self.logit_scale = nn.Parameter(torch.ones(num_tokens))
+
+        self.ignore_index = ce_ignore_index
+
+    @torch.no_grad()
+    def norm_weights_(self):
+        for module in self.modules():
+            if not isinstance(module, NormLinear):
+                continue
+
+            module.norm_weights_()
+
+    def forward(
+        self,
+        ids,
+        return_loss = False
+    ):
+
+        if return_loss:
+            ids, labels = ids[:, :-1], ids[:, 1:]
+
+        token_embed = self.token_embed.weight
+        tokens = token_embed[ids]
+
+        for (attn, ff), (attn_alpha, ff_alpha) in zip(self.layers, self.residual_lerp_scales):
+
+            attn_out = l2norm(attn(tokens))
+            tokens = l2norm(tokens.lerp(attn_out, attn_alpha))
+
+            ff_out = l2norm(ff(tokens))
+            tokens = l2norm(tokens.lerp(ff_out, ff_alpha))
+
+        if exists(self.to_logits):
+            logits = self.to_logits(tokens)
+        else:
+            # tied embeddings
+            logits = einsum(tokens, token_embed, 'b n d, c d -> b n c')
+
+        logits = logits * self.logit_scale * (self.dim ** 0.5)
+
+        if not return_loss:
+            return logits
+
+        loss = F.cross_entropy(
+            rearrange(logits, 'b n c -> b c n'),
+            labels,
+            ignore_index = self.ignore_index
+        )
+
+        return loss
