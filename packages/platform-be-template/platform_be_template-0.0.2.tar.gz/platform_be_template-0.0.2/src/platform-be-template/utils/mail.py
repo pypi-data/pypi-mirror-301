eca956bad030ee3235650f16bd68d2628f5206b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:38 +0000

    Add package platform-be-template/0.0.2/platform_be_template-0.0.2.tar.gz

---
+++ packages/platform-be-template/platform_be_template-0.0.2.tar.gz/platform_be_template-0.0.2/src/platform-be-template/utils/mail.py
@@ -0,0 +1,10 @@
+from utils.requests import get_json_response_from_other_server
+
+
+def send_mail(body: str, title: str, recipients: list[str], sender: str, mail_api_url: str):
+    get_json_response_from_other_server()
+    pass
+
+
+def cancel_mal():
+    pass
