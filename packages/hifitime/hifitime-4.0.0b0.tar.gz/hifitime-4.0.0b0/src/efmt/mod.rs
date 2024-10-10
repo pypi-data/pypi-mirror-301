245bcd3e38d334e9826917f593fa8f3f826571e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:44 +0000

    Add package hifitime/4.0.0b0/hifitime-4.0.0b0.tar.gz

---
+++ packages/hifitime/hifitime-4.0.0b0.tar.gz/hifitime-4.0.0b0/src/efmt/mod.rs
@@ -0,0 +1,19 @@
+/*
+ * Hifitime, part of the Nyx Space tools
+ * Copyright (C) 2017-onwards Christopher Rabotin <christopher.rabotin@gmail.com> et al. (cf. https://github.com/nyx-space/hifitime/graphs/contributors)
+ * This Source Code Form is subject to the terms of the Apache
+ * v. 2.0. If a copy of the Apache License was not distributed with this
+ * file, You can obtain one at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Documentation: https://nyxspace.com/
+ */
+
+pub mod consts;
+pub mod format;
+pub mod formatter;
+
+pub use format::Format;
+pub use formatter::Formatter;
+
+#[cfg(kani)]
+mod kani_verif;
