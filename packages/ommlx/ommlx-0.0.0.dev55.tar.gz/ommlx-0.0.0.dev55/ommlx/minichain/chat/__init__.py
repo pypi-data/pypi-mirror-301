c7eb1535e7445586a7218a19b0495a6177c947ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:07 +0000

    Add package ommlx/0.0.0.dev55/ommlx-0.0.0.dev55.tar.gz

---
+++ packages/ommlx/ommlx-0.0.0.dev55.tar.gz/ommlx-0.0.0.dev55/ommlx/minichain/chat/__init__.py
@@ -0,0 +1,50 @@
+from .formats import (  # noqa
+    JSON_RESPONSE_FORMAT,
+    JsonResponseFormat,
+    ResponseFormat,
+    TEXT_RESPONSE_FORMAT,
+    TextResponseFormat,
+)
+
+from .history import (  # noqa
+    ChatHistory,
+    ChatHistoryModel,
+    ListChatHistory,
+)
+
+from .messages import (  # noqa
+    AiMessage,
+    Chat,
+    Message,
+    SystemMessage,
+    ToolExecutionResultMessage,
+    UserMessage,
+)
+
+from .models import (  # noqa
+    ChatInput,
+    ChatModel,
+    ChatOutput,
+    ChatRequest,
+    ChatResponse,
+)
+
+from .options import (  # noqa
+    ChatRequestOption,
+    ChatRequestOptions,
+)
+
+from .tools import (  # noqa
+    Tool,
+    ToolExecutionRequest,
+    ToolParameters,
+    ToolSpecification,
+)
+
+
+##
+
+
+from omlish.lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('omlish.marshal', '.marshal', __package__)
