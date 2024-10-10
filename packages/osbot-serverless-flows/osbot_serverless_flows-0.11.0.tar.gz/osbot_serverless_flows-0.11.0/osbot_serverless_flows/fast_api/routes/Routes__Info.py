92b5e7cddc8c8a8ef69b78eb4a2ce7cc19a8230f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:04:08 +0000

    Add package osbot-serverless-flows/0.11.0/osbot_serverless_flows-0.11.0.tar.gz

---
+++ packages/osbot-serverless-flows/osbot_serverless_flows-0.11.0.tar.gz/osbot_serverless_flows-0.11.0/osbot_serverless_flows/fast_api/routes/Routes__Info.py
@@ -0,0 +1,18 @@
+from osbot_fast_api.api.Fast_API_Routes import Fast_API_Routes
+
+from osbot_serverless_flows.utils.Version import version__osbot_serverless_flows
+
+
+class Routes__Info(Fast_API_Routes):
+    tag : str = 'info'
+
+    def ping(self):
+        return {"pong": "42"}
+
+    def version(self):
+        return { "version" : version__osbot_serverless_flows}
+
+    def setup_routes(self):
+        self.add_route_get(self.ping   )
+        self.add_route_get(self.version)
+        return self
