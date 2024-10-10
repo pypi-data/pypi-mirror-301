fb015b705ce6ec5e629ce438d6120ce7c7ec09e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:23 +0000

    Add package types-aiobotocore-codestar-connections/2.15.2/types_aiobotocore_codestar_connections-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-codestar-connections/types_aiobotocore_codestar_connections-2.15.2.tar.gz/types_aiobotocore_codestar_connections-2.15.2/types_aiobotocore_codestar_connections/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for codestar-connections service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_codestar_connections import (
+        Client,
+        CodeStarconnectionsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("codestar-connections") as client:
+        client: CodeStarconnectionsClient
+        ...
+
+    ```
+"""
+
+from .client import CodeStarconnectionsClient
+
+Client = CodeStarconnectionsClient
+
+__all__ = ("Client", "CodeStarconnectionsClient")
