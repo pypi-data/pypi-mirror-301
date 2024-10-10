8aa96168d57765c4d244f944d870ab509c5e6abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:27 +0000

    Add package types-aiobotocore-iotsecuretunneling/2.15.2/types_aiobotocore_iotsecuretunneling-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-iotsecuretunneling/types_aiobotocore_iotsecuretunneling-2.15.2.tar.gz/types_aiobotocore_iotsecuretunneling-2.15.2/types_aiobotocore_iotsecuretunneling/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for iotsecuretunneling service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iotsecuretunneling import (
+        Client,
+        IoTSecureTunnelingClient,
+    )
+
+    session = get_session()
+    async with session.create_client("iotsecuretunneling") as client:
+        client: IoTSecureTunnelingClient
+        ...
+
+    ```
+"""
+
+from .client import IoTSecureTunnelingClient
+
+Client = IoTSecureTunnelingClient
+
+__all__ = ("Client", "IoTSecureTunnelingClient")
