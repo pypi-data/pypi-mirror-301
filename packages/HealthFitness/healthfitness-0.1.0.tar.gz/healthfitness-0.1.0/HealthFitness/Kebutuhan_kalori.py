ef8db0b14b7cca38935c8326fc4a611b95d7af58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:39 +0000

    Add package HealthFitness/0.1.0/healthfitness-0.1.0.tar.gz

---
+++ packages/HealthFitness/healthfitness-0.1.0.tar.gz/healthfitness-0.1.0/HealthFitness/Kebutuhan_kalori.py
@@ -0,0 +1,49 @@
+"""Menghitung kebutuhan kalori harian berdasarkan statistik tubuh dan tingkat aktivitas."""
+
+def kebutuhan_kalori_harian(berat, tinggi, usia, jenis_kelamin, tingkat_aktivitas):
+    try:
+        # Hitung BMR
+        if jenis_kelamin.upper() == 'L':
+            bmr = 10 * berat + 6.25 * tinggi - 5 * usia + 5
+        elif jenis_kelamin.upper() == 'P':
+            bmr = 10 * berat + 6.25 * tinggi - 5 * usia - 161
+        else:
+            raise ValueError("Jenis kelamin tidak valid. Gunakan 'L' untuk laki-laki atau 'P' untuk perempuan.")
+        
+        # tingkat aktivitas
+        if tingkat_aktivitas.lower() == 'rendah':
+            kalori_harian = bmr * 1.2
+        elif tingkat_aktivitas.lower() == 'sedang':
+            kalori_harian = bmr * 1.55
+        elif tingkat_aktivitas.lower() == 'tinggi':
+            kalori_harian = bmr * 1.9
+        else:
+            raise ValueError("Tingkat aktivitas tidak valid. Gunakan 'rendah', 'sedang', atau 'tinggi'.")
+        
+        return kalori_harian
+    except:
+        return "Inputan invalid"
+
+# while True:
+#     try:
+#         # Input pengguna
+#         berat = int(input('Masukkan berat badan (kg) : ')) 
+#         tinggi = int(input('Masukkan tinggi badan (cm) : '))  
+#         usia = int(input('Masukkan usia (tahun) : '))   
+#         jenis_kelamin = (input('Masukkan jenis kelamin (L/P) : ')).upper()  
+#         tingkat_aktivitas = (input('Masukkan tingkat aktivitas (rendah/sedang/tinggi) : ')).lower()  
+
+#         # kebutuhan kalori
+#         kebutuhan_kalori = kebutuhan_kalori_harian(berat, tinggi, usia, jenis_kelamin, tingkat_aktivitas)
+#         print(f"Kebutuhan kalori harian: {kebutuhan_kalori:.2f} kalori")
+
+#     except ValueError:
+#         print(f"Kesalahan: Input tidak valid")
+    
+#     # apakah pengguna ingin menghitung lagi
+#     ulang = input("Ingin menghitung lagi? (ya/tidak): ").lower()
+#     if ulang == 'tidak':
+#         print("Terima kasih! Program selesai.")
+#         break
+
+
---
+++ packages/HealthFitness/healthfitness-0.1.0.tar.gz/healthfitness-0.1.0/HealthFitness/langkahToMeter.py
@@ -0,0 +1,33 @@
+def panjang_langkah(jenis_kelamin, tinggi_badan):
+    if jenis_kelamin == 1:  
+        if 1.5 <= tinggi_badan <= 1.59:
+            return 0.625
+        elif 1.6 <= tinggi_badan <= 1.69:
+            return 0.675
+        elif 1.7 <= tinggi_badan <= 1.79:
+            return 0.725
+        elif 1.8 <= tinggi_badan <= 1.89:
+            return 0.775
+        elif 1.9 <= tinggi_badan <= 2:
+            return 0.875
+    elif jenis_kelamin == 2: 
+        if 1.5 <= tinggi_badan <= 1.59:
+            return 0.595
+        elif 1.6 <= tinggi_badan <= 1.69:
+            return 0.645
+        elif 1.7 <= tinggi_badan <= 1.79:
+            return 0.695
+        elif 1.8 <= tinggi_badan <= 1.89:
+            return 0.745
+        elif 1.9 <= tinggi_badan <= 2:
+            return 0.795
+    return "Tinggi di luar rentang."
+
+def hitung_jarak(banyak_langkah, panjang_langkah):
+    if not isinstance(panjang_langkah, float):
+        return panjang_langkah
+    jarak = banyak_langkah * panjang_langkah  
+    return jarak
+
+
+
