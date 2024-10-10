8c5166873b52d7d50f6abcdf8163fd296179bceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:19 +0000

    Add package pyglpk/0.0/pyglpk-0.0.tar.gz

---
+++ packages/pyglpk/pyglpk-0.0.tar.gz/pyglpk-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Thomas Finley',
+    author_email='tfinley@gmail.com',
+    description='PyGLPK, a Python module encapsulating GLPK.',
+    install_requires=['glpk'],
+    long_description='Use `glpk <https://pypi.python.org/pypi/glpk/>`_ instead.',
+    maintainer='Bradford D. Boyle',
+    maintainer_email='bradford.d.boyle@gmail.com',
+    name='pyglpk',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/glpk/',
+    version='0.0',
+    zip_safe=False,
+)
