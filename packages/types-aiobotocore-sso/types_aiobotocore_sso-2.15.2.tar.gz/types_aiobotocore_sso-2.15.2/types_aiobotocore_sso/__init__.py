aff86a310e930a6a0f5a5bffbf0ad919f69b9145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:28 +0000

    Add package types-aiobotocore-sso/2.15.2/types_aiobotocore_sso-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-sso/types_aiobotocore_sso-2.15.2.tar.gz/types_aiobotocore_sso-2.15.2/types_aiobotocore_sso/__init__.py
@@ -0,0 +1,32 @@
+"""
+Main interface for sso service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sso import (
+        Client,
+        ListAccountRolesPaginator,
+        ListAccountsPaginator,
+        SSOClient,
+    )
+
+    session = get_session()
+    async with session.create_client("sso") as client:
+        client: SSOClient
+        ...
+
+
+    list_account_roles_paginator: ListAccountRolesPaginator = client.get_paginator("list_account_roles")
+    list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+    ```
+"""
+
+from .client import SSOClient
+from .paginator import ListAccountRolesPaginator, ListAccountsPaginator
+
+Client = SSOClient
+
+
+__all__ = ("Client", "ListAccountRolesPaginator", "ListAccountsPaginator", "SSOClient")
