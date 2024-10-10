fa014cc8deae5089f15b90e76f8527c0fbfa3de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:14 +0000

    Add package toolmate/0.3.94/toolmate-0.3.94.tar.gz

---
+++ packages/toolmate/toolmate-0.3.94.tar.gz/toolmate-0.3.94/toolmate/plugins/bible.py
@@ -0,0 +1,38 @@
+"""
+CUV Bible
+Reference: https://bible.fhl.net/json
+cuv = requests.get("https://bible.fhl.net/json/qb.php?gb=0&chap=3&sec=6&chineses=出")
+cuv.text.encode().decode('unicode_escape')
+
+NET Bible
+Reference: https://labs.bible.org/
+https://labs.bible.org/api/?passage=John+3:16-17;%20Deut%206:4
+"""
+
+from toolmate import config, print1
+from toolmate.utils.bible_verse_parser import BibleVerseParser
+
+def extract_bible_references(function_args):
+    content = config.currentMessages[-1]["content"]
+    config.toolTextOutput = BibleVerseParser(False).extractAllReferencesReadable(content)
+    if config.toolTextOutput:
+        print2("```references")
+        print1(config.toolTextOutput)
+        print2("```")
+    return ""
+
+functionSignature = {
+    "examples": [
+        "Extract Bible references",
+    ],
+    "name": "extract_bible_references",
+    "description": "Extract Bible references from a block of text",
+    "parameters": {
+        "type": "object",
+        "properties": {},
+        "required": [],
+    },
+}
+
+config.addFunctionCall(signature=functionSignature, method=extract_bible_references)
+config.inputSuggestions.append("Extract Bible references: ")
