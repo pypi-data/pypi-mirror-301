fd7df7842fcc3dd5d364ec3ba9ed532435bb0754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:04:05 +0000

    Add package dify-plugin/0.0.1b11/dify_plugin-0.0.1b11.tar.gz

---
+++ packages/dify-plugin/dify_plugin-0.0.1b11.tar.gz/dify_plugin-0.0.1b11/dify_plugin/core/entities/plugin/io.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ....core.server.__base.request_reader import RequestReader
+
+from ....core.server.__base.response_writer import ResponseWriter
+
+
+class PluginInStreamEvent(Enum):
+    Request = "request"
+    BackwardInvocationResponse = "backwards_response"
+
+    @classmethod
+    def value_of(cls, v: str):
+        for e in cls:
+            if e.value == v:
+                return e
+        raise ValueError(f"Invalid value for PluginInStream.Event: {v}")
+
+
+class PluginInStreamBase:
+    def __init__(
+        self,
+        session_id: str,
+        event: PluginInStreamEvent,
+        data: dict,
+    ) -> None:
+        self.session_id = session_id
+        self.event = event
+        self.data = data
+
+
+class PluginInStream(PluginInStreamBase):
+    def __init__(
+        self,
+        session_id: str,
+        event: PluginInStreamEvent,
+        data: dict,
+        reader: "RequestReader",
+        writer: "ResponseWriter",
+    ):
+        self.reader = reader
+        self.writer = writer
+        super().__init__(session_id, event, data)
