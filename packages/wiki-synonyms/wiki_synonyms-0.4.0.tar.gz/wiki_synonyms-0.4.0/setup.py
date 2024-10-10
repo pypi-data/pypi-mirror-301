f9399679d9ea63d51c332af347f1c4f2799f4b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:41 +0000

    Add package wiki-synonyms/0.4.0/wiki_synonyms-0.4.0.tar.gz

---
+++ packages/wiki-synonyms/wiki_synonyms-0.4.0.tar.gz/wiki_synonyms-0.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wiki_synonyms',
+    version='0.4.0',
+    description='Python package for Wiki synonyms',
+    url='https://github.com/iis-research-team/wiki-synonyms',
+    author='IIS Research Team',
+    author_email='bruches@bk.ru',
+    license='MIT',
+    packages=find_packages(include=['wiki_synonyms', 'wiki_synonyms.*']),
+    data_files=[('wiki_synonyms.data', ['wiki_synonyms/data/synonyms_1.json', 'wiki_synonyms/data/synonyms_2.json', 'wiki_synonyms/data/synonyms_3.json'])],
+    include_package_data=True,
+    install_requires=[
+                      'numpy',
+                      ],
+)
