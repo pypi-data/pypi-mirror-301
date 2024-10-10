9302d0d99c3cac5a0174e31f10a7ca03d6dbdc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:23 +0000

    Add package jupyter-tikz/0.5.6/jupyter_tikz-0.5.6-py3-none-any.whl

---
+++ packages/jupyter-tikz/jupyter_tikz-0.5.6-py3-none-any.whl/jupyter_tikz/__init__.py
@@ -0,0 +1,9 @@
+__author__ = """Lucas Lima Rodrigues"""
+__email__ = "lucaslrodri@gmail.com"
+__version__ = "0.1.0"
+
+from .jupyter_tikz import _ARGS, TexDocument, TexFragment, TikZMagics
+
+
+def load_ipython_extension(ipython):  # pragma: no cover
+    ipython.register_magics(TikZMagics)
