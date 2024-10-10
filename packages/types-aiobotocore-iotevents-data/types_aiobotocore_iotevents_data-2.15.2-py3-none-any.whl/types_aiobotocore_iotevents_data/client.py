35e124cb074cebd9400e45bd1e05a1fa42121f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:26 +0000

    Add package types-aiobotocore-iotevents-data/2.15.2/types_aiobotocore_iotevents_data-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-iotevents-data/types_aiobotocore_iotevents_data-2.15.2-py3-none-any.whl/types_aiobotocore_iotevents_data/client.py
@@ -0,0 +1,256 @@
+"""
+Type annotations for iotevents-data service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iotevents_data.client import IoTEventsDataClient
+
+    session = get_session()
+    async with session.create_client("iotevents-data") as client:
+        client: IoTEventsDataClient
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    BatchAcknowledgeAlarmRequestRequestTypeDef,
+    BatchAcknowledgeAlarmResponseTypeDef,
+    BatchDeleteDetectorRequestRequestTypeDef,
+    BatchDeleteDetectorResponseTypeDef,
+    BatchDisableAlarmRequestRequestTypeDef,
+    BatchDisableAlarmResponseTypeDef,
+    BatchEnableAlarmRequestRequestTypeDef,
+    BatchEnableAlarmResponseTypeDef,
+    BatchPutMessageRequestRequestTypeDef,
+    BatchPutMessageResponseTypeDef,
+    BatchResetAlarmRequestRequestTypeDef,
+    BatchResetAlarmResponseTypeDef,
+    BatchSnoozeAlarmRequestRequestTypeDef,
+    BatchSnoozeAlarmResponseTypeDef,
+    BatchUpdateDetectorRequestRequestTypeDef,
+    BatchUpdateDetectorResponseTypeDef,
+    DescribeAlarmRequestRequestTypeDef,
+    DescribeAlarmResponseTypeDef,
+    DescribeDetectorRequestRequestTypeDef,
+    DescribeDetectorResponseTypeDef,
+    ListAlarmsRequestRequestTypeDef,
+    ListAlarmsResponseTypeDef,
+    ListDetectorsRequestRequestTypeDef,
+    ListDetectorsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("IoTEventsDataClient",)
+
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    InternalFailureException: Type[BotocoreClientError]
+    InvalidRequestException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceUnavailableException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+
+
+class IoTEventsDataClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        IoTEventsDataClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#exceptions)
+        """
+
+    async def batch_acknowledge_alarm(
+        self, **kwargs: Unpack[BatchAcknowledgeAlarmRequestRequestTypeDef]
+    ) -> BatchAcknowledgeAlarmResponseTypeDef:
+        """
+        Acknowledges one or more alarms.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_acknowledge_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_acknowledge_alarm)
+        """
+
+    async def batch_delete_detector(
+        self, **kwargs: Unpack[BatchDeleteDetectorRequestRequestTypeDef]
+    ) -> BatchDeleteDetectorResponseTypeDef:
+        """
+        Deletes one or more detectors that were created.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_delete_detector)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_delete_detector)
+        """
+
+    async def batch_disable_alarm(
+        self, **kwargs: Unpack[BatchDisableAlarmRequestRequestTypeDef]
+    ) -> BatchDisableAlarmResponseTypeDef:
+        """
+        Disables one or more alarms.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_disable_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_disable_alarm)
+        """
+
+    async def batch_enable_alarm(
+        self, **kwargs: Unpack[BatchEnableAlarmRequestRequestTypeDef]
+    ) -> BatchEnableAlarmResponseTypeDef:
+        """
+        Enables one or more alarms.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_enable_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_enable_alarm)
+        """
+
+    async def batch_put_message(
+        self, **kwargs: Unpack[BatchPutMessageRequestRequestTypeDef]
+    ) -> BatchPutMessageResponseTypeDef:
+        """
+        Sends a set of messages to the IoT Events system.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_put_message)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_put_message)
+        """
+
+    async def batch_reset_alarm(
+        self, **kwargs: Unpack[BatchResetAlarmRequestRequestTypeDef]
+    ) -> BatchResetAlarmResponseTypeDef:
+        """
+        Resets one or more alarms.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_reset_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_reset_alarm)
+        """
+
+    async def batch_snooze_alarm(
+        self, **kwargs: Unpack[BatchSnoozeAlarmRequestRequestTypeDef]
+    ) -> BatchSnoozeAlarmResponseTypeDef:
+        """
+        Changes one or more alarms to the snooze mode.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_snooze_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_snooze_alarm)
+        """
+
+    async def batch_update_detector(
+        self, **kwargs: Unpack[BatchUpdateDetectorRequestRequestTypeDef]
+    ) -> BatchUpdateDetectorResponseTypeDef:
+        """
+        Updates the state, variable values, and timer settings of one or more detectors
+        (instances) of a specified detector
+        model.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.batch_update_detector)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#batch_update_detector)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#close)
+        """
+
+    async def describe_alarm(
+        self, **kwargs: Unpack[DescribeAlarmRequestRequestTypeDef]
+    ) -> DescribeAlarmResponseTypeDef:
+        """
+        Retrieves information about an alarm.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.describe_alarm)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#describe_alarm)
+        """
+
+    async def describe_detector(
+        self, **kwargs: Unpack[DescribeDetectorRequestRequestTypeDef]
+    ) -> DescribeDetectorResponseTypeDef:
+        """
+        Returns information about the specified detector (instance).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.describe_detector)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#describe_detector)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#generate_presigned_url)
+        """
+
+    async def list_alarms(
+        self, **kwargs: Unpack[ListAlarmsRequestRequestTypeDef]
+    ) -> ListAlarmsResponseTypeDef:
+        """
+        Lists one or more alarms.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.list_alarms)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#list_alarms)
+        """
+
+    async def list_detectors(
+        self, **kwargs: Unpack[ListDetectorsRequestRequestTypeDef]
+    ) -> ListDetectorsResponseTypeDef:
+        """
+        Lists detectors (the instances of a detector model).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client.list_detectors)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/#list_detectors)
+        """
+
+    async def __aenter__(self) -> "IoTEventsDataClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/iotevents-data.html#IoTEventsData.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iotevents_data/client/)
+        """
