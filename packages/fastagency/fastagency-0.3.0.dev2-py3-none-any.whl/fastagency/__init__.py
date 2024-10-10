6e1cf7f5aeba50c74f4bbfb40b531b0a8b6edaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:25 +0000

    Add package fastagency/0.3.0.dev2/fastagency-0.3.0.dev2-py3-none-any.whl

---
+++ packages/fastagency/fastagency-0.3.0.dev2-py3-none-any.whl/fastagency/__init__.py
@@ -0,0 +1,29 @@
+"""The fastest way to bring multi-agent workflows to production."""
+
+from .__about__ import __version__
+from .app import FastAgency
+from .base import UI, Workflow, WorkflowsProtocol
+from .messages import (
+    FunctionCallExecution,
+    IOMessage,
+    MessageType,
+    MultipleChoice,
+    SuggestedFunctionCall,
+    TextInput,
+    TextMessage,
+)
+
+__all__ = [
+    "UI",
+    "FastAgency",
+    "FunctionCallExecution",
+    "IOMessage",
+    "MessageType",
+    "MultipleChoice",
+    "SuggestedFunctionCall",
+    "TextInput",
+    "TextMessage",
+    "WorkflowsProtocol",
+    "Workflow",
+    "__version__",
+]
---
+++ packages/fastagency/fastagency-0.3.0.dev2-py3-none-any.whl/fastagency/ui/mesop/send_prompt.py
@@ -0,0 +1,31 @@
+from collections.abc import Iterable
+
+import mesop as me
+
+from ...base import ProviderProtocol
+from .data_model import State
+from .mesop import MesopMessage, MesopUI, run_workflow
+
+
+def send_prompt_to_autogen(
+    provider: ProviderProtocol, name: str
+) -> Iterable[MesopMessage]:
+    mesop_io = run_workflow(provider, name=name)
+    state = me.state(State)
+    state.conversation.fastagency = mesop_io.id
+    return mesop_io.get_message_stream()
+
+
+def send_user_feedback_to_autogen(user_response: str) -> Iterable[MesopMessage]:
+    state = me.state(State)
+    mesop_id = state.conversation.fastagency
+    mesop_io = MesopUI.get_conversation(mesop_id)
+    mesop_io.respond(user_response)
+    return mesop_io.get_message_stream()
+
+
+def get_more_messages() -> Iterable[MesopMessage]:
+    state = me.state(State)
+    mesop_id = state.conversation.fastagency
+    mesop_io = MesopUI.get_conversation(mesop_id)
+    return mesop_io.get_message_stream()
