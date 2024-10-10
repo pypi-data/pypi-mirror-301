20ca2cf42b1471b0fa4aba5e3a3bab2896527282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:21 +0000

    Add package pex/2.20.3/pex-2.20.3.tar.gz

---
+++ packages/pex/pex-2.20.3.tar.gz/pex-2.20.3/tests/conftest.py
@@ -0,0 +1,53 @@
+# Copyright 2021 Pex project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import
+
+import pytest
+
+import testing
+from pex.interpreter import PythonInterpreter
+from pex.platforms import Platform
+from testing import PY27, PY38, PY39, PY310, ensure_python_interpreter
+
+
+@pytest.fixture(scope="session")
+def pex_project_dir():
+    # type: () -> str
+    return testing.pex_project_dir()
+
+
+@pytest.fixture
+def current_interpreter():
+    # type: () -> PythonInterpreter
+    return PythonInterpreter.get()
+
+
+@pytest.fixture
+def current_platform(current_interpreter):
+    # type: (PythonInterpreter) -> Platform
+    return current_interpreter.platform
+
+
+@pytest.fixture
+def py27():
+    # type: () -> PythonInterpreter
+    return PythonInterpreter.from_binary(ensure_python_interpreter(PY27))
+
+
+@pytest.fixture
+def py38():
+    # type: () -> PythonInterpreter
+    return PythonInterpreter.from_binary(ensure_python_interpreter(PY38))
+
+
+@pytest.fixture
+def py39():
+    # type: () -> PythonInterpreter
+    return PythonInterpreter.from_binary(ensure_python_interpreter(PY39))
+
+
+@pytest.fixture
+def py310():
+    # type: () -> PythonInterpreter
+    return PythonInterpreter.from_binary(ensure_python_interpreter(PY310))
