f0dff97db2786dc48c82e254d14c84f5654a9e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:36 +0000

    Add package uflow/0.0.1/uflow-0.0.1.tar.gz

---
+++ packages/uflow/uflow-0.0.1.tar.gz/uflow-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='uflow',
+    version='0.0.1',
+    description='A placeholder for a future project',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=[],
+    install_requires=[],
+)
