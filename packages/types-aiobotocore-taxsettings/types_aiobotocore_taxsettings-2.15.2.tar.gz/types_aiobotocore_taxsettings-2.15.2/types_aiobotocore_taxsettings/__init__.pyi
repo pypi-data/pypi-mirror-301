fd274281173cf83751ed9e1d5392be704ac499d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package types-aiobotocore-taxsettings/2.15.2/types_aiobotocore_taxsettings-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-taxsettings/types_aiobotocore_taxsettings-2.15.2.tar.gz/types_aiobotocore_taxsettings-2.15.2/types_aiobotocore_taxsettings/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for taxsettings service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_taxsettings import (
+        Client,
+        ListTaxRegistrationsPaginator,
+        TaxSettingsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("taxsettings") as client:
+        client: TaxSettingsClient
+        ...
+
+
+    list_tax_registrations_paginator: ListTaxRegistrationsPaginator = client.get_paginator("list_tax_registrations")
+    ```
+"""
+
+from .client import TaxSettingsClient
+from .paginator import ListTaxRegistrationsPaginator
+
+Client = TaxSettingsClient
+
+__all__ = ("Client", "ListTaxRegistrationsPaginator", "TaxSettingsClient")
