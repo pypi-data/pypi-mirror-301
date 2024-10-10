9be8040e390ec5b1df81ea2b9965369f2964848d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:04:02 +0000

    Add package shiboken6/6.8.0/shiboken6-6.8.0-cp39-abi3-win_amd64.whl

---
+++ packages/shiboken6/shiboken6-6.8.0-cp39-abi3-win_amd64.whl/shiboken6/_config.py
@@ -0,0 +1,12 @@
+shiboken_library_soversion = str(6.8)
+
+version = "6.8.0"
+version_info = (6, 8, 0, "", "")
+
+__build_date__ = '2024-10-09T15:59:19+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.8.0'
+
