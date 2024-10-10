4e41f2b0634fbebdc86eef9859cd8ec935d92a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:16 +0000

    Add package baml-py/0.60.0/baml_py-0.60.0-cp38-abi3-musllinux_1_1_aarch64.whl

---
+++ packages/baml-py/baml_py-0.60.0-cp38-abi3-musllinux_1_1_aarch64.whl/baml_py/errors.py
@@ -0,0 +1,18 @@
+from .baml_py import (
+    BamlError,
+    BamlClientError,
+    BamlClientHttpError,
+    BamlInvalidArgumentError,
+)
+
+# hack to get the BamlValidationError class which is a custom error
+from .baml_py.errors import BamlValidationError
+
+
+__all__ = [
+    "BamlError",
+    "BamlClientError",
+    "BamlClientHttpError",
+    "BamlInvalidArgumentError",
+    "BamlValidationError",
+]
