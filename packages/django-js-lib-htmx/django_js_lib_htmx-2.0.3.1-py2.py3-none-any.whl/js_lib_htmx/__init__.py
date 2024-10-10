ded122b4a9f4cd36bc7e109f10103a85af3245aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:04:14 +0000

    Add package django-js-lib-htmx/2.0.3.1/django_js_lib_htmx-2.0.3.1-py2.py3-none-any.whl

---
+++ packages/django-js-lib-htmx/django_js_lib_htmx-2.0.3.1-py2.py3-none-any.whl/js_lib_htmx/__init__.py
@@ -0,0 +1,8 @@
+"""HTMX Repackaged for Django
+
+[HTMX](https://htmx.org/) packaged in a Django reusable app.
+
+"""
+__version__ = "2.0.3.1"
+
+default_app_config = 'js_lib_htmx.apps.JsLibHtmxConfig'
