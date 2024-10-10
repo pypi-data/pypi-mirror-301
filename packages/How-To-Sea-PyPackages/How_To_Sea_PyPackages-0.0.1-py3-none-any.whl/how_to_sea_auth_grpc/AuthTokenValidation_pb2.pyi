cbf81bf29153c56bcd5b5bb102d5040d163478ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:04:03 +0000

    Add package How-To-Sea-PyPackages/0.0.1/How_To_Sea_PyPackages-0.0.1-py3-none-any.whl

---
+++ packages/How-To-Sea-PyPackages/How_To_Sea_PyPackages-0.0.1-py3-none-any.whl/how_to_sea_auth_grpc/AuthTokenValidation_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Token(_message.Message):
+    __slots__ = ("token",)
+    TOKEN_FIELD_NUMBER: _ClassVar[int]
+    token: str
+    def __init__(self, token: _Optional[str] = ...) -> None: ...
+
+class TokenStatus(_message.Message):
+    __slots__ = ("valid",)
+    VALID_FIELD_NUMBER: _ClassVar[int]
+    valid: bool
+    def __init__(self, valid: bool = ...) -> None: ...
