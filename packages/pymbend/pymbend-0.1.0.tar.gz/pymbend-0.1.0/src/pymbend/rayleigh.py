37b40ce2adadaddff8b7ad27757ec7586b0f95bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:26 +0000

    Add package pymbend/0.1.0/pymbend-0.1.0.tar.gz

---
+++ packages/pymbend/pymbend-0.1.0.tar.gz/pymbend-0.1.0/src/pymbend/rayleigh.py
@@ -0,0 +1,35 @@
+import numpy as np  # type: ignore
+from numpy.typing import ArrayLike, NDArray  # type: ignore
+
+from pymbend.mbend import MbendMethod, mbend
+
+
+def bend_generalized_rayleigh(
+    numerator: ArrayLike,
+    denominator: ArrayLike,
+    numerator_weight: ArrayLike | None = None,
+    invert_weights: bool = False,
+    max_iter: int = 10000,
+    small_positive: float = 1e-4,
+    tol: float = 1e-5,
+) -> NDArray:
+    numerator = np.asarray(numerator)
+    denominator = np.asarray(denominator)
+
+    cholesky = np.linalg.cholesky(denominator)
+    cholesky_inv = np.linalg.inv(cholesky)
+
+    transformed_numerator = cholesky_inv @ numerator @ cholesky_inv.T
+
+    bent_transformed = mbend(
+        transformed_numerator,
+        numerator_weight,
+        invert_weights=invert_weights,
+        max_iter=max_iter,
+        small_positive=small_positive,
+        method=MbendMethod.ZIETZ,
+        tol=tol,
+    )
+
+    bent_numerator = cholesky @ bent_transformed @ cholesky.T
+    return bent_numerator
