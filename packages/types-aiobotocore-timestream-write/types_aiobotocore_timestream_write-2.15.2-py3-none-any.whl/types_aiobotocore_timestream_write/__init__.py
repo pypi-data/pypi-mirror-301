0e8ddf3ac74ab136c6845d70a4f165b19fa66218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package types-aiobotocore-timestream-write/2.15.2/types_aiobotocore_timestream_write-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-timestream-write/types_aiobotocore_timestream_write-2.15.2-py3-none-any.whl/types_aiobotocore_timestream_write/__init__.py
@@ -0,0 +1,26 @@
+"""
+Main interface for timestream-write service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_timestream_write import (
+        Client,
+        TimestreamWriteClient,
+    )
+
+    session = get_session()
+    async with session.create_client("timestream-write") as client:
+        client: TimestreamWriteClient
+        ...
+
+    ```
+"""
+
+from .client import TimestreamWriteClient
+
+Client = TimestreamWriteClient
+
+
+__all__ = ("Client", "TimestreamWriteClient")
