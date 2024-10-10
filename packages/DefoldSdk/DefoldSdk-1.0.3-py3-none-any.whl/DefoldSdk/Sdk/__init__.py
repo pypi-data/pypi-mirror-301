f4c0934497024294b5b94eb1525292e626d3f0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:04:07 +0000

    Add package DefoldSdk/1.0.3/DefoldSdk-1.0.3-py3-none-any.whl

---
+++ packages/DefoldSdk/DefoldSdk-1.0.3-py3-none-any.whl/DefoldSdk/Sdk/__init__.py
@@ -0,0 +1,20 @@
+import sys , os 
+from DefoldSdk.naitivesdk import SdkDefold as sdk 
+import importlib , glob 
+
+
+local_files = {
+    os.path.basename(file).removesuffix(".py")
+    for file in glob.glob(os.path.join(os.path.dirname(__file__),'*.py'))
+    if not os.path.basename(file) in {'__init__.py','naitivesdk.py'}
+    }
+
+for local_file in local_files : 
+    sub_module = importlib.import_module(f'.{local_file}', package = 'DefoldSdk.Sdk')
+    for cls_name in dir(sub_module) :
+        cls = getattr(sub_module,cls_name)
+        if hasattr(cls,'__mule__') : 
+            if getattr(cls , '__mule__') == True  : 
+                setattr(sdk,cls_name,cls)
+
+
