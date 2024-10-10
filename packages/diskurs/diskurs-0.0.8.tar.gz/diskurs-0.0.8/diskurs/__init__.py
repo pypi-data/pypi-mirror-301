07917031f39694975139ca4d882d9823f94276dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:04:04 +0000

    Add package diskurs/0.0.8/diskurs-0.0.8.tar.gz

---
+++ packages/diskurs/diskurs-0.0.8.tar.gz/diskurs-0.0.8/diskurs/__init__.py
@@ -0,0 +1,23 @@
+from .config import ToolDependency
+from .dispatcher import SynchronousConversationDispatcher
+from .forum import create_forum_from_config
+from .entities import Conversation, PromptArgument, LongtermMemory, DiskursInput
+from .prompt import PromptValidationError
+from .tools import tool
+
+from .multistep_agent import MultiStepAgent
+from .conductor_agent import ConductorAgent
+
+__all__ = [
+    "create_forum_from_config",
+    "Conversation",
+    "PromptArgument",
+    "LongtermMemory",
+    "DiskursInput",
+    "MultiStepAgent",
+    "ConductorAgent",
+    "SynchronousConversationDispatcher",
+    "ToolDependency",
+    "PromptValidationError",
+    "tool",
+]
