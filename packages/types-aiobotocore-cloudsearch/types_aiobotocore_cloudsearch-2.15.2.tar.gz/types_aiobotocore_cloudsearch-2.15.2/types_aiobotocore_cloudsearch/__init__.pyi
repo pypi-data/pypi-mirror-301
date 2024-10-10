f0c3a4cb94c7595f2b858580f9acde0bd1baaee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 14:03:26 +0000

    Add package types-aiobotocore-cloudsearch/2.15.2/types_aiobotocore_cloudsearch-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-cloudsearch/types_aiobotocore_cloudsearch-2.15.2.tar.gz/types_aiobotocore_cloudsearch-2.15.2/types_aiobotocore_cloudsearch/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for cloudsearch service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cloudsearch import (
+        Client,
+        CloudSearchClient,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudsearch") as client:
+        client: CloudSearchClient
+        ...
+
+    ```
+"""
+
+from .client import CloudSearchClient
+
+Client = CloudSearchClient
+
+__all__ = ("Client", "CloudSearchClient")
