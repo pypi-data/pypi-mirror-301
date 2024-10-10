2ac1b54ea84bb8e8a6caf6d5d09f924485a4ce40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:39 +0000

    Add package xcoll/0.5.5/xcoll-0.5.5-py3-none-any.whl

---
+++ packages/xcoll/xcoll-0.5.5-py3-none-any.whl/xcoll/__init__.py
@@ -0,0 +1,24 @@
+# copyright ############################### #
+# This file is part of the Xcoll package.   #
+# Copyright (c) CERN, 2024.                 #
+# ######################################### #
+
+from .general import _pkg_root, __version__, citation
+
+from .beam_elements import BlackAbsorber, BlackCrystal, EverestBlock, EverestCollimator, EverestCrystal, \
+                           BlowUp, EmittanceMonitor, collimator_classes, crystal_classes, element_classes
+from .install import install_elements
+from .line_tools import assign_optics_to_collimators, open_collimators, send_to_parking, enable_scattering, disable_scattering
+from .scattering_routines.everest import materials, Material, CrystalMaterial
+from .colldb import CollimatorDatabase
+from .interaction_record import InteractionRecord
+from .rf_sweep import RFSweep
+from .initial_distribution import *
+from .lossmap import LossMap
+
+# Deprecated
+from ._manager import CollimatorManager
+
+# print("If you use Xcoll in your simulations, please cite us :-)")
+# print(citation)
+
