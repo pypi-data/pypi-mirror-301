eaba2f44b720a2879ae1e0b9c867154f24f38605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:07:44 +0000

    Add package dbscan1d/0.2.3/dbscan1d-0.2.3-py3-none-any.whl

---
+++ packages/dbscan1d/dbscan1d-0.2.3-py3-none-any.whl/dbscan1d/version.py
@@ -0,0 +1,9 @@
+"""Module for reporting the version of dbscan1d."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("dbscan1d")
+# package is not installed
+except PackageNotFoundError:
+    __version__ = "0.0.0"
