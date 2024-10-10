3eca55f0dd3d8352581301f3e025f6b45e323cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:22 +0000

    Add package scratchattachplus/0.2.1/scratchattachplus-0.2.1.tar.gz

---
+++ packages/scratchattachplus/scratchattachplus-0.2.1.tar.gz/scratchattachplus-0.2.1/scratchattachplus/__init__.py
@@ -0,0 +1,23 @@
+#import
+from .scratchattachplus import *
+
+#ver
+__version__ = '0.2.1'
+__name__ = "scratchattachplus"
+__url__ = "https://github.com/kakeruzoku/scratchattachplus"
+__changelog__ = """[https://github.com/kakeruzoku/scratchattachplus/blob/main/changelog.md]
+v0.2.0 プロジェクトの作成の追加
+v0.1.3 きにすんな
+v0.1.2 エラーの修正
+v0.1.1 エラーの修正
+v0.1.0 エラーの修正+ScratchAttachクラウドリクエストのクライアント機能の追加
+v0.0.1 共有
+"""
+
+
+
+# ScratchAttach:https://github.com/timmccool/scratchattach
+# python setup.py sdist
+# python setup.py bdist_wheel
+# twine upload --repository pypi dist/*
+
