45d6cacc3081592cf8cdbf716e07d1199f7272a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:08:28 +0000

    Add package textual-textarea/0.14.3/textual_textarea-0.14.3.tar.gz

---
+++ packages/textual-textarea/textual_textarea-0.14.3.tar.gz/textual_textarea-0.14.3/src/textual_textarea/__init__.py
@@ -0,0 +1,13 @@
+from textual_textarea.messages import (
+    TextAreaClipboardError,
+    TextAreaSaved,
+)
+from textual_textarea.path_input import PathInput
+from textual_textarea.text_editor import TextEditor
+
+__all__ = [
+    "TextEditor",
+    "PathInput",
+    "TextAreaClipboardError",
+    "TextAreaSaved",
+]
---
+++ packages/textual-textarea/textual_textarea-0.14.3.tar.gz/textual_textarea-0.14.3/src/textual_textarea/__main__.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import sys
+
+from textual.app import App, ComposeResult
+from textual.widgets import Placeholder
+
+from textual_textarea import TextEditor
+
+
+class FocusablePlaceholder(Placeholder, can_focus=True):
+    pass
+
+
+class TextApp(App, inherit_bindings=False):
+    CSS = """
+    TextEditor {
+        height: 1fr;
+    }
+    Placeholder {
+        height: 0fr;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        try:
+            language = sys.argv[1]
+        except IndexError:
+            language = "python"
+        yield FocusablePlaceholder()
+        self.editor = TextEditor(
+            language=language,
+            theme="monokai",
+            use_system_clipboard=True,
+            id="ta",
+        )
+        yield self.editor
+
+    def on_mount(self) -> None:
+        self.editor.focus()
+
+        def _completer(prefix: str) -> list[tuple[str, str]]:
+            words = [
+                "satisfy",
+                "season",
+                "second",
+                "seldom",
+                "select",
+                "self",
+                "separate",
+                "set",
+                "space",
+                "super",
+                "supercalifragilisticexpialadocioussupercalifragilisticexpialadocious",
+            ]
+            return [(w, w) for w in words if w.startswith(prefix)]
+
+        self.editor.word_completer = _completer
+
+
+app = TextApp()
+app.run()
