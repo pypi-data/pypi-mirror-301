6e2d5e703214ed1ab7b0c7011f0449b118b6d435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:38 +0000

    Add package metbit/5.0.5/metbit-5.0.5.tar.gz

---
+++ packages/metbit/metbit-5.0.5.tar.gz/metbit-5.0.5/metbit/peak_processe.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+import pandas as pd
+import numpy as np
+
+
+class peak_chops:
+
+    def __init__(self, data: pd.DataFrame, ppm: list = None) -> None:
+
+
+        #if ppm is None check column of X is can convert to float?
+        if ppm is None:
+            text = f'''
+            ------------------------------------------- \n
+            The columns of data can't convert to ppm \n
+            please assign ppm parameter as list of ppm \n
+            -------------------------------------------
+            '''
+            try:
+                ppm = data.columns.astype(float).to_list()
+            except:
+                raise print(text)
+
+        else:
+            pass
+
+        self.data
+        self.ppm
+
+
+
+
+    def cut_peak(self, first_ppm: float, second_ppm: float):
+        
+        X = self.data
+        ppm = self.ppm 
+
+
+        
+        first_index = ppm.get_loc(min(ppm, key=lambda x: abs(x - first_ppm)))
+        second_index = ppm.get_loc(min(ppm, key=lambda x: abs(x - second_ppm)))
+        
+        X.drop(columns=X.iloc[:, first_index:second_index].columns, inplace=True)
+        ppm = X.columns.astype(float)
+        
+        return X, ppm
