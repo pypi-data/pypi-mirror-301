a0365a4e88a190972fe650b34b4d3ce5152b3b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:20 +0000

    Add package moapy/0.9.1/moapy-0.9.1.tar.gz

---
+++ packages/moapy/moapy-0.9.1.tar.gz/moapy-0.9.1/moapy/dgnengine/steel_boltconnection.py
@@ -0,0 +1,13 @@
+from moapy.auto_convert import auto_schema
+from moapy.data_post import ResultMD
+from moapy.steel_pre import SteelConnectMember_EC, SteelPlateMember, ConnectType, SteelBolt, Welding
+from moapy.dgnengine.base import generate_report, load_dll
+
+@auto_schema
+def report_ec3_bolt_connection(conn: SteelConnectMember_EC, plate: SteelPlateMember, conType: ConnectType, Bolt: SteelBolt, weld: Welding) -> ResultMD:
+    dll = load_dll()
+    json_data_list = [conn.supporting.json(), conn.supported.json(), plate.json(), conType.json(), Bolt.json(), weld.json()]
+    return generate_report(dll, 'Report_EC3_BoltConnection', json_data_list)
+
+# res = report_ec3_bolt_connection(SteelConnectMember_EC(), SteelPlateMember(), ConnectType(), SteelBolt(), Welding())
+# print(res.md)
