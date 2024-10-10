bfc62eebcb87619ecfbdfb672c3f11f15d46f6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package types-aiobotocore-kinesisanalytics/2.15.2/types_aiobotocore_kinesisanalytics-2.15.2.tar.gz

---
+++ packages/types-aiobotocore-kinesisanalytics/types_aiobotocore_kinesisanalytics-2.15.2.tar.gz/types_aiobotocore_kinesisanalytics-2.15.2/types_aiobotocore_kinesisanalytics/client.pyi
@@ -0,0 +1,338 @@
+"""
+Type annotations for kinesisanalytics service client.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_kinesisanalytics.client import KinesisAnalyticsClient
+
+    session = get_session()
+    async with session.create_client("kinesisanalytics") as client:
+        client: KinesisAnalyticsClient
+    ```
+"""
+
+import sys
+from typing import Any, Dict, Mapping, Type
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+
+from .type_defs import (
+    AddApplicationCloudWatchLoggingOptionRequestRequestTypeDef,
+    AddApplicationInputProcessingConfigurationRequestRequestTypeDef,
+    AddApplicationInputRequestRequestTypeDef,
+    AddApplicationOutputRequestRequestTypeDef,
+    AddApplicationReferenceDataSourceRequestRequestTypeDef,
+    CreateApplicationRequestRequestTypeDef,
+    CreateApplicationResponseTypeDef,
+    DeleteApplicationCloudWatchLoggingOptionRequestRequestTypeDef,
+    DeleteApplicationInputProcessingConfigurationRequestRequestTypeDef,
+    DeleteApplicationOutputRequestRequestTypeDef,
+    DeleteApplicationReferenceDataSourceRequestRequestTypeDef,
+    DeleteApplicationRequestRequestTypeDef,
+    DescribeApplicationRequestRequestTypeDef,
+    DescribeApplicationResponseTypeDef,
+    DiscoverInputSchemaRequestRequestTypeDef,
+    DiscoverInputSchemaResponseTypeDef,
+    ListApplicationsRequestRequestTypeDef,
+    ListApplicationsResponseTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    StartApplicationRequestRequestTypeDef,
+    StopApplicationRequestRequestTypeDef,
+    TagResourceRequestRequestTypeDef,
+    UntagResourceRequestRequestTypeDef,
+    UpdateApplicationRequestRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("KinesisAnalyticsClient",)
+
+class BotocoreClientError(Exception):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    ClientError: Type[BotocoreClientError]
+    CodeValidationException: Type[BotocoreClientError]
+    ConcurrentModificationException: Type[BotocoreClientError]
+    InvalidApplicationConfigurationException: Type[BotocoreClientError]
+    InvalidArgumentException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ResourceInUseException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ResourceProvisionedThroughputExceededException: Type[BotocoreClientError]
+    ServiceUnavailableException: Type[BotocoreClientError]
+    TooManyTagsException: Type[BotocoreClientError]
+    UnableToDetectSchemaException: Type[BotocoreClientError]
+    UnsupportedOperationException: Type[BotocoreClientError]
+
+class KinesisAnalyticsClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        KinesisAnalyticsClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.exceptions)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#exceptions)
+        """
+
+    async def add_application_cloud_watch_logging_option(
+        self, **kwargs: Unpack[AddApplicationCloudWatchLoggingOptionRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_cloud_watch_logging_option)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#add_application_cloud_watch_logging_option)
+        """
+
+    async def add_application_input(
+        self, **kwargs: Unpack[AddApplicationInputRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_input)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#add_application_input)
+        """
+
+    async def add_application_input_processing_configuration(
+        self, **kwargs: Unpack[AddApplicationInputProcessingConfigurationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_input_processing_configuration)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#add_application_input_processing_configuration)
+        """
+
+    async def add_application_output(
+        self, **kwargs: Unpack[AddApplicationOutputRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_output)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#add_application_output)
+        """
+
+    async def add_application_reference_data_source(
+        self, **kwargs: Unpack[AddApplicationReferenceDataSourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.add_application_reference_data_source)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#add_application_reference_data_source)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.can_paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#can_paginate)
+        """
+
+    async def close(self) -> None:
+        """
+        Closes underlying endpoint connections.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.close)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#close)
+        """
+
+    async def create_application(
+        self, **kwargs: Unpack[CreateApplicationRequestRequestTypeDef]
+    ) -> CreateApplicationResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.create_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#create_application)
+        """
+
+    async def delete_application(
+        self, **kwargs: Unpack[DeleteApplicationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#delete_application)
+        """
+
+    async def delete_application_cloud_watch_logging_option(
+        self, **kwargs: Unpack[DeleteApplicationCloudWatchLoggingOptionRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_cloud_watch_logging_option)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#delete_application_cloud_watch_logging_option)
+        """
+
+    async def delete_application_input_processing_configuration(
+        self, **kwargs: Unpack[DeleteApplicationInputProcessingConfigurationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_input_processing_configuration)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#delete_application_input_processing_configuration)
+        """
+
+    async def delete_application_output(
+        self, **kwargs: Unpack[DeleteApplicationOutputRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_output)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#delete_application_output)
+        """
+
+    async def delete_application_reference_data_source(
+        self, **kwargs: Unpack[DeleteApplicationReferenceDataSourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.delete_application_reference_data_source)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#delete_application_reference_data_source)
+        """
+
+    async def describe_application(
+        self, **kwargs: Unpack[DescribeApplicationRequestRequestTypeDef]
+    ) -> DescribeApplicationResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.describe_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#describe_application)
+        """
+
+    async def discover_input_schema(
+        self, **kwargs: Unpack[DiscoverInputSchemaRequestRequestTypeDef]
+    ) -> DiscoverInputSchemaResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.discover_input_schema)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#discover_input_schema)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.generate_presigned_url)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#generate_presigned_url)
+        """
+
+    async def list_applications(
+        self, **kwargs: Unpack[ListApplicationsRequestRequestTypeDef]
+    ) -> ListApplicationsResponseTypeDef:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.list_applications)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#list_applications)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Retrieves the list of key-value tags assigned to the application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.list_tags_for_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#list_tags_for_resource)
+        """
+
+    async def start_application(
+        self, **kwargs: Unpack[StartApplicationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.start_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#start_application)
+        """
+
+    async def stop_application(
+        self, **kwargs: Unpack[StopApplicationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.stop_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#stop_application)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Adds one or more key-value tags to a Kinesis Analytics application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.tag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        Removes one or more tags from a Kinesis Analytics application.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.untag_resource)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#untag_resource)
+        """
+
+    async def update_application(
+        self, **kwargs: Unpack[UpdateApplicationRequestRequestTypeDef]
+    ) -> Dict[str, Any]:
+        """
+        .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client.update_application)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/#update_application)
+        """
+
+    async def __aenter__(self) -> "KinesisAnalyticsClient":
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/)
+        """
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesisanalytics.html#KinesisAnalytics.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesisanalytics/client/)
+        """
