98e4eb534b0df1b0a5586450a7c9d5786f3e56fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package vlm-ocr/0.1.0/vlm-ocr-0.1.0.tar.gz

---
+++ packages/vlm-ocr/vlm-ocr-0.1.0.tar.gz/vlm-ocr-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vlm-ocr',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        line.strip() for line in open('requirements.txt')
+    ],
+    author='Ethan Bailie',
+    author_email='eabailie@uwaterloo.ca',
+    description='Allows you to easily use a VLM for OCR purposes. Currently only OpenAI, working on adding more VLM compatibility',
+    url='https://github.com/ethanbailie/vlm-ocr',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
