b1fddd8925b56a0631fde6c0d08c9ec00599fda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:35 +0000

    Add package aritmatika/0.0.6/aritmatika-0.0.6-py3-none-any.whl

---
+++ packages/aritmatika/aritmatika-0.0.6-py3-none-any.whl/aritmatika/__init__.py
@@ -0,0 +1,39 @@
+from .gaji import (
+    hitung_gaji_pokok,
+    hitung_gaji_bersih,
+    hitung_gaji_lembur,
+    hitung_total_gaji,
+    hitung_rata_rata_gaji
+)
+
+from .bruto_tara_netto import (
+    hitung_bruto,
+    hitung_tara,
+    hitung_netto
+)
+
+from .bunga import (
+    bunga_tunggal,
+    bunga_majemuk
+)
+
+from .pajak import (
+    ppn,
+    total_bayar,
+    njkp,
+    pbb,
+    hitung_pph
+)
+
+from .rabat_diskon import (
+    hitung_diskon,
+    hitung_persen_diskon,
+    hitung_rabat
+)
+
+from .untung_rugi import (
+    hitung_untung,
+    hitung_rugi,
+    persentase_untung,
+    persentase_rugi
+)
