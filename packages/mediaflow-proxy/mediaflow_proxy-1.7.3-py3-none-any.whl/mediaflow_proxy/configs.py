9184880501b23ac6875d26f8c34d485ea1173105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:45 +0000

    Add package mediaflow-proxy/1.7.3/mediaflow_proxy-1.7.3-py3-none-any.whl

---
+++ packages/mediaflow-proxy/mediaflow_proxy-1.7.3-py3-none-any.whl/mediaflow_proxy/configs.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    api_password: str  # The password for accessing the API endpoints.
+    proxy_url: str | None = None  # The URL of the proxy server to route requests through.
+
+    class Config:
+        env_file = ".env"
+        extra = "ignore"
+
+
+settings = Settings()
