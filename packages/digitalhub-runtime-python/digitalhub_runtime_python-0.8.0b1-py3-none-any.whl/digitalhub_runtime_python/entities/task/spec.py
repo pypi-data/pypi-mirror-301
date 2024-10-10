88511be4da79bac78021529f41c76792165fd97e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:52 +0000

    Add package digitalhub-runtime-python/0.8.0b1/digitalhub_runtime_python-0.8.0b1-py3-none-any.whl

---
+++ packages/digitalhub-runtime-python/digitalhub_runtime_python-0.8.0b1-py3-none-any.whl/digitalhub_runtime_python/entities/task/spec.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from digitalhub.entities.task.spec import TaskParamsK8s, TaskSpecK8s
+
+
+class TaskSpecJob(TaskSpecK8s):
+    """Task Job specification."""
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: dict | None = None,
+        volumes: list | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list | None = None,
+        envs: list | None = None,
+        secrets: list | None = None,
+        profile: str | None = None,
+        backoff_limit: int | None = None,
+        schedule: str | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function,
+            node_selector,
+            volumes,
+            resources,
+            affinity,
+            tolerations,
+            envs,
+            secrets,
+            profile,
+            **kwargs,
+        )
+
+        self.backoff_limit = backoff_limit
+        self.schedule = schedule
+
+
+class TaskParamsJob(TaskParamsK8s):
+    """
+    TaskParamsJob model.
+    """
+
+    backoff_limit: int = None
+    """Backoff limit."""
+
+    schedule: str = None
+    """Schedule."""
+
+
+class TaskSpecBuild(TaskSpecK8s):
+    """Task Build specification."""
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: dict | None = None,
+        volumes: list | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list | None = None,
+        envs: list | None = None,
+        secrets: list | None = None,
+        profile: str | None = None,
+        instructions: list | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function,
+            node_selector,
+            volumes,
+            resources,
+            affinity,
+            tolerations,
+            envs,
+            secrets,
+            profile,
+            **kwargs,
+        )
+
+        self.instructions = instructions
+
+
+class TaskParamsBuild(TaskParamsK8s):
+    """
+    TaskParamsBuild model.
+    """
+
+    instructions: list[str] = None
+    """Build instructions."""
+
+
+class TaskSpecServe(TaskSpecK8s):
+    """Task Serve specification."""
+
+    def __init__(
+        self,
+        function: str,
+        node_selector: dict | None = None,
+        volumes: list | None = None,
+        resources: dict | None = None,
+        affinity: dict | None = None,
+        tolerations: list | None = None,
+        envs: list | None = None,
+        secrets: list | None = None,
+        profile: str | None = None,
+        replicas: int | None = None,
+        service_type: str | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            function,
+            node_selector,
+            volumes,
+            resources,
+            affinity,
+            tolerations,
+            envs,
+            secrets,
+            profile,
+            **kwargs,
+        )
+
+        self.replicas = replicas
+        self.service_type = service_type
+
+
+class TaskParamsServe(TaskParamsK8s):
+    """
+    TaskParamsServe model.
+    """
+
+    replicas: int = None
+    service_type: Literal["ClusterIP", "NodePort", "LoadBalancer"] = "NodePort"
