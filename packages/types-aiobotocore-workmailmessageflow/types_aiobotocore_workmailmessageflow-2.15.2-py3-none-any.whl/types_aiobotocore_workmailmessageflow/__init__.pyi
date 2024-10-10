5cf335f50fd249d255c2970f3461837f20add110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:16 +0000

    Add package types-aiobotocore-workmailmessageflow/2.15.2/types_aiobotocore_workmailmessageflow-2.15.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-workmailmessageflow/types_aiobotocore_workmailmessageflow-2.15.2-py3-none-any.whl/types_aiobotocore_workmailmessageflow/__init__.pyi
@@ -0,0 +1,25 @@
+"""
+Main interface for workmailmessageflow service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_workmailmessageflow import (
+        Client,
+        WorkMailMessageFlowClient,
+    )
+
+    session = get_session()
+    async with session.create_client("workmailmessageflow") as client:
+        client: WorkMailMessageFlowClient
+        ...
+
+    ```
+"""
+
+from .client import WorkMailMessageFlowClient
+
+Client = WorkMailMessageFlowClient
+
+__all__ = ("Client", "WorkMailMessageFlowClient")
