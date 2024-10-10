ef9f02e8ea270ddd9e0b1bdbbb51abea88a63cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:15 +0000

    Add package drvelasquezq/0.0.1/drvelasquezq-0.0.1-py3-none-any.whl

---
+++ packages/drvelasquezq/drvelasquezq-0.0.1-py3-none-any.whl/holamundoplayer/player.py
@@ -0,0 +1,28 @@
+"""
+este es el modulo que incluye la clase de 
+reproductor de musica
+"""
+
+
+class Player:
+    """
+    Esta clase crea un reproductor de musica
+    """
+
+    def play(self, song):
+        """
+        Reproduce la canción que recibio como parametro
+
+        Parameters:
+        song (str): este es un string con el path de la canción
+
+        Returns:
+        int: devuelve 1 si reproduce con exito, en caso de fracaso devuelve 0
+        """
+        print("reproduciendo canción")
+
+    def stop(self):
+        """
+        Detiene la cancion
+        """
+        print("stopping")
