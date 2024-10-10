4e4db0641f8b83680c49e297edbac931849db85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 16:03:54 +0000

    Add package Zcan/0.2/Zcan-0.2.tar.gz

---
+++ packages/Zcan/Zcan-0.2.tar.gz/Zcan-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='Zcan',
+    version='0.2',
+    description='A small example package',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
