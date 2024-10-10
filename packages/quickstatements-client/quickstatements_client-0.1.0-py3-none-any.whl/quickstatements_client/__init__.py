b53bf967e966a5e4b6728e54fdc98cedfbf20ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:03:42 +0000

    Add package quickstatements-client/0.1.0/quickstatements_client-0.1.0-py3-none-any.whl

---
+++ packages/quickstatements-client/quickstatements_client-0.1.0-py3-none-any.whl/quickstatements_client/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""A data model and client for Wikidata QuickStatements."""
+
+from .client import QuickStatementsClient, post_lines
+from .model import (
+    CreateLine,
+    DateLine,
+    DateQualifier,
+    EntityLine,
+    EntityQualifier,
+    Line,
+    Qualifier,
+    TextLine,
+    TextQualifier,
+    lines_to_new_tab,
+    lines_to_url,
+    render_lines,
+)
+
+__all__ = [
+    # Client
+    "QuickStatementsClient",
+    # Data model
+    "EntityQualifier",
+    "DateQualifier",
+    "TextQualifier",
+    "Qualifier",
+    "CreateLine",
+    "TextLine",
+    "EntityLine",
+    "DateLine",
+    "Line",
+    # Line renderers
+    "post_lines",
+    "render_lines",
+    "lines_to_url",
+    "lines_to_new_tab",
+]
---
+++ packages/quickstatements-client/quickstatements_client-0.1.0-py3-none-any.whl/quickstatements_client/maintenance/annotate_missing_followed_by.py
@@ -0,0 +1,49 @@
+"""A maintenance script for adding missing "followed by" relations to preprint/postprints."""
+
+import sys
+
+import click
+
+from quickstatements_client import EntityLine, QuickStatementsClient
+from quickstatements_client.sources.utils import query_wikidata
+
+#: A SPARQL query that identifies postprints that are annotated
+#: with the "follows" relationship, but whose preprint doesn't
+#: have the "followed by" relationship pointing backwards
+SPARQL = """\
+SELECT ?preprint ?postprint
+WHERE {
+  ?preprint wdt:P31 wd:Q580922 .
+  ?postprint wdt:P155 ?preprint .
+  FILTER NOT EXISTS {
+    ?preprint wdt:P156 ?postprint .
+  }
+}
+"""
+
+
+@click.command()
+@click.option("--non-interactive", is_flag=True)
+def main(non_interactive: bool):
+    """Add missing "followed by" relationships to preprints."""
+    lines = [
+        EntityLine(
+            subject=record["preprint"],
+            predicate="P156",
+            target=record["postprint"],
+        )
+        for record in query_wikidata(SPARQL)
+    ]
+    if not lines:
+        click.secho("No 'followed by' relations to add", fg="yellow")
+        return sys.exit(0)
+
+    client = QuickStatementsClient()
+    if non_interactive:
+        client.post(lines, batch_name="Postprint Maintenance")
+    else:
+        client.open_new_tab(lines)
+
+
+if __name__ == "__main__":
+    main()
