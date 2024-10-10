563dbd86292822f5b4f70620eba1952410dab0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 03:08:06 +0000

    Add package fast-bioservices/0.3.5/fast_bioservices-0.3.5.tar.gz

---
+++ packages/fast-bioservices/fast_bioservices-0.3.5.tar.gz/fast_bioservices-0.3.5/src/fast_bioservices/utils.py
@@ -0,0 +1,25 @@
+from difflib import SequenceMatcher
+from typing import List, Union
+
+
+def flatten(lists: list) -> list:
+    data = []
+    for item in lists:
+        if isinstance(item, list):
+            data.extend(flatten(item))
+        else:
+            data.append(item)
+    return data
+
+
+def fuzzy_search(query: str, possibilities: Union[str, List[str]]) -> float:
+    matcher = SequenceMatcher(isjunk=None, a=query)
+    if isinstance(possibilities, str):
+        possibilities = [possibilities]
+
+    # Provide a default value for the ratio
+    ratios: list[float] = [0.0]
+    for possible in possibilities:
+        matcher.set_seq2(possible)
+        ratios.append(matcher.ratio())
+    return max(ratios)
