42cf4bf8aa75aa60f9a071970a32998cff6cd59b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:50 +0000

    Add package flexeval/0.7.2/flexeval-0.7.2.tar.gz

---
+++ packages/flexeval/flexeval-0.7.2.tar.gz/flexeval-0.7.2/flexeval/core/prompt_template/jinja2.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Any
+
+from flexeval.core.utils.jinja2_utils import JINJA2_ENV
+
+from .base import PromptTemplate
+
+
+class Jinja2PromptTemplate(PromptTemplate):
+    """
+    Embed task inputs using Jinja2 template engine.
+
+    Args:
+        template: The Jinja2 template to use.
+    """
+
+    def __init__(self, template: str) -> None:
+        self.template = template
+
+    def embed_inputs(self, input_dict: dict[str, Any]) -> str:
+        return JINJA2_ENV.from_string(self.template).render(input_dict)
+
+    def __repr__(self) -> str:
+        return f"Jinja2PromptTemplate(template={self.template!r})"
