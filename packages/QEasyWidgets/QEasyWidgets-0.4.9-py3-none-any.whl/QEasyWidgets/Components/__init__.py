937442436c2a01e6d0b5341fe9a016a3037f0386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:04:08 +0000

    Add package QEasyWidgets/0.4.9/QEasyWidgets-0.4.9-py3-none-any.whl

---
+++ packages/QEasyWidgets/QEasyWidgets-0.4.9-py3-none-any.whl/QEasyWidgets/Components/__init__.py
@@ -0,0 +1,16 @@
+from .Widget import WidgetBase
+from .Label import LabelBase
+from .Button import ButtonBase, MenuButton, HollowButton
+from .ScrollArea import ScrollAreaBase, VerticalScrollArea
+from .Tree import TreeWidgetBase
+from .List import ListBase
+from .ToolBox import ToolBoxBase
+from .GroupBox import GroupBoxBase
+from .SpinBox import SpinBoxBase, DoubleSpinBoxBase
+from .ComboBox import ComboBoxBase
+from .Edit import LineEditBase, TextEditBase
+from .Browser import TextBrowserBase
+from .Player import MediaPlayerBase
+from .Table import TableBase
+from .ChatWidget import ChatWidgetBase
+from .StatusWidget import StatusWidgetBase
