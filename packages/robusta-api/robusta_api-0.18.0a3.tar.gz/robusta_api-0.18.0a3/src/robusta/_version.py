ad5a2f7ce250e63732c18c4674112a500a47cfea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 15:03:40 +0000

    Add package robusta-api/0.18.0a3/robusta_api-0.18.0a3.tar.gz

---
+++ packages/robusta-api/robusta_api-0.18.0a3.tar.gz/robusta_api-0.18.0a3/src/robusta/_version.py
@@ -0,0 +1,2 @@
+# this is updated by .github/workflows/release.yaml
+__version__ = "0.18.0-alpha.3"
---
+++ packages/robusta-api/robusta_api-0.18.0a3.tar.gz/robusta_api-0.18.0a3/src/robusta/core/sinks/opsgenie/opsgenie_sink_params.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+from robusta.core.sinks.sink_base_params import SinkBaseParams
+from robusta.core.sinks.sink_config import SinkConfigBase
+
+
+class OpsGenieSinkParams(SinkBaseParams):
+    api_key: str
+    teams: List[str] = []
+    tags: List[str] = []
+    host: Optional[str] = None  # NOTE: If None, the default value will be used from opsgenie_sdk
+
+    @classmethod
+    def _get_sink_type(cls):
+        return "opsgenie"
+
+
+class OpsGenieSinkConfigWrapper(SinkConfigBase):
+    opsgenie_sink: OpsGenieSinkParams
+
+    def get_params(self) -> SinkBaseParams:
+        return self.opsgenie_sink
