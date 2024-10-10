379d3394f879815610a9064b196b98714f276109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-10 17:04:02 +0000

    Add package gardener-oci/1.2520.0/gardener_oci-1.2520.0.tar.gz

---
+++ packages/gardener-oci/gardener_oci-1.2520.0.tar.gz/gardener_oci-1.2520.0/setup.oci.py
@@ -0,0 +1,42 @@
+import setuptools
+import os
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def requirements():
+    with open(os.path.join(own_dir, 'requirements.oci.txt')) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+
+            yield line
+
+
+def modules():
+    return [
+    ]
+
+
+def version():
+    with open(os.path.join(own_dir, 'oci', 'VERSION')) as f:
+        return f.read().strip()
+
+
+setuptools.setup(
+    name='gardener-oci',
+    version=version(),
+    description='Gardener OCI lib',
+    long_description='Gardener OCI lib',
+    long_description_content_type='text/markdown',
+    python_requires='>=3.10',
+    py_modules=modules(),
+    packages=['oci'],
+    package_data={
+        'oci': ['VERSION'],
+    },
+    install_requires=list(requirements()),
+    entry_points={
+    },
+)
