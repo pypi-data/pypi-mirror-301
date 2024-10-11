ba68da75f7ebb9158d9fc56e0782c6d563b7592c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:04:22 +0000

    Add package ecodev-front/0.0.56/ecodev_front-0.0.56.tar.gz

---
+++ packages/ecodev-front/ecodev_front-0.0.56.tar.gz/ecodev_front-0.0.56/ecodev_front/constants.py
@@ -0,0 +1,18 @@
+"""
+Callbacks constants
+"""
+CHILDREN = 'children'
+DATA = 'data'
+PATHNAME = 'pathname'
+N_CLICKS = 'n_clicks'
+VALUE = 'value'
+DISABLED = 'disabled'
+STYLE = 'style'
+
+"""
+Unit Converters
+"""
+THOUSAND = 1000
+MILLION = 1_000_000
+BILLION = 1_000_000_000
+TRILLION = 1_000_000_000_000
