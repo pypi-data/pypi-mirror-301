0ced7c3f0ba7c77e7e1328becf223fd6efab0747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:03:33 +0000

    Add package qz-cal/1.0.1/qz_cal-1.0.1.tar.gz

---
+++ packages/qz-cal/qz_cal-1.0.1.tar.gz/qz_cal-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qz_cal",  # 包的名称
+    version="1.0.1",  # 初始版本号
+    author="Your Name",
+    author_email="your_email@example.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/your_package",  # 项目的 GitHub 地址
+    packages=find_packages(),  # 自动找到所有的包
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # 指定许可证
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # 指定 Python 版本要求
+    install_requires=[  # 列出依赖项  
+    ]
+)
