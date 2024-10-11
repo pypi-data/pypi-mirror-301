e0159bb42f4520f225b11233e73e78627bcd594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:44 +0000

    Add package djalgo/0.2.3/djalgo-0.2.3.tar.gz

---
+++ packages/djalgo/djalgo-0.2.3.tar.gz/djalgo-0.2.3/README.md
@@ -0,0 +1,15 @@
+# djalgo: A music composition toolkit
+
+Djalgo is a Python toolkit for music composition. Crafted for musicians and developers alike, Djalgo offers a versatile collection of tools for generating and manipulating musical notes and sequences. Without relying directly on MIDI files, it employs a MIDI-compatible format, enabling users to define each note through a trio of attributes: pitch (in MIDI notation), duration, and offset. Whether you're scripting a symphony, experimenting with algorithms to create melodies, or exploring the mathematical beauty of music, Djalgo makes the art of digital composition accessible and engaging. The name Djalgo was chosen after musician [Django Reinhardt](https://en.wikipedia.org/wiki/Django_Reinhardt), an a mix of *DJ* (disk jockey) and *algorithm*.
+
+## Install
+
+Djalgo is on PyPi.
+
+```
+pip install djalgo
+```
+
+## Usage
+
+To get started, you might follow the companion website at [essicolo.github.io/djalgo/01_getting-started.html](https://essicolo.github.io/djalgo/source/01_getting-started.html). Happy composing! 🎼
---
+++ packages/djalgo/djalgo-0.2.3.tar.gz/djalgo-0.2.3/docs/conf.py
@@ -0,0 +1,49 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# Project information
+project = 'djalgo'
+copyright = '2024, Essi Parent'
+author = 'Essi Parent'
+release = "0.2.3"
+
+# Extensions
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'recommonmark',
+    'nbsphinx',
+    'jupyter_sphinx',
+]
+
+# General configuration
+master_doc = 'index'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+    '.ipynb': 'jupyter_notebook',
+}
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# HTML output configuration
+html_theme = 'furo'
+html_static_path = ['_static']
+templates_path = ['_templates']
+
+# Nbsphinx settings
+nbsphinx_execute = 'never'
+html_logo = '_static/logo.png'
+html_js_files = [
+    'https://cdn.jsdelivr.net/npm/abcjs@6.2.2/dist/abcjs-basic-min.js',
+]
+
+# Jupyter widgets configuration
+jupyter_sphinx_require_url = 'https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js'
+
+def setup(app):
+    app.add_css_file('https://cdn.jsdelivr.net/npm/abcjs@6.2.2/abcjs-audio.css')
+    app.add_js_file('https://cdn.jsdelivr.net/npm/abcjs@6.2.2/dist/abcjs-basic-min.js')
