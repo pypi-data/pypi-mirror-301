0fc89d9d552fd89af16af525e0926fb120b91deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:09 +0000

    Add package do-circuit-breaker/0.1.1/do_circuit_breaker-0.1.1.tar.gz

---
+++ packages/do-circuit-breaker/do_circuit_breaker-0.1.1.tar.gz/do_circuit_breaker-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='do_circuit_breaker',
+    version='0.1.1',
+    description='A custom circuit breaker for Airflow jobs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ankur Kumar',
+    author_email='ankur.kumar@rakuten.com',
+    url='https://github.com/ankurrakuten/do_circuit_breaker',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
