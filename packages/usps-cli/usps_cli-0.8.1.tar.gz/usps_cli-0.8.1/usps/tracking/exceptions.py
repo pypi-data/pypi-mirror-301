355fa47c944075f67adb2bc5e187d250e9adf097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:17 +0000

    Add package usps-cli/0.8.1/usps_cli-0.8.1.tar.gz

---
+++ packages/usps-cli/usps_cli-0.8.1.tar.gz/usps_cli-0.8.1/usps/tracking/exceptions.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2024 iiPython
+
+class StatusNotAvailable(Exception):
+    pass
+
+class MissingElement(Exception):
+    pass
+
+class InvalidElementType(Exception):
+    pass
+
+class NoTextInElement(Exception):
+    pass
