8c1ead2d7ae287a47ed2465fadfbb7bfd7aecda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:46 +0000

    Add package crossfit/0.0.6/crossfit-0.0.6.tar.gz

---
+++ packages/crossfit/crossfit-0.0.6.tar.gz/crossfit-0.0.6/crossfit/backend/torch/model.py
@@ -0,0 +1,95 @@
+# Copyright 2023 NVIDIA CORPORATION
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import cudf
+import cupy as cp
+
+from crossfit.backend.cudf.series import (
+    create_list_series_from_1d_or_2d_ar,
+    create_nested_list_series_from_3d_ar,
+)
+from crossfit.utils.torch_utils import cleanup_torch_cache, concat_and_pad_tensors
+
+
+class Model:
+    def __init__(self, path_or_name: str, max_mem_gb: int = 16, model_output_type: str = "numeric"):
+        self.path_or_name = path_or_name
+        self.max_mem_gb = max_mem_gb
+        if model_output_type in ["numeric", "string"]:
+            self.model_output_type = model_output_type
+        else:
+            raise ValueError(
+                "Invalid model output type provided. Allowed values are : 'string' or 'numeric'."
+            )
+
+    def load_model(self, device="cuda"):
+        raise NotImplementedError()
+
+    def load_tokenizer(self):
+        raise NotImplementedError()
+
+    def load_on_worker(self, worker):
+        raise NotImplementedError()
+
+    def unload_from_worker(self, worker):
+        raise NotImplementedError()
+
+    def call_on_worker(self, worker, *args, **kwargs):
+        return worker.torch_model(*args, **kwargs)
+
+    def get_model(self, worker):
+        if not hasattr(worker, "torch_model"):
+            self.load_on_worker(worker)
+        return worker.torch_model
+
+    def estimate_memory(self, max_num_tokens: int, batch_size: int) -> int:
+        raise NotImplementedError()
+
+    def max_seq_length(self) -> int:
+        raise NotImplementedError()
+
+    def get_model_output(self, all_outputs_ls, index, loader, pred_output_col) -> cudf.DataFrame:
+        # importing here to avoid cyclic import error
+        from crossfit.backend.torch.loader import SortedSeqLoader
+
+        out = cudf.DataFrame(index=index)
+        _index = loader.sort_column(index.values) if type(loader) is SortedSeqLoader else index
+
+        if self.model_output_type == "string":
+            all_outputs = [o for output in all_outputs_ls for o in output]
+            out[pred_output_col] = cudf.Series(data=all_outputs, index=_index)
+            del all_outputs_ls
+            del loader
+        else:
+            outputs = cp.asarray(
+                concat_and_pad_tensors(
+                    all_outputs_ls,
+                    pad_token_id=loader.pad_token_id,
+                    padding_side=loader.padding_side,
+                )
+            )
+            del all_outputs_ls
+            del loader
+            cleanup_torch_cache()
+            if len(outputs.shape) <= 2:
+                out[pred_output_col] = create_list_series_from_1d_or_2d_ar(outputs, _index)
+            elif len(outputs.shape) == 3:
+                out[pred_output_col] = create_nested_list_series_from_3d_ar(outputs, _index)
+            else:
+                raise RuntimeError(f"Unexpected output shape: {outputs.shape}")
+            del outputs
+        del _index
+        cleanup_torch_cache()
+        return out
---
+++ packages/crossfit/crossfit-0.0.6.tar.gz/crossfit-0.0.6/crossfit/backend/torch/op/base.py
@@ -0,0 +1,85 @@
+# Copyright 2023 NVIDIA CORPORATION
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional
+
+import torch
+
+from crossfit.backend.torch.loader import DEFAULT_BATCH_SIZE, InMemoryLoader, SortedSeqLoader
+from crossfit.backend.torch.model import Model
+from crossfit.op.base import Op
+
+
+class Predictor(Op):
+    def __init__(
+        self,
+        model: Model,
+        pre=None,
+        post=None,
+        cols=False,
+        keep_cols=None,
+        batch_size: int = DEFAULT_BATCH_SIZE,
+        max_mem: str = "16GB",
+        sorted_data_loader: bool = True,
+        model_output_col: Optional[str] = None,
+        pred_output_col: str = "preds",
+    ):
+        super().__init__(pre=pre, cols=cols, keep_cols=keep_cols)
+        self.model = model
+        self.post = post
+        self.batch_size = batch_size
+        self.max_mem = max_mem
+        self.max_mem_gb = int(self.max_mem.split("GB")[0]) / 2.5
+        self.sorted_data_loader = sorted_data_loader
+        self.model_output_col = model_output_col
+        self.pred_output_col = pred_output_col
+
+    @torch.no_grad()
+    def call(self, data, partition_info=None):
+        index = data.index.copy()
+        if self.sorted_data_loader:
+            loader = SortedSeqLoader(
+                data[["input_ids", "attention_mask"]],
+                self.model,
+                progress_bar=self.create_progress_bar(len(data), partition_info),
+                initial_batch_size=self.batch_size,
+            )
+        else:
+            loader = InMemoryLoader(
+                data[["input_ids", "attention_mask"]],
+                batch_size=self.batch_size,
+                padding_side=self.model.load_tokenizer().padding_side,
+                progress_bar=self.create_progress_bar(len(data), partition_info),
+                max_seq_len=self.model.max_seq_length(),
+            )
+        del data
+        all_outputs_ls = []
+        for output in loader.map(self.model.get_model(self.get_worker())):
+            if isinstance(output, dict):
+                if self.model_output_col not in output:
+                    raise ValueError(f"Column '{self.model_output_col}' not found in model output.")
+                output = output[self.model_output_col]
+
+            if self.post is not None:
+                output = self.post(output)
+
+            all_outputs_ls.append(output)
+        out = self.model.get_model_output(all_outputs_ls, index, loader, self.pred_output_col)
+        return out
+
+    def meta(self):
+        if self.model.model_output_type == "string":
+            return {self.pred_output_col: "object"}
+        else:
+            return {self.pred_output_col: "float32"}
