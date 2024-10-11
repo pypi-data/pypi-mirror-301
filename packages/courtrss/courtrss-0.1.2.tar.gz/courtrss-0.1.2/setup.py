1c1da191b74ac3ea914908faa119bad1e7f4509c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:16 +0000

    Add package courtrss/0.1.2/courtrss-0.1.2.tar.gz

---
+++ packages/courtrss/courtrss-0.1.2.tar.gz/courtrss-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+requirements = []
+with open('requirements.txt', 'r') as f:
+    for line in f.readlines():
+        requirements.append(line.strip())
+
+setup(
+    name='courtrss',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'courtrss = courtrss.rss_feed:main',
+        ],
+    },
+    author='Jon Cavallie Mester',
+    description='A package to monitor court RSS feeds for keywords.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jonmest/CourtRSS',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
