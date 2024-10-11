916a677e1c917ad4838bb3f95159fad7b7236000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:53 +0000

    Add package xnano/0.0.23/xnano-0.0.23-py3-none-any.whl

---
+++ packages/xnano/xnano-0.0.23-py3-none-any.whl/xnano/lib/__init__.pyi
@@ -0,0 +1,38 @@
+__all__ = [
+    # llm
+    "classifier",
+    "coder",
+    "extractor",
+    "function",
+    "generator",
+    "validator",
+    "planner",
+    "prompter",
+    "qa",
+    "solver",
+    "query",
+    # nlp/data
+    "chunker",
+    "reader",
+
+    # multimodal
+    "image",
+    "audio",
+    "transcribe"
+]
+
+from .classify import classifier
+from .code import coder
+from .extract import extractor
+from .function_constructor import function
+from .generate import generator
+from .validate import validator
+from .plan import planner
+from .prompt import prompter
+from .question_answer import qa
+from .solve import solver
+from .query import query
+from .chunk import chunker
+from .read import reader
+
+from .multimodal import image, audio, transcribe
