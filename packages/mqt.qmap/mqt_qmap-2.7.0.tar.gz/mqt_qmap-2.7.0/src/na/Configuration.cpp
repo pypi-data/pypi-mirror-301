b4539b45d8377e12fe1149456c0745da6aa9783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:14 +0000

    Add package mqt.qmap/2.7.0/mqt_qmap-2.7.0.tar.gz

---
+++ packages/mqt.qmap/mqt_qmap-2.7.0.tar.gz/mqt_qmap-2.7.0/src/na/Configuration.cpp
@@ -0,0 +1,54 @@
+//
+// This file is part of the MQT QMAP library released under the MIT license.
+// See README.md or go to https://github.com/cda-tum/mqt-qmap for more
+// information.
+//
+
+#include "na/Configuration.hpp"
+
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+using json = nlohmann::basic_json<>;
+
+namespace na {
+
+Configuration::Configuration(const std::string& filename) {
+  std::ifstream fs(filename);
+  if (!fs.good()) {
+    std::stringstream ss;
+    ss << "Could not open JSON file " << filename << ".";
+    throw std::runtime_error(ss.str());
+  }
+  *this = Configuration(fs);
+}
+
+Configuration::Configuration(std::istream& fs) {
+  json data;
+  try {
+    fs >> data;
+  } catch (const std::exception& e) {
+    std::stringstream ss;
+    ss << "Error parsing JSON: " << e.what() << '\n';
+    throw std::runtime_error(ss.str());
+  }
+
+  if (data.contains("patch") && data["patch"].is_object()) {
+    auto& patch = data["patch"];
+    if (patch.contains("rows") && patch["rows"].is_number_unsigned()) {
+      patchRows = patch["rows"];
+    }
+    if (patch.contains("cols") && patch["cols"].is_number_unsigned()) {
+      patchCols = patch["cols"];
+    }
+  }
+  if (data.contains("method") && data["method"].is_string()) {
+    method = getMethodOfString(data["method"]);
+  }
+}
+} // namespace na
