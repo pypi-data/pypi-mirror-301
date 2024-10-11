ec0b291d9adb0297b4aa419fae90f395c758b4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:04:59 +0000

    Add package bacore/0.2.2/bacore-0.2.2-cp39-abi3-win32.whl

---
+++ packages/bacore/bacore-0.2.2-cp39-abi3-win32.whl/bacore/config.py
@@ -0,0 +1,17 @@
+"""Configuration module for BACore. Should probably not be used at all.
+
+# Configuration tools
+- Something
+- Something else
+"""
+
+import os
+from bacore.domain import settings
+
+docker_build = os.getenv("DOCKER_BUILD", None)
+
+project_bacore = settings.Project(
+    name="BACore",
+    version="0.1.4",
+    description="BACore is a framework for business analysis and test automation.",
+)
---
+++ packages/bacore/bacore-0.2.2-cp39-abi3-win32.whl/bacore/interfaces/web_fasthtml.py
@@ -0,0 +1,21 @@
+"""FastHTML web interface."""
+from bacore.interactors.file_handler import read_markdown_file
+from bacore.interactors.source_code_reader import get_module_members, get_module_from_name
+from fasthtml.common import A, Div, Li, P, Ul, Titled
+from pathlib import Path
+
+
+def div_from_markdown(file: Path, skip_title: bool = True) -> Div:
+    return Div(read_markdown_file(file=file, skip_title=skip_title), cls='marked')
+
+
+def module_doc(module_name: str, doc_title: str):
+    module = get_module_from_name(module_name=module_name)
+    module_members = get_module_members(module_name=module_name)
+
+    return Titled(doc_title,
+                  P(A('Back', href="/docs")),
+                  Div(module.__doc__, cls='marked'),
+                  Div(P('Here comes more information:'),
+                      Ul(*[Li(member.__name__) for member in module_members]))
+                  )
