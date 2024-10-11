2f929a8e74c145e850ce713434815fdd3f9b2e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:30 +0000

    Add package colour-demosaicing/0.2.6/colour_demosaicing-0.2.6.tar.gz

---
+++ packages/colour-demosaicing/colour_demosaicing-0.2.6.tar.gz/colour_demosaicing-0.2.6/colour_demosaicing/bayer/__init__.py
@@ -0,0 +1,13 @@
+from .masks import masks_CFA_Bayer
+from .mosaicing import mosaicing_CFA_Bayer
+from .demosaicing import *  # noqa: F403
+from . import demosaicing
+
+__all__ = []
+__all__ += [
+    "masks_CFA_Bayer",
+]
+__all__ += [
+    "mosaicing_CFA_Bayer",
+]
+__all__ += demosaicing.__all__
