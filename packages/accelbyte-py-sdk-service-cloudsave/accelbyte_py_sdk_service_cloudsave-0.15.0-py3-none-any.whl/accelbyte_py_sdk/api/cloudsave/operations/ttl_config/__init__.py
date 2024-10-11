16e0d74c7c8ff45d567498e2f43d1fa829cbcee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:11 +0000

    Add package accelbyte-py-sdk-service-cloudsave/0.15.0/accelbyte_py_sdk_service_cloudsave-0.15.0-py3-none-any.whl

---
+++ packages/accelbyte-py-sdk-service-cloudsave/accelbyte_py_sdk_service_cloudsave-0.15.0-py3-none-any.whl/accelbyte_py_sdk/api/cloudsave/operations/ttl_config/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Cloudsave Service."""
+
+__version__ = "3.21.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .delete_game_binary_reco_40da6b import DeleteGameBinaryRecordTTLConfig
+from .delete_game_record_ttl_config import DeleteGameRecordTTLConfig
