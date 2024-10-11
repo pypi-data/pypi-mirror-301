cf2ccb479f3a7db36b1750c3c85e5a29f73d973f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 04:05:10 +0000

    Add package slack-off-mj/0.1.4/slack_off_mj-0.1.4-py3-none-any.whl

---
+++ packages/slack-off-mj/slack_off_mj-0.1.4-py3-none-any.whl/slackoff/toj/utils.py
@@ -0,0 +1,10 @@
+
+class Utils:
+    @staticmethod
+    def handle(call, func, data):
+        if callable(call):
+            return call(data)
+        elif isinstance(call, object):
+            return getattr(call, func)(data)
+        elif isinstance(call, str):
+            return call
