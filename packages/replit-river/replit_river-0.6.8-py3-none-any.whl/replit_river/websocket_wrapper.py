8ce7cb646f20d3d30959a07da911f04070444d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:17 +0000

    Add package replit-river/0.6.8/replit_river-0.6.8-py3-none-any.whl

---
+++ packages/replit-river/replit_river-0.6.8-py3-none-any.whl/replit_river/websocket_wrapper.py
@@ -0,0 +1,37 @@
+import asyncio
+import enum
+import logging
+
+from websockets import WebSocketCommonProtocol
+
+logger = logging.getLogger(__name__)
+
+
+class WsState(enum.Enum):
+    OPEN = 0
+    CLOSING = 1
+    CLOSED = 2
+
+
+class WebsocketWrapper:
+    def __init__(self, ws: WebSocketCommonProtocol) -> None:
+        self.ws = ws
+        self.ws_state = WsState.OPEN
+        self.ws_lock = asyncio.Lock()
+        self.id = ws.id
+
+    async def is_open(self) -> bool:
+        async with self.ws_lock:
+            return self.ws_state == WsState.OPEN
+
+    async def close(self) -> asyncio.Task | None:
+        async with self.ws_lock:
+            if self.ws_state == WsState.OPEN:
+                self.ws_state = WsState.CLOSING
+                task = asyncio.create_task(self.ws.close())
+                task.add_done_callback(
+                    lambda _: logger.debug("old websocket %s closed.", self.ws.id)
+                )
+                self.ws_state = WsState.CLOSED
+                return task
+            return None
