4fcd11fe2cf12a58775463863092a8b949b8ea88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:23 +0000

    Add package eodag-labextension/3.7.0/eodag_labextension-3.7.0.tar.gz

---
+++ packages/eodag-labextension/eodag_labextension-3.7.0.tar.gz/eodag_labextension-3.7.0/src/types.ts
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2022 CS GROUP - France, http://www.c-s.fr
+ * All rights reserved
+ */
+
+export interface IFormInput {
+  geometry: IGeometry;
+  startDate: Date;
+  endDate: Date;
+  productType: string;
+  provider: string;
+  cloud: number;
+  additionnalParameters?: { name: string; value: string }[];
+}
+
+export interface ISearchParameters {
+  dtstart?: string; // format: YYYY-MM-DD
+  dtend?: string; // format: YYYY-MM-DD
+  cloudCover?: number;
+  page?: number;
+  geom?: IGeometry;
+  [key: string]: any;
+}
+
+export interface IGeometry {
+  type: string;
+  coordinates: number[];
+}
+// export type IFeaturePropertie = [string, any];
+export interface IFeaturePropertie {
+  key: string;
+  value: any;
+}
+
+// export type IFeaturePropertie = [string, any];
+export interface IFeaturePropertie {
+  key: string;
+  value: any;
+}
+
+export type MapSettings = { lat: number; lon: number; zoom: number };
