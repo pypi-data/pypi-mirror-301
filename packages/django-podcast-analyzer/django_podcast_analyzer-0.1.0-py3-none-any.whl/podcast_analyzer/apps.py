f1a710c3e69b0555018db848abcf3862e55bef55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:24 +0000

    Add package django-podcast-analyzer/0.1.0/django_podcast_analyzer-0.1.0-py3-none-any.whl

---
+++ packages/django-podcast-analyzer/django_podcast_analyzer-0.1.0-py3-none-any.whl/podcast_analyzer/apps.py
@@ -0,0 +1,15 @@
+# apps.py
+#
+# Copyright (c) 2024 Daniel Andrlik
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class PodcastAnalyzerConfig(AppConfig):
+    name = "podcast_analyzer"
+    verbose_name = _("Podcast Analyzer")
+    default_auto_field = "django.db.models.AutoField"
