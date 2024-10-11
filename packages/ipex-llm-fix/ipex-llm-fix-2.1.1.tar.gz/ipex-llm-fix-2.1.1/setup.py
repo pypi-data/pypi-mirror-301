bd24f38854de7570100ff3cbc7c98e38fc6897a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:03:47 +0000

    Add package ipex-llm-fix/2.1.1/ipex-llm-fix-2.1.1.tar.gz

---
+++ packages/ipex-llm-fix/ipex-llm-fix-2.1.1.tar.gz/ipex-llm-fix-2.1.1/setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """    
+    return "2.1.1"
+
+
+def get_long_description(long_description_file):
+    with open(long_description_file) as f:
+        long_description = f.read()
+
+    return long_description
+
+
+version = get_version('ipex-llm-fix')
+
+
+setup(
+    name='ipex-llm-fix',
+    version=version,
+    url='https://github.com/intel-analytics/ipex-llm',
+    license='Apache-2.0 license',
+    author='Intel',
+    author_email='dep36@qq.com',
+    description='ipex-llm fix version',
+    long_description="ipex-llm fix",
+    long_description_content_type='text/markdown',
+    packages=['ipex_llm'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[       
+    ],
+    classifiers=[     
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+   
+)
