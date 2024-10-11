3f16c2f59fa3ce64830aae25193ba9d50db02aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:03:55 +0000

    Add package palworld-pal-editor/0.10.1/palworld_pal_editor-0.10.1-py3-none-any.whl

---
+++ packages/palworld-pal-editor/palworld_pal_editor-0.10.1-py3-none-any.whl/palworld_pal_editor/gui.py
@@ -0,0 +1,44 @@
+import sys
+import threading
+import time
+import requests
+import traceback
+import webbrowser
+import webview
+from palworld_pal_editor.config import VERSION, Config
+from palworld_pal_editor.utils import LOGGER
+from palworld_pal_editor.webui import main as web_main
+
+
+def main():
+    t = threading.Thread(target=web_main)
+    t.daemon = True
+    t.start()
+    
+    while True:
+        LOGGER.info("Waiting for backend response...")
+        try:
+            response = requests.get(f"http://127.0.0.1:{Config.port}/api/ready")
+            if response.status_code == 200:
+                LOGGER.info("Backend ready, launching GUI...")
+                break
+            time.sleep(0.5)
+        except requests.exceptions.ConnectionError:
+            time.sleep(0.5)
+
+    LOGGER.info("If GUI doesn't work for you, check out this post: https://github.com/KrisCris/Palworld-Pal-Editor/issues/4")
+
+    try:
+        webview.create_window(f"Palworld Pal Editor, developed by _connlost with ❤️. VERSION: {VERSION}", url=f"http://127.0.0.1:{Config.port}/", width=1600, height=1000, min_size=(960, 600))
+        webview.start()
+    except KeyboardInterrupt:
+        pass
+    except:
+        LOGGER.warning(f"Failed Launching pywebview: {traceback.format_exc()}")
+        LOGGER.info(f"Fallback to web browser, opening http://127.0.0.1:{Config.port} ...")
+        threading.Timer(1, lambda: webbrowser.open(f"http://127.0.0.1:{Config.port}") ).start()
+        t.join()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
