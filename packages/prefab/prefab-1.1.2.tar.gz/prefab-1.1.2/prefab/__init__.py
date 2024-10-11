754d6420688259a42b28a74cdc0b7f6333aa6599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:22 +0000

    Add package prefab/1.1.2/prefab-1.1.2.tar.gz

---
+++ packages/prefab/prefab-1.1.2.tar.gz/prefab-1.1.2/prefab/__init__.py
@@ -0,0 +1,23 @@
+"""
+The prefab module predicts and corrects nanofabrication variations in photonic devices.
+
+Usage:
+    import prefab as pf
+"""
+
+__version__ = "1.1.2"
+
+from . import compare, geometry, read, shapes
+from .device import BufferSpec, Device
+from .models import models
+
+__all__ = [
+    "Device",
+    "BufferSpec",
+    "geometry",
+    "read",
+    "shapes",
+    "compare",
+    "models",
+    "__version__",
+]
