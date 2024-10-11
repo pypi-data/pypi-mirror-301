8d76550c0e2fceb7c599547b3f6b037bd5d34fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:31 +0000

    Add package jtoolbox/0.0.3/jtoolbox-0.0.3.tar.gz

---
+++ packages/jtoolbox/jtoolbox-0.0.3.tar.gz/jtoolbox-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jtoolbox',
+    version='0.0.3',
+    description='',
+    author='jongseo_park',
+    author_email='jongsaeu@gmail.com',
+    url='',
+    install_requires=['tqdm', 'numpy', 'pandas', 'scikit-learn', 'rdkit', 'torch', 'torch_geometric'],
+    packages=find_packages(exclude=[]),
+    keywords=[],
+    python_requires='>=3.8',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
