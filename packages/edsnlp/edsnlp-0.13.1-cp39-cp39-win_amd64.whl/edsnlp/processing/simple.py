f710d35c0e544db3f34e6149ac270f2f0a758cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 07:04:49 +0000

    Add package edsnlp/0.13.1/edsnlp-0.13.1-cp39-cp39-win_amd64.whl

---
+++ packages/edsnlp/edsnlp-0.13.1-cp39-cp39-win_amd64.whl/edsnlp/processing/simple.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+import sys
+from contextlib import nullcontext
+from typing import TYPE_CHECKING
+
+from edsnlp.utils.batching import batchify_fns
+from edsnlp.utils.collections import batchify, flatten
+
+from .utils import apply_basic_pipes
+
+if TYPE_CHECKING:
+    from edsnlp.core.lazy_collection import LazyCollection
+
+doc_size_fns = {
+    "words": len,
+}
+
+
+def execute_simple_backend(
+    lc: LazyCollection,
+):
+    """
+    This is the default execution mode which batches the documents and processes each
+    batch on the current process in a sequential manner.
+    """
+    try:
+        torch = sys.modules["torch"]
+        no_grad_ctx = torch.no_grad()
+        autocast_device_type = next(
+            (
+                p.device
+                for name, pipe in lc.torch_components()
+                for p in pipe.parameters()
+            ),
+            torch.device("cpu"),
+        ).type.split(":")[0]
+        autocast_dtype = lc.autocast if lc.autocast is not True else None
+        autocast_ctx = (
+            torch.autocast(
+                device_type=autocast_device_type,
+                dtype=autocast_dtype,
+            )
+            if lc.autocast
+            else nullcontext()
+        )
+        inference_mode_ctx = (
+            torch.inference_mode()
+            if hasattr(torch, "inference_mode")
+            else nullcontext()
+        )
+    except (KeyError, AttributeError):
+        no_grad_ctx = autocast_ctx = inference_mode_ctx = nullcontext()
+    reader = lc.reader
+    writer = lc.writer
+    show_progress = lc.show_progress
+
+    split_into_batches_after = lc.split_into_batches_after
+    if split_into_batches_after is None and (lc.batch_by != "docs" or lc.sort_chunks):
+        split_into_batches_after = next(
+            (s[0] for s in lc.pipeline if s[0] == "_ensure_doc"), None
+        )
+    names = [None] + [step[0] for step in lc.pipeline]
+    chunk_components = lc.pipeline[: names.index(split_into_batches_after)]
+    batch_components = lc.pipeline[names.index(split_into_batches_after) :]
+
+    def process():
+        bar = nullcontext()
+        if show_progress:
+            from tqdm import tqdm
+
+            bar = tqdm(smoothing=0.1, mininterval=5.0)
+
+        with bar, lc.eval(), autocast_ctx, inference_mode_ctx:
+            for docs in batchify(
+                (
+                    subtask
+                    for task, count in reader.read_main()
+                    for subtask in reader.read_worker([task])
+                ),
+                batch_size=lc.chunk_size,
+            ):
+                docs = apply_basic_pipes(docs, chunk_components)
+
+                if lc.sort_chunks:
+                    docs.sort(key=doc_size_fns.get(lc.sort_chunks, len))
+
+                for batch in batchify_fns[lc.batch_by](docs, lc.batch_size):
+                    count = len(batch)
+                    with no_grad_ctx, lc.cache():
+                        batch = apply_basic_pipes(batch, batch_components)
+
+                    if writer is not None:
+                        result, count = writer.write_worker(batch)
+                        if show_progress:
+                            bar.update(count)
+                        yield result
+                    else:
+                        if show_progress:
+                            bar.update(count)
+                        yield batch
+            if writer is not None:
+                result, count = writer.finalize()
+                if show_progress:
+                    bar.update(count)
+                if count:
+                    yield result
+
+    gen = process()
+    return flatten(gen) if writer is None else writer.write_main(gen)
