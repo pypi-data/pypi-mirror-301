075a66f30c7c8ed1114d5bb1209f327802aa00f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:20 +0000

    Add package snarkify-cli/0.1.5/snarkify_cli-0.1.5.tar.gz

---
+++ packages/snarkify-cli/snarkify_cli-0.1.5.tar.gz/snarkify_cli-0.1.5/snarkify_cli/tasks/constants.py
@@ -0,0 +1,10 @@
+from snarkify_cli.tasks.enums import TaskField
+
+DETAILED_TASK_FIELDS = [
+    TaskField.SERVICE_ID,
+    TaskField.CREATED,
+    TaskField.INPUT,
+    TaskField.RESULT,
+    TaskField.STARTED,
+    TaskField.FINISHED,
+]
