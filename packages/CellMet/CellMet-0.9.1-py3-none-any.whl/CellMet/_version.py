0f88f7cafa17b4dd8b7eb884407b801ed6bc8e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:05:43 +0000

    Add package CellMet/0.9.1/CellMet-0.9.1-py3-none-any.whl

---
+++ packages/CellMet/CellMet-0.9.1-py3-none-any.whl/CellMet/_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CellMet SETUP.PY
+#
+short_version = '0.9.1'
+version = '0.9.1'
+full_version = '0.9.1.dev0+6978a7e'
+git_revision = '6978a7e88eaf1becd779cb35d62d772447bdbb6e'
+release = False
+if not release:
+    version = full_version
