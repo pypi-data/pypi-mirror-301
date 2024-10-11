c5cbbb18eb22ce55e09e0642c19fbeb3df9483c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 07:04:26 +0000

    Add package bittensor-wallet/2.0.2/bittensor_wallet-2.0.2-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/bittensor-wallet/bittensor_wallet-2.0.2-cp312-cp312-macosx_11_0_arm64.whl/bittensor_wallet/__init__.py
@@ -0,0 +1,16 @@
+from bittensor_wallet.bittensor_wallet import (
+    # classes
+    Config,
+    Keyfile,
+    Keypair,
+    Wallet,
+    # modules
+    config,
+    errors,
+    keyfile,
+    keypair,
+    utils,
+    wallet,
+)
+
+__version__ = "2.0.0"
