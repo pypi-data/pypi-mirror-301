e4a32d419b6019264d9aa1502eabfba95cb8bbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:02 +0000

    Add package rlmodule/0.1.5.1/rlmodule-0.1.5.1-py3-none-any.whl

---
+++ packages/rlmodule/rlmodule-0.1.5.1-py3-none-any.whl/rlmodule/source/network_cfg.py
@@ -0,0 +1,56 @@
+import gym
+import gymnasium
+import torch.nn as nn
+
+from dataclasses import MISSING
+from collections.abc import Callable
+from typing import List, Sequence, Union
+
+from rlmodule.source.network import GRU, LSTM, MLP, RNN, RnnBase, RnnMlp
+
+# use isaac-lab native configclass if available to avoid it being declared twice
+try:
+    from omni.isaac.lab.utils import configclass
+except ImportError:
+    print("Importing local configclass.")
+    from rlmodule.source.nvidia_utils import configclass
+
+@configclass
+class NetworkCfg:
+    module: Union[nn.Module, Callable[..., nn.Module]] = MISSING
+    input_size: Union[int, Sequence[int], gym.Space, gymnasium.Space] = None # None means value should be inferred
+
+
+@configclass
+class MlpCfg(NetworkCfg):
+    module: type[MLP] = MLP
+
+    hidden_units: Sequence[int] = MISSING
+    activation: type[nn.Module] = MISSING
+    
+
+@configclass
+class RnnBaseCfg(NetworkCfg):
+    num_envs: int = MISSING
+    num_layers: int = MISSING
+    hidden_size: int = MISSING
+    sequence_length: int = MISSING
+
+@configclass
+class RnnCfg(RnnBaseCfg):
+    module: type[RNN] = RNN
+
+@configclass
+class GruCfg(RnnBaseCfg):
+    module: type[GRU] = GRU
+
+@configclass
+class LstmCfg(RnnBaseCfg):
+    module: type[LSTM] = LSTM
+
+@configclass
+class RnnMlpCfg(NetworkCfg):
+    module: type[RnnBase] = RnnMlp
+
+    rnn: RnnBaseCfg = MISSING
+    mlp: MlpCfg = MISSING
