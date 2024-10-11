f0f0fffdf69c8fade93332734b33fc959e32caed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:24 +0000

    Add package accelbyte-py-sdk/0.70.0/accelbyte_py_sdk-0.70.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.70.0.tar.gz/accelbyte_py_sdk-0.70.0/accelbyte_py_sdk/api/session/operations/platform_credential/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Session Service."""
+
+__version__ = "3.19.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .admin_delete_platform_c_b8f2c5 import AdminDeletePlatformCredentials
+from .admin_get_platform_credentials import AdminGetPlatformCredentials
+from .admin_update_platform_c_5be87f import AdminUpdatePlatformCredentials
