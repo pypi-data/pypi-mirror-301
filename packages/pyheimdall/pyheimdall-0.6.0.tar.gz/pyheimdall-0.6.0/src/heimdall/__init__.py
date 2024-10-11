7ce238bacfff308ecd6327e681caab122ae9d8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 04:06:03 +0000

    Add package pyheimdall/0.6.0/pyheimdall-0.6.0.tar.gz

---
+++ packages/pyheimdall/pyheimdall-0.6.0.tar.gz/pyheimdall-0.6.0/src/heimdall/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from heimdall.heimdall import *
+
+__all__ = [
+    'getDatabase',
+    'createDatabase',
+
+    'getProperty', 'getProperties',
+    'createProperty', 'deleteProperty',
+    'replaceProperty', 'updateProperty',
+
+    'getEntity', 'getEntities',
+    'createEntity', 'deleteEntity',
+    'replaceEntity', 'updateEntity',
+    'getAttribute', 'getAttributes',
+    'createAttribute', 'deleteAttribute',
+    'replaceAttribute', 'updateAttribute',
+
+    'getItem', 'getItems',
+    'createItem', 'deleteItem',
+    'replaceItem', 'updateItem',
+
+    'getMetadata', 'getValue', 'getValues',
+    'createMetadata', 'deleteMetadata',
+
+    'Config',  # TODO remove
+    '__copyright__', '__license__',
+    ]
