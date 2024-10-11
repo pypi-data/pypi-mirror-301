9e45a1d9c9a3d08db7d5fc00be95b51d93e86aec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:17 +0000

    Add package vssource/0.11.1/vssource-0.11.1.tar.gz

---
+++ packages/vssource/vssource-0.11.1.tar.gz/vssource-0.11.1/vssource/indexers/misc.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from vstools import core
+
+from .base import Indexer
+
+__all__ = [
+    'BestSource',
+
+    'IMWRI',
+
+    'LSMAS',
+
+    'CarefulSource'
+]
+
+
+class BestSource(Indexer):
+    _source_func = core.lazy.bs.VideoSource  # type: ignore
+
+
+class IMWRI(Indexer):
+    _source_func = core.lazy.imwri.Read
+
+
+class LSMAS(Indexer):
+    _source_func = core.lazy.lsmas.LWLibavSource
+
+
+class CarefulSource(Indexer):
+    _source_func = core.lazy.cs.ImageSource
