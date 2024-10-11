bba2de22336c1fe015ffa3a8a68b163b0aa26016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:20 +0000

    Add package mqt.qudits/0.3.0/mqt_qudits-0.3.0-cp313-cp313t-manylinux_2_24_ppc64le.manylinux_2_28_ppc64le.whl

---
+++ packages/mqt.qudits/mqt_qudits-0.3.0-cp313-cp313t-manylinux_2_24_ppc64le.manylinux_2_28_ppc64le.whl/mqt/qudits/quantum_circuit/gates/z.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from ..components.extensions.gate_types import GateTypes
+from ..gate import Gate
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+    from ..circuit import QuantumCircuit
+    from ..components.extensions.controls import ControlData
+
+
+class Z(Gate):
+    def __init__(
+        self,
+        circuit: QuantumCircuit,
+        name: str,
+        target_qudits: int,
+        dimensions: int,
+        controls: ControlData | None = None,
+    ) -> None:
+        super().__init__(
+            circuit=circuit,
+            name=name,
+            gate_type=GateTypes.SINGLE,
+            target_qudits=target_qudits,
+            dimensions=dimensions,
+            control_set=controls,
+            qasm_tag="z",
+        )
+
+    def __array__(self) -> NDArray:  # noqa: PLW3201
+        matrix = np.zeros((self.dimensions, self.dimensions), dtype="complex")
+        for i in range(self.dimensions):
+            omega = np.mod(2 * i / self.dimensions, 2)
+            omega = omega * np.pi * 1j
+            omega = np.e**omega
+            array = np.zeros(self.dimensions, dtype="complex")
+            array[i] = 1
+            result = omega * np.outer(array, array)
+            matrix += result
+
+        return matrix
+
+    @property
+    def dimensions(self) -> int:
+        assert isinstance(self._dimensions, int), "Dimensions must be an integer in Z gate"
+        return self._dimensions
