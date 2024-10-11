9908e89747f2799f96598b19e985b96970d2fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:14 +0000

    Add package grupob-zoho/0.1.0/grupob_zoho-0.1.0.tar.gz

---
+++ packages/grupob-zoho/grupob_zoho-0.1.0.tar.gz/grupob_zoho-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grupob_zoho",
+    version="0.1.0",
+    author="Gelson Júnior",
+    author_email="gelson.junior@grupobachega.com.br",
+    description="Uma biblioteca para manipular dados no zoho",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/juniorppp/zoho",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
