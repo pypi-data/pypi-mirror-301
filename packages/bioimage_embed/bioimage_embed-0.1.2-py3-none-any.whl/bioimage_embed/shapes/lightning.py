a7814875010f8320e0027d0f509283d1833847ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:07:14 +0000

    Add package bioimage_embed/0.1.2/bioimage_embed-0.1.2-py3-none-any.whl

---
+++ packages/bioimage_embed/bioimage_embed-0.1.2-py3-none-any.whl/bioimage_embed/shapes/lightning.py
@@ -0,0 +1,142 @@
+import torch
+import torchvision
+
+from torch import nn
+from ..lightning import AutoEncoderUnsupervised, AutoEncoderSupervised
+from . import loss_functions as lf
+from types import SimpleNamespace
+
+
+def frobenius_norm_2D_torch(tensor: torch.Tensor) -> torch.Tensor:
+    return torch.norm(tensor, p="fro", dim=(-2, -1), keepdim=True)
+
+
+class MaskEmbedMixin:
+    def batch_to_tensor(self, batch):
+        """
+        Converts a batch of data to a tensor
+        Batch is expected to be normalised to the window size which will be the same or smaller than the image size in question.
+        The batch is also optionally frobenius normalised to make the loss function invariant to the size of the shape.
+        """
+        # x = batch[0].float()
+        output = super().batch_to_tensor(batch)
+        normalised_data = output.data
+        scalings = torch.ones_like(output.data)
+        if hasattr(self.args, "frobenius_norm"):
+            if self.args.frobenius_norm:
+                scalings = frobenius_norm_2D_torch(output.data)
+
+        output.data = normalised_data / scalings
+        output.scalings = scalings
+
+        return output
+
+    def eval_step(self, batch, batch_idx):
+        # Needs to be super because eval_step is overwritten in Supervised
+        model_output = super().eval_step(batch, batch_idx)
+        loss_ops = lf.DistanceMatrixLoss(model_output.recon_x, norm=False)
+
+        shape_loss = torch.sum(
+            torch.stack(
+                [
+                    loss_ops.diagonal_loss(),
+                    loss_ops.symmetry_loss(),
+                    # loss_ops.triangle_inequality(),
+                    loss_ops.non_negative_loss(),
+                    # loss_ops.clockwise_order_loss(),
+                ]
+            )
+        )
+        model_output.loss += shape_loss
+        model_output.shape_loss = shape_loss
+
+        # loss += lf.diagonal_loss(model_output.recon_x)
+        # loss += lf.symmetry_loss(model_output.recon_x)
+        # loss += lf.triangle_inequality_loss(model_output.recon_x)
+        # loss += lf.non_negative_loss(model_output.recon_x)
+
+        return model_output
+
+
+class MaskEmbed(MaskEmbedMixin, AutoEncoderUnsupervised):
+    def __init__(self, model, args=SimpleNamespace()):
+        super().__init__(model, args)
+
+
+class MaskEmbedSupervised(MaskEmbedMixin, AutoEncoderSupervised):
+    def __init__(self, model, args=SimpleNamespace()):
+        super().__init__(model, args)
+
+
+class FixedOutput(nn.Module):
+    def __init__(self, tensor):
+        super().__init__()
+        self.tensor = tensor
+
+    def forward(self, x):
+        return self.tensor
+
+
+class MaskEmbedLatentAugment(MaskEmbed):
+    def __init__(self, model, args_dict):
+        super().__init__(model, **args_dict)
+
+    def get_model_output(self, x, batch_idx, optimizer_idx=0):
+        # Janky!
+        model_output = self.model(x, epoch=batch_idx)
+        # if optimizer_idx == 0:
+        #     loss = self.loss_function(model_output)
+        # if optimizer_idx == 1:
+        #     model_encoder = self.model.encoder
+        #     self.model.encoder = FixedOutput(self.guassian_sample(model_output.z))
+        #     decoder_output = self.model(x, epoch=batch_idx)
+        #     self.model.encoder = model_encoder
+        #     loss = self.loss_function(decoder_output)
+
+        loss = self.loss_function(model_output)
+        return model_output, loss
+
+    def guassian_sample(self, z):
+        return torch.normal(0, 1, size=z.shape).to(self.device)
+        # return torch.randn(*model_output.z.shape).to(self.device)
+
+    def training_step(self, batch, batch_idx, optimizer_idx=0):
+        # results = self.get_results(batch)
+        self.model.train()
+        x = self.batch_to_tensor(batch)
+        model_output, loss = self.get_model_output(
+            x,
+            batch_idx,
+            optimizer_idx=optimizer_idx,
+        )
+        # loss = self.model.training_step(x)
+        # loss = self.loss_function(model_output,optimizer_idx)
+
+        # self.log("train_loss", self.loss)
+        # self.log("train_loss", loss)
+        self.logger.experiment.add_scalar("Loss/train", loss, batch_idx)
+
+        self.logger.experiment.add_image(
+            "input", torchvision.utils.make_grid(x["data"]), batch_idx
+        )
+
+        # if self.PYTHAE_FLAG:
+        self.logger.experiment.add_image(
+            "output",
+            torchvision.utils.make_grid(model_output.recon_x),
+            batch_idx,
+        )
+
+        return loss
+
+    # def configure_optimizers(self):
+    #     opt_ed, lr_s_ed = self.timm_optimizers(self.model)
+    #     opt_d, lr_s_d = self.timm_optimizers(self.model)
+    #     return (
+    #         self.timm_to_lightning(optimizer=opt_ed, lr_scheduler=lr_s_ed),
+    #         self.timm_to_lightning(optimizer=opt_d, lr_scheduler=lr_s_d),
+    #     )
+
+    def configure_optimizers(self):
+        opt_ed, lr_s_ed = self.timm_optimizers(self.model)
+        return self.timm_to_lightning(optimizer=opt_ed, lr_scheduler=lr_s_ed)
---
+++ packages/bioimage_embed/bioimage_embed-0.1.2-py3-none-any.whl/bioimage_embed/shapes/tests/test_shapes.py
@@ -0,0 +1,108 @@
+import pytest
+import torch
+import matplotlib.pyplot as plt
+
+# Note - you must have torchvision installed for this example
+
+# from bioimage_embed.datasets import BroadDataset, DatasetGlob
+from bioimage_embed.shapes.transforms import (
+    MaskToDistogramPipeline,
+    DistogramToMaskPipeline,
+)
+from bioimage_embed.shapes.transforms import (
+    DistogramToCoords,
+)
+
+import numpy as np
+
+# from bioimage_embed.models import VQ_VAE, BioimageEmbed, VAE
+# from bioimage_embed.lightning import AutoEncoderUnsupervised
+# from bioimage_embed.models.legacy import vae
+
+interp_size = 128 * 4
+window_size = 64
+
+latent_dim = 64
+
+channels = 1
+input_dim = (channels, window_size, window_size)
+
+models = []
+
+
+def create_circle_contour(radius, image_size):
+    image = np.zeros(image_size, dtype=np.uint8)
+    center = (image_size[0] // 2, image_size[1] // 2)
+
+    y, x = np.ogrid[: image_size[0], : image_size[1]]
+    distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+
+    image[np.abs(distance - radius) < 1] = 255  # Set contour to white
+
+    return image, distance
+
+
+(test_imgs,) = [create_circle_contour(32, (64, 64))]
+
+
+@pytest.mark.parametrize("model", models)
+# @pytest.mark.parametrize("test_img", test_imgs)
+class TestMask:
+    test_img, test_dist = create_circle_contour(32, (64, 64))
+
+    test_img_torch = torch.tensor(test_img).unsqueeze(0).unsqueeze(0)
+    test_dist_torch = torch.tensor(test_dist).unsqueeze(0).unsqueeze(0)
+
+    def test_pipeline_forward(self, model):
+        # dist = MaskToDistogramPipeline(window_size)(train_dataset_raw[0])
+        # plt.imshow(dist)
+        # plt.savefig("tests/test_mask_to_dist.png")
+        # plt.close()
+        # plt.close()
+        dist = MaskToDistogramPipeline(window_size)(self.test_img_torch)
+        plt.imshow(dist.squeeze())
+        plt.savefig("tests/test_pipeline_forward.png")
+        plt.close()
+        mask = DistogramToMaskPipeline(window_size)(dist)
+        plt.imshow(mask.squeeze())
+        plt.savefig("tests/test_dist_to_mask.png")
+        plt.close()
+
+    def test_mask_to_dist(self, model):
+        dist = MaskToDistogramPipeline(window_size)(self.test_img_torch)
+        assert dist == self.test_dist_torch
+        pass
+
+    def test_dist_to_coord(self, model, test_img):
+        # dist = transformer_dist(train_dataset[-1][0])
+        # TODO Faulty?
+        # test_img = get_test_image(dataset)
+        coords = DistogramToCoords(window_size)(test_img)
+        plt.scatter(coords[-1][0][:, 0], coords[0][0][:, 1])
+        plt.savefig("tests/test_dist_to_coord.png")
+        plt.close()
+
+
+# @pytest.mark.parametrize("model", models)
+# class TestModels:
+
+
+#     def test_dist_to_coord(self, model, test_img):
+#         # test_img = get_test_image(dataset)
+#         # dist = transformer_dist(train_dataset[0][0])
+#         coords = DistogramToCoords(window_size)(test_img)
+#         plt.scatter(coords[0][:, 0], coords[0][:, 1])
+#         plt.savefig("tests/test_dist_to_coord.png")
+#         plt.close()
+
+#     def test_models(self, model, test_img):
+#         # vae = AutoEncoder(1, 1)
+#         # vae = VQ_VAE(channels=1)
+
+#         # test_img = get_test_image(dataset)
+#         # loss, x_recon, perplexity = model(img)
+#         result = model(test_img)
+#         z, log_var = model.encode(test_img)
+#         y_prime = model.decode(z)
+#         # print(f"img_dims:{img.shape} y:_dims:{x_recon.shape}")
+#         print(f"img_dims:{test_img.shape}, z:_dims:{z.shape}")
