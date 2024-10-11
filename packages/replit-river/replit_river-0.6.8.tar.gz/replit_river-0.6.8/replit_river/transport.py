1cc076f4a642416b43a236e8fc9bca7f2ac7a651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:17 +0000

    Add package replit-river/0.6.8/replit_river-0.6.8.tar.gz

---
+++ packages/replit-river/replit_river-0.6.8.tar.gz/replit_river-0.6.8/replit_river/transport.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+from typing import Dict, Tuple
+
+import nanoid  # type: ignore
+
+from replit_river.rpc import (
+    GenericRpcHandler,
+)
+from replit_river.session import Session
+from replit_river.transport_options import TransportOptions
+
+logger = logging.getLogger(__name__)
+
+
+class Transport:
+    def __init__(
+        self,
+        transport_id: str,
+        transport_options: TransportOptions,
+        is_server: bool,
+    ) -> None:
+        self._transport_id = transport_id
+        self._transport_options = transport_options
+        self._is_server = is_server
+        self._sessions: Dict[str, Session] = {}
+        self._handlers: Dict[Tuple[str, str], Tuple[str, GenericRpcHandler]] = {}
+        self._session_lock = asyncio.Lock()
+
+    async def _close_all_sessions(self) -> asyncio.Task:
+        cleanup_tasks: list[asyncio.Task] = []
+        sessions = self._sessions.values()
+        logger.info(
+            f"start closing sessions {self._transport_id}, number sessions : "
+            f"{len(sessions)}"
+        )
+        sessions_to_close = list(sessions)
+
+        # closing sessions requires access to the session lock, so we need to close
+        # them one by one to be safe
+        for session in sessions_to_close:
+            cleanup_task = await session.close()
+            if cleanup_task:
+                cleanup_tasks.append(cleanup_task)
+
+        logger.info(f"Transport closed {self._transport_id}")
+
+        async def cleanup() -> None:
+            for cleanup_task in cleanup_tasks:
+                await cleanup_task
+
+        return asyncio.create_task(cleanup())
+
+    async def _delete_session(self, session: Session) -> None:
+        async with self._session_lock:
+            if session._to_id in self._sessions:
+                del self._sessions[session._to_id]
+
+    def _set_session(self, session: Session) -> None:
+        self._sessions[session._to_id] = session
+
+    def generate_nanoid(self) -> str:
+        return str(nanoid.generate())
