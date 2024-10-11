e74b483a58696a003e760e0d3b767cd5347d417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:21 +0000

    Add package accelbyte-py-sdk-service-session/0.16.0/accelbyte_py_sdk_service_session-0.16.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-session/accelbyte_py_sdk_service_session-0.16.0.tar.gz/accelbyte_py_sdk_service_session-0.16.0/accelbyte_py_sdk/api/session/wrappers/_platform_credential.py
@@ -0,0 +1,358 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: wrapper.j2
+
+# pylint: disable=duplicate-code
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-lines
+# pylint: disable=too-many-locals
+# pylint: disable=too-many-public-methods
+# pylint: disable=too-many-return-statements
+# pylint: disable=too-many-statements
+# pylint: disable=unused-import
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from accelbyte_py_sdk.core import HeaderStr
+from accelbyte_py_sdk.core import get_namespace as get_services_namespace
+from accelbyte_py_sdk.core import run_request
+from accelbyte_py_sdk.core import run_request_async
+from accelbyte_py_sdk.core import same_doc_as
+
+from ..models import ApimodelsPutPlatformCredentialsRequest
+from ..models import ModelsPlatformCredentials
+from ..models import ResponseError
+
+from ..operations.platform_credential import AdminDeletePlatformCredentials
+from ..operations.platform_credential import AdminGetPlatformCredentials
+from ..operations.platform_credential import AdminUpdatePlatformCredentials
+
+
+@same_doc_as(AdminDeletePlatformCredentials)
+def admin_delete_platform_credentials(
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Delete platform credentials. (adminDeletePlatformCredentials)
+
+    Delete platform credentials used for Native Session sync.
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: DELETE
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        204: No Content - (No Content)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminDeletePlatformCredentials.create(
+        namespace=namespace,
+    )
+    return run_request(request, additional_headers=x_additional_headers, **kwargs)
+
+
+@same_doc_as(AdminDeletePlatformCredentials)
+async def admin_delete_platform_credentials_async(
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Delete platform credentials. (adminDeletePlatformCredentials)
+
+    Delete platform credentials used for Native Session sync.
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: DELETE
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        204: No Content - (No Content)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminDeletePlatformCredentials.create(
+        namespace=namespace,
+    )
+    return await run_request_async(
+        request, additional_headers=x_additional_headers, **kwargs
+    )
+
+
+@same_doc_as(AdminGetPlatformCredentials)
+def admin_get_platform_credentials(
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Get platform credentials. These are used to sync PSN sessions. (adminGetPlatformCredentials)
+
+    Get platform credentials used for Native Session sync.
+    PSN:
+    - clientID: Auth Server (Client Credential) ClientID
+    - clientSecret: Auth Server (Client Credential) Secret. For security, only the first few characters are shown.
+    - scope: should be psn:s2s.service (For Sync non PSN member to PSN Session)
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: GET
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsPlatformCredentials (OK)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminGetPlatformCredentials.create(
+        namespace=namespace,
+    )
+    return run_request(request, additional_headers=x_additional_headers, **kwargs)
+
+
+@same_doc_as(AdminGetPlatformCredentials)
+async def admin_get_platform_credentials_async(
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Get platform credentials. These are used to sync PSN sessions. (adminGetPlatformCredentials)
+
+    Get platform credentials used for Native Session sync.
+    PSN:
+    - clientID: Auth Server (Client Credential) ClientID
+    - clientSecret: Auth Server (Client Credential) Secret. For security, only the first few characters are shown.
+    - scope: should be psn:s2s.service (For Sync non PSN member to PSN Session)
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: GET
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsPlatformCredentials (OK)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminGetPlatformCredentials.create(
+        namespace=namespace,
+    )
+    return await run_request_async(
+        request, additional_headers=x_additional_headers, **kwargs
+    )
+
+
+@same_doc_as(AdminUpdatePlatformCredentials)
+def admin_update_platform_credentials(
+    body: ApimodelsPutPlatformCredentialsRequest,
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Update platform credentials. Currently only used to sync PSN sessions. (adminUpdatePlatformCredentials)
+
+    Update platform credentials for Native Session sync. Currently supports PSN platform.
+    Send an empty body to clear data.
+    PSN:
+    - clientID: Auth Server (Client Credential) ClientID
+    - clientSecret: Auth Server (Client Credential) Secret
+    - scope: psn:s2s.service (For Sync non PSN member to PSN Session)
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: PUT
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ApimodelsPutPlatformCredentialsRequest in body
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsPlatformCredentials (OK)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminUpdatePlatformCredentials.create(
+        body=body,
+        namespace=namespace,
+    )
+    return run_request(request, additional_headers=x_additional_headers, **kwargs)
+
+
+@same_doc_as(AdminUpdatePlatformCredentials)
+async def admin_update_platform_credentials_async(
+    body: ApimodelsPutPlatformCredentialsRequest,
+    namespace: Optional[str] = None,
+    x_additional_headers: Optional[Dict[str, str]] = None,
+    **kwargs
+):
+    """Update platform credentials. Currently only used to sync PSN sessions. (adminUpdatePlatformCredentials)
+
+    Update platform credentials for Native Session sync. Currently supports PSN platform.
+    Send an empty body to clear data.
+    PSN:
+    - clientID: Auth Server (Client Credential) ClientID
+    - clientSecret: Auth Server (Client Credential) Secret
+    - scope: psn:s2s.service (For Sync non PSN member to PSN Session)
+
+    Properties:
+        url: /session/v1/admin/namespaces/{namespace}/platform-credentials
+
+        method: PUT
+
+        tags: ["Platform Credential"]
+
+        consumes: ["application/json"]
+
+        produces: ["application/json"]
+
+        securities: [BEARER_AUTH]
+
+        body: (body) REQUIRED ApimodelsPutPlatformCredentialsRequest in body
+
+        namespace: (namespace) REQUIRED str in path
+
+    Responses:
+        200: OK - ModelsPlatformCredentials (OK)
+
+        400: Bad Request - ResponseError (Bad Request)
+
+        401: Unauthorized - ResponseError (Unauthorized)
+
+        403: Forbidden - ResponseError (Forbidden)
+
+        404: Not Found - ResponseError (Not Found)
+
+        500: Internal Server Error - ResponseError (Internal Server Error)
+    """
+    if namespace is None:
+        namespace, error = get_services_namespace()
+        if error:
+            return None, error
+    request = AdminUpdatePlatformCredentials.create(
+        body=body,
+        namespace=namespace,
+    )
+    return await run_request_async(
+        request, additional_headers=x_additional_headers, **kwargs
+    )
