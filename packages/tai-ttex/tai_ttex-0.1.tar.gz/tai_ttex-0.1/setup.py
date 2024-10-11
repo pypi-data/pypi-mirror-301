03250a3d2e83c727af724c32b9d5239444037f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:05 +0000

    Add package tai-ttex/0.1/tai_ttex-0.1.tar.gz

---
+++ packages/tai-ttex/tai_ttex-0.1.tar.gz/tai_ttex-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="tai_ttex",
+    version="0.1",
+    packages=[
+        "ttex",
+    ],
+    install_requires=[],
+    license="GPL3",
+    long_description="Tool for experiments",
+    long_description_content_type="text/x-rst",
+)
