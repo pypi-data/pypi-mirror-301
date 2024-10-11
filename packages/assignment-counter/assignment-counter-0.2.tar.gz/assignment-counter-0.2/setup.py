3ab7c7314b2562205ecdeb3c15aa93e851a6569a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 04:03:54 +0000

    Add package assignment-counter/0.2/assignment-counter-0.2.tar.gz

---
+++ packages/assignment-counter/assignment-counter-0.2.tar.gz/assignment-counter-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='assignment-counter',
+    version='0.2',  # Increment the version
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        # Other dependencies
+    ],
+)
+
