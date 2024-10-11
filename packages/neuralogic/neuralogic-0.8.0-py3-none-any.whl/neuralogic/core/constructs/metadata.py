8d09db8c0af162f4442cd16596467d1b76ccda92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 07:04:13 +0000

    Add package neuralogic/0.8.0/neuralogic-0.8.0-py3-none-any.whl

---
+++ packages/neuralogic/neuralogic-0.8.0-py3-none-any.whl/neuralogic/core/constructs/metadata.py
@@ -0,0 +1,83 @@
+from typing import Union, Iterable, Callable, Optional
+
+from neuralogic.core.constructs.function.function import (
+    Function,
+    AggregationFunction,
+    TransformationFunction,
+    CombinationFunction,
+)
+
+
+class Metadata:
+    __slots__ = "learnable", "transformation", "aggregation", "duplicit_grounding", "combination"
+
+    def __init__(
+        self,
+        learnable: bool = None,
+        transformation: Union[str, TransformationFunction, CombinationFunction] = None,
+        combination: Union[str, CombinationFunction] = None,
+        aggregation: Union[str, AggregationFunction] = None,
+        duplicit_grounding: Optional[bool] = None,
+    ):
+        self.learnable = learnable
+        self.combination = combination
+        self.transformation = transformation
+        self.aggregation = aggregation
+        self.duplicit_grounding = duplicit_grounding
+
+    @staticmethod
+    def from_iterable(iterable: Iterable) -> "Metadata":
+        metadata = Metadata()
+
+        for entry in iterable:
+            if isinstance(entry, Callable) and not isinstance(entry, Function):
+                entry = entry()
+            if isinstance(entry, AggregationFunction):
+                metadata.aggregation = entry
+            elif isinstance(entry, TransformationFunction):
+                metadata.transformation = entry
+            elif isinstance(entry, CombinationFunction):
+                metadata.combination = entry
+            else:
+                raise ValueError(f"Invalid entry for metadata: {entry}")
+        return metadata
+
+    def __str__(self) -> str:
+        metadata_list = []
+        if self.learnable is not None:
+            metadata_list.append(f"learnable={str(self.learnable).lower()}")
+        if self.transformation is not None:
+            metadata_list.append(f"transformation={str(self.transformation)}")
+        if self.combination is not None:
+            metadata_list.append(f"combination={str(self.combination)}")
+        if self.aggregation is not None:
+            metadata_list.append(f"aggregation={str(self.aggregation)}")
+        if self.duplicit_grounding is not None:
+            metadata_list.append(f"duplicit_grounding={str(self.duplicit_grounding)}")
+        return f"[{', '.join(metadata_list)}]"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def combine(self, other: "Metadata") -> "Metadata":
+        return Metadata(
+            learnable=other.learnable if other.learnable is not None else self.learnable,
+            transformation=other.transformation if other.transformation is not None else self.transformation,
+            combination=other.combination if other.combination is not None else self.combination,
+            aggregation=other.aggregation if other.aggregation is not None else self.aggregation,
+            duplicit_grounding=other.duplicit_grounding
+            if other.duplicit_grounding is not None
+            else self.duplicit_grounding,
+        )
+
+    def __add__(self, other: "Metadata") -> "Metadata":
+        return self.combine(other)
+
+    def copy(self) -> "Metadata":
+        return Metadata(
+            learnable=self.learnable,
+            transformation=self.transformation,
+            combination=self.combination,
+            aggregation=self.aggregation,
+            duplicit_grounding=self.duplicit_grounding,
+        )
---
+++ packages/neuralogic/neuralogic-0.8.0-py3-none-any.whl/neuralogic/dataset/logic.py
@@ -0,0 +1,145 @@
+from typing import Optional, List, Union, Sequence
+
+import jpype
+
+from neuralogic.core.constructs.java_objects import JavaFactory
+from neuralogic.core.constructs.relation import BaseRelation
+from neuralogic.core.constructs.rule import Rule
+from neuralogic.dataset.base import BaseDataset
+from neuralogic.core.constructs.factories import R
+
+DatasetEntries = Union[BaseRelation, Rule]
+
+
+class Sample:
+    __slots__ = (
+        "query",
+        "example",
+    )
+
+    def __init__(
+        self, query: Optional[BaseRelation], example: Optional[Union[Sequence[DatasetEntries], DatasetEntries]]
+    ):
+        self.query = query
+
+        if example is None:
+            example = []
+
+        if not isinstance(example, Sequence):
+            self.example = [example]
+        else:
+            self.example = example
+
+    def __str__(self) -> str:
+        return str(self.query)
+
+    def __len__(self) -> int:
+        if self.example is None:
+            return 0
+        return len(self.example)
+
+
+class Dataset(BaseDataset):
+    r"""
+    Dataset encapsulating (learning) samples in the form of logic format, allowing users to fully take advantage of the
+    PyNeuraLogic library.
+    """
+
+    __slots__ = ("samples", "_examples", "_queries")
+
+    def __init__(self, samples: Optional[Union[List[Sample], Sample]] = None):
+        self.samples = samples
+
+        if self.samples is None:
+            self.samples = []
+        elif not isinstance(self.samples, list):
+            self.samples = [self.samples]
+
+        self._examples = []
+        self._queries = []
+
+    def set_samples(self, samples: List[Sample]):
+        self.samples = samples
+
+    def add_samples(self, samples: List[Sample]):
+        self.samples.extend(samples)
+
+    def add_sample(self, sample: Sample):
+        self.samples.append(sample)
+
+    def add(self, query: BaseRelation, example: Optional[List[DatasetEntries]]):
+        self.samples.append(Sample(query, example))
+
+    def __getitem__(self, item: int) -> Sample:
+        return self.samples[item]
+
+    def __setitem__(self, key: int, value: Sample):
+        self.samples[key] = value
+
+    def __delitem__(self, key: int):
+        del self.samples
+
+    def __str__(self):
+        return ". ".join(str(s) for s in self.samples)
+
+    def __len__(self):
+        return len(self.samples)
+
+    # Deprecated
+    def add_example(self, example):
+        self.add_examples([example])
+
+    def add_examples(self, examples: List):
+        self._examples.extend(examples)
+
+    def add_query(self, query):
+        self.add_queries([query])
+
+    def add_queries(self, queries: List):
+        self._queries.extend(queries)
+
+    def set_examples(self, examples: List):
+        self._examples = examples
+
+    def set_queries(self, queries: List):
+        self._queries = queries
+
+    def generate_features(self, feature_depth: int = 1, count_groundings: bool = True):
+        java_factory = JavaFactory()
+
+        clauses = []
+        vertex_lit = R.get("__vert")
+        vertex_lit.predicate.special = False
+        vertex_lit.predicate.hidden = False
+
+        for sample in self.samples:
+            vertex = set()
+
+            for e in sample.example:
+                if isinstance(e, Rule):
+                    vertex.update(self._get_constants(e.head))
+
+                    for rel in e.body:
+                        vertex.update(self._get_constants(rel))
+                if isinstance(e, BaseRelation):
+                    vertex.update(self._get_constants(e))
+
+            example = [vertex_lit(vert) for vert in vertex]
+            example.extend(sample.example)
+
+            clauses.append(java_factory.to_clause(example))
+
+        clause = jpype.java.util.ArrayList(clauses)
+
+        namespace = "cz.cvut.fel.ida.logic.features.generation"
+
+        jpype.JClass(f"{namespace}.FeatureGenerationSettings").COUNT_GROUNDINGS = count_groundings
+        features = jpype.JClass(f"{namespace}.FeatureGenerator").generateFeatures(clause, feature_depth)
+
+        table = [[int(i) for i in feats] for feats in features.table]
+        clauses = [str(clause) for clause in features.features]
+
+        return table, clauses
+
+    def _get_constants(self, relation: BaseRelation):
+        return [term for term in relation.terms if not str(relation)[0].isupper()]
