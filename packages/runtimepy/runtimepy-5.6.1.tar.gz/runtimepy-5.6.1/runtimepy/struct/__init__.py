20b3173792546099a47b31d57455c164969ac306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:23 +0000

    Add package runtimepy/5.6.1/runtimepy-5.6.1.tar.gz

---
+++ packages/runtimepy/runtimepy-5.6.1.tar.gz/runtimepy-5.6.1/runtimepy/struct/__init__.py
@@ -0,0 +1,42 @@
+"""
+A module implementing a runtime structure base.
+"""
+
+# built-in
+from logging import getLogger as _getLogger
+
+# third-party
+from vcorelib.io.types import JsonObject as _JsonObject
+
+# internal
+from runtimepy.channel.environment.command.processor import (
+    ChannelCommandProcessor,
+)
+from runtimepy.mixins.environment import ChannelEnvironmentMixin
+from runtimepy.mixins.logging import LoggerMixinLevelControl
+
+
+class RuntimeStructBase(LoggerMixinLevelControl, ChannelEnvironmentMixin):
+    """A base runtime structure."""
+
+    log_level_channel: bool = True
+
+    def __init__(self, name: str, config: _JsonObject) -> None:
+        """Initialize this instance."""
+
+        self.name = name
+        LoggerMixinLevelControl.__init__(self, logger=_getLogger(self.name))
+        ChannelEnvironmentMixin.__init__(self)
+        if self.log_level_channel:
+            self.setup_level_channel(self.env)
+        self.command = ChannelCommandProcessor(self.env, self.logger)
+        self.config = config
+
+    def poll(self) -> None:
+        """
+        A method that other runtime entities can call to perform canonical
+        updates to this struct's environment.
+        """
+
+
+StructMap = dict[str, RuntimeStructBase]
