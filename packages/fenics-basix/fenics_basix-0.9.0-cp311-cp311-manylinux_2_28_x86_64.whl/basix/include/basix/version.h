fe180b5c6c6455b438fb541961518b5fa11cac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:03:32 +0000

    Add package fenics-basix/0.9.0/fenics_basix-0.9.0-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/fenics-basix/fenics_basix-0.9.0-cp311-cp311-manylinux_2_28_x86_64.whl/basix/include/basix/version.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Chris Richardson
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+#pragma once
+
+#define BASIX_VERSION 0.9.0
+#define BASIX_VERSION_MAJOR 0
+#define BASIX_VERSION_MINOR 9
+#define BASIX_VERSION_PATCH 0
