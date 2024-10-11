e74fe745a2aedbefab7d1083f8300ff213befcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:02 +0000

    Add package mtmai/0.3.1128/mtmai-0.3.1128-py3-none-any.whl

---
+++ packages/mtmai/mtmai-0.3.1128-py3-none-any.whl/mtmai/cli/worker.py
@@ -0,0 +1,24 @@
+def register_worker_commands(cli):
+    @cli.command()
+    def worker():
+        from prefect import deploy
+        from prefect import flow, serve
+        from mtmai.flows.flow_site import flow_cms_site
+        from mtmai.flows.article_gen import flow_article_gen
+
+        deploy_flow_article_gen = flow_article_gen.to_deployment(name="flow_article_gen")
+        deploy_flow_cms_site = flow_cms_site.to_deployment(name="flow_cms_site")
+        serve(deploy_flow_article_gen, deploy_flow_cms_site)
+        # deploy(
+        #   # Use the `to_deployment` method to specify configuration
+        #   #specific to each deployment
+        #   flow_article_gen.to_deployment("my-deployment-1"),
+        #   # my_flow_2.to_deployment("my-deployment-2"),
+
+        #   # Specify shared configuration for both deployments
+        #   image="my-docker-image:dev",
+        #   push=False,
+        #   work_pool_name="my-work-pool",
+        # )
+
+        print("启动 worker")
