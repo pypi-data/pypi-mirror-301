15fde5d2d735c984e3cbcb49c7194d39ae6c3068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:27 +0000

    Add package quixstreams/3.0.0/quixstreams-3.0.0-py3-none-any.whl

---
+++ packages/quixstreams/quixstreams-3.0.0-py3-none-any.whl/quixstreams/state/types.py
@@ -0,0 +1,217 @@
+import logging
+from typing import Protocol, Any, Optional, Tuple, List
+
+logger = logging.getLogger(__name__)
+
+
+class WindowedState(Protocol):
+    """
+    A windowed state to be provided into `StreamingDataFrame` window functions.
+    """
+
+    def get_window(
+        self, start_ms: int, end_ms: int, default: Any = None
+    ) -> Optional[Any]:
+        """
+        Get the value of the window defined by `start` and `end` timestamps
+        if the window is present in the state, else default
+
+        :param start_ms: start of the window in milliseconds
+        :param end_ms: end of the window in milliseconds
+        :param default: default value to return if the key is not found
+        :return: value or None if the key is not found and `default` is not provided
+        """
+        ...
+
+    def update_window(self, start_ms: int, end_ms: int, value: Any, timestamp_ms: int):
+        """
+        Set a value for the window.
+
+        This method will also update the latest observed timestamp in state partition
+        using the provided `timestamp`.
+
+        :param start_ms: start of the window in milliseconds
+        :param end_ms: end of the window in milliseconds
+        :param value: value of the window
+        :param timestamp_ms: current message timestamp in milliseconds
+        """
+        ...
+
+    def get_latest_timestamp(self) -> int:
+        """
+        Get the latest observed timestamp for the current state partition.
+
+        Use this timestamp to determine if the arriving event is late and should be
+        discarded from the processing.
+
+        :return: latest observed event timestamp in milliseconds
+        """
+        ...
+
+    def expire_windows(
+        self, duration_ms: int, grace_ms: int = 0
+    ) -> List[Tuple[Tuple[int, int], Any]]:
+        """
+        Get a list of expired windows from RocksDB considering the current
+        latest timestamp, window duration and grace period.
+
+        It also marks the latest found window as expired in the expiration index, so
+        calling this method multiple times will yield different results for the same
+        "latest timestamp".
+
+        :param duration_ms: duration of the windows in milliseconds
+        :param grace_ms: grace period in milliseconds. Default - "0"
+        """
+        ...
+
+
+class WindowedPartitionTransaction(Protocol):
+    @property
+    def failed(self) -> bool:
+        """
+        Return `True` if transaction failed to update data at some point.
+
+        Failed transactions cannot be re-used.
+        :return: bool
+        """
+        ...
+
+    @property
+    def completed(self) -> bool:
+        """
+        Return `True` if transaction is successfully completed.
+
+        Completed transactions cannot be re-used.
+        :return: bool
+        """
+        ...
+
+    @property
+    def prepared(self) -> bool:
+        """
+        Return `True` if transaction is prepared completed.
+
+        Prepared transactions cannot receive new updates, but can be flushed.
+        :return: bool
+        """
+        ...
+
+    def prepare(self, processed_offset: int):
+        """
+        Produce changelog messages to the changelog topic for all changes accumulated
+        in this transaction and prepare transcation to flush its state to the state
+        store.
+
+        After successful `prepare()`, the transaction status is changed to PREPARED,
+        and it cannot receive updates anymore.
+
+        If changelog is disabled for this application, no updates will be produced
+        to the changelog topic.
+
+        :param processed_offset: the offset of the latest processed message
+        """
+
+    def as_state(self, prefix: Any) -> WindowedState: ...
+
+    def get_window(
+        self,
+        start_ms: int,
+        end_ms: int,
+        prefix: bytes,
+        default: Any = None,
+    ) -> Optional[Any]:
+        """
+        Get the value of the window defined by `start` and `end` timestamps
+        if the window is present in the state, else default
+
+        :param start_ms: start of the window in milliseconds
+        :param end_ms: end of the window in milliseconds
+        :param prefix: a key prefix
+        :param default: default value to return if the key is not found
+        :return: value or None if the key is not found and `default` is not provided
+        """
+        ...
+
+    def update_window(
+        self, start_ms: int, end_ms: int, value: Any, timestamp_ms: int, prefix: bytes
+    ):
+        """
+        Set a value for the window.
+
+        This method will also update the latest observed timestamp in state partition
+        using the provided `timestamp`.
+
+        :param start_ms: start of the window in milliseconds
+        :param end_ms: end of the window in milliseconds
+        :param value: value of the window
+        :param timestamp_ms: current message timestamp in milliseconds
+        :param prefix: a key prefix
+        """
+        ...
+
+    def get_latest_timestamp(self) -> int:
+        """
+        Get the latest observed timestamp for the current state partition.
+
+        Use this timestamp to determine if the arriving event is late and should be
+        discarded from the processing.
+
+        :return: latest observed event timestamp in milliseconds
+        """
+        ...
+
+    def expire_windows(self, duration_ms: int, prefix: bytes, grace_ms: int = 0):
+        """
+        Get a list of expired windows from RocksDB considering the current
+        latest timestamp, window duration and grace period.
+
+        It also marks the latest found window as expired in the expiration index, so
+        calling this method multiple times will yield different results for the same
+        "latest timestamp".
+
+        :param duration_ms: duration of the windows in milliseconds
+        :param prefix: a key prefix
+        :param grace_ms: grace period in milliseconds. Default - "0"
+        """
+        ...
+
+    def flush(
+        self,
+        processed_offset: Optional[int] = None,
+        changelog_offset: Optional[int] = None,
+    ):
+        """
+        Flush the recent updates to the storage.
+
+        :param processed_offset: offset of the last processed message, optional.
+        :param changelog_offset: offset of the last produced changelog message,
+            optional.
+        """
+
+    @property
+    def changelog_topic_partition(self) -> Optional[Tuple[str, int]]:
+        """
+        Return the changelog topic-partition for the StorePartition of this transaction.
+
+        Returns `None` if changelog_producer is not provided.
+
+        :return: (topic, partition) or None
+        """
+
+    def __enter__(self): ...
+
+    def __exit__(self, exc_type, exc_val, exc_tb): ...
+
+
+class PartitionRecoveryTransaction(Protocol):
+    """
+    A class for managing recovery for a StorePartition from a changelog message
+    """
+
+    def write_from_changelog_message(self): ...
+
+    def flush(self):
+        """
+        Flush the recovery update to the storage.
+        """
+        ...
