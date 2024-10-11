21c681e8da7337e8d2359c13f556d343e838ecf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:07:14 +0000

    Add package bioimage_embed/0.1.2/bioimage_embed-0.1.2.tar.gz

---
+++ packages/bioimage_embed/bioimage_embed-0.1.2.tar.gz/bioimage_embed-0.1.2/bioimage_embed/models/bolts/__init__.py
@@ -0,0 +1,15 @@
+# from .vqvae import *
+# from .vae import *
+
+from .vae import (
+    ResNet18VAEEncoder,
+    ResNet18VAEDecoder,
+    ResNet50VAEEncoder,
+    ResNet50VAEDecoder,
+)
+from .vqvae import (
+    ResNet50VQVAEEncoder,
+    ResNet50VQVAEDecoder,
+    ResNet18VQVAEEncoder,
+    ResNet18VQVAEDecoder,
+)
