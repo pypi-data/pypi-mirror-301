9187ff778f9d7548ea3c16a69148e9c821b1dfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:07:21 +0000

    Add package kstreams/0.24.5/kstreams-0.24.5.tar.gz

---
+++ packages/kstreams/kstreams-0.24.5.tar.gz/kstreams-0.24.5/kstreams/types.py
@@ -0,0 +1,30 @@
+import typing
+
+from kstreams import ConsumerRecord, RecordMetadata
+
+if typing.TYPE_CHECKING:
+    from .serializers import Serializer  #  pragma: no cover
+
+Headers = typing.Dict[str, str]
+EncodedHeaders = typing.Sequence[typing.Tuple[str, bytes]]
+StreamFunc = typing.Callable
+NextMiddlewareCall = typing.Callable[[ConsumerRecord], typing.Awaitable[None]]
+EngineHooks = typing.Sequence[typing.Callable[[], typing.Any]]
+
+
+class Send(typing.Protocol):
+    def __call__(
+        self,
+        topic: str,
+        value: typing.Any = None,
+        key: typing.Any = None,
+        partition: typing.Optional[int] = None,
+        timestamp_ms: typing.Optional[int] = None,
+        headers: typing.Optional[Headers] = None,
+        serializer: typing.Optional["Serializer"] = None,
+        serializer_kwargs: typing.Optional[typing.Dict] = None,
+    ) -> typing.Awaitable[RecordMetadata]: ...
+
+
+D = typing.TypeVar("D")
+Deprecated = typing.Annotated[D, "deprecated"]
