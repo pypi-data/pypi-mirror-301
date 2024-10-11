923f8db57095b98ce5cfab0ce4326eb69435ccdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:03:23 +0000

    Add package adafruit-circuitpython-testrepo/3.2.2/adafruit_circuitpython_testrepo-3.2.2.tar.gz

---
+++ packages/adafruit-circuitpython-testrepo/adafruit_circuitpython_testrepo-3.2.2.tar.gz/adafruit_circuitpython_testrepo-3.2.2/examples/testrepo_simpletest.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2019 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""Repo exists for test purposes only."""
+
+
+def test():
+    return None
+
+
+while True:
+    x = 1
+    y = 2
+    z = 3
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
+    print(x + 88)
+    print(x + 72)
---
+++ packages/adafruit-circuitpython-testrepo/adafruit_circuitpython_testrepo-3.2.2.tar.gz/adafruit_circuitpython_testrepo-3.2.2/examples/testrepo_simpletest_two.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2019 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""Repo exists for test purposes only."""
+while True:
+    x = 1
+    y = 2
+    z = 3
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
+    print(x + 1)
+    print(y + 10)
+    print(z + 100)
