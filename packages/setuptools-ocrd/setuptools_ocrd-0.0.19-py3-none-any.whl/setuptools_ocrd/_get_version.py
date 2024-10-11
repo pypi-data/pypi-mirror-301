8d1f0ca578fc63d9bf0e133e082f2a832477c781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 04:03:53 +0000

    Add package setuptools-ocrd/0.0.19/setuptools_ocrd-0.0.19-py3-none-any.whl

---
+++ packages/setuptools-ocrd/setuptools_ocrd-0.0.19-py3-none-any.whl/setuptools_ocrd/_get_version.py
@@ -0,0 +1,13 @@
+import json
+import os.path
+
+
+def _get_version():
+    OCRD_TOOL_JSON = "ocrd-tool.json"
+    if not os.path.exists(OCRD_TOOL_JSON):
+        return None
+
+    with open(OCRD_TOOL_JSON, "r") as fp:
+        return json.load(fp)["version"]
+
+    return None
