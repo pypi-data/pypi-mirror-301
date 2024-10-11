69c1e72e83c144c4d9073f74171da126ca0aa419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:03:34 +0000

    Add package zhutools/0.0.1/zhutools-0.0.1.tar.gz

---
+++ packages/zhutools/zhutools-0.0.1.tar.gz/zhutools-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='zhutools',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/GodsLeft/',
+    license='MIT',
+    author='yaguangzhu@qq.com',
+    description='我的个人python工具库',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
