96b913698e087811abac39098b0017ea88573939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:31 +0000

    Add package mangokit/1.1.11/mangokit-1.1.11.tar.gz

---
+++ packages/mangokit/mangokit-1.1.11.tar.gz/mangokit-1.1.11/mangokit/tools/data_processor/random_file.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Project: 芒果测试平台
+# @Description: 
+# @Time   : 2023-12-21 11:15
+# @Author : 毛鹏
+
+
+class RandomFileData:
+    """获取文件对象"""
+
+    @classmethod
+    def get_file(cls, **kwargs) -> str:
+        """传入文件名称，返回文件"""
+        pass
