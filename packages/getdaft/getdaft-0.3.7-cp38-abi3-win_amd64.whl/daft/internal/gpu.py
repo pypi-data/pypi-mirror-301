a1dbd0090879ca8e55b5d8d334da1c48e171e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:14 +0000

    Add package getdaft/0.3.7/getdaft-0.3.7-cp38-abi3-win_amd64.whl

---
+++ packages/getdaft/getdaft-0.3.7-cp38-abi3-win_amd64.whl/daft/internal/gpu.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import subprocess
+
+from daft.dependencies import ET
+
+
+def cuda_device_count():
+    """Returns the number of CUDA devices detected by nvidia-smi command"""
+    try:
+        nvidia_smi_output = subprocess.check_output(["nvidia-smi", "-x", "-q"])
+    except Exception:
+        return 0
+    root = ET.fromstring(nvidia_smi_output.decode("utf-8"))
+    attached_gpus = root.find("attached_gpus")
+    if attached_gpus is None:
+        return 0
+    return int(attached_gpus.text)
