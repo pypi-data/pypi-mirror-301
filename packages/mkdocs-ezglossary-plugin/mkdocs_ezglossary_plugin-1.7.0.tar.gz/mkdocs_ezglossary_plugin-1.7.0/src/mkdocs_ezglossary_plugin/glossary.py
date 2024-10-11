9d416088ff359feb07337a615dac82e4568cea09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:04:23 +0000

    Add package mkdocs-ezglossary-plugin/1.7.0/mkdocs_ezglossary_plugin-1.7.0.tar.gz

---
+++ packages/mkdocs-ezglossary-plugin/mkdocs_ezglossary_plugin-1.7.0.tar.gz/mkdocs_ezglossary_plugin-1.7.0/src/mkdocs_ezglossary_plugin/glossary.py
@@ -0,0 +1,137 @@
+import hashlib
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_id(section: str, term: str, linktype: str, n: str):
+    return str(hashlib.md5(f"{section}_{term}_{linktype}_{n}".encode()).hexdigest())
+
+
+class Entry:
+    """ An entry in the glossary. """
+
+    def __init__(self, target, page, definition, section, term):
+        self.target = target
+        """ The anchor to directly point to this specific link. """
+
+        self.page = page
+        """ The url of the page to which this entry points to. """
+
+        self.definition = definition
+        """ The definition of the term. """
+
+        self.section = section
+        """ The section of the term of this entry. (Since `v1.7.0a1`) """
+
+        self.term = term
+        """ The term of this entry. (Since v1.7.0a1) """
+
+    def __repr__(self):
+        return self.page.url
+
+
+class Glossary:
+    """ The complete glossary for all sections """
+
+    def __init__(self, ignore_case):
+        self.clear()
+        self.ignore_case = ignore_case
+
+    def add(self, section, term, linktype, page, definition=None, anchor=None):
+        term = term.strip()
+        log.debug(f"glossary.add({section}, {term}, {linktype}, '{definition}', {anchor})")
+        links = self._links(section, term, linktype)
+        _id = get_id(section, term, linktype, len(links))
+        anchor = _id if anchor is None else anchor
+        links[_id] = Entry(anchor, page, definition, section, term)
+        return _id
+
+    def has(self, section: str) -> bool:
+        """ Check if the glossary has a section named **section**.
+
+            Args:
+                section:
+                    The name of the section to check
+
+            Returns:
+                    True, if a section with the given name exists.
+        """
+        return section in self._glossary
+
+    def get(self, section: str, term: str, linktype: str) -> list[Entry]:
+        """ Get a list of [Entry][mkdocs_ezglossary_plugin.glossary.Entry] instances
+            for a specific **term** in a **section**.
+
+            Args:
+                section:
+                    The name of the section
+                term:
+                    The term for which the [Entry] instances should be retreived.
+                linktype:
+                    Defines which type of links should be returned.
+
+                    `refs`
+                    : returns all references to the term
+
+                    `defs`
+                    : returns all definitions for the term
+
+            Returns:
+                A list of either `definitions` or `references` for the given term.
+        """
+        links = self._links(section, term, linktype).values()
+        return list(links)
+
+    def definition(self, section: str, term: str) -> str:
+        """ Get the definition for a term.
+
+            Args:
+                section:
+                    The name of the section
+                term:
+                    The term to get the definition for
+
+            Returns:
+                The definition of the term
+        """
+        defs = self.get(section, term, 'defs')
+        if not defs:
+            return ""
+        return defs[0].definition
+
+    def terms(self, section: str) -> list[str]:
+        """ Return a list of entries for a specific section.
+
+            Args:
+                section:
+                    The name of the section.
+
+            Returns:
+                A list of all terms in the specific section.
+        """
+        keys = list(self._section(section).keys())
+        keys.sort()
+        return keys
+
+    def clear(self):
+        self._glossary = {}
+
+    def _links(self, section, term, linktype):
+        return self._term(section, term)[linktype]
+
+    def _term(self, section, term):
+        _section = self._section(section)
+        if term in _section:
+            return self._section(section)[term]
+        if self.ignore_case:
+            for candidate in _section.keys():
+                if candidate.upper() == term.upper():
+                    return self._section(section)[candidate]
+        self._section(section)[term] = {'defs': {}, 'refs': {}}
+        return self._section(section)[term]
+
+    def _section(self, section):
+        if section not in self._glossary:
+            self._glossary[section] = {}
+        return self._glossary[section]
---
+++ packages/mkdocs-ezglossary-plugin/mkdocs_ezglossary_plugin-1.7.0.tar.gz/mkdocs_ezglossary_plugin-1.7.0/tests/test_link.py
@@ -0,0 +1,301 @@
+import logging
+import mock
+
+from yaxp import xpath
+
+from mkdocs_ezglossary_plugin.glossary import get_id
+
+log = logging.getLogger(__name__)
+
+
+def _hash(s):
+    return str(hash(s))
+
+
+def test_link_no_title(simple, config):
+    html = mock.render_single(simple, config)
+
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 0),
+                        title="",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="third")
+    assert len(html.xpath(str(dl))) == 1
+
+
+def test_link_case_sensitive(simple, config):
+    html = mock.render_single(simple, config)
+
+    assert not _has_reference(html,
+                              section='test',
+                              text='First',
+                              term='first',
+                              title='',
+                              target='simple.md')
+
+
+def test_link_ignore_case(simple, config):
+    config['ignore_case'] = True
+    html = mock.render_single(simple, config)
+
+    assert _has_reference(html,
+                          section='test',
+                          text='First',
+                          term='first',
+                          title='',
+                          target='simple.md')
+
+
+def test_link_short_title(simple, config):
+    config['tooltip'] = "short"
+    html = mock.render_single(simple, config)
+    log.debug(html)
+
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 0),
+                        title="third term",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="third")
+    assert len(html.xpath(str(dl))) == 1
+
+
+def test_link_full_title(simple, config):
+    config['tooltip'] = "full"
+    html = mock.render_single(simple, config)
+    log.debug(html)
+
+    dl = xpath.body.p.a(_class="mkdocs-ezglossary-link",
+                        id=get_id("test", "third", "refs", 0),
+                        title="*detailed description of third term",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="third")
+    assert len(html.xpath(str(dl))) == 1
+
+
+def test_link_replace_html(simple, config):
+    config['tooltip'] = "full"
+    html = mock.render_single(simple, config)
+    log.debug(html)
+
+    dl = xpath.body.p.a(id=get_id("test", "second", "refs", 0),
+                        title="*this text is formatted",
+                        href="../simple.md#" + get_id("test", "second", "defs", 0),
+                        text="mysecond")
+    assert len(html.xpath(str(dl))) == 1
+
+
+def test_link_second_ref(simple, summary, config):
+    config['tooltip'] = "full"
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 1),
+                        title="*third term",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="third")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 2),
+                        title="*third term",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="mythird")
+    assert len(summary.xpath(str(dl))) == 1
+
+
+def test_link_default_ref_dis(simple, summary, config):
+    """ Ensure definitions for the default sections are
+        ignored when the configuration `use_default` is set
+        to `False`.
+    """
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(_class="mkdocs-ezglossary-link",
+                        id=get_id("_", "default", "refs", 0),
+                        title="",
+                        href="../simple.md#" + get_id("_", "default", "defs", 0),
+                        text="default")
+    assert len(summary.xpath(str(dl))) == 0
+
+
+def test_link_default_ref_enabled(simple, summary, config):
+    """ Ensure definitions for the default sections are
+        replaced when the configuration `use_default` is set
+        to `True`.
+    """
+    config['use_default'] = True
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(_class="mkdocs-ezglossary-link",
+                        id=get_id("_", "default", "refs", 0),
+                        title="",
+                        href="../simple.md#" + get_id("_", "default", "defs", 0),
+                        text="default")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(_class="mkdocs-ezglossary-link",
+                        id=get_id("_", "default2", "refs", 0),
+                        title="",
+                        href="../simple.md#" + get_id("_", "default2", "defs", 0),
+                        text="mydef2")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(_class="mkdocs-ezglossary-link",
+                        id=get_id("_", "default3", "refs", 0),
+                        title="",
+                        href="../simple.md#" + get_id("_", "default3", "defs", 0),
+                        text="mydef3")
+    assert len(summary.xpath(str(dl))) == 1
+
+
+def test_markdown_links_disabled(simple, summary, config):
+    """ Ensure that markdown links are ignored when `markdown_links` is set to false.
+    """
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(href="test:third",
+                        text="mythird")
+    assert len(summary.xpath(str(dl))) == 1
+
+
+def test_markdown_links_enabled(simple, summary, config):
+    """ Ensure markdown links are resolved when `markdown_links` is set to true.
+    """
+    config['markdown_links'] = True
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 1),
+                        title="",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="third")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(id=get_id("test", "third", "refs", 2),
+                        title="",
+                        href="../simple.md#" + get_id("test", "third", "defs", 0),
+                        text="mythird")
+    assert len(summary.xpath(str(dl))) == 1
+
+
+def test_unicode(simple, summary, config):
+    """ Unicode links are processed.
+    """
+    config['markdown_links'] = True
+    config['use_default'] = True
+    config['tooltip'] = "short"
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(id=get_id("demo", "🚧", "refs", 0),
+                        title="demo 🚧🚧🚧",
+                        href="../simple.md#" + get_id("demo", "🚧", "defs", 0),
+                        text="🚧")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(id=get_id("_", "🚧🚧", "refs", 0),
+                        title="default 🚧🚧🚧",
+                        href="../simple.md#" + get_id("_", "🚧🚧", "defs", 0),
+                        text="refers to 🚧")
+    assert len(summary.xpath(str(dl))) == 1
+
+
+def test_plurals_inflect(simple, summary, config):
+    """ Plurals are mapped to singulars
+    """
+    config['tooltip'] = "full"
+    config['plurals'] = 'inflect'
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='children',
+                          term='child',
+                          title='children definition',
+                          target='simple.md')
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='geese',
+                          term='goose',
+                          title='goose definition',
+                          target='simple.md')
+
+
+def test_plurals_en(simple, summary, config):
+    """ Plurals are mapped to singulars
+    """
+    config['tooltip'] = "full"
+    config['plurals'] = 'en'
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='children',
+                          term='child',
+                          title='children definition',
+                          target='simple.md')
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='geese',
+                          term='goose',
+                          title='goose definition',
+                          target='simple.md')
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='grandchildren',
+                          term='grandchild',
+                          title='grandchild definition',
+                          target='simple.md')
+
+
+def test_plural_priority(simple, summary, config):
+    """ If the plural term is defined, this should be used in priority
+    """
+    config['tooltip'] = "full"
+    config['plurals'] = 'en'
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    assert _has_reference(summary,
+                          section='plurals',
+                          text='potatoes',
+                          term='potatoes',
+                          title='potatoes definition',
+                          target='simple.md')
+
+
+def test_plurals_disabled(simple, summary, config):
+    """ Plurals are not mapped to singulars, if not enabled
+    """
+    config['tooltip'] = "full"
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    assert not _has_reference(summary,
+                              section='plurals',
+                              text='children',
+                              term='child',
+                              title='children definition',
+                              target='simple.md')
+
+
+def _has_reference(document, section, text, term, title, target):
+    dl = xpath.body.p.a(id=get_id(section, text, "refs", 0),
+                        title=title,
+                        href=f"../{target}#" + get_id(section, term, "defs", 0),
+                        text=text)
+    return len(document.xpath(str(dl))) == 1
+
+
+def test_hyphens(simple, summary, config):
+    """ Hyphens in terms are supported
+    """
+    config['markdown_links'] = True
+    summary = mock.render([simple, summary], config)['summary.md']
+    log.debug(summary)
+
+    dl = xpath.body.p.a(id=get_id("test", "hyphens-abc def", "refs", 0),
+                        href="../simple.md#" + get_id("test", "hyphens-abc def", "defs", 0),
+                        text="hyphens-abc def")
+    assert len(summary.xpath(str(dl))) == 1
+    dl = xpath.body.p.a(id=get_id("test", "hyphens-abc xyz", "refs", 0),
+                        href="../simple.md#" + get_id("test", "hyphens-abc xyz", "defs", 0),
+                        text="hyphens-abc xyz")
+    assert len(summary.xpath(str(dl))) == 1
