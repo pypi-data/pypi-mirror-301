8abe5048167a19edd5faca9b88d89025f7c78983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:03 +0000

    Add package acrome-smd/2.0.5/acrome_smd-2.0.5.tar.gz

---
+++ packages/acrome-smd/acrome_smd-2.0.5.tar.gz/acrome_smd-2.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="acrome-smd",
+    version="2.0.5",
+    author="Furkan Kırlangıç",
+    author_email="furkankirlangic@acrome.net",
+    description="Python library for interfacing with Acrome Smart Motion Devices (SMD) products.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Acrome-Smart-Motion-Devices/python-library",
+    project_urls={
+        "Bug Tracker": "https://github.com/Acrome-Smart-Motion-Devices/python-library/issues",
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(exclude=['tests', 'test']),
+    install_requires=["pyserial", "stm32loader", "crccheck", "requests", "packaging"],
+    python_requires=">=3.7"
+)
