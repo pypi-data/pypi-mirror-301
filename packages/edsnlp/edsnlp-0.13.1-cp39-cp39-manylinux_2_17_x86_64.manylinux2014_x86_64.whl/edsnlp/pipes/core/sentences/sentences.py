d21e24917da3c039c47ca977cdea256b6228af3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 07:04:49 +0000

    Add package edsnlp/0.13.1/edsnlp-0.13.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.13.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/edsnlp/pipes/core/sentences/sentences.py
@@ -0,0 +1,57 @@
+from typing import List, Optional
+
+from spacy.tokens import Doc
+
+from edsnlp.core import PipelineProtocol
+
+from ...base import BaseComponent
+from .fast_sentences import FastSentenceSegmenter
+from .terms import punctuation
+
+
+class SentenceSegmenter(BaseComponent):
+    """
+    Segments the Doc into sentences using a rule-based strategy,
+    specific to AP-HP documents.
+
+    Applies the same rule-based pipeline as spaCy's sentencizer,
+    and adds a simple rule on the new lines : if a new line is followed by a
+    capitalised word, then it is also an end of sentence.
+    """
+
+    def __init__(
+        self,
+        nlp: PipelineProtocol,
+        name: Optional[str] = "sentences",
+        punct_chars: Optional[List[str]] = None,
+        use_endlines: bool = None,
+        ignore_excluded: bool = True,
+    ):
+        """
+        Parameters
+        ----------
+        nlp: PipelineProtocol
+            The EDS-NLP pipeline
+        name: Optional[str]
+            The name of the component
+        punct_chars: Optional[List[str]]
+            Punctuation characters.
+        use_endlines: bool
+            Whether to use endlines prediction.
+        ignore_excluded: bool
+            Whether to ignore excluded tokens.
+        """
+        super().__init__(nlp, name)
+
+        if punct_chars is None:
+            punct_chars = punctuation
+
+        self.fast_segmenter = FastSentenceSegmenter(
+            vocab=nlp.vocab,
+            punct_chars=punct_chars,
+            use_endlines=use_endlines,
+            ignore_excluded=ignore_excluded,
+        )
+
+    def __call__(self, doc: Doc):
+        return self.fast_segmenter(doc)
