63701332c789ee709ed334d9129dfaf03c306728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:04:49 +0000

    Add package pynlcd/0.1.4/pynlcd-0.1.4.tar.gz

---
+++ packages/pynlcd/pynlcd-0.1.4.tar.gz/pynlcd-0.1.4/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynlcd",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "gdal",
+        "numpy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pynlcd=pynlcd.nlcd:main",
+        ],
+    },
+)
+
+
+# from setuptools import setup
+
+# setup(
+#   name="pynlcd",
+#   version="0.1.2",
+#   packages=["pynlcd"],
+#   install_requires=[
+#       "gdal",
+#       "numpy",
+#   ],
+#   entry_points={
+#       "console_scripts": [
+#           "pynlcd=pynlcd:main",
+#       ],
+#   },
+# )
+
+
+# from setuptools import setup
+
+# setup(
+#   name="pynlcd",
+#   version="0.1.2",
+#   packages=["pynlcd"],
+#   install_requires=[
+#       "gdal",
+#       "numpy",
+#   ],
+#   entry_points={
+#       "console_scripts": [
+#           "pynlcd=pynlcd.nlcd:main",
+#       ],
+#   },
+# )
