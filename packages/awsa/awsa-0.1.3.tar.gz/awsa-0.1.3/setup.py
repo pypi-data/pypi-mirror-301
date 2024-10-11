2e8221371e939c8d7aa26d85b64cb4ea6bf11915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:03:21 +0000

    Add package awsa/0.1.3/awsa-0.1.3.tar.gz

---
+++ packages/awsa/awsa-0.1.3.tar.gz/awsa-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awsa',
+    version='0.1.3',
+    description='AISAK-O agent for information collection and summary.',
+    author='Mandela Logan',
+    author_email='mandelakorilogan@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers',
+        'beautifulsoup4',
+        'edge-tts',
+        'nest_asyncio',
+        'IPython',                   
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
