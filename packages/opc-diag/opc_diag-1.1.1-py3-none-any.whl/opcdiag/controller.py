55be2a170d2e2bdf893e7ae9e81b651072f843db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:19 +0000

    Add package opc-diag/1.1.1/opc_diag-1.1.1-py3-none-any.whl

---
+++ packages/opc-diag/opc_diag-1.1.1-py3-none-any.whl/opcdiag/controller.py
@@ -0,0 +1,83 @@
+"""Command-line application controller for opc-diag."""
+
+from __future__ import annotations
+
+from opcdiag.model import Package
+from opcdiag.presenter import DiffPresenter, ItemPresenter
+from opcdiag.view import OpcView
+
+_CONTENT_TYPES_URI = "[Content_Types].xml"
+
+
+class OpcController:
+    """Mediate between the command-line interface and the package model entities.
+
+    Orchestrates the execution of user commands by creating entity objects, delegating work to
+    them, and using the appropriate view object to format the results to be displayed.
+    """
+
+    def browse(self, pkg_path: str, uri_tail: str):
+        """Display pretty-printed XML of part with *uri_tail* in package at `pkg_path`."""
+        pkg = Package.read(pkg_path)
+        pkg_item = pkg.find_item_by_uri_tail(uri_tail)
+        item_presenter = ItemPresenter(pkg_item)
+        OpcView.pkg_item(item_presenter)
+
+    def diff_item(self, package_1_path: str, package_2_path: str, uri_tail: str):
+        """
+        Display the meaningful differences between the item identified by
+        *uri_tail* in the package at *package_1_path* and its counterpart in
+        the package at *package_2_path*. Each path can be either a standard
+        zip package (e.g. a .pptx file) or a directory containing an extracted
+        package.
+        """
+        package_1 = Package.read(package_1_path)
+        package_2 = Package.read(package_2_path)
+        diff = DiffPresenter.named_item_diff(package_1, package_2, uri_tail)
+        OpcView.item_diff(diff)
+
+    def diff_pkg(self, package_1_path: str, package_2_path: str):
+        """
+        Display the meaningful differences between the packages at
+        *package_1_path* and *package_2_path*. Each path can be either a
+        standard zip package (e.g. .pptx file) or a directory containing an
+        extracted package.
+        """
+        package_1 = Package.read(package_1_path)
+        package_2 = Package.read(package_2_path)
+        content_types_diff = DiffPresenter.named_item_diff(package_1, package_2, _CONTENT_TYPES_URI)
+        rels_diffs = DiffPresenter.rels_diffs(package_1, package_2)
+        xml_part_diffs = DiffPresenter.xml_part_diffs(package_1, package_2)
+        OpcView.package_diff(content_types_diff, rels_diffs, xml_part_diffs)
+
+    def extract_package(self, package_path: str, extract_dirpath: str):
+        """
+        Extract the contents of the package at *package_path* to individual
+        files in a directory at *extract_dirpath*.
+        """
+        package = Package.read(package_path)
+        package.prettify_xml()
+        package.save_to_dir(extract_dirpath)
+
+    def repackage(self, package_path: str, new_package_path: str):
+        """
+        Write the contents of the package found at *package_path* to a new
+        zip package at *new_package_path*.
+        """
+        package = Package.read(package_path)
+        package.save(new_package_path)
+
+    def substitute(self, uri_tail: str, src_pkg_path: str, tgt_pkg_path: str, new_pkg_path: str):
+        """
+        Substitute the package item identified by *uri_tail* from the package
+        at *src_pkg_path* into the package at *tgt_pkg_path* and save the
+        resulting package at *new_pkg_path*. This can be handy for
+        identifying which package item(s) in the source package are causing a
+        "repair needed" error when loading the target package in MS Office.
+        """
+        package_1 = Package.read(src_pkg_path)
+        package_2 = Package.read(tgt_pkg_path)
+        pkg_item = package_1.find_item_by_uri_tail(uri_tail)
+        package_2.substitute_item(pkg_item)
+        package_2.save(new_pkg_path)
+        OpcView.substitute(pkg_item.uri, src_pkg_path, tgt_pkg_path, new_pkg_path)
