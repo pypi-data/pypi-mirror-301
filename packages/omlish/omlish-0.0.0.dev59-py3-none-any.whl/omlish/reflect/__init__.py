4e627631e8782404a7ccbeb71b0f5e1f322ff0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:25 +0000

    Add package omlish/0.0.0.dev59/omlish-0.0.0.dev59-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev59-py3-none-any.whl/omlish/reflect/__init__.py
@@ -0,0 +1,40 @@
+from .types import (  # noqa
+    ANY,
+    Annotated,
+    Any,
+    Generic,
+    NewType,
+    TYPES,
+    Type,
+    Union,
+    get_orig_bases,
+    get_orig_class,
+    get_params,
+    is_type,
+    is_union_type,
+    type_,
+)
+
+from .ops import (  # noqa
+    get_concrete_type,
+    get_underlying,
+    strip_annotations,
+    strip_objs,
+    to_annotation,
+    types_equivalent,
+)
+
+from .isinstance import (  # noqa
+    KNOWN_ISINSTANCE_GENERICS,
+    isinstance_of,
+)
+
+from .subst import (  # noqa
+    ALIAS_UPDATING_GENERIC_SUBSTITUTION,
+    DEFAULT_GENERIC_SUBSTITUTION,
+    GenericSubstitution,
+    generic_mro,
+    get_generic_bases,
+    get_type_var_replacements,
+    replace_type_vars,
+)
