dac0c8c671ba489d932c61b79e133238737d9959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:26 +0000

    Add package natural-language-terminal/0.1.4/natural_language_terminal-0.1.4.tar.gz

---
+++ packages/natural-language-terminal/natural_language_terminal-0.1.4.tar.gz/natural_language_terminal-0.1.4/setup.py
@@ -0,0 +1,38 @@
+# setup.py
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+# Read the contents of requirements.txt
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
+
+setup(
+    name='natural-language-terminal',
+    version='0.1.4',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    author='Balaji Rama',
+    author_email='balajirw10@gmail.com',
+    python_requires='>=3.6',
+    # py_modules=['nlt'],  # Add this line
+    install_requires=['openai', 'typer', 'rich', 'python-dotenv'],
+    entry_points={
+        "console_scripts": [
+            "@nlt=natural_language_terminal.cli.app:app",
+        ],
+    },
+    package_data={
+        'natural-language-terminal': [
+            'shell_scripts/*.sh',
+            'shell_scripts/*.bat'
+        ],
+    },
+)
