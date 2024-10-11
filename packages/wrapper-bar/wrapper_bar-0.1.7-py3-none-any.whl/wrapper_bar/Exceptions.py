aa7d3bd8d430f3acd763f2c545127c7e409aa1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 08:04:18 +0000

    Add package wrapper-bar/0.1.7/wrapper_bar-0.1.7-py3-none-any.whl

---
+++ packages/wrapper-bar/wrapper_bar-0.1.7-py3-none-any.whl/wrapper_bar/Exceptions.py
@@ -0,0 +1,19 @@
+
+class WrapperCodeDefinitionError(Exception):
+    pass
+
+# exception for downloadWrapper
+class NoConnection(Exception):
+    pass
+class NotADirectory(Exception):
+    pass
+class DownloadFileNameErr(Exception):
+    pass
+class typeErr(Exception):
+    pass
+class GitHubTokenMissing(Exception):
+    pass
+class ConnectionErr(Exception):
+    pass
+class DownloadFileNotFound(Exception):
+    pass
