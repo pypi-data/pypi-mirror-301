9d40ebc7411fc5aa826217be387edd2fab1d82de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:05:44 +0000

    Add package sql-scan/0.3/sql_scan-0.3.tar.gz

---
+++ packages/sql-scan/sql_scan-0.3.tar.gz/sql_scan-0.3/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+from setuptools import setup, find_packages
+with open("README.md","r") as f:
+    description = f.read()
+setup(
+    name='sql_scan',
+    version='0.3',
+    description='SQL file processing package for extracting table names and WHERE conditions',
+    author='Meghsham Jambhulkar',
+    author_email='meghshamofficial@gmail.com',
+    packages=find_packages(),
+    install_requires=['sqlparse'],
+    entry_points={
+        'console_scripts': [
+            'sql_scan=sql_scan:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+    long_description= description,
+    long_description_content_type="text/markdown",
+)
