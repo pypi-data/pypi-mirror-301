971c67779cd42d89ffd02f6528289075729f2010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 04:04:16 +0000

    Add package aait-store-cut-part-009/0.0.1/aait_store_cut_part_009-0.0.1.tar.gz

---
+++ packages/aait-store-cut-part-009/aait_store_cut_part_009-0.0.1.tar.gz/aait_store_cut_part_009-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+def package_files(directory):
+	paths = []
+	for (path, directories, filenames) in os.walk(directory):
+		for filename in filenames:
+			paths.append(os.path.relpath(os.path.join(path, filename), directory))
+	return paths
+
+
+binary_files = package_files('src/aait_store_cut-part_009/input')
+
+setup(
+	name='aait_store_cut-part_009',
+	version='0.0.1',
+	description='Package with binary files and subfolders',
+	packages=find_packages(where='src'),
+	package_dir={'': 'src'},
+	package_data = {
+		'aait_store_cut-part_009': binary_files,
+	},
+	include_package_data=True,
+	install_requires=['aait_store_cut-part_008==0.0.1', ],
+)
