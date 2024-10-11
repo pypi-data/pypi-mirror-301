dec0382397d59a3edd386c719d3c198f76725d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:04:28 +0000

    Add package easyllm-kit/0.0.6.3/easyllm_kit-0.0.6.3-py3-none-any.whl

---
+++ packages/easyllm-kit/easyllm_kit-0.0.6.3-py3-none-any.whl/easyllm_kit/models/__init__.py
@@ -0,0 +1,9 @@
+from easyllm_kit.models.base import LLM
+from easyllm_kit.models.openai import GPT4o
+from easyllm_kit.models.llama import Llama3
+from easyllm_kit.models.perplexity import Perplexity
+
+__all__ = ['LLM',
+           'GPT4o',
+           'Llama3',
+           'Perplexity']
