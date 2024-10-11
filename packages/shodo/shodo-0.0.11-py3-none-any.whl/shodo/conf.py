8f79927f690aa285f9ada6026441762cfbbd173f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:01 +0000

    Add package shodo/0.0.11/shodo-0.0.11-py3-none-any.whl

---
+++ packages/shodo/shodo-0.0.11-py3-none-any.whl/shodo/conf.py
@@ -0,0 +1,54 @@
+import json
+import os
+from pathlib import Path
+
+SHODO_TOKEN = "SHODO_API_TOKEN"
+SHODO_ROOT = "SHODO_API_ROOT"
+OLD_CREDENTIALS_PATH = "~/.shodo/credentials"
+
+
+def get_path():
+    config_path = Path(os.environ.get("XDG_CONFIG_HOME", "~/.config/"))
+    config_path = config_path.expanduser()
+    return config_path / "shodo/credentials"
+
+
+def save_credentials(root: str, token: str):
+    c = get_path()
+    if not c.parent.exists():
+        c.parent.mkdir()
+
+    if c.exists():
+        d = json.loads(c.read_text(encoding="utf-8"))
+    else:
+        d = {}
+
+    d["default"] = {
+        SHODO_ROOT: root,
+        SHODO_TOKEN: token,
+    }
+    c.write_text(json.dumps(d, indent=2), encoding="utf-8")
+
+
+def load_credentials():
+    p = get_path()
+    if not p.exists():
+        p = Path(OLD_CREDENTIALS_PATH).expanduser()
+        if not p.exists():
+            raise FileNotFoundError(
+                "Use 'shodo login' to save credentials before running"
+            )
+
+    d = json.loads(p.read_text(encoding="utf-8"))
+    return d["default"]
+
+
+def conf():
+    if SHODO_TOKEN in os.environ and SHODO_ROOT in os.environ:
+        c = os.environ
+    else:
+        c = load_credentials()
+    return {
+        "API_ROOT": c[SHODO_ROOT],
+        "API_TOKEN": c[SHODO_TOKEN],
+    }
---
+++ packages/shodo/shodo-0.0.11-py3-none-any.whl/shodo/lint.py
@@ -0,0 +1,85 @@
+import time
+from dataclasses import asdict, dataclass
+from typing import Dict, Optional
+
+from shodo.api import lint_create, lint_result
+
+
+@dataclass
+class Pos:
+    line: int
+    ch: int
+
+    def __str__(self):
+        return f"{self.line+1}:{self.ch+1}"
+
+    def astuple(self):
+        return self.line, self.ch
+
+
+@dataclass
+class Message:
+    ERROR = "error"
+    WARNING = "warning"
+
+    code: str
+    message: str
+    severity: str
+    from_: Pos
+    to: Pos
+    index: int
+    index_to: int
+    score: float
+    before: Optional[str] = None
+    after: Optional[str] = None
+    operation: Optional[str] = None
+    meta: Optional[Dict] = None
+
+    @classmethod
+    def load(cls, data):
+        data["from_"] = Pos(**data.pop("from"))
+        data["to"] = Pos(**data.pop("to"))
+        return cls(**data)
+
+    def asdict(self):
+        data = asdict(self)
+        del data["from_"]
+        data["from"] = self.from_.astuple()
+        data["to"] = self.to.astuple()
+        return data
+
+
+class LintFailed(Exception):
+    pass
+
+
+class Lint:
+    STATUS_PROCESSING = "processing"
+    STATUS_FAILED = "failed"
+
+    def __init__(self, body, lint_id):
+        self.body = body
+        self.lint_id = lint_id
+        self.body = None
+        self.status = self.STATUS_PROCESSING
+        self.messages = []
+
+    def results(self):
+        while self.status == self.STATUS_PROCESSING:
+            time.sleep(0.5)
+            self.status, messages = lint_result(self.lint_id)
+            msgs = [Message.load(m) for m in messages]
+            self.messages = sorted(msgs, key=lambda m: (m.from_.line, m.from_.ch))
+
+        if self.status == self.STATUS_FAILED:
+            raise LintFailed
+
+        return self.messages
+
+    def __repr__(self):
+        return f"Lint({self.lint_id})"
+
+    @classmethod
+    def start(cls, body, is_html=False):
+        lint_id = lint_create(body, is_html)
+        return cls(body, lint_id)
