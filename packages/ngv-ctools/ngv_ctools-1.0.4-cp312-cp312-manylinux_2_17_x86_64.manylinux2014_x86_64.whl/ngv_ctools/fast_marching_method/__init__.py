ae657cac325a85e5983d09c229d71287cf4e6635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:07:57 +0000

    Add package ngv-ctools/1.0.4/ngv_ctools-1.0.4-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ngv-ctools/ngv_ctools-1.0.4-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ngv_ctools/fast_marching_method/__init__.py
@@ -0,0 +1,3 @@
+"""Module for propagating waves on 3D surface meshes using the fast marching method"""
+from .._ngv_ctools.fast_marching_method import second_order_solutions
+from .._ngv_ctools.fast_marching_method import grow_waves_on_triangulated_surface
