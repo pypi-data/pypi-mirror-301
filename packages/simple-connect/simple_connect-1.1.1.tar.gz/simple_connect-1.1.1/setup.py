5eb5371ed574402c7a250bfd267f1599c5734010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:05:12 +0000

    Add package simple-connect/1.1.1/simple_connect-1.1.1.tar.gz

---
+++ packages/simple-connect/simple_connect-1.1.1.tar.gz/simple_connect-1.1.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 01:46:03 2018
+
+@author: Saeed
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_connect",
+    version="1.1.1",
+    author="Saeed",
+    author_email="sae.ar2@gmail.com",
+    description="Simplify Connection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/saeed-abdul-rahim/simple_connect',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'httplib2', 'oauth2client',
+        'pandas', 'pymysql', 'sqlalchemy', 'sshtunnel',
+        'google-api-python-client', 'boto3', 'tqdm', 'paramiko'
+    ]
+)
