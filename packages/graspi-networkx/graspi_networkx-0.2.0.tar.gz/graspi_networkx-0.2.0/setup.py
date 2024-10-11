ad9d3077b14b0fecd6bdde83220213edf6d8a0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:04:17 +0000

    Add package graspi-networkx/0.2.0/graspi_networkx-0.2.0.tar.gz

---
+++ packages/graspi-networkx/graspi_networkx-0.2.0.tar.gz/graspi_networkx-0.2.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='graspi_networkx',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        'contourpy==1.1.0',
+        'cycler==0.12.1',
+        'fonttools==4.54.1',
+        'kiwisolver==1.4.7',
+        'matplotlib==3.7.5',
+        'networkx==3.1',
+        'numpy>=2.0.0,<2.1',
+        'packaging==24.1',
+        'pandas==2.2.3',
+        'pillow==10.4.0',
+        'pyparsing==3.1.4',
+        'PyQt6==6.7.1',
+        'PyQt6-Qt6==6.7.3',
+        'PyQt6_sip==13.8.0',
+        'python-dateutil==2.9.0.post0',
+        'pytz==2024.2',
+        'scipy>=1.9.0,<1.14',
+        'six==1.16.0',
+        'tzdata==2024.2'
+    ],
+    python_requires='>=3.6,<3.10',
+    author="Qi Pan",
+    author_email="mleung8@buffalo.edu",
+    description="A package that utilizes NetworkX functionality for GraSPI",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/gobrin111/graspi_networkx",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta"
+    ],
+)
