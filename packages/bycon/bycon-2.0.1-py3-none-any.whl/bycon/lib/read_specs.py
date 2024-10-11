ee007d43638dcad4b0188d35b94be1ea9acd5e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 14:05:21 +0000

    Add package bycon/2.0.1/bycon-2.0.1-py3-none-any.whl

---
+++ packages/bycon/bycon-2.0.1-py3-none-any.whl/bycon/lib/read_specs.py
@@ -0,0 +1,126 @@
+import inspect, json, re, yaml
+from deepmerge import always_merger
+from json_ref_dict import RefDict, materialize
+from os import path, pardir, scandir, environ
+from pathlib import Path
+from pymongo import MongoClient
+
+from cgi_parsing import prdbug
+from config import *
+
+################################################################################
+
+def read_service_definition_files():
+    """podmd
+    Reading the config from the same wrapper dir:
+    module
+      |
+      |- lib - read_specs.py
+      |- definitions - __name__.yaml
+    podmd"""
+    conf_dir = path.join(PKG_PATH, "definitions")
+    if not path.isdir(conf_dir):
+        return
+    b_d_fs = [ f.name for f in scandir(conf_dir) if f.is_file() ]
+    b_d_fs = [ f for f in b_d_fs if f.endswith("yaml") ]
+    b_d_fs = [ Path(f).stem for f in b_d_fs ]
+
+    for d in b_d_fs:
+        o = {}
+        ofp = path.join( conf_dir, f'{d}.yaml' )
+        with open( ofp ) as od:
+            o = yaml.load( od , Loader=yaml.FullLoader)
+        BYC.update({d: o})
+
+
+################################################################################
+
+def update_rootpars_from_local_or_HOST():
+    # avoiding re-parsing of directories, e.g. during init stage
+    p_c_p = BYC.get("parsed_config_paths", [])
+    if LOC_PATH in p_c_p:
+        return
+
+    p_c_p.append(LOC_PATH)
+    s_f = path.join(LOC_PATH, 'services_entity_defaults.yaml')
+    s = load_yaml_empty_fallback(s_f)
+    BYC.update({"entity_defaults": always_merger.merge(BYC.get("entity_defaults", {}), s)})
+
+    # overwriting installation-wide defaults with instance-specific ones
+    # _i.e._ matching the current domain (to allow presentation of different
+    # Beacon instances from the same server)
+    i_ovr_f = path.join(LOC_PATH, "instance_overrides.yaml")
+    i_ovr = load_yaml_empty_fallback(i_ovr_f)
+
+    if "local" in i_ovr:
+        i_o_bdfs = i_ovr["local"].get("beacon_defaults", {})
+        i_o_edfs = i_ovr["local"].get("entity_defaults", {})
+        BYC.update({"beacon_defaults": always_merger.merge(BYC.get("beacon_defaults", {}), i_o_bdfs)})
+        BYC.update({"entity_defaults": always_merger.merge(BYC.get("entity_defaults", {}), i_o_edfs)})
+    if not "local" in ENV:
+        instance = "___none___"
+        host = environ.get("HTTP_HOST", "local")
+        for i_k, i_v in i_ovr.items():
+            doms = i_v.get("domains", [])
+            if host in doms:
+                instance = i_k
+                break
+        if instance in i_ovr:
+            i_o_bdfs = i_ovr[instance].get("beacon_defaults", {})
+            i_o_edfs = i_ovr[instance].get("entity_defaults", {})
+            BYC.update({"beacon_defaults": always_merger.merge(BYC.get("beacon_defaults", {}), i_o_bdfs)})
+            BYC.update({"entity_defaults": always_merger.merge(BYC.get("entity_defaults", {}), i_o_edfs)})
+
+    # TODO: better way to define which files are parsed from local
+    for p in BYC.get("loc_mod_pars", []):
+        f = path.join(LOC_PATH, f'{p}.yaml')
+        d = load_yaml_empty_fallback(f)
+        prdbug(f'...LOC_PATH file => {p}')
+        BYC.update({p: always_merger.merge(BYC.get(p, {}), d)})
+
+    return
+
+
+################################################################################
+
+def dbstats_return_latest():
+    # TODO: This is too hacky & should be moved to an external function
+    # which updates the database_definitions / beacon_info yamls...
+    stats = MongoClient(host=DB_MONGOHOST)[HOUSEKEEPING_DB][HOUSEKEEPING_INFO_COLL].find( { }, { "_id": 0 } ).sort( {"date": -1} ).limit( 1 )
+    stats = list(stats)
+    if len(stats) > 0:
+        return stats[0]
+    else:
+        return []
+
+
+################################################################################
+
+def datasets_update_latest_stats():
+    results = []
+    stat = dbstats_return_latest()
+    for coll_id, coll in BYC["dataset_definitions"].items():
+        if not coll_id in BYC.get("BYC_DATASET_IDS", []):
+            continue
+        if "datasets" in stat:
+            if (ds_vs := stat[ "datasets" ].get(coll_id)):
+                if "filtering_terms" in BYC["response_entity_id"]:
+                    coll.update({ "filtering_terms": ds_vs.get("filtering_terms", []) } )
+        # TODO: Add stats ...
+        results.append(coll)
+    return results
+
+
+################################################################################
+
+def load_yaml_empty_fallback(yp):
+    y = {}
+    try:
+        # print(yp)
+        with open( yp ) as yd:
+            y = yaml.load( yd , Loader=yaml.FullLoader)
+    except Exception as e:
+        # print(e)
+        pass
+    return y
+
---
+++ packages/bycon/bycon-2.0.1-py3-none-any.whl/bycon/lib/service_utils.py
@@ -0,0 +1,123 @@
+import inspect
+import re
+from deepmerge import always_merger
+from os import environ, path
+
+from args_parsing import *
+from config import *
+from beacon_auth import *
+from bycon_helpers import return_paginated_list, prdbug, prdbughead, set_debug_state, test_truthy
+from dataset_parsing import select_dataset_ids
+from parse_filters_request import parse_filters
+
+
+################################################################################
+
+def set_beacon_defaults():
+    defaults: object = BYC["beacon_defaults"].get("defaults", {})
+    for d_k, d_v in defaults.items():
+        BYC.update( { d_k: d_v } )  
+
+
+################################################################################
+
+def initialize_bycon_service():
+    update_requested_schema_from_request()
+    set_special_modes()
+    select_dataset_ids()
+    prdbug(f'initialize_bycon_service - ds_id: {BYC["BYC_DATASET_IDS"]}')
+    set_user_name()
+    set_returned_granularities()    
+    parse_filters()
+
+
+################################################################################
+
+def set_special_modes():
+    if "test_mode" in BYC_PARS:
+        BYC.update({"TEST_MODE": test_truthy(BYC_PARS["test_mode"])})
+    if BYC.get("TEST_MODE") is True:
+        BYC_PARS.update({"include_handovers": True})
+
+
+################################################################################
+
+def set_entities():
+    """
+    This function evaluates the definitions for the entities and their selection
+    by path elements (including aliases) or parameters and updates the global
+    BYC definitions.
+    """
+    b_e_d = BYC.get("entity_defaults", {})
+
+    dealiased_path_ids = {}
+    for e, e_d in b_e_d.items():
+        p_id = e_d.get("request_entity_path_id")
+        dealiased_path_ids.update({p_id: e})
+        for p_a_s in e_d.get("request_entity_path_aliases", []):
+            dealiased_path_ids.update({p_a_s: e})
+
+    # this allows to override the path values with parameters
+    # it should only apply to special cases (e.g. overriding the standard
+    # biosample table export in services with individuals) or for command
+    # line testing
+    if (e_p_id := BYC_PARS.get("request_entity_path_id", "___none___")) in dealiased_path_ids.keys():
+        BYC.update({"request_entity_path_id": e_p_id})
+    if (e_p_id := BYC_PARS.get("response_entity_path_id", "___none___")) in dealiased_path_ids.keys():
+        BYC.update({"response_entity_path_id": e_p_id})
+
+    p_i_d = BYC.get("request_entity_path_id", "___none___")
+    if p_i_d not in dealiased_path_ids.keys():
+        p_i_d = "info"
+    rp_i_d = BYC.get("response_entity_path_id", "___none___")
+    if rp_i_d not in dealiased_path_ids.keys():
+        rp_i_d = p_i_d
+
+    # after settling the paths we can get the entity ids
+    q_id = dealiased_path_ids[p_i_d]
+    r_id = dealiased_path_ids[rp_i_d]
+
+    rq_d = b_e_d.get(q_id)
+    rp_d = b_e_d.get(r_id)
+
+    prdbug(f'rq_d: {rq_d}')
+    prdbug(f'rp_d: {rp_d}')
+
+    p_p = rq_d.get("path_id_value_bycon_parameter", "id")
+
+    BYC.update({
+        "path_id_value_bycon_parameter": p_p,
+        "request_entity_id": rq_d.get("request_entity_id", q_id),
+        "request_entity_path_id": p_i_d,
+        "response_entity_id": rp_d.get("response_entity_id", q_id),
+        "response_entity_path_id": rp_i_d,
+        "response_entity": rp_d,
+        "response_schema": rp_d.get("response_schema", "beaconInfoResponse"),
+        "bycon_response_class": rp_d.get("bycon_response_class", "BeaconInfoResponse")
+    })
+
+    if (rpidv := BYC.get("request_entity_path_id_value")):
+        BYC_PARS.update({p_p: rpidv})
+
+
+################################################################################
+
+def update_requested_schema_from_request():
+    # query_meta may come from the meta in a POST
+    b_qm = BYC.get("query_meta", {})
+
+    # TODO: check if correct schema in request
+    if "requested_schema" in BYC_PARS:
+        BYC.update({"response_entity_id": BYC_PARS.get("requested_schema", BYC["response_entity_id"])})
+    elif "requested_schemas" in b_qm:
+        BYC.update({"response_entity_id": b_qm["requested_schemas"][0].get("entity_type", BYC["response_entity_id"])})
+
+
+################################################################################
+
+def set_response_schema():
+    r_e = BYC.get("response_entity", {})
+    r_s = r_e.get("response_schema", "beaconInfoResponse")
+    BYC.update({"response_schema": r_s})
+
+
