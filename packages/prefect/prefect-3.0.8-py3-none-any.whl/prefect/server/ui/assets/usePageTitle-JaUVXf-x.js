889850853059377955d1f3174b1e2c03027530ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:03:54 +0000

    Add package prefect/3.0.8/prefect-3.0.8-py3-none-any.whl

---
+++ packages/prefect/prefect-3.0.8-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-JaUVXf-x.js
@@ -0,0 +1,2 @@
+import{i as a,bA as c,m as i}from"./index-DGGjalVM.js";function u(...t){const r=[...t,"Prefect Server"],o=a(()=>r.map(e=>i(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=o.value)}export{u};
+//# sourceMappingURL=usePageTitle-JaUVXf-x.js.map
