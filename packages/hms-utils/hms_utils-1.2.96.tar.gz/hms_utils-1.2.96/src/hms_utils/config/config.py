7dd65bd690130453a3154bec00308edb318ad473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:03:53 +0000

    Add package hms-utils/1.2.96/hms_utils-1.2.96.tar.gz

---
+++ packages/hms-utils/hms_utils-1.2.96.tar.gz/hms_utils-1.2.96/src/hms_utils/config/config.py
@@ -0,0 +1,31 @@
+from typing import Callable, Optional, Union
+from hms_utils.config.config_with_secrets import ConfigWithSecrets
+from hms_utils.config.config_with_aws_macros import ConfigWithAwsMacros
+from hms_utils.dictionary_parented import DictionaryParented as JSON
+
+
+class Config(ConfigWithSecrets, ConfigWithAwsMacros):
+
+    def __init__(self, config: JSON,
+                 name: Optional[str] = None,
+                 path_separator: Optional[str] = None,
+                 custom_macro_lookup: Optional[Callable] = None,
+                 warning: Optional[Union[Callable, bool]] = None,
+                 raise_exception: bool = False,
+                 secrets: bool = False,
+                 noaws: bool = False, **kwargs) -> None:
+
+        super().__init__(config=config,
+                         name=name,
+                         path_separator=path_separator,
+                         custom_macro_lookup=custom_macro_lookup,
+                         warning=warning,
+                         raise_exception=raise_exception,
+                         secrets=secrets,
+                         noaws=noaws, **kwargs)
+
+    @staticmethod
+    def default():
+        config = Config("~/.config/hms/config.json")
+        config.merge(Config("~/.config/hms/secrets.json"))
+        return config
