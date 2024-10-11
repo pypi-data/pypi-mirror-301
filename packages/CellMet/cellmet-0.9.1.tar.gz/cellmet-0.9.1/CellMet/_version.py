89d6f4b00e3bfaa7a13318e451318de9a3a75e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 16:05:17 +0000

    Add package CellMet/0.9.1/cellmet-0.9.1.tar.gz

---
+++ packages/CellMet/cellmet-0.9.1.tar.gz/cellmet-0.9.1/CellMet/_version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CellMet SETUP.PY
+#
+short_version = '0.9.1'
+version = '0.9.1'
+full_version = '0.9.1.dev0+0c83190'
+git_revision = '0c831902c22ccaba1ecec187426ef8ffdb4bff42'
+release = False
+if not release:
+    version = full_version
