57dba5fa21c05f164eb1e19c565abb6f1e802859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 05:05:20 +0000

    Add package picows/1.5.0/picows-1.5.0-cp39-cp39-win_amd64.whl

---
+++ packages/picows/picows-1.5.0-cp39-cp39-win_amd64.whl/picows/__init__.py
@@ -0,0 +1,29 @@
+from .picows import (
+    WSError,
+    WSMsgType,
+    WSCloseCode,
+    WSFrame,
+    WSTransport,
+    WSListener,
+    WSUpgradeRequest,
+    ws_connect,
+    ws_create_server,
+    PICOWS_DEBUG_LL
+)
+
+
+__all__ = [
+    'WSError',
+    'WSMsgType',
+    'WSCloseCode',
+    'WSFrame',
+    'WSTransport',
+    'WSListener',
+    'WSUpgradeRequest',
+    'ws_connect',
+    'ws_create_server',
+    'PICOWS_DEBUG_LL'
+]
+
+__version__ = "1.5.0"
+__author__ = "Taras Kozlov"
