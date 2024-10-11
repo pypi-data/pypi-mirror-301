84ef7b69a1098e66e179e43bda03346c1410fd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:08:27 +0000

    Add package Country-Converter-Toolkit/0.0.1/Country_Converter_Toolkit-0.0.1-py3-none-any.whl

---
+++ packages/Country-Converter-Toolkit/Country_Converter_Toolkit-0.0.1-py3-none-any.whl/Modul/Modul5.py
@@ -0,0 +1,199 @@
+exchange_rates = {
+    'AFN': 77.0,  # Afghanistan
+    'ALL': 120.0,  # Albania
+    'DZD': 120.0,  # Algeria
+    'ADP': 1.2,  # Andorra
+    'AOA': 350.0,  # Angola
+    'XCD': 2.7,  # Antigua and Barbuda
+    'ARS': 60.0,  # Argentina
+    'AMD': 500.0,  # Armenia
+    'AUD': 1.48,  # Australia
+    'EUR': 0.84,  # Austria
+    'AZN': 1.7,  # Azerbaijan
+    'BSD': 1.0,  # Bahamas
+    'BHD': 0.38,  # Bahrain
+    'BDT': 80.0,  # Bangladesh
+    'BBD': 2.0,  # Barbados
+    'BYN': 2.5,  # Belarus
+    'EUR': 0.84,  # Belgium
+    'BZD': 2.0,  # Belize
+    'XOF': 650.0,  # Benin
+    'BTN': 70.0,  # Bhutan
+    'BOB': 6.9,  # Bolivia
+    'BAM': 1.8,  # Bosnia and Herzegovina
+    'BWP': 11.0,  # Botswana
+    'BRL': 4.5,  # Brazil
+    'BND': 1.4,  # Brunei
+    'BGN': 1.8,  # Bulgaria
+    'XOF': 650.0,  # Burkina Faso
+    'BIF': 1800.0,  # Burundi
+    'CVE': 110.0,  # Cape Verde
+    'KHR': 4000.0,  # Cambodia
+    'XAF': 650.0,  # Cameroon
+    'CAD': 1.32,  # Canada
+    'XAF': 650.0,  # Central African Republic
+    'XAF': 650.0,  # Chad
+    'CLP': 800.0,  # Chile
+    'CNY': 6.9,  # China
+    'COP': 3500.0,  # Colombia
+    'KMF': 450.0,  # Comoros
+    'CDF': 1800.0,  # Republic of the Congo
+    'CDF': 1800.0,  # Democratic Republic of the Congo
+    'CRC': 600.0,  # Costa Rica
+    'HRK': 7.5,  # Croatia
+    'CUP': 25.0,  # Cuba
+    'EUR': 0.84,  # Cyprus
+    'CZK': 25.0,  # Czech Republic
+    'DKK': 6.6,  # Denmark
+    'DJF': 180.0,  # Djibouti
+    'XCD': 2.7,  # Dominica
+    'DOP': 50.0,  # Dominican Republic
+    'USD': 1.0,  # Ecuador
+    'EGP': 16.0,  # Egypt
+    'SVC': 8.7,  # El Salvador
+    'XAF': 650.0,  # Equatorial Guinea
+    'ERN': 15.0,  # Eritrea
+    'EUR': 0.84,  # Estonia
+    'SZL': 15.0,  # Eswatini
+    'ETB': 30.0,  # Ethiopia
+    'FJD': 2.2,  # Fiji
+    'EUR': 0.84,  # Finland
+    'EUR': 0.84,  # France
+    'XAF': 650.0,  # Gabon
+    'GMD': 50.0,  # Gambia
+    'GEL': 3.0,  # Georgia
+    'EUR': 0.84,  # Germany
+    'GHS': 5.5,  # Ghana
+    'EUR': 0.84,  # Greece
+    'XCD': 2.7,  # Grenada
+    'GTQ': 7.5,  # Guatemala
+    'GNF': 9000.0,  # Guinea
+    'XOF': 650.0,  # Guinea-Bissau
+    'GYD': 200.0,  # Guyana
+    'HTG': 50.0,  # Haiti
+    'HNL': 24.0,  # Honduras
+    'HUF': 300.0,  # Hungary
+    'ISK': 130.0,  # Iceland
+    'INR': 70.0,  # India
+    'IDR': 14245.0,  # Indonesia
+    'IRR': 120000.0,  # Iran
+    'IQD': 1200.0,  # Iraq
+    'EUR': 0.84,  # Ireland
+    'EUR': 0.84,  # Italy
+    'JMD': 130.0,  # Jamaica
+    'JPY': 109.0,  # Japan
+    'JOD': 0.7,  # Jordan
+    'KZT': 400.0,  # Kazakhstan
+    'KES': 100.0,  # Kenya
+    'AUD': 1.48,  # Kiribati
+    'KPW': 900.0,  # North Korea
+    'KRW': 1200.0,  # South Korea
+    'KWD': 0.3,  # Kuwait
+    'KGS': 70.0,  # Kyrgyzstan
+    'LAK': 9000.0,  # Laos
+    'EUR': 0.84,  # Latvia
+    'LBP': 1500.0,  # Lebanon
+    'LSL': 15.0,  # Lesotho
+    'LRD': 150.0,  # Liberia
+    'LYD': 4.5,  # Libya
+    'CHF': 0.92,  # Liechtenstein
+    'EUR': 0.84,  # Lithuania
+    'EUR': 0.84,  # Luxembourg
+    'MGA': 3500.0,  # Madagascar
+    'MWK': 750.0,  # Malawi
+    'MYR': 4.2,  # Malaysia
+    'MVR': 15.0,  # Maldives
+    'XOF': 650.0,  # Mali
+    'EUR': 0.84,  # Malta
+    'USD': 1.0,  # Marshall Islands
+    'MRO': 350.0,  # Mauritania
+    'MUR': 35.0,  # Mauritius
+    'MXN': 20.0,  # Mexico
+    'USD': 1.0,  # Micronesia
+    'MDL': 20.0,  # Moldova
+    'EUR': 0.84,  # Monaco
+    'MNT': 2800.0,  # Mongolia
+    'EUR': 0.84,  # Montenegro
+    'MAD': 10.0,  # Morocco
+    'MZN': 60.0,  # Mozambique
+    'MMK': 1500.0,  # Myanmar
+    'NAD': 15.0,  # Namibia
+    'AUD': 1.48,  # Nauru
+    'NPR': 110.0,  # Nepal
+    'EUR': 0.84,  # Netherlands
+    'NZD': 1.5,  # New Zealand
+    'NIO': 30.0,  # Nicaragua
+    'XOF': 650.0,  # Niger
+    'NGN': 350.0,  # Nigeria
+    'MKD': 55.0,  # North Macedonia
+    'NOK': 9.5,  # Norway
+    'OMR': 0.38,  # Oman
+    'PKR': 150.0,  # Pakistan
+    'USD': 1.0,  # Palau
+    'ILS': 3.5,  # Palestina
+    'PAB': 1.0,  # Panama
+    'PGK': 3.5,  # Papua New Guinea
+    'PYG': 6000.0,  # Paraguay
+    'PEN': 3.5,  # Peru
+    'PHP': 50.0,  # Philippines
+    'PLN': 4.5,  # Poland
+    'EUR': 0.84,  # Portugal
+    'QAR': 3.6,  # Qatar
+    'RON': 4.5,  # Romania
+    'RUB': 70.0,  # Russia
+    'RWF': 900.0,  # Rwanda
+    'WST': 2.5,  # Samoa
+    'SAR': 3.8,  # Saudi Arabia
+    'XOF': 650.0,  # Senegal
+    'RSD': 110.0,  # Serbia
+    'SCR': 15.0,  # Seychelles
+    'SLL': 9000.0,  # Sierra Leone
+    'SGD': 1.4,  # Singapore
+    'EUR': 0.84,  # Slovakia
+    'EUR': 0.84,  # Slovenia
+    'SOS': 550.0,  # Somalia
+    'EUR': 0.84,  # Spain
+    'LKR': 180.0,  # Sri Lanka
+    'SDG': 45.0,  # Sudan
+    'SEK': 10.0,  # Sweden
+    'CHF': 0.92,  # Switzerland
+    'SYP': 500.0,  # Syria
+    'TWD': 30.0,  # Taiwan
+    'TJS': 10.0,  # Tajikistan
+    'TZS': 2300.0,  # Tanzania
+    'THB': 30.0,  # Thailand
+    'XOF': 650.0,  # Togo
+    'TTD': 6.7,  # Trinidad and Tobago
+    'TND': 2.8,  # Tunisia
+    'TRY': 6.5,  # Turkey
+    'TMT': 3.5,  # Turkmenistan
+    'AUD': 1.48,  # Tuvalu
+    'UGX': 3500.0,  # Uganda
+    'UAH': 25.0,  # Ukraine
+    'AED': 3.7,  # United Arab Emirates
+    'USD': 1.0,  # United States
+    'GBP': 0.76,  # United Kingdom
+    'UYU': 40.0,  # Uruguay
+    'UZS': 9000.0,  # Uzbekistan
+    'VUV': 110.0,  # Vanuatu
+    'VEF': 3000.0,  # Venezuela
+    'VND': 23000.0,  # Vietnam
+    'YER': 250.0,  # Yemen
+    'ZMW': 15.0,  # Zambia
+    'ZWL': 350.0  # Zimbabwe
+}
+
+def currency_conversion(amount, from_currency, to_currency):
+    if from_currency not in exchange_rates or to_currency not in exchange_rates:
+        return "Currency not recognized"
+    
+    from_rate = exchange_rates[from_currency]
+    to_rate = exchange_rates[to_currency]
+    
+    converted_amount = amount * (to_rate / from_rate)
+    
+    if converted_amount < 1:
+        result = converted_amount
+    else:
+        result = "{:,.2f}".format(converted_amount).replace(",", ".")
+    return result 
