ea25c4731ab6d86a76b9e1bb801f73416f4322fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 07:04:46 +0000

    Add package edsnlp/0.13.1/edsnlp-0.13.1-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/edsnlp/edsnlp-0.13.1-cp38-cp38-macosx_11_0_arm64.whl/edsnlp/utils/typing.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, Any, Generic, List, TypeVar, Union
+
+import pydantic
+from confit.errors import patch_errors
+
+T = TypeVar("T")
+
+
+class MetaAsList(type):
+    def __init__(cls, name, bases, dct):
+        super().__init__(name, bases, dct)
+        cls.item = Any
+
+    def __getitem__(self, item):
+        new_type = MetaAsList(self.__name__, (self,), {})
+        new_type.item = item
+        return new_type
+
+    def validate(cls, value, config=None):
+        if isinstance(value, dict):
+            value = [value]
+        if not isinstance(value, list):
+            value = [value]
+        try:
+            return pydantic.parse_obj_as(List[cls.item], value)
+        except pydantic.ValidationError as e:
+            e = patch_errors(e, drop_names=("__root__",))
+            e.model = cls
+            raise e
+
+    def __get_validators__(cls):
+        yield cls.validate
+
+
+class AsList(Generic[T], metaclass=MetaAsList):
+    pass
+
+
+if TYPE_CHECKING:
+    AsList = Union[T, List[T]]  # noqa: F811
