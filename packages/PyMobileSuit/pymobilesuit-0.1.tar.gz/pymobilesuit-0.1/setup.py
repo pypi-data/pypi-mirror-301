472048e529ba2a336a04c8d02c57c2dccb81d9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 03:07:14 +0000

    Add package PyMobileSuit/0.1/pymobilesuit-0.1.tar.gz

---
+++ packages/PyMobileSuit/pymobilesuit-0.1.tar.gz/pymobilesuit-0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File    :   setup.py
+@Contact :   ferdinandsukhoi@outlook.com
+@License :   (C)Copyright 2020 HIT-ReFreSH
+
+@Modify Time      @Author    @Version    @Description
+------------      -------    --------    -----------
+2020-5-15   Ferdinand Sukhoi      0.1.4   Light-weight tool to generate markdown-formatted lab reports.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="PyMobileSuit",
+    version="0.1",
+    keywords=["commandline", "cli", "framework", "HIT-ReFreSH"],
+    description="To generate markdown-formatted lab reports.",
+    long_description="MobileSuit provides an easy way to build Console App quickly. For python.",
+    license="MIT",
+
+    url="https://github.com/HIT-ReFreSH/PyMobileSuit",
+    author="Ferdinand Sukhoi",
+    author_email="ferdinandsukhoi@outlook.com",
+
+    packages=find_packages(),
+    package_data={
+        '':[
+            "*Resources/*.yaml",
+        ]
+    },
+    include_package_data=True,
+    platforms="any",
+    python_requires='>=3.9',
+    install_requires=["colour"]
+)
