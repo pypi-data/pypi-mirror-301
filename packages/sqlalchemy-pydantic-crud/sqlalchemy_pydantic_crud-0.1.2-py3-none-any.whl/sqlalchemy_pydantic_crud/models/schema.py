025ea906214b6a3deb0a7b111e11cb163dc57f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 06:04:23 +0000

    Add package sqlalchemy-pydantic-crud/0.1.2/sqlalchemy_pydantic_crud-0.1.2-py3-none-any.whl

---
+++ packages/sqlalchemy-pydantic-crud/sqlalchemy_pydantic_crud-0.1.2-py3-none-any.whl/sqlalchemy_pydantic_crud/models/schema.py
@@ -0,0 +1,11 @@
+from typing import TypeVar
+
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True, extra="ignore")
+
+
+
+BaseSchemaType = TypeVar("BaseSchemaType", bound=BaseSchema)
