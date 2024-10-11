e76c316635821a7063c4dd9907285c6a3ffa1f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-11 15:04:19 +0000

    Add package tealogger/0.8.9/tealogger-0.8.9-py2.py3-none-any.whl

---
+++ packages/tealogger/tealogger-0.8.9-py2.py3-none-any.whl/tealogger/handler.py
@@ -0,0 +1,12 @@
+"""
+Handler Module
+~~~~~~~~~~~~~~
+
+The module implements the handler functionality of the Tea Logger.
+"""
+
+import logging
+
+
+class StandardSteamHandler(logging.StreamHandler):
+    """Standard Steam Handler"""
