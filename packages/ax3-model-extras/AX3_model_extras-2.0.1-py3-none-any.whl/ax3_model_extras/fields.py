2170fcd729775f0cea4c651bbb2f22ae69c94ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 06:04:52 +0000

    Add package ax3-model-extras/2.0.1/AX3_model_extras-2.0.1-py3-none-any.whl

---
+++ packages/ax3-model-extras/AX3_model_extras-2.0.1-py3-none-any.whl/ax3_model_extras/fields.py
@@ -0,0 +1,94 @@
+from io import BytesIO
+
+from django.core.exceptions import ValidationError
+from django.db.models import ImageField
+from django.utils.translation import gettext_lazy as _
+from PIL import Image
+from resizeimage import resizeimage
+from resizeimage.imageexceptions import ImageSizeError
+
+
+class OptimizedImageField(ImageField):
+    def __init__(self, *args, **kwargs):
+        """
+        `optimized_image_output_size` must be a tuple and `optimized_image_resize_method` can be
+        'crop', 'cover', 'contain', 'width', 'height' or 'thumbnail'.
+        """
+        self.optimized_image_output_size = kwargs.pop('optimized_image_output_size', None)
+        self.optimized_image_resize_method = kwargs.pop('optimized_image_resize_method', 'cover')
+        self.optimized_file_formats = kwargs.pop('optimized_file_formats', ['JPEG', 'PNG', 'GIF'])
+        self.optimized_image_quality = kwargs.pop('optimized_image_quality', 75)
+
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+
+        # Remove the arguments from the kwargs dict.
+        kwargs.pop('optimized_image_output_size', None)
+        kwargs.pop('optimized_image_resize_method', None)
+        kwargs.pop('optimized_file_formats', None)
+        kwargs.pop('optimized_image_quality', None)
+
+        return name, path, args, kwargs
+
+    def save_form_data(self, instance, data):
+        updating_image = data and getattr(instance, self.name) != data
+        if updating_image:
+
+            if self.optimized_image_quality < 0 or self.optimized_image_quality > 100:
+                raise ValidationError({self.name: [_('The allowed quality is from 0 to 100')]})
+
+            try:
+                data = self.optimize_image(
+                    image_data=data,
+                    output_size=self.optimized_image_output_size,
+                    resize_method=self.optimized_image_resize_method,
+                    quality=self.optimized_image_quality,
+                )
+            except ImageSizeError:
+                raise ValidationError({self.name: [_('Image too small to be scaled')]})
+            except OSError:
+                raise ValidationError({self.name: [_('The image is invalid or corrupted')]})
+
+        super().save_form_data(instance, data)
+
+    def optimize_image(self, image_data, output_size, resize_method, quality):
+        """Optimize an image that has not been saved to a file."""
+        img = Image.open(image_data)
+
+        if img.format not in self.optimized_file_formats:
+            raise ValidationError({self.name: [_('Image format unsupported')]})
+
+        # GIF files needs strict size validation
+        if img.format == 'GIF' and output_size and output_size != (img.width, img.height):
+            raise ValidationError({self.name: [_('GIF image size unsupported')]})
+
+        # Check if is a supported format for optimization
+        if img.format not in ['JPEG', 'PNG']:
+            return image_data
+
+        # If output_size content 0.
+        if output_size and output_size[0] == 0:
+            output_size = (img.width, output_size[1])
+        elif output_size and output_size[1] == 0:
+            output_size = (output_size[0], img.height)
+
+        # If output_size is set, resize the image with the selected resize_method.
+        if output_size and output_size != (img.width, img.height):
+            output_image = resizeimage.resize(resize_method, img, output_size)
+        else:
+            output_image = img
+
+        # If the file extension is JPEG, convert the output_image to RGB
+        if img.format == 'JPEG':
+            output_image = output_image.convert('RGB')
+
+        bytes_io = BytesIO()
+        output_image.save(bytes_io, format=img.format, optimize=True, quality=quality)
+
+        image_data.seek(0)
+        image_data.file.write(bytes_io.getvalue())
+        image_data.file.truncate()
+
+        return image_data
---
+++ packages/ax3-model-extras/AX3_model_extras-2.0.1-py3-none-any.whl/ax3_model_extras/validators.py
@@ -0,0 +1,69 @@
+from django.core.validators import BaseValidator
+from django.utils.translation import gettext_lazy as _
+
+
+class FileSizeValidator(BaseValidator):
+    """Validate a file size. Size must be given in KB.
+    """
+    message = _('The weight of the file must be less than the maximum allowed size')
+    code = 'file_size'
+
+    def compare(self, a, b):
+        return a.size > 1024 * b
+
+
+class ImageDimensionValidator(BaseValidator):
+    """Validate an image dimensions. Should receive a tuple like (width, height).
+    """
+    message = _('The image must have the specified dimensions')
+    code = 'file_dimension'
+
+    def compare(self, a, b):
+        width, height = b
+        if width == 0:
+            return a.height != height
+
+        if height == 0:
+            return a.width != width
+
+        return a.width != width or a.height != height
+
+
+class ImageMinDimensionValidator(BaseValidator):
+    """Validate an image dimensions. Should receive a tuple like (width, height).
+    """
+    message = _('The image must have at least the specified dimensions')
+    code = 'file_dimension'
+
+    def compare(self, a, b):
+        width, height = b
+        if width == 0:
+            return a.height < height
+
+        if height == 0:
+            return a.width < width
+
+        return a.width < width or a.height < height
+
+
+class MimetypeValidator(BaseValidator):
+    """
+    Validate a file mimetype.
+    """
+    message = _('The document does not have the correct format')
+    code = 'file_mimetype'
+
+    def compare(self, a, b):
+        try:
+            import magic
+        except ImportError as e:
+            raise ImportError(
+                "The 'python-magic' library is required for this functionality. "
+                "Additionally, ensure that the 'libmagic' library is installed on your system. "
+            ) from e
+
+        try:
+            mime = magic.from_buffer(a.open().read(2048), mime=True)
+            return mime not in b
+        except AttributeError:
+            return True
