107d62b70d0c17a89bd1a7aeda1032c94c998d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:23 +0000

    Add package csiread/1.4.1/csiread-1.4.1-cp39-cp39-win_amd64.whl

---
+++ packages/csiread/csiread-1.4.1-cp39-cp39-win_amd64.whl/csiread/__init__.py
@@ -0,0 +1,7 @@
+from .core import (Intel, Atheros, Nexmon, AtherosPull10, NexmonPull46,
+				   NexmonPull256, ESP32, Picoscenes)
+from .utils import scidx, calib, phy_ifft, phy_fft
+
+
+__version__ = "1.4.1"
+__version_picoscenes__ = "2022-01-21"
