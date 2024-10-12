1ea652d9b3811091f068387b2b5cf9c4f5cd93f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:06:51 +0000

    Add package hydrogenpay-python/1.0.0/hydrogenpay_python-1.0.0-py3-none-any.whl

---
+++ packages/hydrogenpay-python/hydrogenpay_python-1.0.0-py3-none-any.whl/hydrogenpay_python/confirmpayment.py
@@ -0,0 +1,36 @@
+import json
+import requests
+from hydrogenpay_python.base import HydrogenpayBase
+from hydrogenpay_python.exceptions import ServerError
+
+
+class ConfirmPayment(HydrogenpayBase):
+    def __init__(self, sandboxKey, liveKey, mode, setEnv):
+        super(
+            ConfirmPayment,
+            self).__init__(
+            sandboxKey,
+            liveKey,
+            mode,
+            setEnv)
+
+    def _preliminaryResponseChecks(self, response, TypeOfErrorToRaise, name):
+        try:
+            responseJson = response.json()
+        except BaseException:
+            raise ServerError(
+                {"error": True, "name": name, "errMsg": response})
+
+        # check for data parameter in response
+        if not responseJson.get("data", None):
+            raise TypeOfErrorToRaise({"error": True,
+                                      "name": name,
+                                      "errMsg": responseJson.get("message",
+                                                                 "Server is down")})
+
+        # check for 200 response
+        if not response.ok:
+            errMsg = response["data"].get("message", None)
+            raise TypeOfErrorToRaise({"error": True, "errMsg": errMsg})
+
+        return responseJson
