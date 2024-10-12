bee6420242bbf49469ec6d09693be8e3ab7d7d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:36 +0000

    Add package spxr/0.1.15/spxr-0.1.15.tar.gz

---
+++ packages/spxr/spxr-0.1.15.tar.gz/spxr-0.1.15/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spxr',
+    version='0.1.15',
+    author='Areg Sargsyan',
+    author_email='quant0027@gmail.com',
+    description='Custom functions for repetitive tasks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AregSP/spxr',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
