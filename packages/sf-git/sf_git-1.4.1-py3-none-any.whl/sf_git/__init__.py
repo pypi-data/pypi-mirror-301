9f80f2c4fe7215ea4f067169a912d22c386064a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:20 +0000

    Add package sf-git/1.4.1/sf_git-1.4.1-py3-none-any.whl

---
+++ packages/sf-git/sf_git-1.4.1-py3-none-any.whl/sf_git/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+__version__ = "1.4.1"
+
+HERE = Path(__file__).parent
+DOTENV_PATH: Path = HERE / "sf_git.conf"
+
+if DOTENV_PATH.is_file():
+    print(f"loading dotenv from {DOTENV_PATH}")
+    load_dotenv(dotenv_path=DOTENV_PATH)
