1c0bbfcc0b87d52fdc20cc7b571efbe8c6017a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:20 +0000

    Add package stringtoolsfpat/0.0.67/stringtoolsfpat-0.0.67.tar.gz

---
+++ packages/stringtoolsfpat/stringtoolsfpat-0.0.67.tar.gz/stringtoolsfpat-0.0.67/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
+
+setup(
+    name="stringtoolsfpat",
+    version="0.0.67",
+    packages=find_packages(),
+    description="String tools",
+    long_description=README_MD,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fptiangco/stringtoolsfpat",
+    download_url="https://github.com/fptiangco/stringtoolsfpat",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/fptiangco/stringtoolsfpat/issues",
+        "Source": "https://github.com/fptiangco/stringtoolsfpat/",
+    },
+    keywords="stringtoolsfpat",
+)
