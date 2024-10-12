188fb3ab00b41681a97c3f8676dfd31a9d4d89e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:12 +0000

    Add package stringtoolsfpat/0.0.66/stringtoolsfpat-0.0.66-py3-none-any.whl

---
+++ packages/stringtoolsfpat/stringtoolsfpat-0.0.66-py3-none-any.whl/stringtoolsfpat/__init__.py
@@ -0,0 +1,35 @@
+class StringToolsFpat:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def lower(string: str):
+        """
+        Convert string to lower case
+
+        Args:
+            param1 (string): The string to be converted
+
+        Returns:
+            str
+
+
+        """
+
+        return string.lower()
+
+    @staticmethod
+    def upper(string: str):
+        """
+        Convert string to upper case
+
+        Args:
+            param1 (string): The string to be converted
+
+        Returns:
+            str
+
+
+        """
+
+        return string.upper()
