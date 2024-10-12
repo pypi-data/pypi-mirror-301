9b57a94efc1dd36e49f9b6bb100494029e30085a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 06:04:59 +0000

    Add package mach-cctt/0.0.98/mach_cctt-0.0.98-py3-none-any.whl

---
+++ packages/mach-cctt/mach_cctt-0.0.98-py3-none-any.whl/mach_cctt/aave/event.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+from mach_client import Token
+
+
+@dataclass
+class ChoseNextToken:
+    token: Token
+    interest_rate: float
+
+
+@dataclass
+class Withdraw:
+    amounts: list[tuple[Token, float]]
+
+
+@dataclass
+class Supply:
+    token: Token
+    amount: float
+
+
+@dataclass
+class ChooseTokenError:
+    exception: Exception
+
+
+@dataclass
+class WithdrawError:
+    token: Token
+    amount: float
+    exception: Exception
+
+
+@dataclass
+class ConvertError:
+    src_token: Token
+    dest_token: Token
+    error: object
+
+
+@dataclass
+class SupplyError:
+    token: Token
+    amount: float
+    exception: Exception
+
+
+AaveError = ChooseTokenError | WithdrawError | ConvertError | SupplyError
+
+AaveEvent = ChoseNextToken | Withdraw | Supply | AaveError
