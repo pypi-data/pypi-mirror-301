10f5e3e53ba833e3f940815a9463a0b8a93c27aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:27 +0000

    Add package actionstreamer/1.1.28/actionstreamer-1.1.28-py3-none-any.whl

---
+++ packages/actionstreamer/actionstreamer-1.1.28-py3-none-any.whl/actionstreamer/Model/__init__.py
@@ -0,0 +1,4 @@
+from .Model import Event, EventWithNames, EventType, EventStatus, VideoClip, RecordingParameters, TranscodingParameters, TransferArgs, File, RTMPParameters, WifiConnection, NameValuePair, EventDetails, StandardResult, EpochRange, DeviceObject, CreateVideoClip, VideoClipType
+
+
+__all__ = ['Event', 'EventWithNames', 'EventType', 'EventStatus', 'VideoClip', 'RecordingParameters', 'TranscodingParameters', 'TransferArgs', 'File', 'RTMPParameters', 'WifiConnection', 'NameValuePair', 'EventDetails', 'StandardResult', 'EpochRange', 'DeviceObject', 'CreateVideoClip', 'VideoClipType']
