1c462defa6cbb1475bfa848f67b7ae78cc3b73b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:08:50 +0000

    Add package kaxe/0.4.1/kaxe-0.4.1.tar.gz

---
+++ packages/kaxe/kaxe-0.4.1.tar.gz/kaxe-0.4.1/src/kaxe/plot/__init__.py
@@ -0,0 +1,11 @@
+
+from .themes import Themes
+from .log import LogPlot
+from .standard import Plot, XYPLOT
+from .polar import PolarPlot, POLARPLOT
+from .box import BoxPlot
+from .empty import EmptyPlot, EmptyWindow
+
+class identities:
+    XYPLOT = XYPLOT
+    POLAR = POLARPLOT
