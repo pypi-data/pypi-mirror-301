a722d71b3c2341a8f46ff38d2f3eeb42998eacc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:51 +0000

    Add package xiaocTools/0.0.2/xiaocTools-0.0.2.tar.gz

---
+++ packages/xiaocTools/xiaocTools-0.0.2.tar.gz/xiaocTools-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="xiaocTools",
+    version="0.0.2",
+    author="xiaoc",
+    author_email="i@xiaoc.cn",
+    description="自己封装的一些常用方法的python包",
+    long_description="### 自己封装的一些常用方法的python包",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
