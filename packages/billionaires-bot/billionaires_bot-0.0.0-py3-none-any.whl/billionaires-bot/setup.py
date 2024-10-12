d28c20dc50a3e4e4adb4cfb1f9833e4f09381843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:05:19 +0000

    Add package billionaires-bot/0.0.0/billionaires_bot-0.0.0-py3-none-any.whl

---
+++ packages/billionaires-bot/billionaires_bot-0.0.0-py3-none-any.whl/billionaires-bot/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="billionaires-bot", # Put your username here!
+    author="__token__", # Your name here!
+    author_email="therichmanbycasino@gmail.com", # Your e-mail here!
+    description="billionaires-bot", # A short description here!
+    long_description_content_type="text/markdown",
+    url="https://github.com/therichmanbycasino/Billionaires-Bot", # Link your package website here! (most commonly a GitHub repo)
+    packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], # Enter meta data into the classifiers list!
+    python_requires='>=3.12', # The version requirement for Python to run your package!
+)
