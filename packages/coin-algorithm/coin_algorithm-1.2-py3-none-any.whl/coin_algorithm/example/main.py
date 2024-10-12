0422430e21cd61f468307f647d6dfaff164c6a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:28 +0000

    Add package coin-algorithm/1.2/coin_algorithm-1.2-py3-none-any.whl

---
+++ packages/coin-algorithm/coin_algorithm-1.2-py3-none-any.whl/coin_algorithm/example/main.py
@@ -0,0 +1,17 @@
+import os, sys
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
+sys.path.insert(0, parent_dir_path)
+
+import logging
+
+from coin_algorithm.server import binance_server
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+    # back_test_server.serve(port='8888', bot_module='bot.my_ta_bot', bot_class='MyTABot')
+    binance_server.serve(port='8888', bot_module='bot.my_simple_bot', bot_class='SimpleBot',
+                         api_key='',
+                         secret_key='',
+                         is_test_net=True)
