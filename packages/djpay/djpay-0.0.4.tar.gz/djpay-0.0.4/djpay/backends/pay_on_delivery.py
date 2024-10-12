557b40fe8b147b66fd6e581dea6963eae8c53f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:35 +0000

    Add package djpay/0.0.4/djpay-0.0.4.tar.gz

---
+++ packages/djpay/djpay-0.0.4.tar.gz/djpay-0.0.4/djpay/backends/pay_on_delivery.py
@@ -0,0 +1,19 @@
+# standard
+from typing import Any
+
+# internal
+from .base import BaseBackend
+from ..models import Bill
+
+
+class PayOnDelivery(BaseBackend):
+    """Pay On Delivery"""
+
+    identifier = "pay-on-delivery"
+    label = "Pay on delivery"
+
+    def pay(self, amount: int, **extra: Any) -> Bill:
+        return Bill.objects.create(backend=self.identifier, amount=amount, extra=extra)
+
+    def verify(self, bill_id: int, **kwargs: Any) -> Bill:
+        return Bill.objects.get(id=bill_id)
