94a3fe3c311a910bbe0c8fe0bcee8d6b3bc5a953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:34 +0000

    Add package inc-counter/0.6/inc_counter-0.6.tar.gz

---
+++ packages/inc-counter/inc_counter-0.6.tar.gz/inc_counter-0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inc-counter',
+    version='0.6',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'inc-counter': ['templates/*.html'],  # Include all HTML files in the templates directory
+    },
+    install_requires=[
+        'flask',
+        'flask_cors',
+        # Add other dependencies here (e.g., React-related packages if needed)
+    ],
+    entry_points={
+        'console_scripts': [
+            'start-counter=inc_counter.app:main',  # This command will run the main() function in inc_counter/app.py
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Flask',
+        'Operating System :: OS Independent',
+    ],
+)
