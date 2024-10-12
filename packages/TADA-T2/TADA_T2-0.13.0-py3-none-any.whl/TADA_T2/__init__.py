9abb6f081f0965e3a33bd67f34cc32a77fa1b9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:03:53 +0000

    Add package TADA-T2/0.13.0/TADA_T2-0.13.0-py3-none-any.whl

---
+++ packages/TADA-T2/TADA_T2-0.13.0-py3-none-any.whl/TADA_T2/__init__.py
@@ -0,0 +1,6 @@
+"""A Tensorflow2 compatible version of the TADA transcriptional activation domain predictor."""
+
+# Add imports here
+from TADA_T2.TADA import *
+
+from TADA_T2._version import __version__
