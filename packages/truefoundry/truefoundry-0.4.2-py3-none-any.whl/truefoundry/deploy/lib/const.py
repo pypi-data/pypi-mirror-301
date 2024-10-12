6dd02934dd311d771b9770b986a072ae8efa636b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:08:56 +0000

    Add package truefoundry/0.4.2/truefoundry-0.4.2-py3-none-any.whl

---
+++ packages/truefoundry/truefoundry-0.4.2-py3-none-any.whl/truefoundry/deploy/lib/const.py
@@ -0,0 +1,9 @@
+from truefoundry.deploy.io.rich_output_callback import RichOutputCallBack
+
+ENTITY_JSON_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+RICH_OUTPUT_CALLBACK = RichOutputCallBack()
+
+SFY_DEBUG_ENV_KEY = "SFY_DEBUG"
+TFY_DEBUG_ENV_KEY = "TFY_DEBUG"
+SFY_INTERNAL_ENV_KEY = "SFY_INTERNAL"
+TFY_INTERNAL_ENV_KEY = "TFY_INTERNAL"
