ff65be3e97555490e4118c1a9bc03d98ae4f95cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:04:48 +0000

    Add package numtoolsfpat/0.0.7/numtoolsfpat-0.0.7.tar.gz

---
+++ packages/numtoolsfpat/numtoolsfpat-0.0.7.tar.gz/numtoolsfpat-0.0.7/numtoolsfpat/__init__.py
@@ -0,0 +1,19 @@
+class NumToolsFpat:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def to_int(string: str):
+        """
+        Convert string to int
+
+        Args:
+            param1 (string): The string to be converted
+
+        Returns:
+            str
+
+
+        """
+
+        return int(string)
