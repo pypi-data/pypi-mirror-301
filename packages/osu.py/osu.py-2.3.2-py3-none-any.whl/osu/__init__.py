5c4a4c3a0ace8b24976aa0269e04c5819658c7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:22 +0000

    Add package osu.py/2.3.2/osu.py-2.3.2-py3-none-any.whl

---
+++ packages/osu.py/osu.py-2.3.2-py3-none-any.whl/osu/__init__.py
@@ -0,0 +1,17 @@
+from .auth import *
+from .client import Client
+from .exceptions import *
+from .notification import NotificationWebsocket
+from .asyncio.client import AsynchronousClient
+from .enums import *
+from .objects import *
+from .util import (
+    BeatmapsetSearchFilter,
+    PlaylistItemUtil,
+    NotificationsUtil,
+    IdentitiesUtil,
+)
+from .results import *
+
+
+__version__ = "2.3.2"
