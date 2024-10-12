8723f5b6dbdf00b11a058707f1a86ea8e35d71fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:47 +0000

    Add package ironflock/1.0.3/ironflock-1.0.3-py3-none-any.whl

---
+++ packages/ironflock/ironflock-1.0.3-py3-none-any.whl/ironflock/__init__.py
@@ -0,0 +1,10 @@
+from ironflock.AutobahnConnection import (
+    create_application_component,
+    create_application_session,
+)
+
+from ironflock.ironflock import IronFlock
+
+__all__ = ["IronFlock", "create_application_component", "create_application_session"]
+
+__version__ = "0.0.19"
