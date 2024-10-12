f7b16ec384b30bc52d3172333a07be6e040f978e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:07:09 +0000

    Add package dria/0.0.25/dria-0.0.25-py3-none-any.whl

---
+++ packages/dria/dria-0.0.25-py3-none-any.whl/dria/factory/__init__.py
@@ -0,0 +1,26 @@
+from .workflows import *
+from .persona import *
+from .subtopic import *
+
+__all__ = [
+    "Clair",
+    "EvolveComplexity",
+    "ScoreComplexity",
+    "EvolveInstruct",
+    "EvolveQuality",
+    "GenerateGraph",
+    "TextRetrieval",
+    "TextMatching",
+    "TextClassification",
+    "SemanticTriplet",
+    "MagPie",
+    "Simple",
+    "EvaluatePrediction",
+    "ValidatePrediction",
+    "SelfInstruct",
+    "PersonaPipeline",
+    "SubTopicPipeline",
+    "WebFactCheck",
+    "WebMultiChoice",
+    "WebSearch",
+]
---
+++ packages/dria/dria-0.0.25-py3-none-any.whl/dria/factory/workflows/magpie_instruct/instruction.md
@@ -0,0 +1,11 @@
+You are {{instructor_persona}}, engaging in a conversation with {{responding_persona}}.
+
+Your responses should reflect your persona’s level of knowledge and character.
+
+Conversation History:
+
+{{chat}}
+
+As {{instructor_persona}}, please provide the next reply in the conversation.
+
+Response:
