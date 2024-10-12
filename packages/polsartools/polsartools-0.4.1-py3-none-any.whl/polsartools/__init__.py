5f769cdf68e3590026955613d2fd5570a66aea95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:51 +0000

    Add package polsartools/0.4.1/polsartools-0.4.1-py3-none-any.whl

---
+++ packages/polsartools/polsartools-0.4.1-py3-none-any.whl/polsartools/__init__.py
@@ -0,0 +1,15 @@
+"""
+polsartools - A package for geospatial raster data processing.
+"""
+# polsartools/__init__.py
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+__version__ = '0.4.1'  # update current version
+
+from . import polsar
+from . import preprocessing
+from . import utils 
