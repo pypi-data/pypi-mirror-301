bfee50a03fdd6c2222f4441d9eee56a05aba21c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:38 +0000

    Add package riscflow/0.0.3/riscflow-0.0.3.tar.gz

---
+++ packages/riscflow/riscflow-0.0.3.tar.gz/riscflow-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riscflow',
+    version="0.0.3",
+    description='A library for control flow graph analysis of RISC-V assembly',
+    author='Akshit Sharma',
+    author_email='akshitsharma@mines.edu',
+    packaes=find_packages(),
+    install_requires=[
+    ],
+)
