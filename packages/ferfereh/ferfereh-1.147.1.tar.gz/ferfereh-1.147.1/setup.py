68d33843621395acd03707e3c17d472ac98088a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 15:06:05 +0000

    Add package ferfereh/1.147.1/ferfereh-1.147.1.tar.gz

---
+++ packages/ferfereh/ferfereh-1.147.1.tar.gz/ferfereh-1.147.1/setup.py
@@ -0,0 +1,18 @@
+from ferfereh import NAME, VERSION, DESCRIPTION
+from blueness.pypi import setup
+
+
+setup(
+    filename=__file__,
+    repo_name="ferfereh",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[NAME],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            ".abcli/**/*.sh",
+        ],
+    },
+)
