1de93709ac68f08934479dc75d068e58f4cdcbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 06:04:18 +0000

    Add package mqtt-rest/0.5.0/mqtt_rest-0.5.0.tar.gz

---
+++ packages/mqtt-rest/mqtt_rest-0.5.0.tar.gz/mqtt_rest-0.5.0/mqtt_rest/plugins/_helper_commands/install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+COMMANDS_URL=$(dirname "{{request.url}}")
+PLUGIN_NAME=$(basename "$COMMANDS_URL")
+
+#{% if description %}
+cat << EOF
+{{ description }}
+EOF
+#{% endif %}
+
+echo -n "Do you want to install ${PLUGIN_NAME} (mqtt-rest)? [y/n] "
+read -r user_choice
+user_choice=$(echo "$user_choice" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$user_choice" == "no" || "$user_choice" == "n" ]]; then
+    echo "Exiting the script."
+    exit 0
+fi
+
+#{% if dependencies %}
+commands=({{ dependencies|join(', ', attribute='command') }})
+packages=({{ dependencies|join(', ', attribute='package') }})
+not_installed=()
+
+# Check if the dependencies are installed if not then add related package to not_installed array
+for command in "${commands[@]}"; do
+    if ! command -v "$command" &> /dev/null; then
+        not_installed+=("${packages[$(echo "${commands[@]}" | tr ' ' '\n' | grep -n "$command" | cut -d: -f1)]}")
+    fi
+done
+
+if [ ${#not_installed[@]} -ne 0 ]; then
+    echo "The following dependencies are not installed: ${not_installed[*]}"
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run this script as root or install them before running."
+        exit 1
+    fi
+    echo "Installing the dependencies..."
+    apt update
+    apt install -y "${not_installed[@]}"
+fi
+#{% endif %}
+
+PLUGIN_DIR="$HOME/mqtt-rest/${PLUGIN_NAME}"
+mkdir -p "${PLUGIN_DIR}"
+DOWNLOAD_LOCATION="${PLUGIN_DIR}/manager"
+wget -q -O "$DOWNLOAD_LOCATION" "${COMMANDS_URL}/manager"
+if [ $? -ne 0 ]; then
+    echo "Failed to download the manager for ${PLUGIN_NAME} plugin."
+    exit 1
+fi
+chmod +x "$DOWNLOAD_LOCATION"
+
+echo "The ${PLUGIN_NAME} plugin has been installed successfully."
+echo
+echo "You can now run the plugin using the following command:"
+echo "$DOWNLOAD_LOCATION"
+echo
+
+$DOWNLOAD_LOCATION -h
---
+++ packages/mqtt-rest/mqtt_rest-0.5.0.tar.gz/mqtt_rest-0.5.0/mqtt_rest/plugins/sensors/routes.py
@@ -0,0 +1,71 @@
+"""
+This module contains the routes for the sensors plugin.
+"""
+
+import logging
+from fastapi import APIRouter, Request
+from fastapi.responses import PlainTextResponse
+from mqtt_rest.plugins.helper import Command, get_installer, get_single_job
+from mqtt_rest.plugins.sensors.parsers import parse_sensors
+from mqtt_rest.db import add_device, get_device, remove_device
+
+PLUGIN_NAME = "sensors"
+logger = logging.getLogger("uvicorn.error")
+router = APIRouter(prefix=f"/{PLUGIN_NAME}")
+last_report = {}
+
+
+def get_device_name(name: str):
+    name = name.replace(" ", "_")
+    return f"{name} {PLUGIN_NAME}"
+
+
+@router.get("/install")
+async def get_install(request: Request):
+    return get_installer(
+        request,
+        dependencies=[Command(command="sensors", package="lm-sensors")],
+        description="""
+        This plugin uses the "sensors" command to get the temperature of the CPU and other sensors.
+        """,
+    )
+
+
+@router.get("/manager")
+async def get_manager(request: Request):
+    return get_single_job(request, data_command="sensors")
+
+
+@router.put("/submit/{name}")
+async def submit(request: Request, name: str, expiry: int = 3600):
+    global last_report
+    device_name = get_device_name(name)
+    last_report[device_name] = (await request.body()).decode()
+    values = parse_sensors(last_report[device_name])
+    device = get_device(device_name, create=False)
+    if not device:
+        configuration_url = str(request.url).replace("/submit/", "/report/")
+        device = add_device(
+            name=device_name,
+            model="mqtt-plugin",
+            configuration_url=configuration_url,
+            expire_after=expiry,
+        )
+        for key, value in values.items():
+            device.get_sensor(key, value, bulk=True, unit="°C")
+        device.get_sensor("ip", "detecting...", bulk=False)
+    device.bulk_update(values)
+    device.update("ip", str(request.client.host))
+
+
+@router.delete("/delete/{name}")
+async def delete(name: str):
+    global last_report
+    device_name = get_device_name(name)
+    last_report.pop(device_name)
+    remove_device(device_name)
+
+
+@router.get("/report/{name}", response_class=PlainTextResponse)
+async def report(name: str):
+    return last_report.get(get_device_name(name))
