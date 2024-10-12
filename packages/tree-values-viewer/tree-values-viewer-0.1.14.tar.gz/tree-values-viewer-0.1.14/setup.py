2b7202fb1c65fa6406631c84474e3bc151f08d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:00 +0000

    Add package tree-values-viewer/0.1.14/tree-values-viewer-0.1.14.tar.gz

---
+++ packages/tree-values-viewer/tree-values-viewer-0.1.14.tar.gz/tree-values-viewer-0.1.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tree-values-viewer",
+    version="0.1.14",
+    author="Burak Keskin",
+    author_email="me@burak.dev",
+    description="A tool to view project tree and file contents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keskinbu/tree-values-viewer",
+    packages=find_packages(),
+    install_requires=[
+        'prettytable',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            'view-project=tree_values.tree_values:main',
+        ],
+    },
+)
