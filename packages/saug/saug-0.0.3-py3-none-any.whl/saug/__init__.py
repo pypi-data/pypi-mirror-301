80990bf550cf208eff39244bde96dc9ee7cb8b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:25 +0000

    Add package saug/0.0.3/saug-0.0.3-py3-none-any.whl

---
+++ packages/saug/saug-0.0.3-py3-none-any.whl/saug/__init__.py
@@ -0,0 +1,27 @@
+from saug import multi_lens_distortion
+from saug import convolve2d
+from saug import smooth_random_map
+from saug import elastic_transform
+from saug import twirl_distortion01
+from saug import twirl_distortion02
+from saug import wave_distortion01
+from saug import wave_distortion02
+from saug import wave_distortion
+from saug import pixelate_image
+from saug import cartesian_to_polar_image_stretched
+from saug import mirror_effect
+from saug import tilt_shift_effect
+from saug import ripple_effect
+from saug import zoom_blur
+from saug import space_distortion_v1
+from saug import wind_distortion
+from saug import squeeze_stretch_effect
+from saug import smooth_lens_distortion
+from saug import crystallize_distortion
+from saug import honeycomb_distortion
+from saug import moving_blur
+from saug import warp_bubbles_effect
+
+
+
+
