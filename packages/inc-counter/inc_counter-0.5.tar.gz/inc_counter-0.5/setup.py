db9dbe5bdc1b6a59e036b01482d4116d7ee6d620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:24 +0000

    Add package inc-counter/0.5/inc_counter-0.5.tar.gz

---
+++ packages/inc-counter/inc_counter-0.5.tar.gz/inc_counter-0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inc-counter',
+    version='0.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask_cors',
+        # Add other dependencies here (e.g., React-related packages if needed)
+    ],
+    entry_points={
+        'console_scripts': [
+            'start-counter=inc_counter.app:main',  # This command will run the main() function in inc_counter/app.py
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Flask',
+        'Operating System :: OS Independent',
+    ],
+)
