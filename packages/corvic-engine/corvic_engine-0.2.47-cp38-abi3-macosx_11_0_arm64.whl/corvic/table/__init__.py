f5f29efd4963c37f8a0f8b8ac8fdeab8bf3edbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:27 +0000

    Add package corvic-engine/0.2.47/corvic_engine-0.2.47-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/corvic-engine/corvic_engine-0.2.47-cp38-abi3-macosx_11_0_arm64.whl/corvic/table/__init__.py
@@ -0,0 +1,33 @@
+"""Tabular data."""
+
+# since these defs are in the public api for table, export them here
+# as a convenience
+from corvic.op_graph import (
+    FeatureType,
+    Field,
+    RowFilter,
+    Schema,
+    feature_type,
+    row_filter,
+)
+from corvic.table.table import (
+    DataclassAsTypedMetadataMixin,
+    MetadataValue,
+    NotReadyError,
+    Table,
+    TypedMetadata,
+)
+
+__all__ = [
+    "DataclassAsTypedMetadataMixin",
+    "FeatureType",
+    "Field",
+    "MetadataValue",
+    "NotReadyError",
+    "RowFilter",
+    "Schema",
+    "Table",
+    "TypedMetadata",
+    "feature_type",
+    "row_filter",
+]
