1637cf0c82834492b410f786440137ab26ed0692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:23 +0000

    Add package markov-chain-ltv/9.1.1/markov_chain_ltv-9.1.1.tar.gz

---
+++ packages/markov-chain-ltv/markov_chain_ltv-9.1.1.tar.gz/markov_chain_ltv-9.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='markov-chain-ltv', #Package_Name
+      version='9.1.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
