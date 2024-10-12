84dcaa5cba4b9b628b9350c829a4ee8504181fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 06:04:17 +0000

    Add package ithappens/7.1.1/ithappens-7.1.1-py3-none-any.whl

---
+++ packages/ithappens/ithappens-7.1.1-py3-none-any.whl/ithappens/__init__.py
@@ -0,0 +1,10 @@
+"""It Happens
+
+A Python package for creating your own Shit Happens playing cards.
+"""
+
+from ithappens.style.styles.registration import _register
+from ithappens.style.fonts.manage import _add_fonts
+
+_add_fonts()
+_register()
