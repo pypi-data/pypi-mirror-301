ebff21d09229a1cd345a762821649c9fcff7e5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:04 +0000

    Add package midas-quant/0.9.3/midas_quant-0.9.3-py3-none-any.whl

---
+++ packages/midas-quant/midas_quant-0.9.3-py3-none-any.whl/midas_quant/__init__.py
@@ -0,0 +1,20 @@
+from .feed import *
+from .environments import *
+from .trader import *
+import gymnasium as gym
+
+
+__version__ = '0.9.3'
+
+
+gym.register(
+    id="StockSpotEnv",
+    entry_point="midas_quant:StockSpotEnv",
+    kwargs={
+        "feeder": None,
+        "balance": 1_000_000,
+        "quantity": 10,
+        "fee": 0.015,
+        "tax": 0.38
+    }
+)
