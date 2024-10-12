c49a656efe26c008d5d47929b8d3e8255e32bcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:04:49 +0000

    Add package r2r/3.2.11/r2r-3.2.11.tar.gz

---
+++ packages/r2r/r2r-3.2.11.tar.gz/r2r-3.2.11/core/parsers/text/text_parser.py
@@ -0,0 +1,14 @@
+# type: ignore
+from typing import AsyncGenerator
+
+from core.base.abstractions import DataType
+from core.base.parsers.base_parser import AsyncParser
+
+
+class TextParser(AsyncParser[DataType]):
+    """A parser for raw text data."""
+
+    async def ingest(self, data: DataType) -> AsyncGenerator[DataType, None]:
+        if isinstance(data, bytes):
+            data = data.decode("utf-8")
+        yield data
