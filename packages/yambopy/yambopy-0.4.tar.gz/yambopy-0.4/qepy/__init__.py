3d27c058cd646d079f57a389b0cb836aeed9f6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:08:07 +0000

    Add package yambopy/0.4/yambopy-0.4.tar.gz

---
+++ packages/yambopy/yambopy-0.4.tar.gz/yambopy-0.4/qepy/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2018 Henrique Pereira Coutada Miranda
+# All rights reserved.
+#
+# This file is part of yambopy
+#
+"""
+Scripts to manipulate Quantum Espresso input files
+
+Also able to read output files in xml format (datafile.xml or datafile-schema.xml)
+
+"""
+import os
+
+class qepyenv():
+    PW = "pw.x"
+    PH = "ph.x"
+    DYNMAT = "dynmat.x"
+    PSEUDODIR = os.path.join(os.path.dirname(__file__),'data','pseudos')
+    CONV_THR = 1e-8
+
+from qepy.lattice import *
+from qepy.xml import *
+from qepy.bravais import *
+from qepy.pw import *
+from qepy.pwxml import *
+from qepy.projwfc import *
+from qepy.projwfcxml import *
+from qepy.ph import *
+from qepy.dynmat import *
+from qepy.matdyn import *
+from qepy.unfolding import *
+#from qepy.unfoldingyambo import * # broken ; also causes circular import issue while importing YamboLatticeDB -> yambopy.kpoints -> qepy.lattice -> qepy.unfoldingyambo -> YamboLatticeDB  
+from qepy.supercell import *
+from qepy.upf_interface.ppupf import *
