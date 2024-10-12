ea29d7e3697062a893bc34a79b42e9638be6e7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:22 +0000

    Add package pyxbrain/1.1.0/pyxbrain-1.1.0-py3-none-any.whl

---
+++ packages/pyxbrain/pyxbrain-1.1.0-py3-none-any.whl/xbrain/xbrain_tool.py
@@ -0,0 +1,43 @@
+import openai
+import json
+tools = []
+
+class Tool:
+    def __init__(self, model):
+        self.model = model
+
+    def __call__(self, func):
+        # 利用 openai 官方的转换函数，提取name
+        function = openai.pydantic_function_tool(self.model)
+        tools.append({
+            "name": function["function"]["name"],
+            "description": function["function"].get("description", ""),
+            "model":  self.model,
+            "func": func
+        })
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+
+def _get_tool_by_name(name):
+    for tool in tools:
+        if tool["name"] == name:
+            return tool
+    return None
+
+def run_tool(openai_res):
+    res = []
+    for tool_call in openai_res.tool_calls:
+        info = _get_tool_by_name(tool_call.function.name)
+        tool_func = info["func"]
+        run_res = tool_func(**json.loads(tool_call.function.arguments))
+        run_res = run_res if run_res is not None else ""
+        print("##run action## \n", 
+              "action name: ", tool_call.function.name, "\n", 
+              "action arguments: ", json.loads(tool_call.function.arguments), "\n", 
+              "action result: ", run_res, "\n\n"
+              )
+        
+        res.append(run_res)
+    return res
