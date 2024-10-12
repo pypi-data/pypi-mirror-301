937fc4966134fe72eb1cc2119e8bc4648a374f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:32 +0000

    Add package promptgenio/0.1/promptgenio-0.1.tar.gz

---
+++ packages/promptgenio/promptgenio-0.1.tar.gz/promptgenio-0.1/promptgenio/__init__.py
@@ -0,0 +1,1 @@
+from .promptgenio import PromptGenio
---
+++ packages/promptgenio/promptgenio-0.1.tar.gz/promptgenio-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='promptgenio',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'groq',
+    ],
+    author='Christian Madsen',
+    author_email='hej@echristiannmadsen.dk',
+    description='A Python package for PromptGenio integration',
+    url='https://github.com/christiannmadsen/promptgenio-python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
