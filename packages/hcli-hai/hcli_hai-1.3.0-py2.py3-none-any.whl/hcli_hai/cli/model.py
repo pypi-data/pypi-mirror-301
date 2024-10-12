42197399f163553409b0c47e06a60b0171815b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 15:06:03 +0000

    Add package hcli-hai/1.3.0/hcli_hai-1.3.0-py2.py3-none-any.whl

---
+++ packages/hcli-hai/hcli_hai-1.3.0-py2.py3-none-any.whl/hcli_hai/cli/model.py
@@ -0,0 +1,25 @@
+gpt_3_5_turbo = {
+               'model' : "gpt-3.5-turbo",
+               'temperature' : 0.7,
+               'max_tokens' : 2048,
+               'n' : 1,
+               'stop' : None,
+               'frequency_penalty' : 0.5,
+               'presence_penalty' : 0.5
+           }
+
+claude_3_opus_20240229 = {
+               'model' : "claude-3-opus-20240229",
+               'max_tokens' : 2048
+           }
+
+claude_3_haiku_20240307 = {
+               'model' : "claude-3-haiku-20240307",
+               'max_tokens' : 2048
+           }
+
+claude_3_5_sonnet_20240620 = {
+               'model' : "claude-3-5-sonnet-20240620",
+               'max_tokens' : 2048
+           }
+
