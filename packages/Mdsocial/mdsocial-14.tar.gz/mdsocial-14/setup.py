4aec91fd13269340a210a36f500efd54fa132e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:08:15 +0000

    Add package Mdsocial/14/mdsocial-14.tar.gz

---
+++ packages/Mdsocial/mdsocial-14.tar.gz/mdsocial-14/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="Mdsocial",
+    version="14", 
+    description="A social media library that helps developers who work in extracting website data and making it easier for them",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    author="Mode", 
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    package_dir={"": "Code"},
+    packages=find_packages(where="Code"),
+    python_requires=">=3.7",
+)
