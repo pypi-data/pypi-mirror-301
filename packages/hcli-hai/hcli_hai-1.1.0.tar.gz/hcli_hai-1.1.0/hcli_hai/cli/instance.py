c71835cb0d9ac18688d8aeff714b32aba3668a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:20 +0000

    Add package hcli-hai/1.1.0/hcli_hai-1.1.0.tar.gz

---
+++ packages/hcli-hai/hcli_hai-1.1.0.tar.gz/hcli_hai-1.1.0/hcli_hai/cli/instance.py
@@ -0,0 +1,19 @@
+openai_gpt_3_5_turbo = {
+               'model' : "gpt-3.5-turbo",
+               'temperature' : 0.7,
+               'max_tokens' : 2048,
+               'n' : 1,
+               'stop' : None,
+               'frequency_penalty' : 0.5,
+               'presence_penalty' : 0.5
+           }
+
+anthropic_claude_3_5_sonnet = {
+               'model' : "claude-3-5-sonnet-20240620",
+               'max_tokens' : 2048
+           }
+
+anthropic_claude_3_opus = {
+               'model' : "claude-3-opus-20240229",
+               'max_tokens' : 2048
+           }
