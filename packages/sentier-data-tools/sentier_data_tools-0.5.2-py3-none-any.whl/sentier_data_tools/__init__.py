33365ef6d2b9fb97b51f54da4630215b27007af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:07:56 +0000

    Add package sentier-data-tools/0.5.2/sentier_data_tools-0.5.2-py3-none-any.whl

---
+++ packages/sentier-data-tools/sentier_data_tools-0.5.2-py3-none-any.whl/sentier_data_tools/__init__.py
@@ -0,0 +1,40 @@
+"""sentier_data_tools."""
+
+__all__ = (
+    "__version__",
+    "Dataset",
+    "DatasetKind",
+    "Datapackage",
+    "DefaultDataSource",
+    "Demand",
+    "Flow",
+    "FlowIRI",
+    "GeonamesIRI",
+    "ModelTermIRI",
+    "ProductIRI",
+    "reset_local_database",
+    "RunConfig",
+    "SentierModel",
+    "UnitIRI",
+    "get_conversion_factor",
+)
+
+__version__ = "0.5.2"
+
+import sentier_data_tools.patch_pandas
+from sentier_data_tools.datapackage import Datapackage
+from sentier_data_tools.iri import (
+    FlowIRI,
+    GeonamesIRI,
+    ModelTermIRI,
+    ProductIRI,
+    UnitIRI,
+)
+from sentier_data_tools.local_storage import (
+    Dataset,
+    DatasetKind,
+    DefaultDataSource,
+    reset_local_database,
+)
+from sentier_data_tools.model import Demand, Flow, RunConfig, SentierModel
+from sentier_data_tools.unit_conversion import get_conversion_factor
---
+++ packages/sentier-data-tools/sentier_data_tools-0.5.2-py3-none-any.whl/sentier_data_tools/example/model.py
@@ -0,0 +1,62 @@
+from sentier_data_tools import (
+    DatasetKind,
+    Demand,
+    Flow,
+    FlowIRI,
+    GeonamesIRI,
+    ModelTermIRI,
+    ProductIRI,
+    SentierModel,
+)
+from sentier_data_tools.logs import stdout_feedback_logger as logger
+
+
+class WaterElectrolysisModel(SentierModel):
+    provides = [ProductIRI("http://openenergy-platform.org/ontology/oeo/OEO_00010379")]
+    aliases = {
+        ModelTermIRI(
+            "https://vocab.sentier.dev/model-terms/electrolyser/capacity_factor"
+        ): "capacity_factor",
+        ModelTermIRI(
+            "https://vocab.sentier.dev/model-terms/electrolyser/product_lifetime"
+        ): "lifetime",
+        ModelTermIRI(
+            "https://vocab.sentier.dev/model-terms/energy/elec_energy_serv_dem"
+        ): "elec_energy_serv_dem",
+        ProductIRI("https://vocab.sentier.dev/products/electrolyzer"): "electrolyzer",
+        ProductIRI(
+            "http://openenergy-platform.org/ontology/oeo/OEO_00010379"
+        ): "hydrogen",
+    }
+
+    def get_electrolysis_inventory(self) -> None:
+        bom_electrolysis = self.get_model_data(
+            self, product=self.hydrogen, kind=DatasetKind.BOM
+        )
+
+    def run(self) -> tuple[list[Demand], list[Flow]]:
+        self.prepare()
+
+    # def prepare(self) -> None:
+    #     self.get_model_data()
+    #     self.data_validity_checks()
+    #     self.resample()
+
+    # Calculate bill of materials for main product
+    # Pull in BOM data for hydrogen
+
+    # def get_model_data(self) -> list[pd.DataFrame]:
+    #     pass
+
+    # def prepare(self) -> None:
+    #     self.get_model_data(abbreviate_iris=abbreviate_iris)
+    #     self.data_validity_checks()
+    #     self.resample()
+
+    # @abstractmethod
+    # def data_validity_checks(self) -> None:
+    #     pass
+
+    # @abstractmethod
+    # def run(self) -> tuple[list[Demand], list[Flow]]:
+    #     pass
