e1c32f6f6b066dcc94c18d00efd546e3cdcd66ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:04:46 +0000

    Add package fmmax/0.10.1/fmmax-0.10.1-py3-none-any.whl

---
+++ packages/fmmax/fmmax-0.10.1-py3-none-any.whl/fmmax/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+__version__ = "v0.10.1"
+
+from . import (
+    basis,
+    beams,
+    farfield,
+    fft,
+    fields,
+    fmm,
+    fmm_matrices,
+    scattering,
+    sources,
+    utils,
+    vector,
+)
