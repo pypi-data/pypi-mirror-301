9bc09a447c5538226d14b321c3d09ee10c61a1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:22 +0000

    Add package alignai/0.3.0.dev1/alignai-0.3.0.dev1-py3-none-any.whl

---
+++ packages/alignai/alignai-0.3.0.dev1-py3-none-any.whl/alignai/utils.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Mapping, Dict
+
+from google.protobuf.timestamp_pb2 import Timestamp
+from alignai.proto.ingestion.v1alpha.event_pb2 import EventProperties
+
+
+def datetime_to_timestamp(dt: datetime) -> Timestamp:
+    timestamp = Timestamp()
+    timestamp.FromDatetime(dt)
+    return timestamp
+
+
+CustomProperties = Dict[str, str]
+
+
+def serialize_custom_properties(
+    custom_properties: CustomProperties,
+) -> Mapping[str, EventProperties.CustomPropertyValue]:
+    serialized_properties = {}
+
+    for key, value in custom_properties.items():
+        if isinstance(value, str):
+            serialized_properties[key] = EventProperties.CustomPropertyValue(string_value=value)
+
+    return serialized_properties
