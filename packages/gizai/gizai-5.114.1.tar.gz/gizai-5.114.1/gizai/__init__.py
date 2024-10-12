ebc8702fc3901c3d35f9ed5f44b38ecfc4fcf2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 15:06:00 +0000

    Add package gizai/5.114.1/gizai-5.114.1.tar.gz

---
+++ packages/gizai/gizai-5.114.1.tar.gz/gizai-5.114.1/gizai/__init__.py
@@ -0,0 +1,11 @@
+NAME = "gizai"
+
+ICON = "🔻"
+
+DESCRIPTION = f"{ICON} a recipe for AI languages."
+
+VERSION = "5.114.1"
+
+REPO_NAME = "giza"
+
+MARQUEE = "https://github.com/kamangir/giza/raw/main/assets/giza.png"
