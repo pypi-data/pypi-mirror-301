26e959a4d9a4c59ae424fa40ac966bdfe3838273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:05:33 +0000

    Add package spond-classes/0.12.0/spond_classes-0.12.0.tar.gz

---
+++ packages/spond-classes/spond_classes-0.12.0.tar.gz/spond_classes-0.12.0/spond_classes/__init__.py
@@ -0,0 +1,20 @@
+"""Main module."""
+
+# Explicitly import classes and functions into the package namespace to define the API.
+
+from .event import Event, EventType
+from .group import Group
+from .member import Member
+from .profile_ import Profile
+from .role import Role
+from .subgroup import Subgroup
+
+__all__ = [
+    "Event",
+    "EventType",
+    "Group",
+    "Member",
+    "Profile",
+    "Role",
+    "Subgroup",
+]
