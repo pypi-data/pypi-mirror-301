94c04e78b4f3159fe26730869687a28d89c6163d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:30 +0000

    Add package omuplugin-discordrpc/0.7.0/omuplugin_discordrpc-0.7.0-py3-none-any.whl

---
+++ packages/omuplugin-discordrpc/omuplugin_discordrpc-0.7.0-py3-none-any.whl/omuplugin_discordrpc/const.py
@@ -0,0 +1,20 @@
+from omu import App, Identifier
+
+from .version import VERSION
+
+PLUGIN_ID = Identifier.from_key("com.omuapps:plugin-discordrpc")
+PLUGIN_APP = App(
+    PLUGIN_ID,
+    version=VERSION,
+    metadata={
+        "locale": "ja",
+        "name": {
+            "ja-JP": "Discord RPCプラグイン",
+            "en-US": "Discord RPC Plugin",
+        },
+    },
+)
+
+DISCORD_CLIENT_ID = 207646673902501888
+PORT_MIN = 6463
+PORT_MAX = 6473
