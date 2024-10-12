e433fb31423d3f62e4784b517d266b6501164727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:05:19 +0000

    Add package diffsims/0.6.1/diffsims-0.6.1.tar.gz

---
+++ packages/diffsims/diffsims-0.6.1.tar.gz/diffsims-0.6.1/diffsims/release_info.py
@@ -0,0 +1,31 @@
+name = "diffsims"
+version = "0.6.1"
+author = "Duncan Johnstone, Phillip Crout"
+copyright = "Copyright 2017-2024, The diffsims developers"
+# Initial committer first, then listed by line additions
+credits = [
+    "Duncan Johnstone",
+    "Phillip Crout",
+    "Håkon Wiik Ånes",
+    "Carter Francis",
+    "Eric Prestat",
+    "Rob Tovey",
+    "Simon Høgås",
+    "Ben Martineau",
+    "Niels Cautaerts",
+    "Joonatan Laulainen",
+    "Isabel Wood",
+    "Sean Collins",
+    "Stef Smeets",
+    "Alex Borrelli",
+    "Tiarnan Doherty",
+    "Jedrzej Morzy",
+    "Endre Jacobsen",
+    "Tina Bergh",
+    "Tomas Ostasevicius",
+    "Eirik Opheim",
+]
+license = "GPLv3+"
+maintainer = "Duncan Johnstone, Phillip Crout, Håkon Wiik Ånes, Carter Francis"
+email = "pyxem.team@gmail.com"
+status = "Development"
