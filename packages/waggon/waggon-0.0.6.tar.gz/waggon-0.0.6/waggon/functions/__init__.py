11cefe5747956d8ef2e0cc7976d27d1351d3fa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:21 +0000

    Add package waggon/0.0.6/waggon-0.0.6.tar.gz

---
+++ packages/waggon/waggon-0.0.6.tar.gz/waggon-0.0.6/waggon/functions/__init__.py
@@ -0,0 +1,13 @@
+from .base import Function
+from .test_functions import three_hump_camel, rosenbrock, ackley, levi, himmelblau, tang, holder
+
+__all__ = [
+    'Function',
+    'three_hump_camel',
+    'rosenbrock',
+    'ackley',
+    'levi',
+    'himmelblau',
+    'tang',
+    'holder'
+]
---
+++ packages/waggon/waggon-0.0.6.tar.gz/waggon-0.0.6/waggon/optim/__init__.py
@@ -0,0 +1,7 @@
+from .base import Optimiser
+from .optim import SurrogateOptimiser
+
+__all__ = [
+    'Optimiser',
+    'SurrogateOptimiser'
+]
