65c0c1f16e1f57e00f951d0eb59b5e1a41a8b2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:37 +0000

    Add package scmver/1.8/scmver-1.8-py3-none-any.whl

---
+++ packages/scmver/scmver-1.8-py3-none-any.whl/scmver/cli.py
@@ -0,0 +1,234 @@
+#
+# scmver.cli
+#
+#   Copyright (c) 2019-2024 Akinori Hattori <hattya@gmail.com>
+#
+#   SPDX-License-Identifier: MIT
+#
+
+from __future__ import annotations
+from collections.abc import Callable, Sequence
+import re
+from typing import Any, Optional
+
+try:
+    import click
+except ImportError:
+    raise SystemExit("Missing dependencies, try 'pip install scmver[cli]'")
+
+from . import __version__, core, setuptools
+
+
+__all__ = ['run']
+
+F = Callable[..., Any]
+
+
+def run(args: Optional[Sequence[str]] = None) -> None:
+    cli(args=args, prog_name=__package__)
+
+
+class _Group(click.Group):
+
+    def get_command(self, ctx: click.Context, name: str) -> Optional[click.Command]:
+        m = {n: super(_Group, self).get_command(ctx, n) for n in self.list_commands(ctx) if n.startswith(name)}
+        if name in m:
+            return m[name]
+        elif len(m) > 1:
+            ctx.fail(f'command "{name}" is ambiguous: {" ".join(sorted(m))}')
+        for cmd in m.values():
+            return cmd
+        return None
+
+
+class _Local(click.ParamType):
+
+    name = 'text'
+    CO_VARARGS = 0x0004
+
+    def convert(self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> Any:
+        m: dict[str, Any] = {}
+        try:
+            exec(value, {}, m)
+        except (NameError, SyntaxError):
+            return value
+
+        for v in m.values():
+            if callable(v):
+                if (v.__code__.co_argcount < 1
+                    and not v.__code__.co_flags & self.CO_VARARGS):
+                    self.fail(f'"{v}" does not have arguments.', param, ctx)
+                return v
+        self.fail('Callable object does not found.', param, ctx)
+
+
+class _Regex(click.ParamType):
+
+    name = 'regex'
+
+    def __init__(self, group: Optional[Sequence[str]] = None) -> None:
+        super().__init__()
+        self.group = group or []
+
+    def convert(self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> Any:
+        try:
+            value = re.compile(value)
+        except re.error as e:
+            self.fail(str(e), param, ctx)
+        for g in self.group:
+            if g not in value.groupindex:
+                self.fail(f'Regex does not have the {g} group.', param, ctx)
+        return value
+
+
+def _options(options: Sequence[F]) -> F:
+    def _options(func: F) -> F:
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _options
+
+
+_next_version_options = (
+    click.option('-s', '--spec',
+                 help='Construct public version identifiers.'),
+    click.option('-l', '--local',
+                 type=_Local(),
+                 help='Construct local version identifiers.'),
+    click.option('-v', '--version',
+                 type=_Regex(group=['version']),
+                 help='Regular expression to extract the version.'),
+)
+_stat_options = (
+    click.option('--bzr-tag',
+                 metavar='REGEX',
+                 help='Regular expression to filter tags.'),
+    click.option('--darcs-tag',
+                 metavar='REGEX',
+                 help='Regular expression to filter tags.'),
+    click.option('--fsl-tag',
+                 metavar='REGEX',
+                 help='Regular expression to filter tags.'),
+    click.option('--git-tag',
+                 metavar='GLOB',
+                 help='Glob pattern to filter tags.'),
+    click.option('--hg-tag',
+                 metavar='REGEX',
+                 help='Regular expression to filter tags.'),
+    click.option('--svn-tag',
+                 metavar='REGEX',
+                 help='Regular expression to filter tags.'),
+    click.option('--svn-trunk',
+                 metavar='PATH',
+                 help='Relative repository path of the trunk directory.'),
+    click.option('--svn-branches',
+                 metavar='PATH',
+                 help='Relative repository path of the branches directory.'),
+    click.option('--svn-tags',
+                 metavar='PATH',
+                 help='Relative repository path of the tags directory.'),
+)
+
+
+@click.group(cls=_Group)
+@click.version_option(version=__version__)
+def cli() -> None:
+    """A package version manager based on SCM tags."""
+
+
+@cli.command()
+@click.argument('file',
+                type=click.Path(dir_okay=False, writable=True),
+                required=True)
+@_options(_next_version_options)
+@click.option('-t', '--template',
+              help='File template.')
+@_options(_stat_options)
+def generate(file: str, template: Optional[str], **opts: Any) -> None:
+    """Generate a file with the version."""
+
+    opts = _merge_config(opts)
+    info = _stat('.', **opts)
+    if not info:
+        return
+    version = _next_version(info, **opts)
+
+    kwargs = {}
+    if template is not None:
+        kwargs['template'] = template.replace('\\r\\n', '\n').replace('\\n', '\n')
+    core.generate(file, version, info, **kwargs)
+
+
+@cli.command()
+@click.argument('spec')
+@click.option('-p', '--path',
+              help='Search path for modules.')
+def load(spec: str, path: Optional[str]) -> None:
+    """Show a value of the specified object.
+
+    SPEC is in the "package.module:some.attribute" format.
+    """
+
+    click.echo(core.load_version(spec, path))
+
+
+@cli.command()
+@_options(_next_version_options)
+@_options(_stat_options)
+def next(**opts: Any) -> None:
+    """Calculate a next version from the version."""
+
+    opts = _merge_config(opts)
+    info = _stat('.', **opts)
+    if not info:
+        return
+
+    click.echo(_next_version(info, **opts))
+
+
+@cli.command()
+@_options(_stat_options)
+def stat(**opts: Any) -> None:
+    """Show the working directory status."""
+
+    opts = _merge_config(opts)
+    info = _stat('.', **opts)
+    if not info:
+        return
+
+    if info.tag != '0.0':
+        click.echo(f'Tag:      {info.tag}')
+    click.echo(f'Distance: {info.distance}')
+    if info.revision:
+        click.echo(f'Revision: {info.revision}')
+    click.echo(f'Dirty:    {info.dirty}')
+    if info.branch:
+        click.echo(f'Branch:   {info.branch}')
+
+
+def _merge_config(a: dict[str, Any]) -> dict[str, Any]:
+    return (setuptools.load_cfg() or core.load_project() or {}) | a
+
+
+def _next_version(info: core.SCMInfo, **opts: Any) -> Optional[str]:
+    kwargs = {k: opts[k]
+              for k in ('spec', 'local', 'version')
+              if opts[k] is not None}
+    return core.next_version(info, **kwargs)
+
+
+def _stat(path: str, **opts: Any) -> Optional[core.SCMInfo]:
+    kwargs = {k: opts[n]
+              for k, n in (
+                  ('bazaar.tag', 'bzr_tag'),
+                  ('darcs.tag', 'darcs_tag'),
+                  ('fossil.tag', 'fsl_tag'),
+                  ('git.tag', 'git_tag'),
+                  ('mercurial.tag', 'hg_tag'),
+                  ('subversion.tag', 'svn_tag'),
+                  ('subversion.trunk', 'svn_trunk'),
+                  ('subversion.branches', 'svn_branches'),
+                  ('subversion.tags', 'svn_tags'),
+              )
+              if opts[n] is not None}
+    return core.stat(path, **kwargs)
---
+++ packages/scmver/scmver-1.8-py3-none-any.whl/scmver/git.py
@@ -0,0 +1,94 @@
+#
+# scmver.git
+#
+#   Copyright (c) 2019-2024 Akinori Hattori <hattya@gmail.com>
+#
+#   SPDX-License-Identifier: MIT
+#
+
+from __future__ import annotations
+import os
+import re
+import sys
+from typing import Any, Optional, Union
+
+from . import core, util
+from ._typing import Path
+
+
+__all__ = ['parse', 'version', 'run']
+
+_TAG = 'git.tag'
+# environ
+_env: tuple[str, ...] = ('GIT_CONFIG_NOSYSTEM', 'GIT_CONFIG_SYSTEM', 'GIT_CONFIG_GLOBAL', 'HOME', 'XDG_CONFIG_HOME')
+
+_version_re = re.compile(r"""
+    \A
+    git \s+
+    version \s+
+
+    (?P<release>
+        [0-9]+ (?:\. [0-9]+)+
+    )
+    (?P<alpha>[a-z]+)?
+    (?:
+        - rc (?P<rc>[0-9]+)
+    )?
+    # Git for Windows
+    (?:
+        \. (?P<windows_s>windows | msysgit)
+        \. (?P<windows_n>[0-9]+)
+    )?
+    \Z
+""", re.VERBOSE)
+
+
+def parse(root: Path, name: Optional[str] = '.git', **kwargs: Any) -> Optional[core.SCMInfo]:
+    if name == '.git':
+        args = ['describe', '--dirty=+', '--tags', '--abbrev=40', '--long', '--always']
+        if _TAG in kwargs:
+            args += ('--match', kwargs[_TAG])
+        out = run(*args, cwd=root)[0].strip().rsplit('-', 2)
+
+        branch: Optional[str] = run('rev-parse', '--abbrev-ref', 'HEAD', cwd=root)[0].strip()
+        if branch == 'HEAD':
+            branch = run('symbolic-ref', '--short', 'HEAD', cwd=root)[0].strip() or None
+
+        if len(out) == 3:
+            return core.SCMInfo(out[0], int(out[1]), out[2][1:].rstrip('+'), out[2].endswith('+'), branch)
+        elif out[0]:
+            return core.SCMInfo(distance=len(run('rev-list', 'HEAD', '--', cwd=root)[0].splitlines()),
+                                revision=out[0].rstrip('+'),
+                                dirty=out[0].endswith('+'),
+                                branch=branch)
+        elif branch:
+            return core.SCMInfo(dirty=any(l for l in run('status', '--porcelain', cwd=root)[0].splitlines() if l[0] != '?'),
+                                branch=branch)
+    return None
+
+
+def version() -> tuple[Union[int, str], ...]:
+    m = _version_re.match(run('--version')[0].strip())
+    if not m:
+        return ()
+
+    v: tuple[Union[int, str], ...] = tuple(map(int, m.group('release').split('.')))
+    if len(v) < 4:
+        v += (0,) * (4 - len(v))
+    if m.group('alpha'):
+        v += (m.group('alpha'),)
+    if m.group('rc'):
+        v += ('rc', int(m.group('rc')))
+    if m.group('windows_s'):
+        v += (m.group('windows_s'), int(m.group('windows_n')))
+    return v
+
+
+def run(*args: str, **kwargs: Any) -> tuple[str, str]:
+    env = {k: os.environ[k] for k in _env if k in os.environ}
+    if 'env' in kwargs:
+        env.update(kwargs['env'])
+    kwargs['env'] = env
+    if sys.platform == 'win32':
+        kwargs['encoding'] = 'utf-8'
+    return util.exec_((util.command('git'), '-c', 'core.quotepath=false') + args, **kwargs)
