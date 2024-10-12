438c3bf2d16a005e49eaea0c397bb034e54487f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 15:06:06 +0000

    Add package kdb-python/1.0.1/kdb-python-1.0.1.tar.gz

---
+++ packages/kdb-python/kdb-python-1.0.1.tar.gz/kdb-python-1.0.1/unit_test/pc/test_context_click.py
@@ -0,0 +1,25 @@
+from kdb import report
+from unit_test.pc import hidden_ads
+from kdb.webdriver import kdb_driver
+
+
+def context_click_test():
+    report.add_comment("Test context click keyword/api")
+    # start browser
+    kdb_driver.start_browser()
+    # load page for test
+    kdb_driver.open_url('https://demoqa.com/buttons')
+    hidden_ads()
+
+    # TODO click an element in viewport
+    report.add_comment(">>> IN VIEWPORT")
+    kdb_driver.verify_text_on_page('You have done a right click', reverse=True, timeout=3)
+    kdb_driver.screen_shot()
+    # click to the "Click Me" button
+    kdb_driver.context_click("id=rightClickBtn")
+    # verify text to confirm right click success
+    kdb_driver.verify_text_on_page('You have done a right click')
+    kdb_driver.screen_shot()
+
+    # close browser
+    kdb_driver.close_browser()
---
+++ packages/kdb-python/kdb-python-1.0.1.tar.gz/kdb-python-1.0.1/unit_test/pc/test_screen_shot.py
@@ -0,0 +1,18 @@
+from kdb import report
+from kdb.webdriver import kdb_driver
+
+
+def click_test():
+    report.add_comment("Test screen_shot on chrome")
+    # start browser
+    kdb_driver.start_browser('chrome')
+    # load page for test.
+    kdb_driver.open_url('https://stackoverflow.com/')
+
+    kdb_driver.screen_shot(is_full_page=True)
+    kdb_driver.screen_shot_component('id=p-pricing-grid-business')
+    kdb_driver.screen_shot()
+    kdb_driver.screen_shot(scroll_to_element_locator='//p[contains(text(), "Thousands of organizations")]')
+
+    # close browser
+    kdb_driver.close_browser()
