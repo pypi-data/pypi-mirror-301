a0c8649f7e64d66fd11614144810998c8e689884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:25 +0000

    Add package mailship/0.1.1/mailship-0.1.1.tar.gz

---
+++ packages/mailship/mailship-0.1.1.tar.gz/mailship-0.1.1/src/mailship/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .operations import GmailAutomator
+from .email_parser import EmailParserError
+from .email_listener import EmailListenerError
+
+__all__ = ['GmailAutomator', 'EmailParserError', 'EmailListenerError']
