f56b54677d9b03055227c1d0473c54fe9980d0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:06:51 +0000

    Add package hydrogenpay-python/1.0.0/hydrogenpay_python-1.0.0.tar.gz

---
+++ packages/hydrogenpay-python/hydrogenpay_python-1.0.0.tar.gz/hydrogenpay_python-1.0.0/hydrogenpay_python/hydrogenpay.py
@@ -0,0 +1,27 @@
+from hydrogenpay_python.paymentservice import PaymentService
+from hydrogenpay_python.base import HydrogenpayBase
+from hydrogenpay_python.transfer import Transfer
+from hydrogenpay_python.confirmpayment import ConfirmPayment
+from hydrogenpay_python.banktransfer import BankTransfer
+
+
+class Hydrogenpay:
+
+    def __init__(self, sandboxKey, liveKey, mode, setEnv=True):
+        """ This is main organizing object. It contains the following:\n
+            hydrogenpay.PaymentService -- For payment service\n
+            hydrogenpay.BankTransfer -- For bank transfer transaction\n
+            hydrogenpay.Transfer -- For bank transfers initiation\n
+
+        """
+
+        classes = (
+            PaymentService,
+            BankTransfer,
+            Transfer,
+            ConfirmPayment,
+            )
+
+        for _class in classes:
+            attr = _class(sandboxKey, liveKey, mode, setEnv)
+            setattr(self, _class.__name__, attr)
