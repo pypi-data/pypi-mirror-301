3f3c60581f9799bd88ec53d1fea94e3aede7cc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:41 +0000

    Add package mlgame/10.5.2/mlgame-10.5.2-py3-none-any.whl

---
+++ packages/mlgame/mlgame-10.5.2-py3-none-any.whl/mlgame/argument/model.py
@@ -0,0 +1,68 @@
+import datetime
+import os
+from typing import Union, List, Optional
+
+import pydantic
+from pydantic import FilePath, validator, DirectoryPath
+from pathlib import Path
+from mlgame.utils.io import check_folder_existed_and_readable_or_create
+
+
+class MLGameArgument(pydantic.BaseModel):
+    """
+    Data Entity to handle parsed cli arguments
+    """
+    fps: int = 30
+    progress_frame_frequency: int = 300
+    one_shot_mode: bool = False
+    ai_clients: Optional[List[FilePath]] = None
+    is_manual: bool = False
+    no_display: bool = True
+    ws_url: pydantic.AnyUrl = None
+    game_folder: DirectoryPath
+    game_params: List[str]
+    output_folder: Union[Path, None] = None
+    progress_folder: Union[Path, None] = None
+    is_sound_on : bool = True
+    is_debug: bool = False
+
+    @validator('is_manual', always=True)
+    def update_manual(cls, v, values) -> bool:
+        if 'ai_clients' in values:
+            return values['ai_clients'] is None
+        return True
+
+    @validator('output_folder')
+    def update_output_folder(cls, v, values):
+        if v is None:
+            return None
+        path = os.path.join(
+            str(v),
+            datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+        )
+        if check_folder_existed_and_readable_or_create(path):
+            return path
+
+    @validator('progress_folder')
+    def update_progress_folder(cls, v, values):
+        if v is None:
+            return None
+        path = os.path.join(
+            str(v),
+            datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+        )
+        if check_folder_existed_and_readable_or_create(path):
+            return path
+
+
+class UserNumConfig(pydantic.BaseModel):
+    """
+    Data Entity to handle user_num in game_config.json
+    """
+    min: int
+    max: int
+
+    @validator('max')
+    def max_should_be_larger_than_min(cls, v, values):
+        assert v >= values['min']
+        return v
---
+++ packages/mlgame/mlgame-10.5.2-py3-none-any.whl/mlgame/core/process.py
@@ -0,0 +1,102 @@
+import time
+from multiprocessing import Process, Pipe
+
+from mlgame.core.communication import GameCommManager, MLCommManager, TransitionCommManager
+from mlgame.core.env import TIMEOUT
+from mlgame.core.executor import AIClientExecutor, WebSocketExecutor, ProgressLogExecutor
+from mlgame.utils.enum import get_ai_name
+from mlgame.utils.logger import logger
+
+
+def create_process_of_ws_and_start(game_comm: GameCommManager, ws_url) -> Process:
+    recv_pipe_for_game, send_pipe_for_ws = Pipe(False)
+    recv_pipe_for_ws, send_pipe_for_game = Pipe(False)
+    ws_comm = TransitionCommManager(recv_pipe_for_ws, send_pipe_for_ws)
+    game_comm.add_comm_to_others("ws", recv_pipe_for_game, send_pipe_for_game)
+    ws_executor = WebSocketExecutor(ws_uri=ws_url, ws_comm=ws_comm)
+    process = Process(target=ws_executor.run, name="ws")
+    # process = ws_executor
+    process.start()
+    # time.sleep(0.1)
+    return process
+
+
+def create_process_of_ai_clients_and_start(
+        game_comm: GameCommManager, path_of_ai_clients: list, game_params: dict) -> list:
+    """
+    return a process list to main process and bind pipes to `game_comm`
+    """
+    ai_process = []
+    for index, ai_client in enumerate(path_of_ai_clients):
+        ai_name = get_ai_name(index)
+        recv_pipe_for_game, send_pipe_for_ml = Pipe(False)
+        recv_pipe_for_ml, send_pipe_for_game = Pipe(False)
+        game_comm.add_comm_to_ml(
+            ai_name,
+            recv_pipe_for_game, send_pipe_for_game)
+        ai_comm = MLCommManager(ai_name)
+        ai_comm.set_comm_to_game(
+            recv_pipe_for_ml, send_pipe_for_ml)
+        ai_executor = AIClientExecutor(ai_client.__str__(), ai_comm, ai_name=ai_name, game_params=game_params)
+        process = Process(target=ai_executor.run,
+                          name=ai_name)
+        process.start()
+        ai_process.append(process)
+    return ai_process
+
+
+def create_process_of_progress_log_and_start(game_comm: GameCommManager, progress_folder,
+                                             progress_frame_frequency) -> Process:
+    recv_pipe_for_game, send_pipe_for_pl = Pipe(False)
+    recv_pipe_for_pl, send_pipe_for_game = Pipe(False)
+    pl_comm = TransitionCommManager(recv_pipe_for_pl, send_pipe_for_pl)
+    game_comm.add_comm_to_others("pl", recv_pipe_for_game, send_pipe_for_game)
+    pl_executor = ProgressLogExecutor(progress_folder=progress_folder,
+                                      progress_frame_frequency=progress_frame_frequency, pl_comm=pl_comm)
+    process = Process(target=pl_executor.run, name="pl")
+    process.start()
+    # time.sleep(0.1)
+    return process
+
+
+def terminate(game_comm: GameCommManager, ai_process: list, ws_proc: Process, progress_proc: Process):
+    logger.info("Main process will terminate ai process")
+    # 5.terminate
+    for ai_proc in ai_process:
+        # Send stop signal to all alive ml processes
+        if ai_proc.is_alive():
+            game_comm.send_to_ml(
+                None, ai_proc.name)
+            ai_proc.terminate()
+    logger.info("Main process will terminate ws process")
+
+    if ws_proc is not None:
+        timeout = time.time() + TIMEOUT
+        print(f"wait to close ws for timeout : {TIMEOUT} s")
+        while True:
+            time.sleep(0.5)
+            if time.time() > timeout:
+                print("Force to terminate ws proc ")
+                ws_proc.kill()
+                ws_proc.join()
+                break
+            elif not ws_proc.is_alive():
+                break
+            print("wait to close ws .")
+        logger.info(f"use {time.time() - timeout + TIMEOUT} to close.")
+
+    if progress_proc is not None:
+        timeout = time.time() + TIMEOUT
+        print(f"wait to close progress for timeout : {TIMEOUT} s")
+        while True:
+            time.sleep(0.5)
+            if time.time() > timeout:
+                print("Force to terminate progress_proc proc ")
+                progress_proc.kill()
+                progress_proc.join()
+                break
+            elif not progress_proc.is_alive():
+                break
+            print("wait to close progress_proc .")
+        logger.info(f"use {time.time() - timeout + TIMEOUT} to close.")
+    logger.info("Game is terminated")
