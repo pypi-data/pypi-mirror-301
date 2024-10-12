97a449f9819b916ec21016c1779eab7dad660398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:46 +0000

    Add package SimpleWindow/0.8/simplewindow-0.8.tar.gz

---
+++ packages/SimpleWindow/simplewindow-0.8.tar.gz/simplewindow-0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SimpleWindow",
+    version="0.8",
+    description="A package to easily create windows in Python using PyOpenGL, GLFW, OpenCV-Python, NumPy, pywin32 and ctypes",
+    long_description=open("README.md").read(),
+    author="Glas42",
+    license="GPL-3.0",
+    packages=["SimpleWindow"],
+    python_requires=">=3.9",
+    install_requires=[
+        "pyopengl",
+        "glfw",
+        "opencv-python",
+        "numpy",
+        "pywin32",
+    ],
+)
