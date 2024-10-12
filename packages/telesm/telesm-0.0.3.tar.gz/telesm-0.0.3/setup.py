97906190e31c970bb45b5baac049cce9e618f1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:02 +0000

    Add package telesm/0.0.3/telesm-0.0.3.tar.gz

---
+++ packages/telesm/telesm-0.0.3.tar.gz/telesm-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telesm',
+    version='0.0.3',
+    description='A command-line dictionary app using WordNet',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Pedram Behroozi',
+    author_email='pedrambehroozi@gmail.com',
+    url='https://github.com/pedrambehroozi/telesm',
+    packages=find_packages(),
+    install_requires=['nltk'],
+    entry_points={
+        'console_scripts': [
+            'telesm=telesm.main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
