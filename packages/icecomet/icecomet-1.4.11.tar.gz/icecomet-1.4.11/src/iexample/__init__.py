f4ccb49f436f9e02164646a805948b5e641815e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:52 +0000

    Add package icecomet/1.4.11/icecomet-1.4.11.tar.gz

---
+++ packages/icecomet/icecomet-1.4.11.tar.gz/icecomet-1.4.11/src/iexample/__init__.py
@@ -0,0 +1,21 @@
+#pythonXcommandLine
+import pythonXcommandLine.capture_output as _
+import pythonXcommandLine.main as _
+import pythonXcommandLine.sent_input as _
+
+
+#method_and_module
+import method_and_module.open_file.open_binary as _
+import method_and_module.open_file.open_clipboard as _
+import method_and_module.open_file.open_csv as _
+import method_and_module.open_file.open_json as _
+import method_and_module.open_file.open_not_with as _
+import method_and_module.open_file.open_txt as _
+
+import method_and_module.L_pandas as _
+import method_and_module.M_os as _
+import method_and_module.M_statistics as _
+import method_and_module.O_multiprocessing as _
+import method_and_module.operation_list as _
+import method_and_module.operation_number as _
+import method_and_module.operation_string as _
