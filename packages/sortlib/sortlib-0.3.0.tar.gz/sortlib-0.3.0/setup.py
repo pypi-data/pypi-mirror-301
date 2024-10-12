fe59d5cfeee7cf7fad8e85cd7354f0a9adc62fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:33 +0000

    Add package sortlib/0.3.0/sortlib-0.3.0.tar.gz

---
+++ packages/sortlib/sortlib-0.3.0.tar.gz/sortlib-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sortlib',  # имя вашей библиотеки
+    version='0.3.0',  # версия
+    author='Vanek',
+    author_email='firi8228@gmail.com',
+    description='Библиотека для сортировки с использованием различных алгоритмов.',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # автоматически найдет пакеты
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # минимальная версия Python
+)
