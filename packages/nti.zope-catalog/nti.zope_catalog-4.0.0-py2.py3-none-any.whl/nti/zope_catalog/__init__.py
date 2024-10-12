c1f56301f19972ccc072a9438895e8a4249fd3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:07:24 +0000

    Add package nti.zope-catalog/4.0.0/nti.zope_catalog-4.0.0-py2.py3-none-any.whl

---
+++ packages/nti.zope-catalog/nti.zope_catalog-4.0.0-py2.py3-none-any.whl/nti/zope_catalog/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+nti.zope_catalog. See interfaces.py
+"""
+
+#: Deprecated. Import from interfaces.py.
+from nti.zope_catalog.interfaces import IMetadataCatalog
