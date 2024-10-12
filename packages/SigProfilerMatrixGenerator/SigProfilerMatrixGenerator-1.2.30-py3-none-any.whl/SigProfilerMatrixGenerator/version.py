999ead3ff5bfd5ccf471ab9b438f612482562f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:29 +0000

    Add package SigProfilerMatrixGenerator/1.2.30/SigProfilerMatrixGenerator-1.2.30-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.30-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.30'
+version = '1.2.30'
+Update = 'v1.2.30: Resolve exome=True issue caused by indentation.'
+
+	
