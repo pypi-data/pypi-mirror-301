6754b96d8162e20396aff7955f963f11ac733943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:21 +0000

    Add package funvtk/1.0.9/funvtk-1.0.9-py3-none-any.whl

---
+++ packages/funvtk/funvtk-1.0.9-py3-none-any.whl/funvtk/__init__.py
@@ -0,0 +1,3 @@
+from .hl import pointsToVTK, pointsToVTKAsTIN, polyLinesToVTK, gridToVTK
+
+__all__ = ["pointsToVTK", "pointsToVTKAsTIN", "polyLinesToVTK", "gridToVTK"]
---
+++ packages/funvtk/funvtk-1.0.9-py3-none-any.whl/funvtk/version.py
@@ -0,0 +1,3 @@
+_MAJOR = 2_MINOR = 0
+_REV = 0
+PYEVTK_VERSION = "%d.%d.%d" % (_MAJOR, _MINOR, _REV)
