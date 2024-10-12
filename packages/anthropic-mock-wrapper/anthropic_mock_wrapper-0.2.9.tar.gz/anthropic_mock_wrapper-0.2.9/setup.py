bd8f2a357fec20cd4b65e5aa80ac4611038fb91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 05:04:38 +0000

    Add package anthropic-mock-wrapper/0.2.9/anthropic_mock_wrapper-0.2.9.tar.gz

---
+++ packages/anthropic-mock-wrapper/anthropic_mock_wrapper-0.2.9.tar.gz/anthropic_mock_wrapper-0.2.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="anthropic_mock_wrapper",
+    version="0.2.9",
+    packages=find_packages(),
+    install_requires=[
+        "anthropic",
+        "lorem",
+    ],
+    extras_require={
+        "dev": [
+            "mypy",
+            "pytest",
+            "pytest-asyncio",
+        ]
+    },
+    author="Anurag Angara",
+    author_email="anurag.angara@gmail.com",
+    description="A lightweight wrapper around the Anthropic Python client for testing purposes",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ps2-controller/anthropic_mock_wrapper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
