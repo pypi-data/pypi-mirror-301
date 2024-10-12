240b22b0aa1747b45ac09da9cc8e08c5b39463cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:04:07 +0000

    Add package buco-db-controller/0.1.10/buco_db_controller-0.1.10.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.10.tar.gz/buco_db_controller-0.1.10/buco_db_controller/models/fixture_stats.py
@@ -0,0 +1,27 @@
+from buco_db_controller.models.team import Team
+from buco_db_controller.models.team_fixture_stats import TeamFixtureStats
+
+
+class FixtureStats:
+    def __init__(
+            self,
+            fixture_id,
+            home_fixture_stats,
+            away_fixture_stats,
+    ):
+        self.fixture_id = fixture_id
+        self.home_fixture_stats = home_fixture_stats
+        self.away_fixture_stats = away_fixture_stats
+
+    @classmethod
+    def from_dict(cls, response):
+        fixture_id = response['parameters']['fixture']
+
+        home_fixture_stats = TeamFixtureStats.from_dict(response['data'][0])
+        away_fixture_stats = TeamFixtureStats.from_dict(response['data'][1])
+
+        return cls(
+            fixture_id=fixture_id,
+            home_fixture_stats=home_fixture_stats,
+            away_fixture_stats=away_fixture_stats,
+        )
