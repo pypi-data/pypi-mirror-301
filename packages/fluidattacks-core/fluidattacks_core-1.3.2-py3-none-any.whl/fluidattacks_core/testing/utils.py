d1f53f45e232b1c05cfc3f90a37decd3408de67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:54 +0000

    Add package fluidattacks-core/1.3.2/fluidattacks_core-1.3.2-py3-none-any.whl

---
+++ packages/fluidattacks-core/fluidattacks_core-1.3.2-py3-none-any.whl/fluidattacks_core/testing/utils.py
@@ -0,0 +1,56 @@
+from .types import (
+    AioDynamoDBTable,
+    AttributeValue,
+    IntegratesDomain,
+)
+from decimal import (
+    Decimal,
+)
+
+
+def _cast_primitive(
+    value: str | bool | int | float | Decimal,
+) -> AttributeValue:
+    if isinstance(value, (str)):
+        return {"S": str(value)}
+    if isinstance(value, (bool)):
+        return {"BOOL": value}
+    return {"N": str(value)}
+
+
+def _cast_objects(value: list | dict) -> AttributeValue:
+    if isinstance(value, list):
+        return {"L": [cast_to_dynamodb(v) for v in value]}
+    return {"M": {key: cast_to_dynamodb(val) for key, val in value.items()}}
+
+
+def cast_to_dynamodb(
+    value: str | bool | int | float | dict | Decimal | None,
+) -> AttributeValue:
+    """Format vulnerabilities to DynamoDB structure.
+
+    Returns:
+        AttributeValue: Vulnerability formatted to DynamoDB types.
+    """
+    if isinstance(value, (str, bool, int, float, Decimal)):
+        return _cast_primitive(value)
+    if isinstance(value, (dict, list)):
+        return _cast_objects(value)
+    return {"NULL": True}
+
+
+async def async_populate_table(
+    table: AioDynamoDBTable,
+    data: IntegratesDomain,
+) -> None:
+    for org in data.orgs:
+        await table.put_item(Item=org.build())
+
+    for group in data.groups:
+        await table.put_item(Item=group.build())
+
+    for finding in data.findings:
+        await table.put_item(Item=finding.build())
+
+    for vuln in data.vulns:
+        await table.put_item(Item=vuln.build())
