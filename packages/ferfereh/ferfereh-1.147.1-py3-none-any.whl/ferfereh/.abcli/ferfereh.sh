37b10fd1d7ac70b8230843d7fec9b4894edad8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 15:06:05 +0000

    Add package ferfereh/1.147.1/ferfereh-1.147.1-py3-none-any.whl

---
+++ packages/ferfereh/ferfereh-1.147.1-py3-none-any.whl/ferfereh/.abcli/ferfereh.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+function ferfereh() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ]; then
+        abcli_show_usage "ferfereh cleanup" \
+            "cleanup ferfereh."
+
+        ferfereh_exif "$@"
+
+        ferfereh_publish "$@"
+        return
+    fi
+
+    if [ "$task" == "cleanup" ]; then
+        python3 -m ferfereh cleanup \
+            --path $abcli_path_git/ferfereh/images/tools
+        return
+    fi
+
+    abcli_generic_task \
+        plugin=ferfereh,task=$task \
+        "${@:2}"
+}
+
+abcli_source_caller_suffix_path /tests
---
+++ packages/ferfereh/ferfereh-1.147.1-py3-none-any.whl/ferfereh/__main__.py
@@ -0,0 +1,41 @@
+import argparse
+from ferfereh import NAME, VERSION, DESCRIPTION
+from ferfereh.logger import logger
+from blueness.argparse.generic import sys_exit
+
+
+parser = argparse.ArgumentParser(NAME, description=f"{NAME}-{VERSION}")
+parser.add_argument(
+    "task",
+    type=str,
+    help="cleanup|description|publish_coords|version",
+)
+parser.add_argument(
+    "--output_filename",
+    type=str,
+)
+parser.add_argument(
+    "--path",
+    type=str,
+)
+args = parser.parse_args()
+
+success = False
+if args.task == "cleanup":
+    from ferfereh.utils import cleanup
+
+    success = cleanup(args.path)
+elif args.task == "description":
+    print(DESCRIPTION)
+    success = True
+elif args.task == "publish_coords":
+    from ferfereh.coords import publish_coords
+
+    success = publish_coords(args.output_filename)
+elif args.task == "version":
+    print(f"{NAME}-{VERSION}")
+    success = True
+else:
+    success = None
+
+sys_exit(logger, NAME, args.task, success)
