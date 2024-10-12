2a89f80f65d393ba48c7284f25d48abd978cd909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 16:04:34 +0000

    Add package stapesai-ssi/0.0.2/stapesai_ssi-0.0.2.tar.gz

---
+++ packages/stapesai-ssi/stapesai_ssi-0.0.2.tar.gz/stapesai_ssi-0.0.2/ssi/types/streaming_data_chunk.py
@@ -0,0 +1,11 @@
+# Path: ssi/types/streaming_data_chunk.py
+# Description: This module contains the StreamingDataChunk pydantic model, which represents a chunk of streaming data.
+
+from pydantic import BaseModel
+
+class StreamingDataChunk(BaseModel):
+    """
+    """
+    language: str
+    transcription: str
+    server_process_time: float
