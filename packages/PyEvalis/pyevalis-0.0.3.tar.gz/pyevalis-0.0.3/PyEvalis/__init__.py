dd4830f80e9e9f6d2fa3ea12620f31e3f4b55212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:08:44 +0000

    Add package PyEvalis/0.0.3/pyevalis-0.0.3.tar.gz

---
+++ packages/PyEvalis/pyevalis-0.0.3.tar.gz/pyevalis-0.0.3/PyEvalis/__init__.py
@@ -0,0 +1,12 @@
+# Version of Module
+from .DecisionTree import DecisionTree
+from .RandomForest import RandomForest
+from .KNN import KNN
+from package.PyEvalis.regression.LogisticRegression import LogisticRegression
+from .NaiveBayes import NaiveBayes
+from .PCA import PCA
+from .SVM import SVM
+from .KMeans import KMeans
+from .regression import *
+
+__version__ = 0.0
