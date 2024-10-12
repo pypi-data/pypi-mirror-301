e7a69a7caba5a7be598964cfd935f132eef336e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 14:05:40 +0000

    Add package cms-model/0.1.0/cms_model-0.1.0-py3-none-any.whl

---
+++ packages/cms-model/cms_model-0.1.0-py3-none-any.whl/cms_model/__init__.py
@@ -0,0 +1,23 @@
+# Import all necessary functions from cms_model.py
+from .cms_model import (
+    Power, 
+    generate_specific_combination, 
+    common, 
+    Merge, 
+    sort, 
+    addition, 
+    subtraction, 
+    multiplication, 
+    division, 
+    find_abc, 
+    find_JW, 
+    read_from_csv, 
+    compile_custom_model, 
+    compile_best_model, 
+    compile_all_models, 
+    predict
+)
+
+# Package metadata
+__version__ = "0.1.0"
+__author__ = "Rifat Hassan"
