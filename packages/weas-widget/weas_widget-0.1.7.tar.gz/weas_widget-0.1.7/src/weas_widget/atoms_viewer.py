07ff5c6057dc5d0639714dfc8cc386e6431bd1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 04:03:56 +0000

    Add package weas-widget/0.1.7/weas_widget-0.1.7.tar.gz

---
+++ packages/weas-widget/weas_widget-0.1.7.tar.gz/weas_widget-0.1.7/src/weas_widget/atoms_viewer.py
@@ -0,0 +1,89 @@
+from .base_class import WidgetWrapper
+from .plugins.vector_field import VectorField
+from .plugins.isosurface import Isosurface
+from .plugins.lattice_plane import LatticePlane
+from copy import deepcopy
+
+
+class AtomsViewer(WidgetWrapper):
+
+    catalog = "viewer"
+
+    _attribute_map = {
+        "model_style": "modelStyle",
+        "selected_atoms_indices": "selectedAtomsIndices",
+        "boundary": "boundary",
+        "color_type": "colorType",
+        "color_by": "colorBy",
+        "color_ramp": "colorRamp",
+        "show_cell": "showCell",
+        "show_bonded_atoms": "showBondedAtoms",
+        "hide_long_bonds": "hideLongBonds",
+        "atom_label_type": "atomLabelType",
+        "material_type": "materialType",
+        "model_sticks": "modelSticks",
+        "atom_scales": "atomScales",
+        "model_polyhedras": "modelPolyhedras",
+        "phonon_setting": "phonon",
+    }
+
+    _extra_allowed_attrs = ["vf", "iso", "lp", "atoms"]
+
+    def __init__(self, _widget):
+
+        super().__init__(_widget)
+        # Initialize plugins
+        setattr(self, "vf", VectorField(_widget))
+        setattr(self, "iso", Isosurface(_widget))
+        setattr(self, "lp", LatticePlane(_widget))
+
+    @property
+    def atoms(self):
+        return self._widget.atoms
+
+    @atoms.setter
+    def atoms(self, atoms):
+        self._widget.atoms = atoms
+        # initialize atomScales
+        if isinstance(atoms, list):
+            atoms = atoms[0]
+        natom = len(atoms["symbols"])
+        self._widget.atomScales = [1] * natom
+        self._widget.modelSticks = [0] * natom
+        self._widget.modelPolyhedras = [0] * natom
+        # magnetic moment vector field
+        # separate spin up and down, add two vector fields
+        if "moment" in atoms["attributes"]["atom"]:
+            moment = atoms["attributes"]["atom"]["moment"]
+            spin_up = [i for i, m in enumerate(moment) if m > 0]
+            spin_down = [i for i, m in enumerate(moment) if m < 0]
+            self.vf.settings = [
+                {
+                    "origins": [atoms["positions"][i] for i in spin_up],
+                    "vectors": [[0, 0, moment[i]] for i in spin_up],
+                    "color": "blue",
+                },
+                {
+                    "origins": [atoms["positions"][i] for i in spin_down],
+                    "vectors": [[0, 0, moment[i]] for i in spin_down],
+                    "color": "red",
+                },
+            ]
+
+    def draw(self):
+        """Redraw the widget."""
+        self._widget.send_js_task({"name": "avr.drawModels", "kwargs": {}})
+
+    def set_attribute(self, name, value, domain="atoms"):
+        """Set an attribute of the widget."""
+        atoms = deepcopy(self._widget.atoms)
+        atoms["attributes"][domain][name] = value
+        self._widget.atoms = atoms
+        # update the widget
+        self._widget.send_js_task(
+            {"name": "avr.setAttribute", "args": [name, value, domain]}
+        )
+
+    def get_attribute(self, name):
+        """Get an attribute of the widget."""
+        raise NotImplementedError("This method is not implemented yet.")
