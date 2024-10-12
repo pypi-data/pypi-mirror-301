2d3d0bb32f21f8f814c1c00701dd64c5b24506d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 03:07:15 +0000

    Add package backtrack-sampler/0.0.12/backtrack_sampler-0.0.12-py3-none-any.whl

---
+++ packages/backtrack-sampler/backtrack_sampler-0.0.12-py3-none-any.whl/provider/base_provider.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import List
+import torch
+
+
+class BaseProvider(ABC):
+    @abstractmethod
+    def encode(self, text: str, add_special_tokens: bool = True) -> List[int]:
+        pass
+
+    @abstractmethod
+    def decode(self, tokens: List[int]) -> str:
+        pass
+
+    @abstractmethod
+    def generate(self, input_ids: List[int], *args, **kwargs) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def get_eos_token_id(self) -> int:
+        pass
+
+    @abstractmethod
+    def crop_cache(self, idx: int) -> None:
+        pass
+
+    @abstractmethod
+    def on_finish(self) -> None:
+        pass
