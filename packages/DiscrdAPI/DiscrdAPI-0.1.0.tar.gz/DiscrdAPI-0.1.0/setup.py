3969311dc0edd9d595a1d1a935168f6d27787c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:51 +0000

    Add package DiscrdAPI/0.1.0/DiscrdAPI-0.1.0.tar.gz

---
+++ packages/DiscrdAPI/DiscrdAPI-0.1.0.tar.gz/DiscrdAPI-0.1.0/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='DiscrdAPI',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    description='DiscordAPI',
+    author='Momwhyareyouhere',
+    author_email='momwhyareyouherebusiness@gmail.com',
+    url='https://github.com/DiscordClients/DiscordAPI'
+)
