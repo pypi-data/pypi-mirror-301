40ba42892e2a125b0c244e0b1190b54c4f1a1105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:05:09 +0000

    Add package fsoft-maid/0.3.4/fsoft_maid-0.3.4.tar.gz

---
+++ packages/fsoft-maid/fsoft_maid-0.3.4.tar.gz/fsoft_maid-0.3.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="fsoft-maid",
+    version="0.3.4",
+    description="Markdown AI Doc creator",
+    author="Fabio Rotondo",
+    author_email="fsoft.devel@gmail.com",
+    url="https://github.com/fsoft72/maid",
+    license="MIT",
+    packages=find_packages(),
+    py_modules=["fsoft_maid"],
+    entry_points={
+        "console_scripts": [
+            "maid=fsoft_maid:main",
+        ],
+    },
+    install_requires=[
+        # List your dependencies here
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
