ddcd9a0229c3c45c6bf8e42d0d757973c4507738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:47 +0000

    Add package vault-service/0.1.0/vault_service-0.1.0.tar.gz

---
+++ packages/vault-service/vault_service-0.1.0.tar.gz/vault_service-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vault-service',
+    version='0.1.0',
+    description='A reusable FastAPI vault utility service for other projects to use hashicorp vault',
+    author='Ankush Bansal',
+    author_email='ankush.bansal@asato.ai',
+    packages=find_packages(include=['vault_service', 'vault_service.*']),
+    install_requires=[
+        'fastapi>=0.114.0',
+        'uvicorn>=0.30.6',
+        'gunicorn>=23.0.0',
+        'hvac>=2.3.0',
+        'python-dotenv>=1.0.1',
+        'pydantic>=2.9.1',
+    ],
+    python_requires='>=3.11',  # Specify the required Python version
+)
