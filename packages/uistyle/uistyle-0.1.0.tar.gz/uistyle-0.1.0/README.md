3414c4c1bf607adba79becf1127ba3f22fd26b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 05:04:01 +0000

    Add package uistyle/0.1.0/uistyle-0.1.0.tar.gz

---
+++ packages/uistyle/uistyle-0.1.0.tar.gz/uistyle-0.1.0/README.md
@@ -0,0 +1,14 @@
+# MyConsoleUI
+
+A beautiful console UI system for Python applications. 
+
+## Features
+- Displays current time and uptime
+- Logs messages with `loguru`
+- Renders ASCII art banners with `pyfiglet`
+- Prints tables with `rich`
+- Uses `colorama` for colored console output
+
+## Installation
+```bash
+pip install ui
---
+++ packages/uistyle/uistyle-0.1.0.tar.gz/uistyle-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uistyle',
+    version='0.1.0',
+    description='A beautiful console UI system for Python applications',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='ravan',
+    author_email='ravanxdil@example.com',
+    url='https://github.com/vivekxd669/myconsoleui',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+        'loguru',
+        'pyfiglet',
+        'rich'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
