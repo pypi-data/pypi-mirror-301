3898faa64fbecef0c890e6cbf7c056b4bccdea6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:04:49 +0000

    Add package edgar/5.6.2/edgar-5.6.2-py3-none-any.whl

---
+++ packages/edgar/edgar-5.6.2-py3-none-any.whl/edgar/__init__.py
@@ -0,0 +1,15 @@
+import glob
+from os.path import basename, dirname, isfile
+
+from .company import Company
+from .document import Document, Documents
+from .edgar import Edgar
+from .txtml import TXTML
+from .xbrl import XBRL, XBRLElement
+
+__version__ = "5.6.2"
+
+modules = glob.glob(dirname(__file__) + "/*.py")
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
---
+++ packages/edgar/edgar-5.6.2-py3-none-any.whl/edgar/txtml.py
@@ -0,0 +1,46 @@
+from lxml.etree import tostring
+import html
+from .utils import get_text_content, extract_text_with_spacing
+
+class TXTML:
+
+  @classmethod
+  def _clean_text_(cls, text):
+    return text.replace('\n', '')
+
+  @classmethod
+  def get_document_type(cls, document):
+    return document.getchildren()[0].text
+
+  @classmethod
+  def get_HTML_from_document(cls, document):
+    properties = {}
+
+    while document.tag != 'text':
+      properties[document.tag] = cls._clean_text_(document.text)
+      docs = document.getchildren()
+      if docs:
+        document = docs[0]
+      else:
+        break
+
+    return document, properties
+
+  @classmethod
+  def parse_full_10K(cls, doc) -> str:
+    text = ""
+    for child in doc.getchildren():
+      if child.tag == 'sec-header':
+          continue
+      html, properties = TXTML.get_HTML_from_document(child)
+      if properties.get('type') and '10-K' in properties['type']:
+        text = f"{text} {extract_text_with_spacing(html)}"
+    return text
+
+  @classmethod
+  def to_xml(cls, doc):
+    return html.unescape(tostring(doc).decode("utf8"))
+
+  @classmethod
+  def to_xml_bytes(cls, doc):
+    return html.unescape(tostring(doc).decode("utf8"))
