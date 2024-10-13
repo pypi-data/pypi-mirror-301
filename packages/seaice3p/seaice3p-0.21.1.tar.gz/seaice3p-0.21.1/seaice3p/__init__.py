f78cf6633d7fbcaa3ad574bba00a12001988044e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:36 +0000

    Add package seaice3p/0.21.1/seaice3p-0.21.1.tar.gz

---
+++ packages/seaice3p/seaice3p-0.21.1.tar.gz/seaice3p-0.21.1/seaice3p/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.21.1"
+
+# Exported functions and classes
+from .params import *
+from .run_simulation import solve, run_batch
+from .grids import Grids
+from .load import load_simulation
