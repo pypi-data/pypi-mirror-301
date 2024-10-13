168fe4050eba46d4a40417e4320512a0edc1ea23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:22 +0000

    Add package trustworthyai-text/0.2.9/trustworthyai_text-0.2.9-py3-none-any.whl

---
+++ packages/trustworthyai-text/trustworthyai_text-0.2.9-py3-none-any.whl/trustworthyai_text/tai_text_insights/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Implementation of RAI Text Insights API."""
+
+from trustworthyai_text.tai_text_insights.tai_text_insights import \
+    RAITextInsights
+
+__all__ = ['RAITextInsights']
