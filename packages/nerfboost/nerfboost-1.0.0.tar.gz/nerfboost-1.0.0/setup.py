b33a11be66716dd078e67fce4b78e5d9b72fac8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 05:04:08 +0000

    Add package nerfboost/1.0.0/nerfboost-1.0.0.tar.gz

---
+++ packages/nerfboost/nerfboost-1.0.0.tar.gz/nerfboost-1.0.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+# Read the README for the long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='nerfboost',
+    version='1.0.0',
+    description='A CUDA-enhanced package for common NeRF model operations',
+    long_description=long_description,  # This will pull the model description from the README
+    long_description_content_type="text/markdown",  # Ensure markdown is correctly interpreted
+    author='Akshay Pappu',
+    license='MIT License (MIT)',
+    python_requires='>=3.8',
+    install_requires=[
+        'torch',  
+    ],
+    ext_modules=[
+        CUDAExtension(
+            name='nerfboost',
+            sources=[
+                'nerfboost/positional_encoding.cu', 
+                'nerfboost/sampling.cu',
+                'nerfboost/volume_rendering.cu',
+                'nerfboost/mlp_network.cu',
+                'nerfboost/rendering_loss.cu',
+                'nerfboost/generate_rays.cu',
+                'nerfboost/pybind_module.cu'
+            ]
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
