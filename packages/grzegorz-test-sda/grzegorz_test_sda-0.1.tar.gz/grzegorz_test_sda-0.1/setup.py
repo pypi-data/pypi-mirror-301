009045231ba1ce7db0a1ca9da598aacdedbbfff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:21 +0000

    Add package grzegorz-test-sda/0.1/grzegorz_test_sda-0.1.tar.gz

---
+++ packages/grzegorz-test-sda/grzegorz_test_sda-0.1.tar.gz/grzegorz_test_sda-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="grzegorz_test_sda",
+    version="0.1",
+    description="Opis",
+    url="http://google.pl",
+    install_requires = [],
+    packages=['pakiet'],
+
+)
