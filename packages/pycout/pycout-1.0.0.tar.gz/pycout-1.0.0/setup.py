57767134bdbd74f4a47bcbd59c11d46ac3b2ad0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:51 +0000

    Add package pycout/1.0.0/pycout-1.0.0.tar.gz

---
+++ packages/pycout/pycout-1.0.0.tar.gz/pycout-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup  # type: ignore
+
+setup(
+    name="pycout",
+    version="1.0.0",
+    packages=["pycout"],
+    url="http://github.com/EnzoBnl/pycout",
+    license="Apache 2.",
+    author="Enzo Bonnal",
+    author_email="enzobonnal@gmail.com",
+    description="""C++'s `cout << "Hello" << " World !" << endl;` in Python.""",
+)
