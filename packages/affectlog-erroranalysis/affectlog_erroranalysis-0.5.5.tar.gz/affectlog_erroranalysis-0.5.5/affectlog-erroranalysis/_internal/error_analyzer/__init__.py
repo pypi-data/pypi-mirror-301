02f7f0b512d318b429f19d70e77b6ee2bcb6e91a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 05:04:07 +0000

    Add package affectlog-erroranalysis/0.5.5/affectlog_erroranalysis-0.5.5.tar.gz

---
+++ packages/affectlog-erroranalysis/affectlog_erroranalysis-0.5.5.tar.gz/affectlog_erroranalysis-0.5.5/affectlog-erroranalysis/_internal/error_analyzer/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Init file, used for backwards compatibility."""
+from affectlog-erroranalysis.analyzer import ModelAnalyzer, PredictionsAnalyzer
+
+__all__ = ['PredictionsAnalyzer', 'ModelAnalyzer']
