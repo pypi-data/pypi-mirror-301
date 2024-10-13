adf7e5f3bdf1e35a8d259f9bb2a5aff6a1960de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:59 +0000

    Add package ktoolbox-pure-py/0.9.0/ktoolbox_pure_py-0.9.0.tar.gz

---
+++ packages/ktoolbox-pure-py/ktoolbox_pure_py-0.9.0.tar.gz/ktoolbox_pure_py-0.9.0/ktoolbox/__main__.py
@@ -0,0 +1,20 @@
+import fire
+from loguru import logger
+
+from ktoolbox.cli import KToolBoxCli
+from ktoolbox.configuration import config
+from ktoolbox.utils import logger_init, uvloop_init
+
+
+def main():
+    try:
+        logger_init(cli_use=True)
+        logger.info(repr(config))
+        uvloop_init()
+        fire.Fire(KToolBoxCli)
+    except KeyboardInterrupt:
+        logger.error("KToolBox was interrupted by the user")
+
+
+if __name__ == "__main__":
+    main()
