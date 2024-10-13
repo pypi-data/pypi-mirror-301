e163ace4445b1077e3e222c39e0fea526d78af2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:04:46 +0000

    Add package nozyio/0.1.3/nozyio-0.1.3-py3-none-any.whl

---
+++ packages/nozyio/nozyio-0.1.3-py3-none-any.whl/nozyio/web/dist/assets/Stack-1B6MSRlq.js
@@ -0,0 +1,1 @@
+import{j as e}from"./reactflow-vendor-DYfacRG4.js";import{r as s}from"./react-vendor-CMZ2fpGT.js";const a=s.forwardRef((t,r)=>e.jsx("div",{ref:r,style:{display:"flex",flexDirection:"column",...t.style},...t}));a.displayName="Stack";export{a as S};
