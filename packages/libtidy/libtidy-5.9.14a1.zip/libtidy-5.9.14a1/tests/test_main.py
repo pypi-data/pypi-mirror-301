74e8869cac0317b5cf92d36cdb9e6a880df07be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:04:35 +0000

    Add package libtidy/5.9.14a1/libtidy-5.9.14a1.zip

---
+++ packages/libtidy/libtidy-5.9.14a1.zip/libtidy-5.9.14a1/tests/test_main.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2024 Adam Karpierz
+# SPDX-License-Identifier: HTMLTIDY
+
+import unittest
+
+import libtidy
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_main(self):
+        pass
