4f91eff0ce47c8a17f8a6802f394689a046a2bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:05:40 +0000

    Add package wwwpy/0.1.24/wwwpy-0.1.24.tar.gz

---
+++ packages/wwwpy/wwwpy-0.1.24.tar.gz/wwwpy-0.1.24/src/wwwpy/remote/designer/log_redirect.py
@@ -0,0 +1,17 @@
+from js import console
+
+from wwwpy.common.designer import log_emit
+from wwwpy.remote import set_timeout
+from wwwpy.server.designer import rpc
+
+
+def redirect_logging():
+    def emit(msg: str):
+        console.log(msg)
+
+        async def _():
+            await rpc.server_console_log(msg)
+
+        set_timeout(_)
+
+    log_emit.add_once(emit)
