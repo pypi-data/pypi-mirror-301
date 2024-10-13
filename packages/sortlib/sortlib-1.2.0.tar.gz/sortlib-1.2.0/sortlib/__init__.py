bb88d312895ddebd790d0f2acc4defc62b5e02e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:41 +0000

    Add package sortlib/1.2.0/sortlib-1.2.0.tar.gz

---
+++ packages/sortlib/sortlib-1.2.0.tar.gz/sortlib-1.2.0/sortlib/__init__.py
@@ -0,0 +1,67 @@
+# your_package/__init__.py
+
+"""
+Your Package Name: Sorting Algorithms Library
+Description: A collection of sorting algorithms implemented in Python.
+"""
+
+# Import the sorting functions from the sorting module
+from .sorting import (
+    merge_sort,
+    quick_sort,
+    bubble_sort,
+    insertion_sort,
+    selection_sort,
+    heap_sort,
+    shell_sort,
+    counting_sort,
+    radix_sort,
+    bucket_sort,
+    comb_sort,
+    cocktail_sort,
+    gnome_sort,
+    cycle_sort,
+    pigeonhole_sort,
+    strand_sort,
+    pancake_sort,
+    bogo_sort,
+    stooge_sort,
+    tim_sort,
+    read_numbers,
+    sort_numbers,
+    format_output,
+    allsort,
+    allformat,
+    write_to_file  # Добавлена новая функция
+)
+
+__all__ = [
+    "merge_sort",
+    "quick_sort",
+    "bubble_sort",
+    "insertion_sort",
+    "selection_sort",
+    "heap_sort",
+    "shell_sort",
+    "counting_sort",
+    "radix_sort",
+    "bucket_sort",
+    "comb_sort",
+    "cocktail_sort",
+    "gnome_sort",
+    "cycle_sort",
+    "pigeonhole_sort",
+    "strand_sort",
+    "pancake_sort",
+    "bogo_sort",
+    "stooge_sort",
+    "tim_sort",
+    "read_numbers",
+    "sort_numbers",
+    "format_output",
+    "allsort",
+    "allformat",
+    "write_to_file"  # Добавлена новая функция
+]
+
+__version__ = "0.1.1"  # Обновлен номер версии
