5963b81f1a32a93d75e18ee27d002408c9acee84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:50 +0000

    Add package web3db/1.0.5/web3db-1.0.5-py3-none-any.whl

---
+++ packages/web3db/web3db-1.0.5-py3-none-any.whl/web3db/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import Base
+from .discord import Discord
+from .email import Email
+from .profile import RemoteProfile, LocalProfile
+from .twitter import Twitter
+from .proxy import Proxy
+from .github import Github
