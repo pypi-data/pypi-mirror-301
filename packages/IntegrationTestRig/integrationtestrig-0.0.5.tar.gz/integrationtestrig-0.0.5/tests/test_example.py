b9289c48166fb3177579adba2211e24de05c553e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:19 +0000

    Add package IntegrationTestRig/0.0.5/integrationtestrig-0.0.5.tar.gz

---
+++ packages/IntegrationTestRig/integrationtestrig-0.0.5.tar.gz/integrationtestrig-0.0.5/tests/test_example.py
@@ -0,0 +1,9 @@
+import requests
+from IntegrationTestRig import IntegrationTestRig
+
+class TestExample(IntegrationTestRig):
+    resource_file = "tests/ExampleService.py"
+
+    def test_example(self):
+        response = requests.get("http://127.0.0.1:8000/")
+        self.assertEqual(response.text, "Hello, World!")
