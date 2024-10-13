c98c2213d2a26c9380d68491c864220fa963ea48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:51 +0000

    Add package pycout/1.0.0/pycout-1.0.0-py3-none-any.whl

---
+++ packages/pycout/pycout-1.0.0-py3-none-any.whl/pycout/cout.py
@@ -0,0 +1,33 @@
+import sys
+from typing import Any, List, Optional
+
+endl = object()
+
+
+class Cout:
+    """
+    ```python
+    from pycout import *
+
+    cout << "Hello" << " World!" << endl
+    ```
+    """
+    def __init__(self, line: str = "") -> None:
+        self.line = line
+
+    def __lshift__(self, other: Any) -> Optional["Cout"]:
+        if other is endl:
+            sys.stdout.write("".join(self.line))
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+            return None
+        return Cout(self.line + str(other))
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Cout) and self.line == other.line
+
+    def __str__(self) -> str:
+        return self.line
+
+
+cout = Cout()
