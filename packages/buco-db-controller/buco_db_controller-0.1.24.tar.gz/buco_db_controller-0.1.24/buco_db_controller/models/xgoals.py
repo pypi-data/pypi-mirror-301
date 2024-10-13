4f5002b07e107d08adc6ee2f6b4d4c6c19bc6af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:30 +0000

    Add package buco-db-controller/0.1.24/buco_db_controller-0.1.24.tar.gz

---
+++ packages/buco-db-controller/buco_db_controller-0.1.24.tar.gz/buco_db_controller-0.1.24/buco_db_controller/models/xgoals.py
@@ -0,0 +1,51 @@
+from buco_db_controller.models.team import Team
+
+
+class XGoals:
+    def __init__(
+            self,
+            fixture_id,
+            date,
+
+            home_team: Team,
+            away_team: Team,
+
+            home_xg,
+            away_xg,
+
+            home_goals,
+            away_goals
+    ):
+        self.fixture_id = fixture_id
+        self.date = date
+
+        self.home_team = home_team
+        self.away_team = away_team
+
+        self.home_xg = home_xg
+        self.away_xg = away_xg
+
+        self.home_goals = home_goals
+        self.away_goals = away_goals
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            fixture_id=data['fixture']['id'],
+            date=data['date'],
+
+            home_team=Team(
+                team_id=data['teams']['home']['id'],
+                name=data['teams']['home']['name'],
+            ),
+            away_team=Team(
+                team_id=data['teams']['away']['id'],
+                name=data['teams']['away']['name'],
+            ),
+
+            home_xg=data['teams']['home']['xg'],
+            away_xg=data['teams']['away']['xg'],
+
+            home_goals=data['teams']['home']['goals'],
+            away_goals=data['teams']['away']['goals']
+        )
