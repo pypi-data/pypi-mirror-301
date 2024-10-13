43075536397db176a5e370d4374b7a1697fcc89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:29 +0000

    Add package pytconf/0.1.18/pytconf-0.1.18-py3-none-any.whl

---
+++ packages/pytconf/pytconf-0.1.18-py3-none-any.whl/pytconf/__init__.py
@@ -0,0 +1,12 @@
+from pytconf.param import ParamCreator  # noqa F401
+from pytconf.config import (  # noqa F401
+    Config,
+    register_main,
+    config_arg_parse_and_launch,
+    register_endpoint,
+    register_function,
+    get_free_args,
+    write_config_file,
+    write_config,
+    rm_config_file,
+)
