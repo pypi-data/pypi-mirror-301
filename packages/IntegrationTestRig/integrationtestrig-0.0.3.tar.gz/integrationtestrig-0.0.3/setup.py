bf2cd243e952194f91b8821017c2c5d9410cc7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:04:15 +0000

    Add package IntegrationTestRig/0.0.3/integrationtestrig-0.0.3.tar.gz

---
+++ packages/IntegrationTestRig/integrationtestrig-0.0.3.tar.gz/integrationtestrig-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="IntegrationTestRig",
+    version="0.0.3",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            # Add any command-line scripts here
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
