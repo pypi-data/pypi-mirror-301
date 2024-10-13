8c9aad44bc94f919e85c817cc055b4bd661d2695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:05:13 +0000

    Add package hybrilang/0.2.2/hybrilang-0.2.2.tar.gz

---
+++ packages/hybrilang/hybrilang-0.2.2.tar.gz/hybrilang-0.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hybrilang',  # Nazwa pakietu na PyPI
+    version='0.2.2',  # Zwiększ numer wersji!
+    description='The Hybrilang programming language',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Artur Wyroslak', # Twoje imię
+    author_email='artur.wyroslak@gmail.com', # Twój email
+    license='MIT', # Licencja
+    packages=find_packages(exclude=("tests",)), # Znajdź wszystkie pakiety
+    install_requires=['llvmlite', 'js2py'], # Zależności 
+    entry_points={
+        'console_scripts': [
+            'hybrilang=hybrilang:main' # Polecenie "hybrilang"
+        ],
+    },
+    classifiers=[  # Opcjonalne metadane dla PyPI
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Minimalna wersja Pythona
+)
