fbca4050d116863387ba4146aedbd3eb4423c89b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:05:21 +0000

    Add package fujin-cli/0.1.0/fujin_cli-0.1.0.tar.gz

---
+++ packages/fujin-cli/fujin_cli-0.1.0.tar.gz/fujin_cli-0.1.0/README.md
@@ -0,0 +1,26 @@
+# fujin
+
+[![PyPI - Version](https://img.shields.io/pypi/v/fujin.svg)](https://pypi.org/project/fujin)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/fujin.svg)](https://pypi.org/project/fujin)
+
+-----
+
+> [!IMPORTANT]
+> This package currently contains no features and is a work-in-progress
+
+
+**Table of Contents**
+
+- [fujin](#fujin)
+  - [Installation](#installation)
+  - [License](#license)
+
+## Installation
+
+```console
+pip install fujin
+```
+
+## License
+
+`fujin` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/fujin-cli/fujin_cli-0.1.0.tar.gz/fujin_cli-0.1.0/src/fujin/utils.py
@@ -0,0 +1,60 @@
+import json
+import urllib.request
+from functools import wraps
+
+from rich.progress import Progress
+from rich.progress import SpinnerColumn
+from rich.progress import TextColumn
+
+
+def with_progress(description, pass_progress=True):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with Progress(
+                SpinnerColumn(),
+                TextColumn("[cyan]{task.description}"),
+                transient=True,
+            ) as progress:
+                task = progress.add_task(description, total=None)
+                if pass_progress:
+                    kwargs["progress"] = lambda desc: progress.update(task, description=desc)
+                result = func(*args, **kwargs)
+                progress.update(task, completed=True)
+                progress.console.print(f"[green]{description} completed!")
+                return result
+
+        return wrapper
+
+    return decorator
+
+
+def _get_asset_id(token, owner, repo, project_name):
+    url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        data = json.loads(response.read().decode())
+        assets = data["assets"]
+    for asset in assets:
+        if asset["name"] == f"{project_name}-x86_64-linux":
+            return asset["id"]
+    msg = f"Asset not found with name {project_name}-x86_64-linux"
+    raise Exception(msg)
+
+
+def curl_binary_download_cmd(*, owner, repo, project_name=None, token=None):
+    curl = "curl -L -H 'Accept: application/octet-stream' "
+    project_name = project_name or repo
+    if not token:
+        return f"{curl} https://github.com/{owner}/{repo}/releases/latest/download/{project_name}-x86_64-linux"
+    asset_id = _get_asset_id(token, owner, repo, project_name)
+    return (
+        f"{curl} -H 'Authorization: Bearer {token}' "
+        f"-H 'X-GitHub-Api-Version: 2022-11-28' "
+        f"https://api.github.com/repos/{owner}/{repo}/releases/assets/{asset_id}"
+    )
