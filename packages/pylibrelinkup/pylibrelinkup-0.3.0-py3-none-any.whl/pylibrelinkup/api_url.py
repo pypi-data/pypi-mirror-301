d9ed1ceda2d9ded619caf4d9c494d2c8ac8342b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:05:13 +0000

    Add package pylibrelinkup/0.3.0/pylibrelinkup-0.3.0-py3-none-any.whl

---
+++ packages/pylibrelinkup/pylibrelinkup-0.3.0-py3-none-any.whl/pylibrelinkup/api_url.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from enum import StrEnum
+from typing import Type, cast
+
+__all__ = ["APIUrl"]
+
+
+class APIUrl(StrEnum):
+    EU = "https://api-eu.libreview.io"
+    EU2 = "https://api-eu2.libreview.io"
+    US = "https://api.libreview.io"
+    AE = "https://api-ae.libreview.io"
+    AP = "https://api-ap.libreview.io"
+    AU = "https://api-au.libreview.io"
+    CA = "https://api-ca.libreview.io"
+    DE = "https://api-de.libreview.io"
+    FR = "https://api-fr.libreview.io"
+    JP = "https://api-jp.libreview.io"
+    LA = "https://api-la.libreview.io"
+
+    @classmethod
+    def from_string(cls: Type[APIUrl], value: str) -> APIUrl:
+        for member in cls:
+            if member.lower() == value.lower():
+                return cast(APIUrl, member)
+        raise ValueError(f"{value} is not a valid {cls.__name__}")
