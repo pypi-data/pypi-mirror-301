8ebac38c6b4fba4a0fc57abef5613ee216a23b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:21 +0000

    Add package test-sda/0.1/test_sda-0.1.tar.gz

---
+++ packages/test-sda/test_sda-0.1.tar.gz/test_sda-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='test-sda',
+    version='0.1',
+    description='Description',
+    url='http://google.pl',
+    install_requires=[],
+    packages=['package']
+)
