f6aad8ec9d0f21cab930f47256cdb13512b0c423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:04:58 +0000

    Add package instacerty/1.0.0/instacerty-1.0.0.tar.gz

---
+++ packages/instacerty/instacerty-1.0.0.tar.gz/instacerty-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name="instacerty",
+    version="1.0.0",
+    description="A python package for generate your certificates instantly!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Madhanraj",
+    author_email="madhanreigns312@gmail.com",
+    url="https://github.com/iammadhanraj/instacerty",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'chardet==5.2.0',
+        'colorama==0.4.6',
+        'pillow==10.4.0',
+        'qrcode==8.0',
+        'reportlab==4.2.5',
+    ],
+    include_package_data=True,
+    package_data={
+        'instacerty': ['static/*'],
+    },
+)
