fb3ff75f81901ae0cf5b08e1721c17a630290060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:04:28 +0000

    Add package colour-hdri/0.2.5/colour_hdri-0.2.5-py3-none-any.whl

---
+++ packages/colour-hdri/colour_hdri-0.2.5-py3-none-any.whl/colour_hdri/generation/hdri.py
@@ -0,0 +1,127 @@
+"""
+HDRI Generation
+===============
+
+Define the HDRI generation objects:
+
+-   :func:`colour_hdri.image_stack_to_HDRI`
+
+See Also
+--------
+`Colour - HDRI - Examples Jupyter Notebooks
+<https://github.com/colour-science/colour-hdri/\
+blob/master/colour_hdri/examples>`__
+
+References
+----------
+-   :cite:`Banterle2011n` : Banterle, F., Artusi, A., Debattista, K., &
+    Chalmers, A. (2011). 2.1.1 Generating HDR Content by Combining Multiple
+    Exposures. In Advanced High Dynamic Range Imaging. A K Peters/CRC Press.
+    ISBN:978-1-56881-719-4
+"""
+
+from __future__ import annotations
+
+import numpy as np
+from colour.hints import ArrayLike, Callable, NDArrayFloat
+from colour.utilities import as_float_array, tsplit, tstack, warning
+
+from colour_hdri.exposure import average_luminance
+from colour_hdri.generation import weighting_function_Debevec1997
+from colour_hdri.utilities import ImageStack
+
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2015 Colour Developers"
+__license__ = "BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
+
+__all__ = [
+    "image_stack_to_HDRI",
+]
+
+
+def image_stack_to_HDRI(
+    image_stack: ImageStack,
+    weighting_function: Callable = weighting_function_Debevec1997,
+    camera_response_functions: ArrayLike | None = None,
+) -> NDArrayFloat | None:
+    """
+    Generate a HDRI from given image stack.
+
+    Parameters
+    ----------
+    image_stack
+        Stack of single channel or multi-channel floating point images. The
+        stack is assumed to be representing linear values except if
+        ``camera_response_functions`` argument is provided.
+    weighting_function
+        Weighting function :math:`w`.
+    camera_response_functions
+        Camera response functions :math:`g(z)` of the imaging system / camera
+        if the stack is representing non-linear values.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`
+        HDRI.
+
+    Warnings
+    --------
+    If the image stack contains images with negative or equal to zero values,
+    unpredictable results may occur and NaNs might be generated. It is
+    thus recommended encoding the images in a wider RGB colourspace or clamp
+    negative values.
+
+    References
+    ----------
+    :cite:`Banterle2011n`
+    """
+
+    image_c: NDArrayFloat | None = None
+    weight_c: NDArrayFloat | None = None
+    for i, image in enumerate(image_stack):
+        if image.data is not None and image.metadata is not None:
+            if image_c is None:
+                image_c = np.zeros(image.data.shape)
+                weight_c = np.zeros(image.data.shape)
+
+            L = 1 / average_luminance(
+                image.metadata.f_number,
+                image.metadata.exposure_time,
+                image.metadata.iso,
+            )
+
+            if np.any(image.data <= 0):
+                warning(
+                    f'"{image.path}" image channels contain negative or equal '
+                    f"to zero values, unpredictable results may occur! Please "
+                    f"consider encoding your images in a wider gamut RGB "
+                    f"colourspace or clamp negative values."
+                )
+
+            weights = weighting_function(image.data)
+            if i == 0:
+                weights[image.data >= 0.5] = 1
+            if i == len(image_stack) - 1:
+                weights[image.data <= 0.5] = 1
+
+            image_data = image.data
+            if camera_response_functions is not None:
+                camera_response_functions = as_float_array(camera_response_functions)
+                samples = np.linspace(0, 1, camera_response_functions.shape[0])
+
+                R, G, B = tsplit(image.data)
+                R = np.interp(R, samples, camera_response_functions[..., 0])
+                G = np.interp(G, samples, camera_response_functions[..., 1])
+                B = np.interp(B, samples, camera_response_functions[..., 2])
+                image_data = tstack([R, G, B])
+
+            image_c += weights * image_data / L
+            weight_c += weights
+
+    if image_c is not None and weight_c is not None:
+        image_c /= weight_c
+
+    return image_c
---
+++ packages/colour-hdri/colour_hdri-0.2.5-py3-none-any.whl/colour_hdri/generation/weighting_functions.py
@@ -0,0 +1,141 @@
+"""
+Weighting Functions
+===================
+
+Define the weighting function objects used when generating HDRIs:
+
+-   :func:`colour_hdri.normal_distribution_function`
+-   :func:`colour_hdri.hat_function`
+-   :func:`colour_hdri.weighting_function_Debevec1997`
+
+References
+----------
+-   :cite:`Debevec1997a` : Debevec, P. E., & Malik, J. (1997). Recovering high
+    dynamic range radiance maps from photographs. Proceedings of the 24th
+    Annual Conference on Computer Graphics and Interactive Techniques -
+    SIGGRAPH "97, August, 369-378. doi:10.1145/258734.258884
+"""
+
+from __future__ import annotations
+
+import numpy as np
+from colour.hints import ArrayLike, NDArrayFloat
+from colour.utilities import as_float_array
+
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2015 Colour Developers"
+__license__ = "BSD-3-Clause - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
+
+__all__ = [
+    "normal_distribution_function",
+    "hat_function",
+    "weighting_function_Debevec1997",
+]
+
+
+def normal_distribution_function(
+    a: ArrayLike, mu: float = 0.5, sigma: float = 0.15
+) -> NDArrayFloat:
+    """
+    Return given array weighted by a normal distribution function.
+
+    Parameters
+    ----------
+    a
+        Array to apply the weighting function onto.
+    mu
+        Mean or expectation.
+    sigma
+        Standard deviation.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`
+        Weighted array.
+
+    Examples
+    --------
+    >>> normal_distribution_function(np.linspace(0, 1, 10))
+    array([ 0.00386592,  0.03470859,  0.18002174,  0.53940751,  0.93371212,
+            0.93371212,  0.53940751,  0.18002174,  0.03470859,  0.00386592])
+    """
+
+    a = as_float_array(a)
+
+    return np.exp(-np.power(a - mu, 2) / (2 * np.power(sigma, 2)))
+
+
+def hat_function(a: ArrayLike) -> NDArrayFloat:
+    """
+    Return given array weighted by a hat function.
+
+    Parameters
+    ----------
+    a
+        Array to apply the weighting function onto.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`
+        Weighted array.
+
+    Examples
+    --------
+    >>> hat_function(np.linspace(0, 1, 10))
+    array([ 0.        ,  0.95099207,  0.99913557,  0.99999812,  1.        ,
+            1.        ,  0.99999812,  0.99913557,  0.95099207,  0.        ])
+    """
+
+    a = as_float_array(a)
+
+    return 1 - (2 * a - 1) ** 12  # pyright: ignore
+
+
+def weighting_function_Debevec1997(
+    a: ArrayLike, domain_l: float = 0.01, domain_h: float = 0.99
+) -> NDArrayFloat:
+    """
+    Return given array weighted by *Debevec (1997)* function.
+
+    Parameters
+    ----------
+    a
+        Array to apply the weighting function onto.
+    domain_l
+        Domain lowest possible value, values less than ``domain_l`` will be set
+        to zero.
+    domain_h
+        Domain highest possible value, values greater than ``domain_h`` will be
+        set to zero.
+
+    Returns
+    -------
+    :class:`numpy.ndarray`
+        Weighted array.
+
+    References
+    ----------
+    :cite:`Debevec1997a`
+
+    Examples
+    --------
+    >>> weighting_function_Debevec1997(np.linspace(0, 1, 10))
+    array([ 0.        ,  0.23273657,  0.48849105,  0.74424552,  1.        ,
+            1.        ,  0.74424552,  0.48849105,  0.23273657,  0.        ])
+    """
+
+    a = as_float_array(a)
+
+    w = np.zeros(a.shape)
+
+    mask = np.where(a <= (domain_l + domain_h) / 2, True, False)
+    w[mask] = a[mask] - domain_l
+    w[~mask] = domain_h - a[~mask]
+    w /= np.max(w)
+
+    w[w < 0] = 0
+
+    return w
