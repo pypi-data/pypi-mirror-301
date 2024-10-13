bee43d371db45f194e3b2e13aad81d62edd6feba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:31 +0000

    Add package chatclicks/0.4.5/chatclicks-0.4.5.tar.gz

---
+++ packages/chatclicks/chatclicks-0.4.5.tar.gz/chatclicks-0.4.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatclicks',
+    version='0.4.5',  # Update the version for each release
+    description='A package for handling chat clicks through websockets.',
+    author='WillPiledriver',
+    author_email='smifthsmurfth@gmail.com',
+    url='https://github.com/WillPiledriver/chatclicks',
+    packages=find_packages(),
+    install_requires=[
+        'python-socketio',
+        'scikit-learn',
+        'numpy',
+        'aiohttp',
+        'asyncio',
+        'screeninfo'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
