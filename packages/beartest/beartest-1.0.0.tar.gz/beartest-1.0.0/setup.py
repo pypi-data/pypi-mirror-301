58a5b62ee9075cec4f44b5a699cce901f4bdde58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:04:57 +0000

    Add package beartest/1.0.0/beartest-1.0.0.tar.gz

---
+++ packages/beartest/beartest-1.0.0.tar.gz/beartest-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='beartest',
+    version='1.0.0',
+    author='bear',
+    author_email='bear@qq.com',
+    description='oh bear!',
+    long_description=long_description,
+    url='https://github.com/Qiyana-bear/bear_pip_test',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
