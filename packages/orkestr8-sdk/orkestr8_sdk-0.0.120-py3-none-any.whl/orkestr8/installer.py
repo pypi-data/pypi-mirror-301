e264c071bad1c943fa508b35abfd2cf3b1ae6cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:05:03 +0000

    Add package orkestr8-sdk/0.0.120/orkestr8_sdk-0.0.120-py3-none-any.whl

---
+++ packages/orkestr8-sdk/orkestr8_sdk-0.0.120-py3-none-any.whl/orkestr8/installer.py
@@ -0,0 +1,21 @@
+"""
+Installs ML package on the machine by unzipping file and installing
+"""
+
+import logging
+import subprocess
+
+logger = logging.getLogger()
+
+
+def install():
+    with open("log.txt", "w") as f:
+        logger.info("Installing foodenie_ml..")
+        subprocess.run(["tar -xvzf foodenie_ml.tar.gz"], stdout=f)
+        subprocess.run(
+            [
+                "cd foodenie_ml && python3 pip install -r requirements.txt && rm ../foodenie_ml.tar.gz"
+            ],
+            shell=True,
+            stdout=f,
+        )
