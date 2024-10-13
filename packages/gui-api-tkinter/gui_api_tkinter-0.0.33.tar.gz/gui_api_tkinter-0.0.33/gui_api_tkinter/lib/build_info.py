3da0492100335f6fe298c2837adce5df6b52f18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 14:04:50 +0000

    Add package gui-api-tkinter/0.0.33/gui_api_tkinter-0.0.33.tar.gz

---
+++ packages/gui-api-tkinter/gui_api_tkinter-0.0.33.tar.gz/gui_api_tkinter-0.0.33/gui_api_tkinter/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.12'
+    os_name = 'posix:ubuntu'
+    version = '0.0.33'
+    git_sha = '01971be3be02367764352abaa18ab4dc0bd0d588'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
