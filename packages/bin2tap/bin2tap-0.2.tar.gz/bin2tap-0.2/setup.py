4b3ce068285f98a358c9d5a539fc3aae874d040d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 15:05:08 +0000

    Add package bin2tap/0.2/bin2tap-0.2.tar.gz

---
+++ packages/bin2tap/bin2tap-0.2.tar.gz/bin2tap-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bin2tap',
+    version='0.2',
+    author='Raül Torralba',
+    description='Convert a binary file into ZX Spectrum TAP file',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        bin2tap=bin2tap.cli:main
+    ''',
+    url = 'https://github.com/rtorralba/bin2tap',
+)
