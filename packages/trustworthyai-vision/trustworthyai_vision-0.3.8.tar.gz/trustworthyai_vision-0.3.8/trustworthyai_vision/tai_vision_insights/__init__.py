ed46bf29555795253715b84e98e9cc6d7f74d3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 03:11:17 +0000

    Add package trustworthyai-vision/0.3.8/trustworthyai_vision-0.3.8.tar.gz

---
+++ packages/trustworthyai-vision/trustworthyai_vision-0.3.8.tar.gz/trustworthyai_vision-0.3.8/trustworthyai_vision/tai_vision_insights/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Implementation of RAI Vision Insights API."""
+
+from trustworthyai_vision.tai_vision_insights.tai_vision_insights import \
+    RAIVisionInsights
+
+__all__ = ['RAIVisionInsights']
