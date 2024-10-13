c65ce61b0535d9d5954438553a8dd36c2ae04a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 05:04:09 +0000

    Add package trustworthyai/0.0.2/trustworthyai-0.0.2.tar.gz

---
+++ packages/trustworthyai/trustworthyai-0.0.2.tar.gz/trustworthyai-0.0.2/trustworthyai/modelanalysis/constants/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) AffectLog SAS
+# Licensed under the MIT License.
+
+"""Defines ModelTask for backcompat by reimporting from new location."""
+
+from affectlog_utils.models import ModelTask
+
+__all__ = ["ModelTask"]
