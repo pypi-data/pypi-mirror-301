812ac46935547b3a872062f7476d1d7b61898319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-13 04:05:00 +0000

    Add package libvcs/0.31.0/libvcs-0.31.0.tar.gz

---
+++ packages/libvcs/libvcs-0.31.0.tar.gz/libvcs-0.31.0/tests/test_pytest_plugin.py
@@ -0,0 +1,111 @@
+"""Tests for libvcs pytest plugin."""
+
+import pathlib
+import shutil
+import textwrap
+
+import pytest
+
+from libvcs.pytest_plugin import CreateRepoPytestFixtureFn
+
+
+@pytest.mark.skipif(not shutil.which("git"), reason="git is not available")
+def test_create_git_remote_repo(
+    create_git_remote_repo: CreateRepoPytestFixtureFn,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+) -> None:
+    """Tests for create_git_remote_repo pytest fixture."""
+    git_remote_1 = create_git_remote_repo()
+    git_remote_2 = create_git_remote_repo()
+
+    assert git_remote_1 != git_remote_2
+
+
+@pytest.mark.skipif(not shutil.which("svn"), reason="svn is not available")
+def test_create_svn_remote_repo(
+    create_svn_remote_repo: CreateRepoPytestFixtureFn,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+) -> None:
+    """Tests for create_svn_remote_repo pytest fixture."""
+    svn_remote_1 = create_svn_remote_repo()
+    svn_remote_2 = create_svn_remote_repo()
+
+    assert svn_remote_1 != svn_remote_2
+
+
+def test_plugin(
+    pytester: pytest.Pytester,
+    monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Tests for libvcs pytest plugin at large."""
+    # Initialize variables
+    pytester.plugins = ["pytest_plugin"]
+    pytester.makefile(
+        ".ini",
+        pytest=textwrap.dedent(
+            """
+[pytest]
+addopts=-vv
+        """.strip(),
+        ),
+    )
+    pytester.makeconftest(
+        textwrap.dedent(
+            r"""
+import pathlib
+import pytest
+
+@pytest.fixture(autouse=True)
+def setup(
+    request: pytest.FixtureRequest,
+    gitconfig: pathlib.Path,
+    set_home: pathlib.Path,
+) -> None:
+    pass
+    """,
+        ),
+    )
+    tests_path = pytester.path / "tests"
+    files = {
+        "example.py": textwrap.dedent(
+            """
+import pathlib
+
+from libvcs.sync.git import GitSync
+from libvcs.pytest_plugin import CreateRepoPytestFixtureFn
+
+def test_repo_git_remote_checkout(
+    create_git_remote_repo: CreateRepoPytestFixtureFn,
+    tmp_path: pathlib.Path,
+    projects_path: pathlib.Path,
+) -> None:
+    git_server = create_git_remote_repo()
+    git_repo_checkout_dir = projects_path / "my_git_checkout"
+    git_repo = GitSync(path=str(git_repo_checkout_dir), url=f"file://{git_server!s}")
+
+    git_repo.obtain()
+    git_repo.update_repo()
+
+    assert git_repo.get_revision() == "initial"
+
+    assert git_repo_checkout_dir.exists()
+    assert pathlib.Path(git_repo_checkout_dir / ".git").exists()
+        """,
+        ),
+    }
+    first_test_key = next(iter(files.keys()))
+    first_test_filename = str(tests_path / first_test_key)
+
+    tests_path.mkdir()
+    for file_name, text in files.items():
+        test_file = tests_path / file_name
+        test_file.write_text(
+            text,
+            encoding="utf-8",
+        )
+
+    # Test
+    result = pytester.runpytest(str(first_test_filename))
+    result.assert_outcomes(passed=1)
