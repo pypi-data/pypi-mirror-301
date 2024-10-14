d0535a8e916aeff35dcd393b9236d84da6ef9c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 05:04:45 +0000

    Add package qa-auto-loc/2.1.2/qa_auto_loc-2.1.2-py3-none-any.whl

---
+++ packages/qa-auto-loc/qa_auto_loc-2.1.2-py3-none-any.whl/auto_loc/run_inspector.py
@@ -0,0 +1,14 @@
+import argparse
+from auto_loc import WebInspector
+
+def main():
+    parser = argparse.ArgumentParser(description="Run Web Inspector")
+    parser.add_argument('--url', type=str, help="URL to inspect", required=True)
+    args = parser.parse_args()
+
+    inspector = WebInspector()
+    inspector.start(args.url)
+    inspector.quit()
+
+if __name__ == "__main__":
+    main()
