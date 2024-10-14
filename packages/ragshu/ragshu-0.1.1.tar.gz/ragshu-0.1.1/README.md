15cdc41d857e0201657145c528d36cdfa44ba67c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:40 +0000

    Add package ragshu/0.1.1/ragshu-0.1.1.tar.gz

---
+++ packages/ragshu/ragshu-0.1.1.tar.gz/ragshu-0.1.1/README.md
@@ -0,0 +1,21 @@
+
+# My Awesome Library
+
+This is a Python library that provides basic math utility functions.
+
+## Installation
+
+You can install this library using `pip`:
+
+```bash
+pip install ragshu
+```
+
+## Usage
+
+```python
+from ragshu import add
+
+result = add(1, 2)
+print(result)  # Output: 3
+```
---
+++ packages/ragshu/ragshu-0.1.1.tar.gz/ragshu-0.1.1/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ragshu',             # Name of the package
+    version='0.1.1',                   # Version of the package
+    packages=find_packages(),          # Automatically find packages in the directory
+    install_requires=[],               # Dependencies (if any)
+    author='Raghav',
+    author_email='raghavshukla2010@gmail.com',
+    description='A simple math utility library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',                     # License type
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
