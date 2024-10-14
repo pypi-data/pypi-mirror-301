c21ae533f30f1da0543d40f63ff3364a3ab0edcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:09 +0000

    Add package cngn-manager/0.0.5/cngn_manager-0.0.5-py3-none-any.whl

---
+++ packages/cngn-manager/cngn_manager-0.0.5-py3-none-any.whl/cngn_manager/constants.py
@@ -0,0 +1,14 @@
+class Network:
+    BSC = "bsc"
+    ETH = "eth"
+    ATC = "atc"
+    MATIC = "matic"
+    TRX = "trx"
+    XBN = "xbn"
+    
+    # Add other network options if necessary
+
+class ProviderType:
+    KORAPAY = "korapay"
+    FLUTTERWAVE = "Flutterwave"
+    # Add other provider types as needed
