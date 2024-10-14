2c180f22175d2d8705474646912d8b5cccc1e933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:07 +0000

    Add package poetry-dockerize-plugin/1.3.1/poetry_dockerize_plugin-1.3.1-py3-none-any.whl

---
+++ packages/poetry-dockerize-plugin/poetry_dockerize_plugin-1.3.1-py3-none-any.whl/poetry_dockerize_plugin/plugin.py
@@ -0,0 +1,48 @@
+import os
+
+from cleo.commands.command import Command
+from cleo.helpers import option
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from poetry_dockerize_plugin.builder import build_image
+
+
+class DockerCommand(Command):
+
+    name = "dockerize"
+    description = "Generate a docker image of your project automatically, without configuration."
+    options = [  # noqa: RUF012
+        option(
+            "path",
+            "Project root path",
+            flag=False,
+            default=os.getcwd(),
+        ),
+        option(
+            "debug",
+            flag=True,
+            description="(poetry-dockerize-plugin) Debug mode",
+        ),
+        option(
+            "generate",
+            description="(poetry-dockerize-plugin) Generate and persist Dockerfile",
+            flag=True,
+        ),
+    ]
+
+    def handle(self) -> int:
+        build_image(
+            path=self.option("path"),
+            verbose=self.option("debug"),
+            generate=self.option("generate"),
+        )
+        return 0
+
+
+def factory():
+    return DockerCommand()
+
+
+class DockerApplicationPlugin(ApplicationPlugin):
+    def activate(self, application):
+        application.command_loader.register_factory("dockerize", factory)
