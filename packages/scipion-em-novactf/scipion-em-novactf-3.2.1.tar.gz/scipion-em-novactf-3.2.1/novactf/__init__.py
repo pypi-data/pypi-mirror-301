bb2541801dfef13b750ab0d1ed9fc6feb766c9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:07 +0000

    Add package scipion-em-novactf/3.2.1/scipion-em-novactf-3.2.1.tar.gz

---
+++ packages/scipion-em-novactf/scipion-em-novactf-3.2.1.tar.gz/scipion-em-novactf-3.2.1/novactf/__init__.py
@@ -0,0 +1,105 @@
+# *****************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# *****************************************************************************
+import os.path
+
+import pwem
+
+
+__version__ = '3.2.1'
+_references = ["Turonova2017"]
+NOVACTF_HOME = 'NOVACTF_HOME'
+
+
+class Plugin(pwem.Plugin):
+    _homeVar = NOVACTF_HOME
+    _url = "https://github.com/scipion-em/scipion-em-novactf"
+
+    @classmethod
+    def _defineVariables(cls):
+        cls._defineEmVar(NOVACTF_HOME, 'novactf-master')
+
+    @classmethod
+    def getEnviron(cls):
+        return None
+
+    @classmethod
+    def getDependencies(cls):
+        neededPrograms = ['wget', 'unzip', 'fftw3', 'fftw3f', 'lib64', 'gcc']
+
+        return neededPrograms
+
+    @classmethod
+    def defineBinaries(cls, env):
+        version = 'master'
+        NOVACTF_INSTALLED = 'novactf_%s_installed' % version
+        warningMsg = "'WARNING: if program fails when executing reinstall in scipion3/software/em/novactf-master " \
+                     "setting the path to the include files from fftw3 as well as following fftw3 libraries: fftw3 " \
+                     "fftw3f. Typically both libraries should be in one folder and thus one library path should be " \
+                     "sufficient. Example command:'" \
+                     "'make includepath = \"path_to_fftw_include_files\" libpath = \"path_to_fftw_libraries\"'" \
+                     "'NovaCTF also requires standard libraries with the standard path being " \
+                     "/usr/lib64 - if the libraries are elsewhere open makefile and change the path accordingly'"
+
+        # Display warning message
+        installationCmd = 'echo %s && ' % warningMsg
+
+        # Download git repo
+        installationCmd += 'wget https://github.com/turonova/novaCTF/archive/master.zip && ' \
+                           'unzip master.zip && '
+
+        # Binaries compilation
+        installationCmd += 'cd novaCTF-master && ' \
+                           'make && ' \
+                           'cd .. && '
+
+        # Create installation finished flag file
+        installationCmd += 'touch %s ' % NOVACTF_INSTALLED
+
+        env.addPackage('novactf',
+                       version=version,
+                       tar='void.tgz',
+                       neededProgs=cls.getDependencies(),
+                       commands=[(installationCmd, NOVACTF_INSTALLED)],
+                       default=True)
+
+    @classmethod
+    def runNovactf(cls, protocol, **kwargs):
+        """ Run NovaCTF command from a given protocol. """ 
+
+        novaCTF_home = cls.getVar(NOVACTF_HOME)
+
+        # We test whether the NOVACTF_HOME variable provides a direct path to the novaCTF binary:
+        if os.path.isfile( novaCTF_home + "/novaCTF" ):
+
+            fullProgram = os.path.join(novaCTF_home, "novaCTF")
+
+        # If not, we expect it under the novaCTF-master folder:
+        else:
+
+            fullProgram = os.path.join(novaCTF_home, "novaCTF-master", "novaCTF")
+
+        args = ' '.join([f"{k} {v}" for k, v in kwargs.items()])
+        protocol.runJob(fullProgram, args, env=cls.getEnviron())
---
+++ packages/scipion-em-novactf/scipion-em-novactf-3.2.1.tar.gz/scipion-em-novactf-3.2.1/novactf/bibtex.py
@@ -0,0 +1,41 @@
+# coding: latin-1
+# *****************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# *****************************************************************************
+"""
+
+@article{Turonova2017,
+title = "Efficient 3D-CTF correction for cryo-electron tomography using NovaCTF improves subtomogram averaging resolution to 3.4 Å",
+journal = "Journal of Structural Biology",
+volume = "199",
+number = "1",
+pages = "187 - 195",
+year = "2017",
+doi = "https://dx.doi.org/10.1016/j.jsb.2017.07.007",
+url = "https://www.sciencedirect.com/science/article/pii/S1047847717301272?via%3Dihub",
+author = "Beata Turonova and Florian K.M.Schur and William Wan and John A.G. Briggs"
+}
+
+"""
