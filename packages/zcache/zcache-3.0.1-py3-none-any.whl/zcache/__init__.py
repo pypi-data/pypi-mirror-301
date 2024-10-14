95ba35edc8a50dd5475c367cf846bc6a743e38fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:07:29 +0000

    Add package zcache/3.0.1/zcache-3.0.1-py3-none-any.whl

---
+++ packages/zcache/zcache-3.0.1-py3-none-any.whl/zcache/__init__.py
@@ -0,0 +1,10 @@
+# -*-coding:utf8;-*-
+from zcache.Core.Database import Database
+from zcache.Core.AsyncDatabase import AsyncDatabase
+
+
+Cache = Database
+AsyncCache = AsyncDatabase
+__version__ = "3.0.1"
+__author__ = "Guangrei <myawn@pm.me>"
+__license__ = "MIT"
