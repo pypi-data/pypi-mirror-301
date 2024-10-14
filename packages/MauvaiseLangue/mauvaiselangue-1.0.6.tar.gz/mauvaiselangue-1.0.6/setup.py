14975801bbca9675feac298ecfb698927e416985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:07:21 +0000

    Add package MauvaiseLangue/1.0.6/mauvaiselangue-1.0.6.tar.gz

---
+++ packages/MauvaiseLangue/mauvaiselangue-1.0.6.tar.gz/mauvaiselangue-1.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MauvaiseLangue',
+    version='1.0.6',  # Incremented version
+    description='A Python module to scrape French insults from Wiktionary',
+    author='john waia',
+    author_email='johnwaia25@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,  # To include files like cache if needed
+)
