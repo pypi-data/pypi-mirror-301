2095dbe529a5b43dabd4179996086bf4e77de509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:07 +0000

    Add package metacopier-api/0.9.1/metacopier_api-0.9.1-py3-none-any.whl

---
+++ packages/metacopier-api/metacopier_api-0.9.1-py3-none-any.whl/metacopier_api/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from metacopier_api.api.account_api_api import AccountAPIApi
+from metacopier_api.api.project_api_api import ProjectAPIApi
+from metacopier_api.api.trading_api_api import TradingAPIApi
+from metacopier_api.api.type_api_api import TypeAPIApi
+
