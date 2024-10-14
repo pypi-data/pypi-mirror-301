e148200091cce8982f5bdf192ffbd1c290cc5de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:12:58 +0000

    Add package aiotus/1.0.0/aiotus-1.0.0-py3-none-any.whl

---
+++ packages/aiotus/aiotus-1.0.0-py3-none-any.whl/aiotus/creation.py
@@ -0,0 +1,104 @@
+"""Implementation of the creation extension.
+
+The
+`creation extension <https://tus.io/protocols/resumable-upload.html#creation>`_
+defines how to reserve space on the server for uploading data to.
+"""
+
+from __future__ import annotations
+
+import asyncio
+import base64
+import io
+from collections.abc import Mapping
+from typing import BinaryIO, Optional
+
+import aiohttp
+import yarl
+
+from . import common
+from .log import logger
+
+
+def _check_metadata_keys(metadata: common.Metadata) -> None:
+    """Check if the metadata keys are valid.
+
+    Raises a 'ValueError' exception if a key is invalid.
+    """
+    for k in metadata:
+        if not k.isascii():
+            raise ValueError("Metadata keys must only contain ASCII characters.")
+
+        if " " in k:
+            raise ValueError("Metadata keys must not contain spaces.")
+
+        if "," in k:
+            raise ValueError("Metadata keys must not contain commas.")
+
+
+def encode_metadata(metadata: common.Metadata) -> str:
+    """Encode the metadata to the value of the metadata header.
+
+    :param metadata: The metadata to encode.
+    :return: The value for the "Upload-Metadata" header.
+    """
+    _check_metadata_keys(metadata)
+
+    def encode_value(value: Optional[bytes]) -> str:
+        if value is None:
+            return ""
+
+        encoded_bytes = base64.b64encode(value)
+        encoded_string = encoded_bytes.decode()
+        return " " + encoded_string
+
+    pairs = [f"{k}{encode_value(v)}" for k, v in metadata.items()]
+    return ",".join(pairs)
+
+
+async def create(
+    session: aiohttp.ClientSession,
+    url: yarl.URL,
+    file: Optional[BinaryIO],
+    metadata: common.Metadata,
+    ssl: common.SSLArgument = True,
+    headers: Optional[Mapping[str, str]] = None,
+) -> yarl.URL:
+    """Create an upload.
+
+    :param session: HTTP session to use for connections.
+    :param url: The creation endpoint of the server.
+    :param file: The file object to upload.
+        Used to determine the length of data to be uploaded. If not given, the
+        corresponding HTTP header is not included in the request.
+    :param metadata: Additional metadata for the upload.
+    :param ssl: SSL validation mode, passed on to aiohttp.
+    :param headers: Optional headers used in the request.
+    :return: The URL to upload the data to.
+    :raises common.ProtocolError: When the server does not comply to the tus protocol.
+    """
+    tus_headers = dict(headers or {})
+    tus_headers["Tus-Resumable"] = common.TUS_PROTOCOL_VERSION
+
+    if file is not None:
+        loop = asyncio.get_event_loop()
+        total_size = await loop.run_in_executor(None, file.seek, 0, io.SEEK_END)
+        tus_headers["Upload-Length"] = str(total_size)
+
+    if metadata_header := encode_metadata(metadata):
+        tus_headers["Upload-Metadata"] = metadata_header
+
+    logger.debug("Creating upload...")
+    async with await session.post(url, headers=tus_headers, ssl=ssl) as response:
+        response.raise_for_status()
+        if response.status != 201:
+            raise common.ProtocolError(
+                f"Wrong status code {response.status}, expected 201."
+            )
+
+        if "Location" not in response.headers:
+            raise common.ProtocolError(
+                'Upload created, but no "Location" header in response.'
+            )
+
+        return yarl.URL(response.headers["Location"])
---
+++ packages/aiotus/aiotus-1.0.0-py3-none-any.whl/aiotus/entrypoint.py
@@ -0,0 +1,110 @@
+"""Defines the commands for executing the module directly."""
+
+from __future__ import annotations
+
+import argparse
+import asyncio
+import logging
+import mimetypes
+import os.path
+import sys
+
+from . import retry
+
+
+def _upload(args: argparse.Namespace) -> int:
+    """Implement the "upload" command.
+
+    Returns the exit status for the program.
+    """
+    metadata = {"filename": os.path.basename(args.file).encode()}
+
+    if mime_type := mimetypes.guess_type(args.file)[0]:
+        metadata["mime_type"] = mime_type.encode()
+
+    for meta in args.metadata:
+        kv = meta.split("=", maxsplit=1)
+        metadata[kv[0]] = kv[1].encode() if (len(kv) == 2) else None
+
+    try:
+        with open(args.file, "rb") as file:
+            if location := asyncio.run(retry.upload(args.endpoint, file, metadata)):
+                print(str(location))
+                return 0
+    except KeyboardInterrupt:  # pragma: no cover
+        pass
+    except Exception as e:
+        logging.error(f"Unable to upload file: {e}")
+
+    return 1
+
+
+def _metadata(args: argparse.Namespace) -> int:
+    """Implement the "metadata" command.
+
+    Returns the exit status for the program.
+    """
+    try:
+        metadata = asyncio.run(retry.metadata(args.location))
+        # Silence mypy, it does not detect the type 'asyncio.run()' returns.
+        assert isinstance(metadata, dict)  # nosec B101
+
+        for k, v in metadata.items():
+            if v is None:
+                print(f"{k}")
+            else:
+                value = repr(v)[2:][:-1]
+                print(f"{k}: {value}")
+
+        return 0
+    except KeyboardInterrupt:  # pragma: no cover
+        return 1
+
+
+def main() -> int:
+    """Entrypoint function for when the module is executed directly.
+
+    :return: Exit status for the program.
+    """
+    if sys.executable:
+        interpreter = os.path.basename(sys.executable)
+    else:
+        interpreter = "python3"
+
+    parser = argparse.ArgumentParser(prog=f"{interpreter} -m aiotus")
+    parser.add_argument("--debug", action="store_true", help="log debug messages")
+    subparsers = parser.add_subparsers()
+
+    parser_upload = subparsers.add_parser(
+        "upload",
+        help="Upload a file to a tus (tus.io) server.",
+    )
+    parser_upload.add_argument(
+        "--metadata",
+        action="append",
+        default=[],
+        help="additional metadata to upload ('key[=value]')",
+    )
+    parser_upload.add_argument(
+        "endpoint", type=str, help="creation URL of the tus server"
+    )
+    parser_upload.add_argument("file", type=str, help="file to upload")
+    parser_upload.set_defaults(func=_upload)
+
+    parser_metadata = subparsers.add_parser(
+        "metadata",
+        help="Query the metadata of a file on a tus (tus.io) server.",
+    )
+    parser_metadata.add_argument(
+        "location", type=str, help="file location on the tus server"
+    )
+    parser_metadata.set_defaults(func=_metadata)
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    return args.func(args)  # type: ignore
