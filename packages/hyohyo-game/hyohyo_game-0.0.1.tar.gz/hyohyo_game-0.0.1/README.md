8783406b8b666cec486859c36fb529270c925d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:05:29 +0000

    Add package hyohyo-game/0.0.1/hyohyo_game-0.0.1.tar.gz

---
+++ packages/hyohyo-game/hyohyo_game-0.0.1.tar.gz/hyohyo_game-0.0.1/README.md
@@ -0,0 +1,12 @@
+## 가위바위보 게임 패키지
+
+이 패키지는 간단한 가위바위보 게임을 제공합니다.
+
+## 사용법
+
+```python
+from rock_paper_scissors import play
+
+result = play('가위')  # '가위', '바위', '보' 중 하나를 선택
+print(result)
+```
---
+++ packages/hyohyo-game/hyohyo_game-0.0.1.tar.gz/hyohyo_game-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hyohyo_game',
+    version='0.0.1',
+    packages=find_packages(),
+    description='rock_paper_scissors package',
+    author='hyojun',
+    author_email='statisticlee@gmail.com',
+)
