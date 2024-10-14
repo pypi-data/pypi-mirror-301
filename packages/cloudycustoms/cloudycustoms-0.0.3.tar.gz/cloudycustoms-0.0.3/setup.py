eb75d178a11da5d5cc83a03f6275fd43cdea1c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 05:04:43 +0000

    Add package cloudycustoms/0.0.3/cloudycustoms-0.0.3.tar.gz

---
+++ packages/cloudycustoms/cloudycustoms-0.0.3.tar.gz/cloudycustoms-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Collection of utility python modules'
+
+# Setting up
+setup(
+    name="cloudycustoms",
+    version=VERSION,
+    author="CloudEater (Nath Abraham)",
+    author_email="<nathCloud@gmail.com>",
+    description=DESCRIPTION,
+    long_description="A collection of utility python modules. Uses python random module",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'utility', 'random', 'numbers'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.12"
+    ]
+)
