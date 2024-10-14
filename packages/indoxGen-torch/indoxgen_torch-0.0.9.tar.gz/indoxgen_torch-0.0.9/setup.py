242747c3998c7cbf6362df434f2444761fadb620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:05:49 +0000

    Add package indoxGen-torch/0.0.9/indoxgen_torch-0.0.9.tar.gz

---
+++ packages/indoxGen-torch/indoxgen_torch-0.0.9.tar.gz/indoxgen_torch-0.0.9/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+# Read the requirements file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Extract package names
+packages = [req.split('==')[0] for req in requirements if '==' in req]
+
+# Read the README file for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='indoxGen-torch',
+    version='0.0.9',
+    license='AGPL-3.0-or-later',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Indox Synthetic Data Generation (GAN-pytorch)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='nerdstudio',
+    author_email='ashkan@nematifamilyfundation.onmicrosoft.com',
+    url='https://github.com/osllmai/IndoxGen/tree/master/libs/indoxGen_torch',
+    keywords=[
+        'AI',
+        'deep learning',
+        'language models',
+        'synthetic data generation',
+        'machine learning',
+        'NLP'
+    ],
+    install_requires=requirements,  # Dependencies from requirements.txt
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.9',
+)
