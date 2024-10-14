64ed98255fba920c39b241791c71e845ab0e6173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:05:47 +0000

    Add package sais-notify/0.0.15/sais_notify-0.0.15.tar.gz

---
+++ packages/sais-notify/sais_notify-0.0.15.tar.gz/sais_notify-0.0.15/sais/notify/__init__.py
@@ -0,0 +1,4 @@
+from sais.notify.auth.auth_info import EnvVarCredentialsProvider
+from sais.notify.handler.message_handler import MessageHandler
+from sais.notify.handler.keepalive_handler import KeepAliveHandler
+from sais.notify.types import NotifyType
