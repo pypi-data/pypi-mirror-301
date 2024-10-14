48ec49131b8933d6e7685e7a392e6876172b7f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:06:39 +0000

    Add package luna-paysure/0.7.1/luna_paysure-0.7.1.tar.gz

---
+++ packages/luna-paysure/luna_paysure-0.7.1.tar.gz/luna_paysure-0.7.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='luna_paysure',
+version='0.7.1',
+author='Peter Rabbit',
+author_email='prabbit@gmail.com',
+description='A very interesting package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
