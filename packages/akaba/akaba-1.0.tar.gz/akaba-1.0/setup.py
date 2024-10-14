92b4db19d94d26da3174bb5f0e2aebc2d0e73461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:13:58 +0000

    Add package akaba/1.0/akaba-1.0.tar.gz

---
+++ packages/akaba/akaba-1.0.tar.gz/akaba-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='akaba',  # Name of your CLI tool
+    version='1.0',
+    py_modules=['akaba'],  # Name of the Python file without the .py extension
+    packages=find_packages(),
+    install_requires=[
+        'Click',  # Required dependency for Click CLI
+        'pydantic-settings',  # Required dependency for config settings
+        'FastAPI',  # Required dependency for FastAPI
+        'SQLAlchemy',  # Required dependency for database operations
+        'alembic',  # Required for database migrations
+        'pydantic',  # Required for data validation
+    ],
+    entry_points='''
+        [console_scripts]
+        akaba=akaba:create_project_structure
+    ''',
+)
