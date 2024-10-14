4aa6bfc628f3b9dcbb74ae463ff57e3060301b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:06:09 +0000

    Add package multilingual-ime/0.1.0b1/multilingual_ime-0.1.0b1.tar.gz

---
+++ packages/multilingual-ime/multilingual_ime-0.1.0b1.tar.gz/multilingual_ime-0.1.0b1/multilingual_ime/deep_models.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+
+class LanguageDetectorModel(nn.Module):
+    def __init__(self, input_shape, num_classes):
+        super(LanguageDetectorModel, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_shape, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, num_classes),
+            nn.Softmax(dim=0),
+        )
+
+    def forward(self, x):
+        return self.model(x)
