8897e911e749117c653d5aacd7498a997f4672ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:13:12 +0000

    Add package sutilibrary/1.5.0/sutilibrary-1.5.0-py3-none-any.whl

---
+++ packages/sutilibrary/sutilibrary-1.5.0-py3-none-any.whl/sutilybrary/__init__.py
@@ -0,0 +1,27 @@
+"""
+Your Package Name: Comprehensive Algorithms Library
+Description: A collection of various algorithms implemented in Python.
+"""
+
+# Import sorting algorithms
+from .sorting import *
+
+# Import searching algorithms
+from .searching import *
+
+from .graph import *
+
+__version__ = "0.6.0"
+
+# You can create a dictionary to group algorithms by category
+algorithms = {
+    'sorting': sorting.__all__,
+    'searching': searching.__all__,
+    'graph': graph.__all__
+}
+
+__all__ = (
+    sorting.__all__ +
+    searching.__all__  + 
+    graph.__all__
+)
