47aecaa25a503f8b39ce250c7bdb735952988702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:06:56 +0000

    Add package kalib/0.17.27/kalib-0.17.27-py3-none-any.whl

---
+++ packages/kalib/kalib-0.17.27-py3-none-any.whl/kalib/_internal.py
@@ -0,0 +1,15 @@
+from kalib.internals import Who
+
+
+def to_bytes(x, charset=None) -> bytes:
+    if not isinstance(x, bytes | str):
+        msg = f'only bytes | str acceptable, not {Who.Is(x)}'
+        raise TypeError(msg)
+    return x.encode(charset or 'ascii') if isinstance(x, str) else x
+
+
+def to_ascii(x, *args, **kw) -> str:
+    if not isinstance(x, bytes | str):
+        msg = f'only bytes | str acceptable, not {Who.Is(x)}'
+        raise TypeError(msg)
+    return x if isinstance(x, str) else to_bytes(x, *args, **kw).decode('ascii')
