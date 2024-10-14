862920602c53e76948830de51718785c694681bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:12:47 +0000

    Add package twitter-cli-scraper/0.1.2/twitter-cli-scraper-0.1.2.tar.gz

---
+++ packages/twitter-cli-scraper/twitter-cli-scraper-0.1.2.tar.gz/twitter-cli-scraper-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twitter-cli-scraper',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'webdriver-manager',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'twitter-scraper=scraper.twitter_scraper:main',
+        ],
+    },
+    author='Muhammad Abdullah',
+    author_email='muhammadabdullah07014@gmail.com',
+    description='A CLI tool for scraping Twitter trends and tweets.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bsse23087/twitter-cli-scraper',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
