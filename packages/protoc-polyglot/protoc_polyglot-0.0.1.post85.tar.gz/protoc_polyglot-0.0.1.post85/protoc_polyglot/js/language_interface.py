dc206f8973a38a4a27b778107695d746b527424e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:46 +0000

    Add package protoc-polyglot/0.0.1.post85/protoc_polyglot-0.0.1.post85.tar.gz

---
+++ packages/protoc-polyglot/protoc_polyglot-0.0.1.post85.tar.gz/protoc_polyglot-0.0.1.post85/protoc_polyglot/js/language_interface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os, sys
+sys.path.insert(0, os.path.abspath('..'))
+from protoc_polyglot.cli import *
+
+
+class LanguageInterface(CommonInterface):
+    def _compile(self, dir_protos:str, dir_output: str, files:list[str]) -> None:
+        shutil.rmtree(dir_output, ignore_errors=True)
+        os.makedirs(dir_output, exist_ok=False)
+        
+        com = f"""protoc \
+        -I={dir_protos} \
+        {" ".join(files)} \
+        --js_out=import_style=commonjs:{dir_output} \
+        --grpc-web_out=import_style=commonjs,mode=grpcwebtext:{dir_output}"""
+        
+        print(com)
+        os.system(com)
