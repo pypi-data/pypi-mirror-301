2cd4772b6dedd216deddd7a050556a02fb740482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:25 +0000

    Add package ashpkg/1.0.0/ashpkg-1.0.0-py3-none-any.whl

---
+++ packages/ashpkg/ashpkg-1.0.0-py3-none-any.whl/divide/__init__.py
@@ -0,0 +1,4 @@
+# this is a dependency file
+# this helps Python look for the available modules in the packge directory
+# also used to mark the directory on the disk as a Python package
+from .divide_by_three import divide_by_three
