a76d89e6145718e51420acd289920430b264179a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 05:04:46 +0000

    Add package pyside-app-core/0.11.0/pyside_app_core-0.11.0-py3-none-any.whl

---
+++ packages/pyside-app-core/pyside_app_core-0.11.0-py3-none-any.whl/pyside_app_core/ui/widgets/tool_bar_ctx.py
@@ -0,0 +1,61 @@
+import contextlib
+from collections.abc import Iterator
+from typing import Literal
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMainWindow, QSizePolicy, QToolBar, QToolButton, QWidget
+
+from pyside_app_core.mixin.object_name_mixin import ObjectNameMixin
+
+ToolBarArea = Literal["top", "bottom", "left", "right"]
+
+_TOOL_BAR_AREA_MAP = {
+    "top": Qt.ToolBarArea.TopToolBarArea,
+    "bottom": Qt.ToolBarArea.BottomToolBarArea,
+    "right": Qt.ToolBarArea.RightToolBarArea,
+    "left": Qt.ToolBarArea.LeftToolBarArea,
+}
+
+
+class ToolBarContext(ObjectNameMixin, QToolBar):
+    def __init__(self, area: ToolBarArea, parent: QMainWindow, *, movable: bool = False) -> None:
+        self._area = area
+        self._actions: list[QAction] = []
+
+        if area == "left":
+            self._border_side = "right"
+        elif area == "right":
+            self._border_side = "left"
+        elif area == "top":
+            self._border_side = "bottom"
+        else:
+            self._border_side = "top"
+
+        self.OBJECT_NAME = f"ToolBar_{area}"
+        super().__init__(self.OBJECT_NAME, parent=parent)
+
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setGeometry(10, 10, self.width(), self.height())
+        self.setMovable(movable)
+        self.setIconSize(QSize(28, 28))
+
+        parent.addToolBar(_TOOL_BAR_AREA_MAP[self._area], self)
+
+    def add_stretch(self) -> None:
+        stretch = QWidget(self)
+        stretch.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.addWidget(stretch)
+
+    @contextlib.contextmanager
+    def add_action(self, name: str, icon: QIcon | None = None) -> Iterator[QAction]:
+        action = QAction(text=name, parent=self)
+        action.setObjectName(f"ToolBarAction_{name}")
+        if icon:
+            action.setIcon(icon)
+
+        self._actions.append(action)
+        if len(self._actions) == 1:
+            _ = next(c for c in self.children() if isinstance(c, QToolButton))
+        yield action
+        self.addAction(action)
