6100807a3549185a755f0cb0bc9e021888b5df22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:07:26 +0000

    Add package kognic-auth/3.4.0/kognic_auth-3.4.0-py3-none-any.whl

---
+++ packages/kognic-auth/kognic_auth-3.4.0-py3-none-any.whl/kognic/auth/__init__.py
@@ -0,0 +1,11 @@
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
+
+DEFAULT_HOST = "https://auth.app.kognic.com"
