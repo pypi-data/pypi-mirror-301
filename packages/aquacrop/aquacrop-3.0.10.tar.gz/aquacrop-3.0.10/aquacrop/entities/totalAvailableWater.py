af964335e535ce7bf6a2fad71ac0d0fbdaaf90c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:12:54 +0000

    Add package aquacrop/3.0.10/aquacrop-3.0.10.tar.gz

---
+++ packages/aquacrop/aquacrop-3.0.10.tar.gz/aquacrop-3.0.10/aquacrop/entities/totalAvailableWater.py
@@ -0,0 +1,29 @@
+from numba import float64, int64, boolean, types
+
+
+spec = [
+    ("Rz", float64),
+    ("Zt", float64),
+]
+
+
+
+class TAW:
+    """
+    TODO: THIS CLASS IS NOT USED - seems to now be in use, CB 14.09.23
+    **Attributes:**\n
+
+
+
+    `Rz` : `float` : .
+
+    `Zt` : `float` : .
+
+
+
+
+    """
+
+    def __init__(self):
+        self.Rz = 0.0
+        self.Zt = 0.0
