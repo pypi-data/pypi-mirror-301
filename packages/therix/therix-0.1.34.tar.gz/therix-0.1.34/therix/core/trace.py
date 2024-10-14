fc0a0fead103ac33dcd60fae0dfd698e047bba2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:06 +0000

    Add package therix/0.1.34/therix-0.1.34.tar.gz

---
+++ packages/therix/therix-0.1.34.tar.gz/therix-0.1.34/therix/core/trace.py
@@ -0,0 +1,8 @@
+from therix.core.pipeline_component import PipelineComponent
+from therix.entities.models import ConfigType
+
+
+class Trace(PipelineComponent):
+    def __init__(self, config):
+        config["host"] = "https://analytics.dev.therix.ai"
+        super().__init__(ConfigType.TRACE_DETAILS, "LANGFUSE", config)
