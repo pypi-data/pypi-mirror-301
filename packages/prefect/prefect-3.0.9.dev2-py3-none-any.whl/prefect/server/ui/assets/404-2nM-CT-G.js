ba7d0b1ef67b22fc6fca11b0c5e56e8bb303353f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:06:43 +0000

    Add package prefect/3.0.9.dev2/prefect-3.0.9.dev2-py3-none-any.whl

---
+++ packages/prefect/prefect-3.0.9.dev2-py3-none-any.whl/prefect/server/ui/assets/404-2nM-CT-G.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,m as a,o as t,de as r}from"./index-DGGjalVM.js";import{u as s}from"./usePageTitle-JaUVXf-x.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,m)=>(t(),o(a(r)))}});export{_ as default};
+//# sourceMappingURL=404-2nM-CT-G.js.map
