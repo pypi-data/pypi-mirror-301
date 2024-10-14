8115f3feb5e09f1764076342eb52802cafcd79ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:23 +0000

    Add package Productivity-Timer/0.1.1/productivity_timer-0.1.1.tar.gz

---
+++ packages/Productivity-Timer/productivity_timer-0.1.1.tar.gz/productivity_timer-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Productivity_Timer",
+    version="0.1.1",
+    description="A timer library for managing multiple timers for multiple individuals.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Abhishek",
+    author_email="abhishekshivtiwari@gmail.com",
+    url='https://github.com/Abhi-shekes/timer', 
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
