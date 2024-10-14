1db48a0abb91d3038afb56834e9f2f04e6b75b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:11 +0000

    Add package crypto-format-checking/1.7.5/crypto_format_checking-1.7.5.tar.gz

---
+++ packages/crypto-format-checking/crypto_format_checking-1.7.5.tar.gz/crypto_format_checking-1.7.5/src/crypto_regex_checker/__init__.py
@@ -0,0 +1,4 @@
+from crypto_regex_checker.errors import FakeUserAgentError, UserAgentError
+from crypto_regex_checker.fake import FakeUserAgent, UserAgent
+from crypto_regex_checker.settings import __version__ as VERSION
+CRC = UserAgent
