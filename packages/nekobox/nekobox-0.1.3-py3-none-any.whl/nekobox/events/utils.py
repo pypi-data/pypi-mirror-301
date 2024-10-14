a04c8864cdd3e83c7199d98766f7a5a264b5cee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:13:10 +0000

    Add package nekobox/0.1.3/nekobox-0.1.3-py3-none-any.whl

---
+++ packages/nekobox/nekobox-0.1.3-py3-none-any.whl/nekobox/events/utils.py
@@ -0,0 +1,26 @@
+import asyncio
+from typing import Type, TypeVar, Callable, Optional, Coroutine
+
+from loguru import logger
+from satori import EventType
+from satori.server import Event
+from lagrange.client.client import Client
+from lagrange.client.events import BaseEvent
+
+TEvent = TypeVar("TEvent", bound=BaseEvent)
+
+
+def event_register(
+    client: Client,
+    queue: asyncio.Queue[Event],
+    event_type: Type[TEvent],
+    handler: Callable[["Client", TEvent], Coroutine[None, None, Optional[Event]]],
+):
+    async def _after_handle(_client: Client, event: TEvent):
+        ev = await handler(_client, event)
+        if ev:
+            if ev.type != EventType.MESSAGE_CREATED:
+                logger.trace(f"Event '{ev.type}' was triggered")
+            await queue.put(ev)
+
+    client.events.subscribe(event_type, handler=_after_handle)
