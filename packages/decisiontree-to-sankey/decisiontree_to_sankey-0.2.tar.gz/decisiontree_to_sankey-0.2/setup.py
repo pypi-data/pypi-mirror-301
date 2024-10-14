d0f48458551820906a9c58c427a53fcad62ecc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:33 +0000

    Add package decisiontree-to-sankey/0.2/decisiontree_to_sankey-0.2.tar.gz

---
+++ packages/decisiontree-to-sankey/decisiontree_to_sankey-0.2.tar.gz/decisiontree_to_sankey-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='decisiontree-to-sankey',
+    version='0.2',
+    description='A package to visualize decision trees as Sankey diagrams using Plotly.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Luke Day',
+    author_email='luke.alexander.day@gmail.com',
+    url='https://github.com/LukeADay/DecisionTree-to-Sankey',
+    packages=['decisiontree_to_sankey'],  # Explicitly specify the package
+    package_dir={'decisiontree_to_sankey': 'src'},    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'plotly',
+        'seaborn',
+    ],
+    python_requires='>=3.6',
+)
