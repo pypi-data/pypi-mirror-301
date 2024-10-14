900dc1a5dca1615309bfecf3797c8ebd87f01e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:05:55 +0000

    Add package pymatio/0.1.1/pymatio-0.1.1-cp39-cp39-win_amd64.whl

---
+++ packages/pymatio/pymatio-0.1.1-cp39-cp39-win_amd64.whl/pymatio/__init__.pyi
@@ -0,0 +1,7 @@
+
+
+def get_library_version() -> tuple:
+    """Get the version of the library."""
+
+def loadmat(filename: str, *, simplify_cells: bool = False, debug_log_enabled: bool = False) -> dict:
+    """Load a MAT file"""
