856cfdbe11d6ff71a1f5d7506d0b1e655493d0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:12:11 +0000

    Add package piprm/0.1.0/piprm-0.1.0-py3-none-any.whl

---
+++ packages/piprm/piprm-0.1.0-py3-none-any.whl/piprm/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy@gmail.com
+
+from .registry import (
+    get_registrys,
+    set_registry,
+    get_use_registry,
+)
+
+
+__all__ = [
+    'get_registrys',
+    'set_registry',
+    'get_use_registry',
+]
---
+++ packages/piprm/piprm-0.1.0-py3-none-any.whl/piprm/cmd/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:
+# Description:
+
+import typer
+from rich.console import Console
+from rich import print
+from piprm import (
+    get_registrys,
+    set_registry,
+    get_use_registry,
+)
+
+console = Console()
+
+app = typer.Typer()
+
+
+@app.command()
+def ls():
+    registrys = get_registrys()
+    use_registry = get_use_registry()
+    is_use = False
+    for r in registrys:
+        name = f"{r.name} "
+        if use_registry and use_registry.name == r.name:
+            is_use = True
+        use = '*' if is_use else ' '
+        print(f"{use} {name:-<20} {r.index_url}")
+        is_use = False
+
+
+@app.command()
+def use(name: str):
+    set_registry(name)
+    msg = f"The registry has been changed to '{name}'."
+    print(msg)
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__":
+    app()
