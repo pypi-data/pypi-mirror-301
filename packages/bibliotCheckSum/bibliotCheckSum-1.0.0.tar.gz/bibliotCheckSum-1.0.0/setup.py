9809a8420a8afe191a05c2dabb4fbdb98024e8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:07:11 +0000

    Add package bibliotCheckSum/1.0.0/bibliotCheckSum-1.0.0.tar.gz

---
+++ packages/bibliotCheckSum/bibliotCheckSum-1.0.0.tar.gz/bibliotCheckSum-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='bibliotCheckSum',
+    version='1.0.0',
+    license='MIT License',
+    author='Alison Vitor',
+    long_description='...',
+    long_description_content_type="text/markdown",
+    author_email='alissonmarqueshm30@gmail.com',
+    keywords='biblioteca checksum binario',
+    description=u'biblioteca para binários',
+    packages=['bibliotCheckSum'],
+    install_requires=['random'],)
