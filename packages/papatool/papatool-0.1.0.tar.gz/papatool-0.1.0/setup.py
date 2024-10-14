6a94be8d88b36cae1ab0bd61484d4b12a08e7ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:33 +0000

    Add package papatool/0.1.0/papatool-0.1.0.tar.gz

---
+++ packages/papatool/papatool-0.1.0.tar.gz/papatool-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='papatool',  # The name of your package
+    version='0.1.0',
+    description='A package tool for managing Python packages',
+    author='Wasit Limprasert',
+    author_email='wasit7@gmail.com',
+    url='https://github.com/wasit7/package-tool',  # Your GitHub repository URL
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'papatool=papatool.papatool:main',  # CLI command to run the tool
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[  # Add any required dependencies here
+        'setuptools',
+        'wheel',
+        'twine'
+    ],
+)
