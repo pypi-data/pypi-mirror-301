572e3a1c540aafc2c6d57f752ec1a478df4a896a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:06 +0000

    Add package couchbase-sqlalchemy/1.0.1/couchbase_sqlalchemy-1.0.1.tar.gz

---
+++ packages/couchbase-sqlalchemy/couchbase_sqlalchemy-1.0.1.tar.gz/couchbase_sqlalchemy-1.0.1/couchbase_sqlalchemy/dbapi/couchbase_dbapi.py
@@ -0,0 +1,220 @@
+import logging
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions
+from sqlalchemy import BigInteger
+from couchbase.options import AnalyticsOptions,ClusterTimeoutOptions
+from sqlalchemy import BigInteger, String, Float, Boolean, DateTime, Date, Time,SmallInteger,Integer
+from datetime import datetime, timedelta
+import struct
+from couchbase.exceptions import CouchbaseException,AnalyticsErrorContext
+import traceback
+
+logging.basicConfig(filename='couchbase-sqlalchemy.log',
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format='%(levelname)s::%(asctime)s::%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logger=logging.getLogger()
+
+paramstyle = 'pyformat'
+
+class Error(Exception):
+    pass
+
+class DatabaseError(Error):
+    pass
+
+class OperationalError(DatabaseError):
+    pass
+
+class IntegrityError(DatabaseError):
+    pass
+
+class CouchbaseConnection:
+    def __init__(self, connection_string, username, password):
+        self.cluster = None
+        logger.info('Initializing cluster connection %s',connection_string)  # Generic message without sensitive data
+        try:
+            timeout_options = ClusterTimeoutOptions(connect_timeout=60)
+            self.cluster = Cluster(connection_string, ClusterOptions(PasswordAuthenticator(username, password)), timeout_options=timeout_options)
+            logger.info('Cluster ready')
+        except Exception as e:
+            logger.error(f"Failed to connect to columnar cluster: {e}")
+            raise Exception(e)
+
+    def cursor(self):
+        return CouchbaseCursor(self.cluster)
+
+    def commit(self):
+        pass
+
+    def rollback(self):
+        pass
+
+    def close(self):
+        if self.cluster is not None:
+            logger.info("Closing cluster connection...")
+            self.cluster.close()
+            self.cluster = None
+            logger.info("Cluster connection closed.")
+        else:
+            logger.warning("Attempted to close an already closed connection.")
+
+class CouchbaseCursor:
+    def __init__(self, cluster):
+        self.cluster = cluster
+        self._rows = []
+        self._description = []
+        self.rowcount = -1
+
+
+    def execute(self, query,l=True ,params=None):
+        self._rows = []
+        try:
+            qresult = self.cluster.analytics_query(query, AnalyticsOptions(raw={"format": "lossless-adm","signature": True, "client-type": "jdbc", "sql-compat": True}))
+        except CouchbaseException as ex:
+            if isinstance(ex.context, AnalyticsErrorContext):
+                print(ex.context.statement)
+                print(ex.context.first_error_code)
+                print(ex.context.first_error_message)
+                print(ex.context.client_context_id)
+            logger.error(traceback.format_exc())
+        self._rows = [row for row in qresult]
+        metadata = qresult.metadata().signature()
+        self._description.clear()
+        if metadata.get('*') == '*':
+            return
+        for i in range(len(metadata['name'])):
+            col_name = metadata['name'][i]
+            col_type = metadata['type'][i]
+            self._description.append((col_name,col_type, None, None, None, None, None))
+
+    @property
+    def description(self):
+        return self._description
+
+    def fetchone(self):
+        column_info = [(desc[0], desc[1]) for desc in self._description]
+        if self._rows:
+            row = self._rows.pop(0)
+            row_ordered_values = []
+            for col_name, col_type in column_info:
+                value = row.get(col_name)
+                if value is not None:
+                    row_ordered_values.append(self.type_conversion(value,col_type))
+                else :
+                    row_ordered_values.append(None)
+            return row_ordered_values
+        else:
+            return None
+
+    def type_mapping(self,col_type):
+        type_mappings = {
+        "int64": BigInteger,
+        "int64?": BigInteger,
+        "int32": Integer,
+        "int32?": Integer,
+        "int16": Integer,
+        "int16?": Integer,
+        "int8": Integer,
+        "int8?": Integer,
+        "double": Float,
+        "double?": Float,
+        "string": String,
+        "string?": String,
+        "boolean": Boolean,
+        "boolean?": Boolean,
+        "datetime": DateTime,
+        "datetime?": DateTime,
+        "time": Time,
+        "time?": Time,
+        "date": Date,
+        "date?": Date,
+        "array?": "array"
+        }
+        return type_mappings.get(col_type)
+
+    def convert_date(self,days):
+        epoch = datetime(1970, 1, 1)
+        target_date = epoch + timedelta(days=days)
+        return target_date.strftime('%Y-%m-%d')
+
+    def convert_time(self,milliseconds):
+        seconds, milliseconds = divmod(milliseconds, 1000)
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+
+    def convert_datetime(self,milliseconds):
+        epoch = datetime(1970, 1, 1)
+        target_datetime = epoch + timedelta(milliseconds=milliseconds)
+        return target_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+
+    def convert_encoded_float(self,encoded_str):
+        parts = encoded_str.split(':')
+        if len(parts) != 2:
+            logger.error(f"Invalid input value for float parts.")
+            raise ValueError("Invalid input format. Expected format 'HEX:NUMBER'.")
+        hex_prefix, number_str = parts
+        try:
+        # Parse the hexadecimal part as an unsigned long long (64-bit integer)
+            int_value = int(number_str)
+        except ValueError as e:
+            logger.error(f"Failed to convert integer to float: {e}")
+            raise ValueError("The numeric part of the input isn't a valid integer.")
+       # Check if the number is negative
+        if int_value & (1 << 63):
+        # If the number is negative, we need to compute the two's complement
+            int_value = -(~int_value + 1 & (2**64 - 1))
+        try:
+        # Convert the 64-bit integer to a double precision float
+            float_value = struct.unpack('>d', struct.pack('>q', int_value))[0]
+        except struct.error as e:
+            logger.error(f"Unexpected error converting encoded float: {e}")
+            raise ValueError("Failed to convert integer to float.")
+        return float_value
+
+    def type_conversion(self,value,col_type):
+        value_type = self.type_mapping(col_type)
+        if value_type == Date and value.startswith("11:"):
+            days_string = value[3:]
+            days = int(days_string)
+            return self.convert_date(days)
+        elif value_type == Time and value.startswith("12:"):
+            time_string = value[3:]
+            time_milliseconds = int(time_string)
+            return self.convert_time(time_milliseconds)
+        elif value_type == DateTime and value.startswith("10:"):
+            datetime_string = value[len("10:"):]
+            milliseconds = int(datetime_string)
+            return self.convert_datetime(milliseconds)
+        elif value_type == Float:
+            return self.convert_encoded_float(value)
+        elif value_type == String:
+            return value[1:]
+        elif value_type == Integer:
+            return value[3:]
+        return value
+
+    def fetchall(self):
+        all_row_values = []
+        column_info = [(desc[0], desc[1]) for desc in self._description]
+        for row in self._rows:
+            row_ordered_values = []
+            for col_name, col_type in column_info:
+                value = row.get(col_name)
+                if value is not None:
+                    row_ordered_values.append(self.type_conversion(value,col_type))
+                else :
+                    row_ordered_values.append(None)
+            all_row_values.append(row_ordered_values)
+        self._rows = []
+        return all_row_values
+
+    def close(self):
+        self._rows = []
+        logger.info("Cursor closed.")
+
+def connect(connection_string, username, password):
+    return CouchbaseConnection(connection_string, username, password)
---
+++ packages/couchbase-sqlalchemy/couchbase_sqlalchemy-1.0.1.tar.gz/couchbase_sqlalchemy-1.0.1/couchbase_sqlalchemy/dialect/couchbase_dialect.py
@@ -0,0 +1,228 @@
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.sql.compiler import IdentifierPreparer
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.types import (
+    BIGINT,
+    BOOLEAN,
+    DATE,
+    DATETIME,
+    FLOAT,
+    TIME,
+    VARCHAR,
+)
+from sqlalchemy.engine.default import DefaultDialect
+from urllib.parse import urlparse, parse_qs, urlencode
+from urllib.parse import unquote
+
+class CouchbaseIdentifierPreparer(IdentifierPreparer):
+    def __init__(self, dialect, **kw):
+        quote = '`'
+        super().__init__(dialect, initial_quote=quote, escape_quote=quote)
+
+    def _quote_free_identifiers(self, *ids):
+        """
+        Unilaterally identifier-quote any number of strings.
+        """
+        return tuple(self.quote(i) for i in ids if i is not None)
+
+    def quote_schema(self, schema, force=None):
+        """
+        Split schema by a dot and merge with required quotes
+        """
+        idents = self._split_schema_by_dot(schema)
+        return ".".join(self._quote_free_identifiers(*idents))
+
+    def _split_schema_by_dot(self, schema):
+        ret = []
+        idx = 0
+        pre_idx = 0
+        in_quote = False
+        while idx < len(schema):
+            if not in_quote:
+                if schema[idx] == "." and pre_idx < idx:
+                    ret.append(schema[pre_idx:idx])
+                    pre_idx = idx + 1
+                elif schema[idx] == '`':
+                    in_quote = True
+                    pre_idx = idx + 1
+            else:
+                if schema[idx] == '`' and pre_idx < idx:
+                    ret.append(schema[pre_idx:idx])
+                    in_quote = False
+                    pre_idx = idx + 1
+            idx += 1
+            if pre_idx < len(schema) and schema[pre_idx] == ".":
+                pre_idx += 1
+        if pre_idx < idx:
+            ret.append(schema[pre_idx:idx])
+        return ret
+
+class CouchbaseDialect(DefaultDialect):
+    name = "couchbase"
+    driver = "couchbase"
+    preparer = CouchbaseIdentifierPreparer
+    supports_alter = False
+    max_identifier_length = 255
+    default_paramstyle = 'pyformat'
+    paramstyle = 'pyformat'
+    supports_native_enum = False
+    supports_native_boolean = True
+    supports_statement_cache = True
+    schema_flag_map = {}
+    @classmethod
+    def dbapi(cls):
+        from couchbase_sqlalchemy.dbapi import couchbase_dbapi
+        return couchbase_dbapi
+
+    def create_connect_args(self, url):
+        parsed_url = urlparse(str(url))
+        username = unquote(parsed_url.username)
+        password = unquote(parsed_url.password)
+        host = unquote(parsed_url.hostname)
+        port = parsed_url.port
+        query_params = parse_qs(parsed_url.query, keep_blank_values=True)
+        ssl_enabled = query_params.get('ssl', ['true'])[0].lower()
+        protocol = 'couchbases' if ssl_enabled=='true' else 'couchbase'
+        query_params.pop('ssl', None)
+        query_string = urlencode(query_params, doseq=True)
+        connection_string = f"{protocol}://{host}"
+        if port:
+            connection_string += f":{port}"
+        if query_string:
+            connection_string += f"?{query_string}"
+        return ([connection_string, username, password], {})
+
+    def has_table(self, connection, table_name, schema=None):
+        """
+        Checks if the table exists
+        """
+        return self._has_object(connection,"TABLE",table_name,schema)
+
+    def _get_table_columns(self, connection, table_name, schema=None):
+        return self.get_columns(self, connection, table_name, schema)
+
+    def _has_object(self, connection, object_type, object_name, schema=None):
+        try:
+            results = connection.execute(
+                """ 
+                SELECT d.DatasetName
+                FROM Metadata.`Dataset` d
+                Where d.DatasetName= "{object_name}}" 
+                """
+            )
+            row = results.fetchone()
+            have = row is not None
+            return have
+        except Exception as e:
+            raise Exception(e)
+
+    def check_two_part_name(self, schema):
+        parts = schema.split('/')
+        if len(parts) == 2:
+            database_name, scope_name = parts
+            return database_name + '.' + scope_name
+        else:
+            return schema
+
+    def get_schema_names(self, connection, **kw):
+        query = """
+            SELECT d.DatabaseName, d.DataverseName
+            FROM Metadata.`Dataverse` d
+        """
+        result = connection.execute(query)
+        schema_names = result.fetchall()
+        schemas = []
+        for r in schema_names:
+            schema_key = r['DataverseName']
+            if r['DatabaseName'] is None:
+                schema_name = self.check_two_part_name(r['DataverseName'])
+                schemas.append(schema_name)
+                self.schema_flag_map[schema_name] = False
+            else:
+                schema_name = r['DatabaseName'] + '.' + r['DataverseName']
+                schemas.append(schema_name)
+                self.schema_flag_map[schema_name] = True
+        return schemas
+
+    def get_table_names(self, connection, schema=None, **kw):
+        return []
+
+    def get_view_names(self, connection, schema=None, **kw):
+        if schema not in self.schema_flag_map:
+            print(f"Schema '{schema}' not found in schema map.")
+            return []
+        is_full_schema = self.schema_flag_map[schema]
+        query_base = """
+        SELECT ds.DatasetName
+        FROM Metadata.`Dataset` ds
+        JOIN Metadata.`Datatype` dt ON ds.DatatypeDataverseName = dt.DataverseName
+        AND ds.DatasetType = 'VIEW' AND ds.DatatypeName = dt.DatatypeName
+        AND array_length(dt.Derived.Record.Fields) > 0
+        """
+        parts = schema.split('.')
+        if is_full_schema:
+            database_name, dataverse_name = parts
+            condition = f"AND ds.DataverseName = '{dataverse_name}' AND ds.DatabaseName = '{database_name}'"
+        elif len(parts) == 2:
+            database_name, scope_name = parts
+            dataverse_name = database_name + '/' + scope_name
+            condition = f"AND ds.DataverseName = '{dataverse_name}'"
+        else:
+            condition = f"AND ds.DataverseName = '{schema}'"
+        query = query_base + condition + ";"
+        view_names = connection.execute(query)
+        result = view_names.fetchall()
+        views = [row['DatasetName'] for row in result]
+        return views
+
+    def get_indexes(self, connection, table_name, schema=None, **kw):
+        return []
+
+    def get_pk_constraint(self, connection, table_name, schema=None, **kw):
+        query = f"""
+                SELECT RAW d.ViewDetails.PrimaryKey
+                FROM Metadata.`Dataset` d 
+                WHERE d.DatasetType = 'VIEW'
+                """
+        result = connection.execute(query)
+        keys = result.fetchall()
+        Primary_Keys= [item for sublist in keys for inner_list in sublist for item in inner_list]
+        pk_info = {
+            'constrained_columns': [],
+            'name': None
+        }
+        pk_info['constrained_columns'] = Primary_Keys
+        return pk_info
+
+    def get_columns(self, connection, table_name, schema=None, **kw):
+        query = f"""
+                SELECT d.Derived.Record.Fields 
+                FROM Metadata.`Datatype` d 
+                WHERE d.DatatypeName = '$d$t$i${table_name}';
+                """
+        result = connection.execute(query)
+        columns_data = result.fetchall()
+        fields = columns_data[0]['Fields']
+        type_map = {
+        "string": VARCHAR,
+        "int64": BIGINT,
+        "datetime": DATETIME,
+        "boolean": BOOLEAN,
+        "double": FLOAT,
+        "date":DATE,
+        "time":TIME
+        }
+        columns = []
+        for field in fields:
+            column_info = {
+            'name': f"{field['FieldName'][1:]}",
+            'type': type_map[field['FieldType'][1:]](),
+            'nullable': field['IsNullable'],
+            'default': None,
+            'autoincrement': field.get('IsAutoincrement', False)
+            }
+            columns.append(column_info)
+        return columns
+
+    def get_foreign_keys(self, connection, table_name, schema=None, **kw):
+        return []
