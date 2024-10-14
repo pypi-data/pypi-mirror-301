f187312e633f3fb010686c29dfe99c704e4d2098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:25 +0000

    Add package tq-datasets/0.0.1.post2/tq_datasets-0.0.1.post2.tar.gz

---
+++ packages/tq-datasets/tq_datasets-0.0.1.post2.tar.gz/tq_datasets-0.0.1.post2/test/test1.py
@@ -0,0 +1,23 @@
+from tq_datasets.dataset_op_interface import TraverseDatasetItems
+from tq_utils import my_json
+
+
+def test_default_method():
+    print()
+    t = TraverseDatasetItems()
+    t.traverse_dataset(r'E:\Workspace\Pycharm\Datasets\data\Dataset-DOWL2\DOWL2\eng\al-monitor',
+                       'json')
+
+
+class TraverseDOWLItems(TraverseDatasetItems):
+    @staticmethod
+    def item_reader(file_abs_path: str) -> str:
+        print('oaushoiasdoiajsodiasoidowifoqn')
+        return my_json.load_json_safe(file_abs_path)
+
+
+def test_override_methods():
+    print()
+    t = TraverseDOWLItems()
+    t.traverse_dataset(r'E:\Workspace\Pycharm\Datasets\data\Dataset-DOWL2\DOWL2\eng\al-monitor',
+                       'json')
