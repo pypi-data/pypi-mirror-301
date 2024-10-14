e2d32a40976fecf86ff309eb961a4ff16d3da97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:20 +0000

    Add package sperrylabelimg/1.0.4/sperrylabelimg-1.0.4.tar.gz

---
+++ packages/sperrylabelimg/sperrylabelimg-1.0.4.tar.gz/sperrylabelimg-1.0.4/setup.py
@@ -0,0 +1,28 @@
+"""
+setup
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='sperrylabelimg',
+    version='1.0.4',
+    description='A GUI for labelling images customized for usage at Sperry Rail',
+    author='Brooklyn Germa',
+    author_email='brooklyn.germa@sperryrail.com',
+    url='https://gitlab.com/bGerma/customlabelimg',
+    license='MIT',
+    # package_dir={'': 'sperryLabelImg'},
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'labelImg==1.8.6'
+    ],
+    entry_points='''
+        [console_scripts]
+        sperrylabelimg=sperryLabelImg.app:main
+    ''',
+)
