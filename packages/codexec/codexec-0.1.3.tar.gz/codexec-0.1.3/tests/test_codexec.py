b1232816542244efe59bb9821da5c6b3ce37fa5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 15:05:30 +0000

    Add package codexec/0.1.3/codexec-0.1.3.tar.gz

---
+++ packages/codexec/codexec-0.1.3.tar.gz/codexec-0.1.3/tests/test_codexec.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `codexec` package."""
+
+
+import unittest
+
+from codexec import codexec
+
+
+class TestCodexec(unittest.TestCase):
+    """Tests for `codexec` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
