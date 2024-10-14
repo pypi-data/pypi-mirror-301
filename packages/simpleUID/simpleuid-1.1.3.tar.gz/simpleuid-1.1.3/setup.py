899a14ffa9d13b5b621e2e7fbd6b0011f1b9fb3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:13:00 +0000

    Add package simpleUID/1.1.3/simpleuid-1.1.3.tar.gz

---
+++ packages/simpleUID/simpleuid-1.1.3.tar.gz/simpleuid-1.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpleUID",
+    version="v1.1.3",
+    author="Wibo Kuipers",
+    author_email="wkuipersoss@gmail.com",
+    description="A simple and intuitive Python package for generating unique IDs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/w-kuipers/simpleUID",
+    project_urls={
+        "Bug Tracker": "https://github.com/w-kuipers/simpleUID/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
