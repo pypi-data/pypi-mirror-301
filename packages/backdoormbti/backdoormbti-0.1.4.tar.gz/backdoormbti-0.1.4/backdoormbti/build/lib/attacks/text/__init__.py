12d78de9478b1e189744fc55cdc8c4962a354272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 03:12:54 +0000

    Add package backdoormbti/0.1.4/backdoormbti-0.1.4.tar.gz

---
+++ packages/backdoormbti/backdoormbti-0.1.4.tar.gz/backdoormbti-0.1.4/backdoormbti/build/lib/attacks/text/__init__.py
@@ -0,0 +1,6 @@
+from .addsent import AddSent
+from .badnet import BadNet
+from .lwp import LWP
+from .stylebkd import StyleBKD
+from .synbkd import SynBKD
+from .bite import  BITE
---
+++ packages/backdoormbti/backdoormbti-0.1.4.tar.gz/backdoormbti-0.1.4/backdoormbti/build/lib/defenses/video/__init__.py
@@ -0,0 +1,1 @@
+from .strip import STRIP
