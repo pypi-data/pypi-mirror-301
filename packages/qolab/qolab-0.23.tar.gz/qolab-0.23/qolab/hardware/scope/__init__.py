12a88d930451b08abd89b728f1b2871ede5c1e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 04:05:41 +0000

    Add package qolab/0.23/qolab-0.23.tar.gz

---
+++ packages/qolab/qolab-0.23.tar.gz/qolab-0.23/qolab/hardware/scope/__init__.py
@@ -0,0 +1,9 @@
+"""
+Provide basic classes to operate scopes
+"""
+
+from .sds1104x import SDS1104X
+from .sds2304x import SDS2304X
+from .sds800xhd import SDS800XHD
+
+__all__ = ["SDS1104X", "SDS2304X", "SDS800XHD"]
