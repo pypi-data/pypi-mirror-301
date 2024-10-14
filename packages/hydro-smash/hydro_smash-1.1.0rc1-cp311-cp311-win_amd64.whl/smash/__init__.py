dadd9de44580cf9690ff9e0eaa64f4000ca859d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 16:05:20 +0000

    Add package hydro-smash/1.1.0rc1/hydro_smash-1.1.0rc1-cp311-cp311-win_amd64.whl

---
+++ packages/hydro-smash/hydro_smash-1.1.0rc1-cp311-cp311-win_amd64.whl/smash/__init__.py
@@ -0,0 +1,63 @@
+from smash import factory, io
+from smash._version_meson import __version__
+from smash.core.model.model import Model
+from smash.core.signal_analysis.evaluation.evaluation import evaluation
+from smash.core.signal_analysis.prcp_indices.prcp_indices import PrecipitationIndices, precipitation_indices
+from smash.core.signal_analysis.segmentation.segmentation import hydrograph_segmentation
+from smash.core.signal_analysis.signatures.signatures import Signatures, signatures
+from smash.core.simulation.control import (
+    bayesian_optimize_control_info,
+    optimize_control_info,
+)
+from smash.core.simulation.estimate.estimate import MultisetEstimate, multiset_estimate
+from smash.core.simulation.optimize.optimize import (
+    BayesianOptimize,
+    Optimize,
+    bayesian_optimize,
+    optimize,
+)
+from smash.core.simulation.options import (
+    default_bayesian_optimize_options,
+    default_optimize_options,
+)
+from smash.core.simulation.run.run import (
+    ForwardRun,
+    MultipleForwardRun,
+    forward_run,
+    multiple_forward_run,
+)
+from smash.factory.samples.samples import Samples
+
+
+def __getattr__(name):
+    raise AttributeError(f"module 'smash' has no attribute '{name}'")
+
+
+__all__ = [
+    "Model",
+    "evaluation",
+    "hydrograph_segmentation",
+    "signatures",
+    "precipitation_indices",
+    "forward_run",
+    "multiple_forward_run",
+    "optimize",
+    "bayesian_optimize",
+    "default_optimize_options",
+    "default_bayesian_optimize_options",
+    "optimize_control_info",
+    "bayesian_optimize_control_info",
+    "multiset_estimate",
+    "io",
+    "factory",
+    "ForwardRun",
+    "MultipleForwardRun",
+    "Optimize",
+    "BayesianOptimize",
+    "MultisetEstimate",
+    "Signatures",
+    "PrecipitationIndices",
+    "Samples",
+]
+
+__all__ += ["__version__"]
