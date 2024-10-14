07f8ab1bfc0a11097645eb881d44d6ac509aec22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-14 14:07:15 +0000

    Add package profissa-lft/1.0.6/profissa_lft-1.0.6-py3-none-any.whl

---
+++ packages/profissa-lft/profissa_lft-1.0.6-py3-none-any.whl/experiment/experiment.py
@@ -0,0 +1,39 @@
+from .pschedulerWrapper import Throughput, Rtt, Latency
+from .constants import *
+
+
+throughput = Throughput().Format(PERFSONAR_JSON_OUTPUT_FORMAT).MaxRuns(MAX_RUNS).Repeat(INTERVAL)
+rtt = Rtt().Format(PERFSONAR_JSON_OUTPUT_FORMAT).MaxRuns(25).Repeat(REPEAT_INTERVAL)
+latency = Latency().Format(PERFSONAR_JSON_OUTPUT_FORMAT).MaxRuns(MAX_RUNS).Repeat(INTERVAL)
+
+
+def runThroughput(testname, sourceIp, targetIp):
+        throughput.Source(sourceIp)\
+                .Dest(targetIp)\
+                .OutputFile(RESULTS_PATH, testname + THROUGHPUT_JSON_FORMAT)\
+                .ThroughputDuration(60)\
+                .mountCommand()
+        print("Running now command " + throughput.command)
+        throughput.run()
+
+
+def runRTT(testname, sourceIp, targetIp):
+        rtt.Source(sourceIp)\
+                .Dest(targetIp)\
+                .OutputFile(RESULTS_PATH, testname + RTT_JSON_FORMAT)\
+                .Count(60)\
+                .mountCommand()
+        print("Running now command " + rtt.command)
+        rtt.run()
+
+
+def runLatency(testname, sourceIp, targetIp):
+        latency.Source(sourceIp)\
+                .Dest(targetIp)\
+                .OutputRaw()\
+                .OutputFile(RESULTS_PATH, testname + LATENCY_JSON_FORMAT)\
+                .PacketCount(60)\
+                .mountCommand()
+        print("Running now command " + latency.command)
+        latency.run()
+        
---
+++ packages/profissa-lft/profissa_lft-1.0.6-py3-none-any.whl/experiment/wired_experiment.py
@@ -0,0 +1,25 @@
+from .experiment import *
+from .emu_emu_wired import EmuEmuWired
+from .emu_phy_wired import EmuPhyWired
+
+
+# wired emu-emu
+emuEmu = EmuEmuWired()
+emuEmu.setup()
+runThroughput(EMU_EMU_WIRED_PREFIX, EMU_EMU_WIRED_H1_IP, EMU_EMU_WIRED_H2_IP)
+runRTT(EMU_EMU_WIRED_PREFIX, EMU_EMU_WIRED_H1_IP, EMU_EMU_WIRED_H2_IP)
+runLatency(EMU_EMU_WIRED_PREFIX, EMU_EMU_WIRED_H1_IP, EMU_EMU_WIRED_H2_IP)
+emuEmu.tearDown()
+
+# wired phy-emu
+emuPhy = EmuPhyWired()
+emuPhy.setup()
+runThroughput(EMU_PHY_WIRED_PREFIX, EMU_PHY_WIRED_H1_IP, EMU_PHY_WIRED_H2_IP)
+runRTT(EMU_PHY_WIRED_PREFIX, EMU_PHY_WIRED_H1_IP, EMU_PHY_WIRED_H2_IP)
+runLatency(EMU_PHY_WIRED_PREFIX, EMU_PHY_WIRED_H1_IP, EMU_PHY_WIRED_H2_IP)
+emuPhy.tearDown()
+
+# wired phy-phy
+runThroughput(PHY_PHY_WIRED_PREFIX, PHY_PHY_WIRED_H1_IP, PHY_PHY_WIRED_H2_IP)
+runRTT(PHY_PHY_WIRED_PREFIX, PHY_PHY_WIRED_H1_IP, PHY_PHY_WIRED_H2_IP)
+runLatency(PHY_PHY_WIRED_PREFIX, PHY_PHY_WIRED_H1_IP, PHY_PHY_WIRED_H2_IP)
