44bba347e03dcc506628656ed3cc670b606089bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:42 +0000

    Add package fotoobo/2.2.0/fotoobo-2.2.0-py3-none-any.whl

---
+++ packages/fotoobo/fotoobo-2.2.0-py3-none-any.whl/fotoobo/cli/faz/main.py
@@ -0,0 +1,28 @@
+"""
+The FortiAnalyzer commands
+"""
+
+import logging
+
+import typer
+
+from fotoobo.cli.faz import get_commands as get
+from fotoobo.helpers import cli_path
+
+app = typer.Typer(no_args_is_help=True, rich_markup_mode="rich")
+log = logging.getLogger("fotoobo")
+
+
+@app.callback()
+def callback(context: typer.Context) -> None:
+    """
+    The faz subcommand callback
+
+    Args:
+        context: The context object of the typer app
+    """
+    cli_path.append(str(context.invoked_subcommand))
+    log.debug("About to execute command: '%s'", context.invoked_subcommand)
+
+
+app.add_typer(get.app, name="get", help="FortiAnalyzer get commands.")
