489e803a5a1085fa54328bf20974c9d27aff1d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 07:05:23 +0000

    Add package vivarium-testing-utils/0.1.1/vivarium_testing_utils-0.1.1.tar.gz

---
+++ packages/vivarium-testing-utils/vivarium_testing_utils-0.1.1.tar.gz/vivarium_testing_utils-0.1.1/CHANGELOG.rst
@@ -0,0 +1,7 @@
+**0.1.1 - 10/14/24**
+
+ - Make name an optional parameter to fuzzy_assert_proportion
+
+**0.1.0 - 03/01/24**
+
+ - Repository creation
---
+++ packages/vivarium-testing-utils/vivarium_testing_utils-0.1.1.tar.gz/vivarium_testing_utils-0.1.1/tests/test_fuzzy_checker.py
@@ -0,0 +1,7 @@
+# TODO: Add tests for fuzzy_checker
+import pytest
+
+
+@pytest.mark.skip("TODO - MIC-5419: Add tests for fuzzy_checker")
+def test_placeholder():
+    assert True
