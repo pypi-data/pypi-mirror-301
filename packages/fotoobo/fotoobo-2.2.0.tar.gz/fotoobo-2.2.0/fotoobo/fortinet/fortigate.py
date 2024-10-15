273f977771b38917c58f2f8714d64e47707e5d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:42 +0000

    Add package fotoobo/2.2.0/fotoobo-2.2.0.tar.gz

---
+++ packages/fotoobo/fotoobo-2.2.0.tar.gz/fotoobo-2.2.0/fotoobo/fortinet/fortigate.py
@@ -0,0 +1,105 @@
+"""
+FortiGate Class
+"""
+
+import logging
+from typing import Any, Dict, Optional
+
+import requests
+
+from fotoobo.exceptions import APIError, GeneralWarning
+
+from .fortinet import Fortinet
+
+log = logging.getLogger("fotoobo")
+
+
+class FortiGate(Fortinet):
+    """
+    Represents one FortiGate (digital twin)
+    """
+
+    def __init__(
+        self,
+        hostname: str = "",
+        token: str = "",
+        **kwargs: Dict[str, str],
+    ) -> None:
+        """
+        Set some initial parameters.
+
+        Args:
+            hostname: The hostname of the FortiGate to connect to
+            token:    API access token from the FortiGate
+            **kwargs: See Fortinet class for available arguments
+        """
+        if not hostname:
+            raise GeneralWarning("No hostname specified")
+
+        super().__init__(hostname=hostname, **kwargs)
+        self.api_url = f"https://{self.hostname}:{self.https_port}/api/v2"
+        self.token = token
+        self.type = "fortigate"
+
+    def api(  # pylint: disable=too-many-arguments
+        self,
+        method: str,
+        url: str = "",
+        headers: Optional[Dict[str, str]] = None,
+        params: Optional[Dict[str, str]] = None,
+        payload: Optional[Dict[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> requests.models.Response:
+        """
+        API request to a FortiGate device.
+        It uses the super.api method but it has to enrich the payload in post requests with the
+        needed session key.
+
+        Args:
+            method:  Request method from [get, post]
+            url:     Rest API URL to request data from
+            params:  Dictionary with parameters (if needed)
+            payload: JSON body for post requests (if needed)
+            timeout: The requests read timeout
+
+        Returns:
+            Response from the request
+        """
+        self.session.headers.update({"Authorization": f"Bearer {self.token}"})
+        return super().api(
+            method, url, payload=payload, params=params, timeout=timeout, headers=headers
+        )
+
+    def backup(self, timeout: int = 10) -> str:
+        """
+        Get the configuration backup from a FortiGate.
+
+        Args:
+            timeout: Timeout in sec to wait for the response
+
+        Returns:
+            Configuration backup as text
+        """
+        data = self.api(
+            "get", "monitor/system/config/backup", params={"scope": "global"}, timeout=timeout
+        )
+        return data.text
+
+    def get_version(self) -> str:
+        """
+        Get FortiGate version
+
+        Returns:
+            FortiGate version
+        """
+        fgt_version: str = ""
+
+        try:
+            response = self.api("get", "monitor/system/status")
+
+        except APIError as err:
+            log.warning("'%s' returned: '%s'", self.hostname, err.message)
+            raise GeneralWarning(f"{self.hostname} returned: {err.message}") from err
+
+        fgt_version = response.json().get("version", "unknown")
+        return fgt_version
---
+++ packages/fotoobo/fotoobo-2.2.0.tar.gz/fotoobo-2.2.0/fotoobo/tools/fgt/get.py
@@ -0,0 +1,72 @@
+"""
+FortiGate get version utility
+"""
+
+import concurrent.futures
+import logging
+from typing import Optional, Tuple
+
+from rich.progress import Progress
+
+from fotoobo.exceptions import GeneralError, GeneralWarning
+from fotoobo.fortinet.fortigate import FortiGate
+from fotoobo.helpers.config import config
+from fotoobo.helpers.result import Result
+from fotoobo.inventory import Inventory
+
+log = logging.getLogger("fotoobo")
+
+
+def version(host: Optional[str] = None) -> Result[str]:
+    """FortiGate get version.
+
+    Get the version(s) of one ore more FortiGates.
+
+    Args:
+        host: The host from the inventory to get the version. If you omit host, it will run over
+              all FortiGates in the inventory.
+
+    Returns:
+        The Result object with all the results
+    """
+
+    def _get_single_version(name: str, fgt: FortiGate) -> Tuple[str, str]:
+        """Get the version from a FortiGate.
+
+        This private method is used for multithreading. It only queries one single FortiGate for its
+        version number status and returns it.
+
+        Args:
+            name: The name of the FortiGate (as defined in the inventory)
+            fgt:  The FortiGate object to query
+
+        Returns:
+            name:   The name of the FortiGate (as defined in the inventory)
+            status: The HA status of the FortiGate (fgt)
+        """
+        log.debug("Getting FortiGate version for '%s'", name)
+        try:
+            fortigate_version = fgt.get_version()
+
+        except (GeneralWarning, GeneralError) as exception:
+            fortigate_version = f"unknown due to {exception.message}"
+
+        return name, fortigate_version
+
+    inventory = Inventory(config.inventory_file)
+    fgts = inventory.get(host, "fortigate")
+    result = Result[str]()
+
+    with Progress() as progress:
+        task = progress.add_task("getting FortiGate versions...", total=len(fgts))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = []
+            for name, fgt in fgts.items():
+                futures.append(executor.submit(_get_single_version, name, fgt))
+
+            for future in concurrent.futures.as_completed(futures):
+                name, fortigate_version = future.result()
+                result.push_result(name, fortigate_version)
+                progress.update(task, advance=1)
+
+    return result
