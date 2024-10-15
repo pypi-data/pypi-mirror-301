7e7e54f294bdedccd792dcdf12cf3bdbbb20097e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:06:22 +0000

    Add package filterlists-api/1.0.10/filterlists_api-1.0.10-py3-none-any.whl

---
+++ packages/filterlists-api/filterlists_api-1.0.10-py3-none-any.whl/filterlists_api/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from filterlists_api.api.filter_lists_api import FilterListsApi
+from filterlists_api.api.languages_api import LanguagesApi
+from filterlists_api.api.licenses_api import LicensesApi
+from filterlists_api.api.maintainers_api import MaintainersApi
+from filterlists_api.api.software_api import SoftwareApi
+from filterlists_api.api.syntaxes_api import SyntaxesApi
+from filterlists_api.api.tags_api import TagsApi
+
