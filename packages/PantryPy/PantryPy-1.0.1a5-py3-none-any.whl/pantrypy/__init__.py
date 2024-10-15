5208afc5b3f6d80f36fb59febb96a1dc6acfc589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:27 +0000

    Add package PantryPy/1.0.1a5/PantryPy-1.0.1a5-py3-none-any.whl

---
+++ packages/PantryPy/PantryPy-1.0.1a5-py3-none-any.whl/pantrypy/__init__.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------------------------
+# PantryPy
+# Copyright (c) 2023 Felipe Amaral dos Santos
+# Licensed under the MIT License (see LICENSE file)
+# -----------------------------------------------------------------------------
+
+from .color_structures import *
+from .commons import *
+from .geometry_structures import *
+from .metadata import *
