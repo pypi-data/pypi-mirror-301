5e328c654aa4ea2d371339a7360360b1260d820d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:35 +0000

    Add package paas-charm/0.1.0/paas_charm-0.1.0-py3-none-any.whl

---
+++ packages/paas-charm/paas_charm-0.1.0-py3-none-any.whl/paas_app_charmer/fastapi/charm.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+
+"""Deprecated FastAPI Charm service."""
+
+import paas_charm.fastapi.charm
+
+
+class Charm(paas_charm.fastapi.charm.Charm):
+    """Deprecated FastAPI charm."""
---
+++ packages/paas-charm/paas_charm-0.1.0-py3-none-any.whl/paas_app_charmer/flask/charm.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Deprecated Flask Charm service."""
+
+import paas_charm.flask.charm
+
+
+class Charm(paas_charm.flask.charm.Charm):
+    """Deprecated Flask charm."""
