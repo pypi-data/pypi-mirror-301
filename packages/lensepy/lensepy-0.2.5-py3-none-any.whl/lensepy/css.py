16cc038220391a05f2a969ea7b3c81ec1d948cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 07:05:17 +0000

    Add package lensepy/0.2.5/lensepy-0.2.5-py3-none-any.whl

---
+++ packages/lensepy/lensepy-0.2.5-py3-none-any.whl/lensepy/css.py
@@ -0,0 +1,25 @@
+
+#### Style Sheet for LEnsE API
+
+# Colors
+# ------
+BLUE_IOGS = '#0A3250'
+ORANGE_IOGS = '#FF960A'
+WHITE = '#000000'
+GRAY = '#727272'
+BLACK = '#FFFFFF'
+
+# Styles
+# ------
+styleH1 = f"font-size:18px; padding:3px; color:{BLUE_IOGS};font-weight: bold;"
+styleH2 = f"font-size:16px; padding:3px; color:{BLUE_IOGS}; font-weight: bold;"
+styleH3 = f"font-size:14px; padding:3px; color:{BLUE_IOGS};"
+styleCheckbox = f"font-size: 14px; padding: 3px; color: {BLUE_IOGS}; font-weight: normal;"
+no_style = f"background-color:{GRAY}; color:{BLACK}; font-size:14px;"
+
+disabled_button = f"background-color:{GRAY}; color:{BLACK}; font-size:14px; border-radius: 10px;"
+unactived_button = f"background-color:{BLUE_IOGS}; color:white; font-size:14px; font-weight:bold; border-radius: 10px;"
+actived_button = f"background-color:{ORANGE_IOGS}; color:white; font-size:14px; font-weight:bold; border-radius: 10px;"
+
+BUTTON_HEIGHT = 35 #px
+OPTIONS_BUTTON_HEIGHT = 20 #pxn = f"background-color:{ORANGE_IOGS}; color:white; font-size:15px; font-weight:bold; border-radius: 10px;"
