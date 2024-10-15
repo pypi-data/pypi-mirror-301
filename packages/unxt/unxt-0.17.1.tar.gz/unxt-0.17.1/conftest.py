1a2dd0c94e86ad980e1c1befbdc3206e931e6f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:05:45 +0000

    Add package unxt/0.17.1/unxt-0.17.1.tar.gz

---
+++ packages/unxt/unxt-0.17.1.tar.gz/unxt-0.17.1/conftest.py
@@ -0,0 +1,44 @@
+"""Doctest configuration."""
+
+import platform
+from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
+
+from sybil import Sybil
+from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
+
+from optional_dependencies import OptionalDependencyEnum, auto
+
+# TODO: stop skipping doctests on Windows when there is uniform support for
+#       numpy 2.0+ scalar repr. On windows it is printed as 1.0 instead of
+#       `np.float64(1.0)`.
+parsers = (
+    [DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE)]
+    if platform.system() != "Windows"
+    else []
+) + [
+    PythonCodeBlockParser(),
+    SkipParser(),
+]
+
+pytest_collect_file = Sybil(
+    parsers=parsers,
+    patterns=["*.rst", "*.py"],
+).pytest()
+
+
+class OptDeps(OptionalDependencyEnum):
+    """Optional dependencies for ``unxt``."""
+
+    ASTROPY = auto()
+    GALA = auto()
+    MATPLOTLIB = auto()
+    ZEROTH = auto()
+
+
+collect_ignore_glob = []
+if not OptDeps.ASTROPY.installed:
+    collect_ignore_glob.append("src/unxt/_interop/unxt_interop_astropy/*")
+if not OptDeps.GALA.installed:
+    collect_ignore_glob.append("src/unxt/_interop/unxt_interop_gala/*")
+if not OptDeps.MATPLOTLIB.installed or not OptDeps.ZEROTH.installed:
+    collect_ignore_glob.append("src/unxt/_interop/unxt_interop_mpl/*")
