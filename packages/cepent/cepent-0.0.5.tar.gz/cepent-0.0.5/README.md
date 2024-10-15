4f3f22cb3b70f35e415473d824ced066aef87a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:25 +0000

    Add package cepent/0.0.5/cepent-0.0.5.tar.gz

---
+++ packages/cepent/cepent-0.0.5.tar.gz/cepent-0.0.5/README.md
@@ -0,0 +1,33 @@
+# cepent
+
+Se encuentra en construccion es una libreria para almacenar errores de procesos ETL
+
+
+
+## Ejemplo de uso
+
+Creando captura de error en caso de proceso rechazado
+
+```python
+from cepent import ErrorHandling
+
+error = ErrorHandling(usr, pwd, bd_pg, bd_port, bd_company  )
+error.handle_error( table_name)
+
+```
+
+## Ejemplo de uso
+
+Creando captura de comienzo , error y fin de procesos de proceso. Para validar sus 3 estados
+
+```python
+from cepent import ErrorHandling
+
+process = ProcessHandling(usr, pwd, bd_pg, bd_port, bd_company , process_name)
+
+
+process.start_process()
+process.handle_error( table_name)
+process.end_process_log()
+```
+proces = ErrorHandling(usr, pwd, bd_pg, bd_port, bd_company )
---
+++ packages/cepent/cepent-0.0.5.tar.gz/cepent-0.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+LICENSE = 'MIT' #Tipo de licencia
+VERSION = '0.0.5'
+DESCRIPTION = 'Catch error pentaho'
+LONG_DESCRIPTION = 'A package for capturing errors encountered during the data extraction process in ETL workflows using Pentaho'
+
+
+INSTALL_REQUIRES = [
+      'psycopg2'
+      ]
+# Setting up
+setup(
+    name="cepent",
+    version=VERSION,
+    author="Ysis Longart (Biwiser)",
+    author_email="ysisl@biwiser.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/biwiser-com/cepent",
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    include_package_data=True,
+    keywords=['python', 'psycopg2', 'catch', 'pentaho'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
