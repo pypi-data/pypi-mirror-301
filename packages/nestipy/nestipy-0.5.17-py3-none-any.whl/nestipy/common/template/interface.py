ff0d7f73c4829136d548a2da1a50ee8b92240446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:48 +0000

    Add package nestipy/0.5.17/nestipy-0.5.17-py3-none-any.whl

---
+++ packages/nestipy/nestipy-0.5.17-py3-none-any.whl/nestipy/common/template/interface.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class TemplateEngine(ABC):
+    def __init__(self, template_dir: str):
+        self.template_dir = template_dir
+
+    @abstractmethod
+    def render(self, template: str, context: dict) -> str:
+        """
+        A specific implementation of rendering content of template file.
+
+        Args:
+            template (str): The template string.
+            context (dict): The context data to be used in rendering.
+
+        Returns:
+            str: The rendered template.
+        """
+        pass
+
+    @abstractmethod
+    def render_str(self, string: str, context: dict) -> str:
+        """
+        Args:
+            string (str): The string to compile.
+            context (dict): The context data to be used in rendering.
+        Returns:
+            str: The rendered string .
+        """
+        pass
+
+    @abstractmethod
+    def get_env(self) -> Any:
+        pass
