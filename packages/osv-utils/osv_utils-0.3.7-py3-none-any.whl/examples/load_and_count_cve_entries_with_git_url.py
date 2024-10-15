45bedd26ebc070a2e9d1c5001ab7232ad21f73f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:32 +0000

    Add package osv-utils/0.3.7/osv_utils-0.3.7-py3-none-any.whl

---
+++ packages/osv-utils/osv_utils-0.3.7-py3-none-any.whl/examples/load_and_count_cve_entries_with_git_url.py
@@ -0,0 +1,21 @@
+from osvutils.core.loader import OSVDataLoader
+from osvutils.core.filters.loader import LoaderFilters
+from osvutils.core.filters.database import DatabaseFilter
+from osvutils.core.filters.references import ReferencesFilter
+
+
+loader = OSVDataLoader(
+    ecosystems=['GIT'],
+    filters=LoaderFilters(
+        database_filter=DatabaseFilter(
+            prefix_is_cve=True
+        ),
+        references_filter=ReferencesFilter(
+            has_fix=True
+        )
+    )
+)
+
+loader()
+
+print(f"{len(loader)} records loaded")
