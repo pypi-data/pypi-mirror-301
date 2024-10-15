517fced0db7dc0b02f3c1aa5d99ba7cabd5f9242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:05:39 +0000

    Add package PyOptik/1.7.2.post0/pyoptik-1.7.2.post0.tar.gz

---
+++ packages/PyOptik/pyoptik-1.7.2.post0.tar.gz/pyoptik-1.7.2.post0/PyOptik/data/sellmeier/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from PyOptik.directories import sellmeier_data_path as material_path
+
+# Get a list of all filenames in the directory
+material_list = [os.path.splitext(f)[0] for f in os.listdir(material_path) if os.path.isfile(os.path.join(material_path, f)) and f.endswith('.yml') and 'test' not in f]
