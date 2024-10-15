c1df5cf88688fe5e5b821361f16f372336a1394d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:06:17 +0000

    Add package python-irodsclient/2.2.0/python-irodsclient-2.2.0.tar.gz

---
+++ packages/python-irodsclient/python-irodsclient-2.2.0.tar.gz/python-irodsclient-2.2.0/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+# Get package version
+version = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'irods/version.py')) as file:
+    exec(file.read(), version)
+
+
+# Get description
+with codecs.open('README.md', 'r', 'utf-8') as file:
+    long_description = file.read()
+
+
+setup(name='python-irodsclient',
+      version=version['__version__'],
+      author='iRODS Consortium',
+      author_email='support@irods.org',
+      description='A python API for iRODS',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      license='BSD',
+      url='https://github.com/irods/python-irodsclient',
+      keywords='irods',
+      classifiers=[
+                   'License :: OSI Approved :: BSD License',
+                   'Development Status :: 5 - Production/Stable',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: 3.11',
+                   'Programming Language :: Python :: 3.12',
+                   'Operating System :: POSIX :: Linux',
+                   ],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+                        'six>=1.10.0',
+                        'PrettyTable>=0.7.2',
+                        'defusedxml',
+                        # - the new syntax:
+                        #'futures; python_version == "2.7"'
+                        ],
+      # - the old syntax:
+      extras_require={ ':python_version == "2.7"': ['futures'],
+                       'tests': ['unittest-xml-reporting']  # for xmlrunner
+                     }
+      )
