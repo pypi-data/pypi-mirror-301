e8bff016cd487fb70ef358ce47ce5ef891db05f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:05:22 +0000

    Add package pincushion/0.1.0/pincushion-0.1.0.tar.gz

---
+++ packages/pincushion/pincushion-0.1.0.tar.gz/pincushion-0.1.0/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#pincushion user --user-id 120066 --archive-path archives/black-in-appalachia
+pincushion user --user-id 52974 --archive-path archives/apiahip
+#pincushion user --user-id 120640 --archive-path archives/invisible-histories
+#pincushion user --user-id 120324 --archive-path archives/manila-town
+#pincushion user --user-id 120074 --archive-path archives/us-colored-troops-coalition
+#pincushion user --user-id 81351 --archive-path archives/mescalero-apache-nation
+
