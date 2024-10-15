ce9ba70a400078105ca9f9632c5fb9bd6a88abd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:45 +0000

    Add package pyside6-overlay-busy-indicator/1.6/pyside6_overlay_busy_indicator-1.6-py3-none-any.whl

---
+++ packages/pyside6-overlay-busy-indicator/pyside6_overlay_busy_indicator-1.6-py3-none-any.whl/qbusyindicator/__init__.py
@@ -0,0 +1,17 @@
+
+"""
+pyside6-overlay-busy-indicator
+======================
+A customizable overlay busy indicator for PySide6 with a smooth fade animation
+
+Examples are available at https://github.com/pohemati/pyside6-overlay-busy-indicator/tree/examples.
+
+:copyright: (c) 2024 by poria hemati (poria.hemati@gmail.com).
+:license: MIT License, see README for more details.
+"""
+
+__version__ = "1.6"
+__author__ = "poria hemati (https://github.com/pohemati/)"
+
+
+from .busyindicator import QOverlayBusyIndicator
