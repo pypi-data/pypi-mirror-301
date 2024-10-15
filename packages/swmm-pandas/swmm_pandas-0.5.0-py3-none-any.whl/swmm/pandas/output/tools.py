d3e92dc642cb12f6fe7f00b35f32fe72aeaa454b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:06:20 +0000

    Add package swmm-pandas/0.5.0/swmm_pandas-0.5.0-py3-none-any.whl

---
+++ packages/swmm-pandas/swmm_pandas-0.5.0-py3-none-any.whl/swmm/pandas/output/tools.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import numpy as np
+from aenum import EnumMeta
+from typing import Dict, List
+
+arrayishNone = (list, tuple, set, np.ndarray, type(None))
+arrayish = (list, tuple, set, np.ndarray)
+
+
+def elements(path: str) -> Dict[str, List[str]]:
+    with open(path, "r") as fil:
+        elements: Dict[str, List[str]] = {}
+        for lin in fil:
+            line = lin.replace("\n", "")
+            if "[" in line:
+                section = line.replace("[", "").replace("]", "").lower().strip()
+                elements[section] = []
+                continue
+            if len(line) > 0:
+                elements[section].append(line)
+    return elements
+
+
+def _enum_get(enum: EnumMeta, name: str) -> int | None:
+    try:
+        return enum.__getitem__(name.upper())
+    except KeyError:
+        return None
+
+
+def _enum_keys(enum: EnumMeta) -> list[str]:
+    return list(map(lambda x: x.lower(), enum.__members__.keys()))
