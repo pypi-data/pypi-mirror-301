cfa9534b7cb964ad57f5d550983d3ac3ed6b2f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:21 +0000

    Add package pkgbox/0.5.4/pkgbox-0.5.4-py3-none-any.whl

---
+++ packages/pkgbox/pkgbox-0.5.4-py3-none-any.whl/pkgbox/__init__.py
@@ -0,0 +1,9 @@
+from pkgbox.fn import *
+from pkgbox import islack
+from pkgbox.date_fn import *
+from pkgbox import charts
+from pkgbox.num_fn import *
+
+
+dt = date_fn.Cdt()
+pysqldf = lambda q: sqldf(q, globals())
