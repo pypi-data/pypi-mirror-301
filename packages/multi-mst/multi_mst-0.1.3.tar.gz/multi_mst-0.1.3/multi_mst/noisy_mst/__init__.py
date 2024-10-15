ec4fb1cf17ac3d06a158f095e35c1ed1dfc0ddb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:31 +0000

    Add package multi-mst/0.1.3/multi_mst-0.1.3.tar.gz

---
+++ packages/multi-mst/multi_mst-0.1.3.tar.gz/multi_mst-0.1.3/multi_mst/noisy_mst/__init__.py
@@ -0,0 +1,9 @@
+import numpy as np
+from .api import NoisyMST, noisyMST
+
+# Force JIT compilation on import
+random_state = np.random.RandomState(42)
+random_data = random_state.random(size=(50, 3))
+NoisyMST().fit(random_data)
+
+__all__ = ["NoisyMST", "noisyMST"]
