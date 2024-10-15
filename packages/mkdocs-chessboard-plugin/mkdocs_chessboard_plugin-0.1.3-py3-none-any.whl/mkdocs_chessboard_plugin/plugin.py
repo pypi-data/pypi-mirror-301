1089795470af7e36a2e84370c737b860b2b97559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:05:19 +0000

    Add package mkdocs-chessboard-plugin/0.1.3/mkdocs_chessboard_plugin-0.1.3-py3-none-any.whl

---
+++ packages/mkdocs-chessboard-plugin/mkdocs_chessboard_plugin-0.1.3-py3-none-any.whl/mkdocs_chessboard_plugin/plugin.py
@@ -0,0 +1,31 @@
+import re
+
+from mkdocs.plugins import BasePlugin
+
+
+class ChessboardPlugin(BasePlugin):
+    def on_page_content(self, html, **kwargs):
+        # Regular expression to find code blocks starting with ```FEN
+        fen_pattern = re.compile(r'<pre><code>FEN\s+([^<]+)</code></pre>', re.MULTILINE)
+
+        def replace_fen(match):
+            fen = match.group(1).strip()
+            return f'''
+                    <div class="chessboard" id="board-{fen.replace(' ', '_')}"></div>
+                    <script>
+                        var board = Chessboard('board-{fen.replace(' ', '_')}', {{
+                            position: '{fen}'
+                        }});
+                    </script>
+                    '''
+
+        # Replace all FEN code blocks in the HTML
+        new_html = fen_pattern.sub(replace_fen, html)
+
+        # Add the necessary CSS and JS for chessboard.js
+        new_html += '''
+                <link rel="stylesheet" href="https://unpkg.com/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css">
+                <script src="https://unpkg.com/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js"></script>
+                '''
+
+        return new_html
