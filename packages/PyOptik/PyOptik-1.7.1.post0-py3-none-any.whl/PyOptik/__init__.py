52e8f532e8bd89a5d1dd2bb772efbe6816515daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:06:44 +0000

    Add package PyOptik/1.7.1.post0/PyOptik-1.7.1.post0-py3-none-any.whl

---
+++ packages/PyOptik/PyOptik-1.7.1.post0-py3-none-any.whl/PyOptik/__init__.py
@@ -0,0 +1,14 @@
+try:
+    from ._version import version as __version__  # noqa: F401
+
+except ImportError:
+    __version__ = "0.0.0"
+
+
+from .utils import build_default_library
+from .material import Material
+from .material import TabulatedMaterial
+from .material import SellmeierMaterial
+from .material import base_class
+
+Material = Material()
