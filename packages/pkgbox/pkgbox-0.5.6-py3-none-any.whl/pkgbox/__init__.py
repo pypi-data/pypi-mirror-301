14ef4d7f88eb6d28f8482751a1d3acaa3e8ef91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:43 +0000

    Add package pkgbox/0.5.6/pkgbox-0.5.6-py3-none-any.whl

---
+++ packages/pkgbox/pkgbox-0.5.6-py3-none-any.whl/pkgbox/__init__.py
@@ -0,0 +1,10 @@
+from pkgbox.fn import *
+from pkgbox import islack
+from pkgbox.date_fn import *
+from pkgbox import charts
+from pkgbox.num_fn import *
+from .memton import Memton
+
+
+dt = date_fn.Cdt()
+pysqldf = lambda q: sqldf(q, globals())
