2a393fb675298cc51f0cb3a1458f92d0fa316025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:05:36 +0000

    Add package riguima-log-reader/0.1.0/riguima_log_reader-0.1.0.tar.gz

---
+++ packages/riguima-log-reader/riguima_log_reader-0.1.0.tar.gz/riguima_log_reader-0.1.0/log_reader/admin.py
@@ -0,0 +1,36 @@
+import django
+from django.contrib import admin, messages
+from django.template.response import TemplateResponse
+from django.urls import path
+
+from log_reader import settings
+from log_reader.models import FileLogReader
+from log_reader.utils import get_log_files, read_file_lines
+
+
+@admin.register(FileLogReader)
+class FileLogReaderAdmin(admin.ModelAdmin):
+    list_filter = ['id']
+
+    def get_urls(self):
+        info = self.model._meta.app_label, self.model._meta.module_name
+        return [
+            path(r'', self.admin_site.admin_view(self.changelist_view), name='%s_%s_changelist' % info),
+        ]
+
+    def changelist_view(self, request, extra_context=None):
+        filename = request.GET.get('file_name', settings.LOG_READER_DEFAULT_FILE)
+        search = request.GET.get('q', None) or None
+        is_valid, file_contents = read_file_lines(file_name=filename, search=search, tenant=request.tenant)
+        if not is_valid:
+            self.message_user(request, file_contents, level=messages.ERROR)
+        log_files = get_log_files(settings.LOG_READER_DIR_PATH, request.tenant)
+
+        context = dict(
+            self.admin_site.each_context(request),
+            log_files=log_files,
+            file_contents=file_contents if is_valid else [],
+            file_name=filename,
+            django_version=django.get_version()
+        )
+        return TemplateResponse(request, "log_reader/admin/change_list.html", context=context)
---
+++ packages/riguima-log-reader/riguima_log_reader-0.1.0.tar.gz/riguima_log_reader-0.1.0/log_reader/utils.py
@@ -0,0 +1,62 @@
+from __future__ import unicode_literals
+
+import django
+import os
+# import re
+import subprocess
+from fnmatch import fnmatch
+from subprocess import PIPE
+
+if django.get_version() >= '4':
+    from django.utils.translation import gettext_lazy as _
+else:
+    from django.utils.translation import ugettext_lazy as _
+
+from log_reader import settings
+
+
+def get_log_files(directory, tenant):
+    for (dir_path, dir_names, filenames) in os.walk(directory):
+        log_files = []
+        all_files = list(filter(lambda x: x.find('~') == -1, filenames))
+        log_files.extend([x for x in all_files if fnmatch(x, settings.LOG_READER_FILES_PATTERN) and x not in settings.LOG_READER_EXCLUDE_FILES and str(tenant) in x])
+        return list(set(log_files))
+    return []
+
+
+def read_file_lines(file_name, search=None, tenant=None):
+    if file_name not in get_log_files(settings.LOG_READER_DIR_PATH, tenant):
+        return False, _("%s file, not found. Please try again." % file_name)
+
+    try:
+        file_path = '%s/%s' % (settings.LOG_READER_DIR_PATH, file_name)
+
+        if search:
+            result = subprocess.run(
+                ['grep', '-m %s' % settings.LOG_READER_MAX_READ_LINES, search, file_path],
+                stdout=PIPE,
+                stderr=PIPE,
+                encoding="utf8",
+            )
+        else:
+            result = subprocess.run(
+                ['tail', '-%s' % settings.LOG_READER_MAX_READ_LINES, file_path],
+                stdout=PIPE,
+                stderr=PIPE,
+                encoding="utf8",
+            )
+        content = str(result.stdout) if result.stdout else None
+    except Exception as e:
+        return False, str(e)
+
+    return True, split_file_content(content)
+
+
+def split_file_content(content):
+    data = content.split(settings.LOG_READER_SPLIT_PATTERN) if content else []
+    # if content and len(res) == 1:
+    #     res = re.findall(settings.LOG_READER_REGEX_SPLIT_PATTERN, content) if content else []
+    res = [x for x in data if len(x) > 5]
+    res.reverse()
+    return res
+
