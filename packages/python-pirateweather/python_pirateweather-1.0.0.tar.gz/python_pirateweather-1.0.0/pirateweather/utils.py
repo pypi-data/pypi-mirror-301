a0369f3fad77d9def975eea398e440ac746353ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 07:05:19 +0000

    Add package python-pirateweather/1.0.0/python_pirateweather-1.0.0.tar.gz

---
+++ packages/python-pirateweather/python_pirateweather-1.0.0.tar.gz/python_pirateweather-1.0.0/pirateweather/utils.py
@@ -0,0 +1,13 @@
+"""Utilities for the Pirate Weather library."""
+
+
+class UnicodeMixin:
+    """Mixin class to handle defining the proper __str__/__unicode__ methods in Python 3."""
+
+    def __str__(self):
+        """Return the unicode representation of the object for Python 3 compatibility."""
+        return self.__unicode__()
+
+
+class PropertyUnavailable(AttributeError):
+    """Raise when a requested property is unavailable in the forecast data."""
