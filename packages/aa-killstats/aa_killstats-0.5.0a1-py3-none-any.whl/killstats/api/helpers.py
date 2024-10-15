5b421cca0317e12a1df25834f37a55e67013c7cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:11:03 +0000

    Add package aa-killstats/0.5.0a1/aa_killstats-0.5.0a1-py3-none-any.whl

---
+++ packages/aa-killstats/aa_killstats-0.5.0a1-py3-none-any.whl/killstats/api/helpers.py
@@ -0,0 +1,43 @@
+from allianceauth.eveonline.models import EveCharacter
+
+from killstats.hooks import get_extension_logger
+
+logger = get_extension_logger(__name__)
+
+
+class KillboardDate:
+    def __init__(self, month, year):
+        self.month = int(month)
+        self.year = int(year)
+
+
+def get_corporations(request):
+    linked_characters = request.user.profile.main_character.character_ownership.user.character_ownerships.select_related(
+        "character", "user"
+    ).all()
+
+    linked_characters = linked_characters.values_list("character_id", flat=True)
+    chars = EveCharacter.objects.filter(id__in=linked_characters)
+
+    corporations = set()
+
+    for char in chars:
+        corporations.add(char.corporation_id)
+
+    return list(corporations)
+
+
+def get_alliances(request):
+    linked_characters = request.user.profile.main_character.character_ownership.user.character_ownerships.select_related(
+        "character", "user"
+    ).all()
+
+    linked_characters = linked_characters.values_list("character_id", flat=True)
+    chars = EveCharacter.objects.filter(id__in=linked_characters)
+
+    alliances = set()
+
+    for char in chars:
+        alliances.add(char.alliance_id)
+
+    return list(alliances)
