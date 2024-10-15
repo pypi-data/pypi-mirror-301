c4852b34eeb0c1885f6f9aa74d422a248e3a1230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:11 +0000

    Add package podme-api/1.2.1/podme_api-1.2.1-py3-none-any.whl

---
+++ packages/podme-api/podme_api-1.2.1-py3-none-any.whl/podme_api/exceptions.py
@@ -0,0 +1,33 @@
+"""podme_api exceptions."""
+
+
+class PodMeApiError(Exception):
+    """Generic PodMe exception."""
+
+
+class PodMeApiNotFoundError(PodMeApiError):
+    """PodMe not found exception."""
+
+
+class PodMeApiConnectionError(PodMeApiError):
+    """PodMe connection exception."""
+
+
+class PodMeApiConnectionTimeoutError(PodMeApiConnectionError):
+    """PodMe connection timeout exception."""
+
+
+class PodMeApiRateLimitError(PodMeApiConnectionError):
+    """PodMe Rate Limit exception."""
+
+
+class PodMeApiAuthenticationError(PodMeApiError):
+    """PodMe authentication exception."""
+
+
+class PodMeApiUnauthorizedError(PodMeApiError):
+    """PodMe unauthorized exception."""
+
+
+class PodMeApiDownloadError(PodMeApiError):
+    """PodMe download exception."""
