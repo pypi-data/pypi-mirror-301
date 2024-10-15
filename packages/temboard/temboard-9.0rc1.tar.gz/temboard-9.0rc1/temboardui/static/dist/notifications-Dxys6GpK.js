881bcab3331cc021f3bcb39716b6b6d2cfd449d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:05 +0000

    Add package temboard/9.0rc1/temboard-9.0rc1.tar.gz

---
+++ packages/temboard/temboard-9.0rc1.tar.gz/temboard-9.0rc1/temboardui/static/dist/notifications-Dxys6GpK.js
@@ -0,0 +1,1 @@
+import{V as e,D as t}from"./datatables.net-vue3-B28JL-vi.js";import"./jquery-BALIvAru.js";import"./highlight.js-CWp04Ajj.js";import"./vue-CSzJdJby.js";e.use(t);new t("#tableNotifications",{order:[[0,"desc"]],pageLength:25});
