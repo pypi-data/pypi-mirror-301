fd6a8e0cc00f0ee08b2e4a34b495edb6e8a818f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:05:29 +0000

    Add package mmguero/1.2.0/mmguero-1.2.0-py3-none-any.whl

---
+++ packages/mmguero/mmguero-1.2.0-py3-none-any.whl/mmguero/__init__.py
@@ -0,0 +1,8 @@
+"""mmguero - Useful helpers by me, for me."""
+
+__version__ = "1.2.0"
+__author__ = "Seth Grover <mero.mero.guero@gmail.com>"
+__all__ = []
+
+from mmguero.mmguero import *
+from mmguero.caselessdictionary import *
