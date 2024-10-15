9f431c018395e068927d68c56e1b62a2146b3da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:06:44 +0000

    Add package ARYAN-KADIYAN/0.0.1/ARYAN_KADIYAN-0.0.1-py3-none-any.whl

---
+++ packages/ARYAN-KADIYAN/ARYAN_KADIYAN-0.0.1-py3-none-any.whl/ARYAN/ARYAN1.py
@@ -0,0 +1,13 @@
+def SUM ():
+    A = int(input("ENTER THE FIRST NO. "))
+    B = int(input("ENTER THE SECOND NO. "))
+    C = A+B
+    print(C)
+
+
+
+def DIFF ():
+    A = int(input("ENTER THE FIRST NO. "))
+    B = int(input("ENTER THE SECOND NO. "))
+    C = A-B
+    print(C)
---
+++ packages/ARYAN-KADIYAN/ARYAN_KADIYAN-0.0.1-py3-none-any.whl/ARYAN/ARYAN2.py
@@ -0,0 +1,14 @@
+def MUL ():
+    A = int(input("ENTER THE FIRST NO. "))
+    B = int(input("ENTER THE SECOND NO. "))
+    C = A*B
+    print(C)
+
+
+
+
+def DIV ():
+    A = int(input("ENTER THE FIRST NO. "))
+    B = int(input("ENTER THE SECOND NO. "))
+    C = A/B
+    print(C)
