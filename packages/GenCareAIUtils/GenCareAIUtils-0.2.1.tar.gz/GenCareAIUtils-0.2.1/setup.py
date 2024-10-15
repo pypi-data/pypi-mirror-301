2b36f2552d89a4845a2fa88848bca3a76247ec9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:22 +0000

    Add package GenCareAIUtils/0.2.1/GenCareAIUtils-0.2.1.tar.gz

---
+++ packages/GenCareAIUtils/GenCareAIUtils-0.2.1.tar.gz/GenCareAIUtils-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GenCareAIUtils",
+    version="0.2.1", 
+    author_email="info@doktereva.nl",
+    description="Helper functions for the GenCareAI project",
+    long_description="Helper functions for the GenCareAI project",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ekrombouts/GenCareAIUtils",
+    packages=find_packages(),
+)
