1135564b2645ad07764a2d6c088f898fba7f4039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:47 +0000

    Add package svgcheck/0.8.1/svgcheck-0.8.1.tar.gz

---
+++ packages/svgcheck/svgcheck-0.8.1.tar.gz/svgcheck-0.8.1/svgcheck/__init__.py
@@ -0,0 +1,7 @@
+# ----------------------------------------------------
+# Copyright The IETF Trust 2018-9, All Rights Reserved
+# ----------------------------------------------------
+
+# Static values
+__version__ = '0.8.1'
+NAME = 'svgcheck'
