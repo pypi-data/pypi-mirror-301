9290b9d77f0a29908f34687d6dd3d4e47fc59d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:05:26 +0000

    Add package mikeio1d/0.8.1/mikeio1d-0.8.1-py3-none-win_amd64.whl

---
+++ packages/mikeio1d/mikeio1d-0.8.1-py3-none-win_amd64.whl/mikeio1d/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import platform
+
+from .mikepath import MikePath
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = "0.8.1"
+
+if "64" not in platform.architecture()[0]:
+    raise Exception("This library has not been tested for a 32 bit system.")
+
+MikePath.setup_mike_installation(sys.path)
+
+import clr
+
+clr.AddReference("System")
+clr.AddReference("System.Runtime")
+clr.AddReference("System.Runtime.InteropServices")
+clr.AddReference("DHI.Generic.MikeZero.DFS")
+clr.AddReference("DHI.Generic.MikeZero.EUM")
+# clr.AddReference('DHI.PFS')
+# clr.AddReference('DHI.Projections')
+clr.AddReference("DHI.Mike1D.Generic")
+clr.AddReference("DHI.Mike1D.ResultDataAccess")
+clr.AddReference("DHI.Mike1D.CrossSectionModule")
+clr.AddReference("DHI.Mike1D.MikeIO")
+
+from .res1d import Res1D
+from .xns11 import Xns11
+
+__all__ = ["Res1D", "Xns11"]
