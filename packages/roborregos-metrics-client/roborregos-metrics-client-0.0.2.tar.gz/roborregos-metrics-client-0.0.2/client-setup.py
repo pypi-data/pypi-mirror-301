41d71f8c74ca78f42e37c1707dd00552b31127ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:46 +0000

    Add package roborregos-metrics-client/0.0.2/roborregos-metrics-client-0.0.2.tar.gz

---
+++ packages/roborregos-metrics-client/roborregos-metrics-client-0.0.2.tar.gz/roborregos-metrics-client-0.0.2/client-setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='roborregos-metrics-client',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pydantic'
+    ],
+    entry_points={
+        'console_scripts': [
+            'roborregos-metrics-worker = src.worker.worker:main',
+        ]
+    }
+)
