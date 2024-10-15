df9f9538f2c138a8b8a238a83e9ab5c4acdcc6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:52 +0000

    Add package mpesaflow/1.2.0/mpesaflow-1.2.0-py3-none-any.whl

---
+++ packages/mpesaflow/mpesaflow-1.2.0-py3-none-any.whl/mpesaflow/resources/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .payments import (
+    PaymentsResource,
+    AsyncPaymentsResource,
+    PaymentsResourceWithRawResponse,
+    AsyncPaymentsResourceWithRawResponse,
+    PaymentsResourceWithStreamingResponse,
+    AsyncPaymentsResourceWithStreamingResponse,
+)
+from .transactions import (
+    TransactionsResource,
+    AsyncTransactionsResource,
+    TransactionsResourceWithRawResponse,
+    AsyncTransactionsResourceWithRawResponse,
+    TransactionsResourceWithStreamingResponse,
+    AsyncTransactionsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "PaymentsResource",
+    "AsyncPaymentsResource",
+    "PaymentsResourceWithRawResponse",
+    "AsyncPaymentsResourceWithRawResponse",
+    "PaymentsResourceWithStreamingResponse",
+    "AsyncPaymentsResourceWithStreamingResponse",
+    "TransactionsResource",
+    "AsyncTransactionsResource",
+    "TransactionsResourceWithRawResponse",
+    "AsyncTransactionsResourceWithRawResponse",
+    "TransactionsResourceWithStreamingResponse",
+    "AsyncTransactionsResourceWithStreamingResponse",
+]
---
+++ packages/mpesaflow/mpesaflow-1.2.0-py3-none-any.whl/mpesaflow/resources/transactions.py
@@ -0,0 +1,214 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import httpx
+
+from .._types import NOT_GIVEN, Body, Query, Headers, NotGiven
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from .._base_client import make_request_options
+from ..types.transaction_status import TransactionStatus
+from ..types.transaction_list_response import TransactionListResponse
+
+__all__ = ["TransactionsResource", "AsyncTransactionsResource"]
+
+
+class TransactionsResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> TransactionsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/stainless-sdks/mpesaflow-python#accessing-raw-response-data-eg-headers
+        """
+        return TransactionsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> TransactionsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/stainless-sdks/mpesaflow-python#with_streaming_response
+        """
+        return TransactionsResourceWithStreamingResponse(self)
+
+    def retrieve(
+        self,
+        transaction_id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> TransactionStatus:
+        """
+        Get transaction status
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not transaction_id:
+            raise ValueError(f"Expected a non-empty value for `transaction_id` but received {transaction_id!r}")
+        return self._get(
+            f"/transaction-status/{transaction_id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=TransactionStatus,
+        )
+
+    def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> TransactionListResponse:
+        """Get all transactions"""
+        return self._get(
+            "/transactions",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=TransactionListResponse,
+        )
+
+
+class AsyncTransactionsResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncTransactionsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/stainless-sdks/mpesaflow-python#accessing-raw-response-data-eg-headers
+        """
+        return AsyncTransactionsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncTransactionsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/stainless-sdks/mpesaflow-python#with_streaming_response
+        """
+        return AsyncTransactionsResourceWithStreamingResponse(self)
+
+    async def retrieve(
+        self,
+        transaction_id: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> TransactionStatus:
+        """
+        Get transaction status
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        if not transaction_id:
+            raise ValueError(f"Expected a non-empty value for `transaction_id` but received {transaction_id!r}")
+        return await self._get(
+            f"/transaction-status/{transaction_id}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=TransactionStatus,
+        )
+
+    async def list(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> TransactionListResponse:
+        """Get all transactions"""
+        return await self._get(
+            "/transactions",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=TransactionListResponse,
+        )
+
+
+class TransactionsResourceWithRawResponse:
+    def __init__(self, transactions: TransactionsResource) -> None:
+        self._transactions = transactions
+
+        self.retrieve = to_raw_response_wrapper(
+            transactions.retrieve,
+        )
+        self.list = to_raw_response_wrapper(
+            transactions.list,
+        )
+
+
+class AsyncTransactionsResourceWithRawResponse:
+    def __init__(self, transactions: AsyncTransactionsResource) -> None:
+        self._transactions = transactions
+
+        self.retrieve = async_to_raw_response_wrapper(
+            transactions.retrieve,
+        )
+        self.list = async_to_raw_response_wrapper(
+            transactions.list,
+        )
+
+
+class TransactionsResourceWithStreamingResponse:
+    def __init__(self, transactions: TransactionsResource) -> None:
+        self._transactions = transactions
+
+        self.retrieve = to_streamed_response_wrapper(
+            transactions.retrieve,
+        )
+        self.list = to_streamed_response_wrapper(
+            transactions.list,
+        )
+
+
+class AsyncTransactionsResourceWithStreamingResponse:
+    def __init__(self, transactions: AsyncTransactionsResource) -> None:
+        self._transactions = transactions
+
+        self.retrieve = async_to_streamed_response_wrapper(
+            transactions.retrieve,
+        )
+        self.list = async_to_streamed_response_wrapper(
+            transactions.list,
+        )
