372e9be950a2eb142ac4bacfea9977642d98d080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:03 +0000

    Add package variant-json-file-utils/0.1.1/variant_json_file_utils-0.1.1-py2.py3-none-any.whl

---
+++ packages/variant-json-file-utils/variant_json_file_utils-0.1.1-py2.py3-none-any.whl/variant_json_file_utils/json/annotated_match_call/record.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Record(BaseModel):
+    """Class for the annotated match call record."""
+    chr: Optional[str] = Field(None, title="The chromosome.")
+    start: Optional[int] = Field(None, title="The start coordinate.")
+    end: Optional[int] = Field(None, title="The end coordinate.")
+    ref: Optional[str] = Field(None, title="The reference.")
+    alt: Optional[str] = Field(None, title="The alternative.")
+    sample_name: Optional[str] = Field(None, title="The sample name.")
+    alleles: Optional[List[str]] = Field(None, title="The alleles.")
+    allele_indices: Optional[List[int]] = Field(None, title="The allele indices.")
+    zyg: Optional[str] = Field(None, title="The zygosity.")
+    is_phased: Optional[bool] = Field(None, title="Whether the variant is phased.")
+    quality: Optional[float] = Field(None, title="The quality.")
+    id: Optional[str] = Field(None, title="The variant ID.")
+    filters: Optional[List[str]] = Field(None, title="The filters.")
+    build: Optional[str] = Field(None, title="The build.")
+    total_depth: Optional[int] = Field(None, title="The total depth.")
+    alt_depth: Optional[int] = Field(None, title="The alternative depth.")
+    ref_depth: Optional[int] = Field(None, title="The reference depth.")
+    vaf: Optional[float] = Field(None, title="The variant allele frequency.")
+    vaf_alt: Optional[float] = Field(None, title="The variant allele frequency for the alternative allele.")
