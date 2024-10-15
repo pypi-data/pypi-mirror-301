ca70a3e50c24b656dd17dd6925814816d8f0ad3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:53 +0000

    Add package vantage-sdk/0.9.4.dev1/vantage_sdk-0.9.4.dev1.tar.gz

---
+++ packages/vantage-sdk/vantage_sdk-0.9.4.dev1.tar.gz/vantage_sdk-0.9.4.dev1/vantage_sdk/config.py
@@ -0,0 +1,10 @@
+DEFAULT_API_HOST = "https://api.vanta.ge"
+API_HOST_VERSION = "v1"
+
+DEFAULT_AUTH_HOST = "https://auth.vanta.ge"
+AUTH_ENDPOINT = "/oauth/token"
+
+DEFAULT_ENCODING = "utf-8"
+
+METADATA_PREFIX = "meta_"
+METADATA_ORDERED_PREFIX = "meta_ordered_"
