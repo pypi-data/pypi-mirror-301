493f8c43f16b497c49b17cc5ddb2ce05f6f8156f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:45 +0000

    Add package pytrpp/0.1.2/pytrpp-0.1.2-py3-none-any.whl

---
+++ packages/pytrpp/pytrpp-0.1.2-py3-none-any.whl/pytrpp/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import logging
+from main import main
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        log = logging.getLogger(__name__)
+        log.info('Exiting...')
+        exit()
+    except Exception as e:
+        log = logging.getLogger(__name__)
+        log.fatal(e)
+        raise
