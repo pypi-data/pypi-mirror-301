391d2af4a427672d7d7e7239a3af25950fff441d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 05:05:14 +0000

    Add package cehrbert-data/0.0.4/cehrbert_data-0.0.4.tar.gz

---
+++ packages/cehrbert-data/cehrbert_data-0.0.4.tar.gz/cehrbert_data-0.0.4/tests/integration_tests/test_generate_training_data.py
@@ -0,0 +1,31 @@
+import unittest
+
+from cehrbert_data.apps.generate_training_data import main
+from cehrbert_data.decorators import AttType
+
+from ..pyspark_test_base import PySparkAbstract
+
+
+class GenerateTrainingDataTest(PySparkAbstract):
+
+    def test_run_pyspark_app(self):
+        main(
+            input_folder=self.get_sample_data_folder(),
+            output_folder=self.get_output_folder(),
+            domain_table_list=["condition_occurrence", "drug_exposure", "procedure_occurrence"],
+            date_filter="1985-01-01",
+            include_visit_type=True,
+            is_new_patient_representation=True,
+            exclude_visit_tokens=False,
+            is_classic_bert=False,
+            include_prolonged_stay=False,
+            include_concept_list=False,
+            gpt_patient_sequence=True,
+            apply_age_filter=True,
+            include_death=False,
+            att_type=AttType.DAY,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
