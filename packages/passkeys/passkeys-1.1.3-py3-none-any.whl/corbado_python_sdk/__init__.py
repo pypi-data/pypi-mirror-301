46e52b9c4f3d764495ad4c116c7855f464ae78ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 06:04:22 +0000

    Add package passkeys/1.1.3/passkeys-1.1.3-py3-none-any.whl

---
+++ packages/passkeys/passkeys-1.1.3-py3-none-any.whl/corbado_python_sdk/__init__.py
@@ -0,0 +1,33 @@
+from .config import Config as Config
+from .corbado_sdk import CorbadoSDK as CorbadoSDK
+from .entities import SessionValidationResult as SessionValidationResult
+from .entities import UserEntity as UserEntity
+from .exceptions import StandardException as StandardException
+from .generated import (
+    Identifier,
+    IdentifierCreateReq,
+    IdentifierStatus,
+    IdentifierType,
+    UserCreateReq,
+    UserStatus,
+    UserUpdateReq,
+)
+from .services import IdentifierService, SessionService, UserService
+
+__all__ = [
+    "SessionValidationResult",
+    "IdentifierCreateReq",
+    "Identifier",
+    "IdentifierStatus",
+    "IdentifierType",
+    "UserCreateReq",
+    "UserUpdateReq",
+    "UserStatus",
+    "StandardException",
+    "UserEntity",
+    "CorbadoSDK",
+    "Config",
+    "IdentifierService",
+    "SessionService",
+    "UserService",
+]
---
+++ packages/passkeys/passkeys-1.1.3-py3-none-any.whl/corbado_python_sdk/entities/__init__.py
@@ -0,0 +1,4 @@
+from .session_validation_result import SessionValidationResult
+from .user_entity import UserEntity as UserEntity
+
+__all__ = ["UserEntity", "SessionValidationResult"]
