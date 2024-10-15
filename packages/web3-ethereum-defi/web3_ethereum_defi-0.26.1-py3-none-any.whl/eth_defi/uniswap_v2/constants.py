ad4ca29622dfffa2ecdb1e04c7d94a67c95fd260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:26 +0000

    Add package web3-ethereum-defi/0.26.1/web3_ethereum_defi-0.26.1-py3-none-any.whl

---
+++ packages/web3-ethereum-defi/web3_ethereum_defi-0.26.1-py3-none-any.whl/eth_defi/uniswap_v2/constants.py
@@ -0,0 +1,13 @@
+UNISWAP_V2_DEPLOYMENTS = {
+    "ethereum": {
+        "factory": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+        "router": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    },
+}
+
+QUICKSWAP_DEPLOYMENTS = {
+    "polygon": {
+        "factory": "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32",
+        "router": "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff",
+    },
+}
