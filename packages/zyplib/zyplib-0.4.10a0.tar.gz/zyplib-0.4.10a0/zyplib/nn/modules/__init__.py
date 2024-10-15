ff495fdfb22e63fe6232fbbef8ce5db19f79e3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 03:10:54 +0000

    Add package zyplib/0.4.10a0/zyplib-0.4.10a0.tar.gz

---
+++ packages/zyplib/zyplib-0.4.10a0.tar.gz/zyplib-0.4.10a0/zyplib/nn/modules/__init__.py
@@ -0,0 +1,22 @@
+from .base import (
+    BaseModule,
+    adaptiveavgpool,
+    adaptivemaxpool,
+    avgpool,
+    batchnorm,
+    conv,
+    convtranspose,
+    dropout,
+    maxpool,
+)
+
+__all__ = [
+    'BaseModule',
+    'conv',
+    'avgpool',
+    'batchnorm',
+    'maxpool',
+    'adaptiveavgpool',
+    'adaptivemaxpool',
+    'convtranspose',
+]
