463eb58ae9e92608587d6d934e730a861d332fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-15 04:06:28 +0000

    Add package nearoute-napalm-plugin/0.1.0/nearoute_napalm_plugin-0.1.0-py3-none-any.whl

---
+++ packages/nearoute-napalm-plugin/nearoute_napalm_plugin-0.1.0-py3-none-any.whl/netbox_napalm_plugin/api/serializers.py
@@ -0,0 +1,29 @@
+from dcim.api.nested_serializers import NestedPlatformSerializer
+from netbox.api.serializers import NetBoxModelSerializer
+from rest_framework import serializers
+
+from netbox_napalm_plugin.models import NapalmPlatformConfig
+
+
+class NapalmPlatformConfigSerializer(NetBoxModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="plugins-api:netbox_napalm_plugin-api:napalmplatformconfig-detail"
+    )
+    platform = NestedPlatformSerializer()
+
+    class Meta:
+        model = NapalmPlatformConfig
+        fields = [
+            "id",
+            "url",
+            "platform",
+            "napalm_driver",
+            "napalm_args",
+            "tags",
+            "created",
+            "last_updated",
+        ]
+
+
+class DeviceNAPALMSerializer(serializers.Serializer):
+    method = serializers.JSONField()
